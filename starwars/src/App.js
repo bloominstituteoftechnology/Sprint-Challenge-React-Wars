4f2f36bca531327252c22d48237b1b53b4a8621a
Cassidy SN Avery <tacolim@gmail.com>
2017-10-27 13:53:50 -0400

    first commit got data working & started styling w/ css

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -26,7 +26,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <div className="Header">React Wars</div>
+        <div className="CharacterCards">
+          {this.state.starwarsChars.map((char, i) => {
+            return (
+              <div key={char.name + i} className="CharCard">
+                <div className="CharName" >{char.name}</div>
+                <div>Homeworld: <a href={char.homeworld}>Homeworld</a></div>
+                <div className="Specs">
+                  <div className="Specs_title">Identifyers</div>
+                  <div className="Spec_Items">
+                    <div className="Spec">Species: <a href={char.species}>Species Info</a></div>
+                    <div className="Spec">Gender: {char.gender}</div>
+                    <div className="Spec">Height: {char.height}</div>
+                    <div className="Spec">Mass: {char.mass}</div>
+                    <div className="Spec">Hair Color: {char.hair_color}</div>
+                    <div className="Spec">Skin Color: {char.skin_color}</div>
+                    <div className="Spec">Eye Color: {char.eye_color}</div>
+                    <div className="Spec">Birth Year: {char.birth_year}</div>
+                  </div>
+                </div>
+                <div>Vehicles: {char.vehicles}</div>
+                <div>Starships: {char.starships}</div>
+                <div>Films: {char.films}</div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
