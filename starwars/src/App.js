da52379fbe33a656c374833bba6fd8e26b8d372a
MannieJay <jhall02258@gmail.com>
2017-10-27 14:57:43 -0400

    complete, could use more styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Data } from './postFramework';
 
 class App extends Component {
   constructor() {
@@ -23,10 +24,53 @@ class App extends Component {
         throw new Error(err);
       });
   }
+
+  // Data (props) {
+  //   return (
+  //       <div className="Data">
+  //           {props.starwarsChars.map((post) => {
+  //               return(
+  //                   <div className="PostList__post" key={post.username}>
+  //                       <div className="PostList__top">
+  //                           <img className="PostList__top-thumb" src={post.thumbnailUrl} alt=""/><span className="PostList__top-name">{post.username}</span>
+  //                       </div>
+  //                       <br />
+  //                       <img className="PostList__post-postimage" src={post.imageUrl} alt="work!"/>
+  //                       <br />
+  //                       {/* buttons */}
+  //                       <div className="PostList__post-likes">{post.likes} likes</div>
+  //                       <br />
+  //                       {post.comments.map((comment) => {
+  //                           return (
+  //                           <div className="PostList__comment" key={comment.text}>
+  //                               <span className="PostList__comment-name">{comment.username}: </span><span className="PostList__comment">{comment.text}</span>
+  //                           </div>
+  //                           );
+  //                       })}
+  //                       <br />
+  //                       <div className="PostList__stamp">{post.timestamp}</div>
+  //                       <div className="PostList__comment-box">
+  //                           <form action="">
+  //                               <input className="PostList__comment-input" type="text" placeholder="Add a comment..."></input>
+  //                           </form>
+  //                       </div>
+  //                   </div>
+  //               )
+  //           })}
+  //       </div>
+  //   ) 
+  // }
+
+
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="Data">
+            <Data starwarsChars={this.state.starwarsChars}/>
+        </div> 
+     
       </div>
     );
   }
