dc3f30b8b60fb25ac87216f8e49564de3834539e
Christian Franco <christian.franco94@icloud.com>
2017-10-27 11:11:03 -0700

    basic styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Card from './components/card';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/card";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -13,7 +13,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch("https://swapi.co/api/people")
       .then(res => {
         return res.json();
       })
@@ -28,9 +28,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        { this.state.starwarsChars.map((character, i)=>(
-          <Card character={character} key={i}/>
-        ))}
+        <section className="characters">
+          {this.state.starwarsChars.map((character, i) => (
+            <Card character={character} key={i} />
+          ))}
+        </section>
       </div>
     );
   }
