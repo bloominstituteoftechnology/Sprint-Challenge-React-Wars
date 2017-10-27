82ce048469bdd0a0ccb56f8977f782592fe65e09
Sam Kim <sams404link@gmail.com>
2017-10-27 12:07:07 -0700

    starwars updated

--- starwars/src/StarwarsCard.js
+++ starwars/src/StarwarsCard.js
@@ -7,39 +7,39 @@ const StarwarsCard = (props) => {
             return(
                 <div  className = "Starwars-cards"> 
                     <div>
-                    <h1 className = "Starwars-element"> {cards.name}</h1>
-                        <p1 className = "Starwars-element"> Height: {cards.height} </p1>
-                        <p2 className = "Starwars-element"> Mass: {cards.mass} </p2>
-                        <p3 className = "Starwars-element"> Hair Color: {cards.hair_color} </p3>
-                        <p4 className = "Starwars-element"> Skin Color: {cards.skin_color} </p4>
-                        <p5 className = "Starwars-element"> Eye Color: {cards.eye_color} </p5>
-                        <p6 className = "Starwars-element"> Birth Year: {cards.birth_year} </p6>
-                        <p7 className = "Starwars-element"> Gender: {cards.gender} </p7>
-                        <p8 className = "Starwars-element"> Homeworld: {cards.homeworld} </p8>
-                        <p9 className = "Starwars-element"> Films: {cards.films.map((film) => {
+                        <h1 className = "Starwars-element"> {cards.name}</h1>
+                        <h3 className = "Starwars-element"> Height: {cards.height} </h3>
+                        <h3 className = "Starwars-element"> Mass: {cards.mass} </h3>
+                        <h3 className = "Starwars-element"> Hair Color: {cards.hair_color} </h3>
+                        <h3 className = "Starwars-element"> Skin Color: {cards.skin_color} </h3>
+                        <h3 className = "Starwars-element"> Eye Color: {cards.eye_color} </h3>
+                        <h3 className = "Starwars-element"> Birth Year: {cards.birth_year} </h3>
+                        <h3 className = "Starwars-element"> Gender: {cards.gender} </h3>
+                        <h3 className = "Starwars-element"> Homeworld: <a href = {cards.homeworld}>{cards.homeworld} </a> </h3>
+                        <h3 className = "Starwars-element"> Films: {cards.films.map((film) => {
                             return(
-                                <p1 className = "Starwars-element">{film} </p1>
+                                <h5 className = "Starwars-element">  <a href = {film}> {film} </a>  </h5>
                             );             
-                        })} </p9>
-                        <p10 className = "Starwars-element"> Species: {cards.species.map((spec) => {
+                        })} </h3>
+                        <h3 className = "Starwars-element"> Species: {cards.species.map((spec) => {
                             return(
-                                <p1 className = "Starwars-element"> {spec} </p1>
+                                <p1 className = "Starwars-element"> <a href = {spec}> {spec} </a> </p1>
                             );
-                        })}</p10>
-                        <p11 className = "Starwars-element"> Vehicles: {cards.vehicles.map((vehicle => {
+                        })}</h3>
+                        <h3 className = "Starwars-element"> Vehicles: {cards.vehicles.map((vehicle => {
                             return(
-                                <p1 className = "Starwars-element"> {vehicle} </p1>
+                                <h5 className = "Starwars-element"> <a href = {vehicle}> {vehicle} </a></h5>
                             );
-                        }))} </p11>
-                        <p12 className = "Starwars-element"> StarShips: {cards.starships.map((starship) => {
+                        }))} </h3>
+                        <h3 className = "Starwars-element"> StarShips: {cards.starships.map((starship) => {
                             return(
-                                <p1 className = "Starwars-element"> {starship} </p1>
+                                <h5 className = "Starwars-element"> <a href = {starship}> {starship} </a> </h5>
                             );
-                        })} </p12>
-                        <p13 className = "Starwars-element"> Created: {cards.created} </p13>
-                        <p14 className = "Starwars-element"> Edited: {cards.edited} </p14>
-                        <p15 className = "Starwars-element"> Url: {cards.url} </p15>
-                       
+                        })} </h3>
+                        <h3 className = "Starwars-element"> Created: {cards.created} </h3>
+                        <h3 className = "Starwars-element"> Edited: {cards.edited} </h3>
+                        <h3 className = "Starwars-element"> Url: <a href = {cards.url}> {cards.url}</a> </h3>
+                        
                     </div>
                 </div>
             );
