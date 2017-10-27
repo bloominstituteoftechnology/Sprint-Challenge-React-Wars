cdd6bba5bb84214cdadda4f706eb0353fba62482
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-27 11:27:05 -0700

    added previous/next buttons

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,14 +6,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      page: 1,
     };
+    this.goToNextPage = this.goToNextPage.bind(this);
+    this.goToPreviousPage = this.goToPreviousPage.bind(this);
   }
   componentDidMount() {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch(`https://swapi.co/api/people?page=${this.state.page}`)
       .then(res => {
         return res.json();
       })
@@ -24,10 +27,22 @@ class App extends Component {
         throw new Error(err);
       });
   }
+  goToNextPage() {
+    this.setState({ page: ++this.state.page, });
+    this.componentDidMount();
+  }
+  goToPreviousPage() {
+    this.setState({ page: --this.state.page, });
+    this.componentDidMount();
+  }
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <nav className='nav-Header'>
+          <button onClick={ this.goToNextPage }>next</button>
+          <h1 className="Header">React Wars</h1>
+          <button onClick={ this.goToPreviousPage } disabled={ !(this.state.page > 1) }>previous</button>
+        </nav>
         <content>
           {this.state.starwarsChars.map(char => {
             return <Card key={ char.url.replace(/[^0-9]/g, '') } char={ char }/>
