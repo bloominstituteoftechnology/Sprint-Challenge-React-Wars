15c9e588a9bfe0ba29c9a831b57f91a91385ec1b
Kia Choi <sgchoi529@email.arizona.edu>
2017-10-27 11:06:58 -0700

    Kia's Commit - Displaying all Data

--- starwars/src/StarWarsList.js
+++ starwars/src/StarWarsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './StarWarsList.css';
 
 const StarWarsList = (props) => {
     return (
@@ -6,7 +7,26 @@ const StarWarsList = (props) => {
             {props.starwarsChars.map((starwarsDetail) => {
                 return (
                     <div className="StarWarsList__Chars">
-                        <h3>{starwarsDetail.name}</h3>
+                        <h2>{starwarsDetail.name}</h2>
+                        <div className="Details">
+                            <ul>
+                            <h4>Physical Attributes</h4>
+                                <li>Birthyear: {starwarsDetail.birth_year}</li>
+                                <li>Gender: {starwarsDetail.gender}</li>
+                                <li>Height: {starwarsDetail.height}</li>
+                                <li>Mass: {starwarsDetail.mass}</li>
+                                <li>Eye-Color: {starwarsDetail.eye_color}</li>
+                                <li>Hair-Color: {starwarsDetail.hair_color}</li>
+                                <li>Skin-Color: {starwarsDetail.skin_color}</li>
+                            </ul>
+                        </div><div className="Details">
+                            <ul>
+                            <h4>Critical Stats</h4>
+                                <li>Films: {starwarsDetail.films.length}</li>
+                                <li>Starships: {starwarsDetail.starships.length}</li>
+                                <li>Vehicles: {starwarsDetail.vehicles.length}</li>
+                            </ul>
+                        </div>
                     </div>
                 );
             })}
