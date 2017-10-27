b8a245ad6f10f719291610995d09f3dd72fce8c3
invegat <mark.c.oliver@gmail.com>
2017-10-27 15:33:55 -0400

    first

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import SwCard from  './components/SwCard'
 
 class App extends Component {
   constructor() {
@@ -17,16 +18,24 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsChars: data.results }, () =>  {})//console.log('sw:',this.state.starwarsChars[0]));
       })
       .catch(err => {
         throw new Error(err);
       });
   }
   render() {
+    const cards = (this.state.starwarsChars.map(c => {
+      return (
+        <li key={c.name}><SwCard character={c} /></li>
+      )
+    }))
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <ul>
+          {cards}
+        </ul>
       </div>
     );
   }
