82986c9a5a4772e7e1635099965581a92cad3f87
David Long <davdaarn@live.com>
2017-10-27 19:39:22 -0500

    url update

--- src/Profile_Pics.js
+++ src/Profile_Pics.js
@@ -1,24 +1,24 @@
 const ProfilePics = {
   'Luke Skywalker':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/luke_skywalker.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/luke_skywalker.png',
   'C-3PO':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/c_3po.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/c_3po.png',
   'R2-D2':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/r2_d2.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/r2_d2.png',
   'Darth Vader':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/darth_vader.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/darth_vader.png',
   'Leia Organa':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/leia_organa.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/leia_organa.png',
   'Owen Lars':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/owen_lars.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/owen_lars.png',
   'Beru Whitesun lars':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/beru_lars.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/beru_lars.png',
   'R5-D4':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/r5_d4.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/r5_d4.png',
   'Biggs Darklighter':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/biggs_darklighter.png',
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/biggs_darklighter.png',
   'Obi-Wan Kenobi':
-    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/starwars/src/assets/obi_wan_kenobi.png'
+    'https://raw.githubusercontent.com/davdaarn/Sprint-Challenge---React/master/src/assets/obi_wan_kenobi.png'
 };
 
 export default ProfilePics;
