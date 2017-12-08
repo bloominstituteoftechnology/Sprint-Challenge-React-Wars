afbbe28eae9676fdbb974258c5bd0e2eab17d05d
cmoore4os <cm4glos@gmail.com>
2017-12-08 18:38:15 -0500

    Consuella Moore -- completed basic of Sprint. CSS still needs work

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CharacterProfile from './components/CharacterProfile/CharacterProfile';
 import './App.css';
 
 class App extends Component {
@@ -27,6 +28,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {this.state.starwarsChars.map((datum, i) => (
+          <CharacterProfile key={i} data={datum} />
+        ))}
       </div>
     );
   }
