10a3421d34e026762d6d18d410809e6218999842
fairlight-excalibur <lidgrt@gmail.com>
2017-12-08 14:31:50 -0600

    answering questions

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './components/person';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -27,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <div><img className="titleImage" src="./starwars.png" width="40%"></img></div>
         {this.state.starwarsChars.map((item, i) => <Person key={i} bio={item} />)}
       </div>
     );
--- starwars/src/components/person.js
+++ starwars/src/components/person.js
@@ -21,11 +21,30 @@ class Person extends Component {
         });
     }
 
-    componentDidMount () {
-        this.fetchAndSet(this.props.bio.homeworld, 'homeworld');
-        this.props.bio.vehicles.map((vehicle, i) => {
-            this.fetchAndSet(vehicle, vehicle[{i}]);
+    fetchArray = (array, destination) => {
+        const newArray = [];
+        array.map((address) => {
+            fetch(address)
+            .then(res => {
+            return res.json();
+            })
+            .then(data => {
+            newArray.push(data.name);
+            })
+            .catch(err => {
+            throw new Error(err);
+            });
         });
+        console.log(newArray);
+        this.setState({ [destination] : newArray });
+    }
+
+    componentDidMount () {
+        this.fetchAndSet(this.props.bio.species, 'species');
+        this.fetchAndSet(this.props.bio.homeworld, 'homeworld');
+        this.fetchArray(this.props.bio.starships, 'starships');
+        this.fetchArray(this.props.bio.vehicles, 'vehicles');
+        
         this.setState({
             birth_year : this.props.bio.birth_year,
             eye_color : this.props.bio.eye_color,
@@ -35,28 +54,24 @@ class Person extends Component {
             mass : this.props.bio.mass,
             name : this.props.bio.name,
             skin_color : this.props.bio.skin_color,
-            species : this.props.bio.species,
-            starships : this.props.bio.starships,
-            vehicles : this.props.bio.vehicles
         });
     }
 
     render () {
-        console.log(this.state);
         return (
             <div className="characterCard">
                 <h1 className="name">{this.state.name}</h1>
                 <ul className="attributeList">
                     <li>Eye Color: {this.state.eye_color} <div className={"eye_color  " + this.state.eye_color}></div></li>
-                    <li>Films</li>
-                    <li>{this.state.homeworld}</li>
-                    <li>{this.state.gender}</li>
-                    <li>{this.state.height} cm</li>
-                    <li>{this.state.mass} kg</li>
-                    <li>{this.state.skin_color} skin</li>
-                    <li>Species</li>
-                    <li>Starships</li>
-                    <li>{this.state.vehicle}</li>
+                    <li>Films: </li>
+                    <li>Homeworld: {this.state.homeworld}</li>
+                    <li>Gender: {this.state.gender=== "n/a" ? "droid" : this.state.gender}</li>
+                    <li>Height: {this.state.height} cm</li>
+                    <li>Mass: {this.state.mass} kg</li>
+                    <li>Skin color: {this.state.skin_color} skin</li>
+                    <li>Species: {this.state.species}</li>
+                    {this.state.starships ? <li>Starships: {this.state.starships.map((starship) => starship + ", ")}</li> : null}
+                    {this.state.vehicles ? <li>Vehicles: {this.state.vehicles.map((vehicle) => vehicle + ", ")}</li> : null}
                 </ul>
             </div>
         );
