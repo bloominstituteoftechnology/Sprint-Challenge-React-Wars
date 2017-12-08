2ccffe3c278994a60d211752d07ff25c7655f702
Pat Collins <padjo@mac.com>
2017-12-08 21:40:34 +0000

    added better styling

--- starwars/src/components/starWarItem.js
+++ starwars/src/components/starWarItem.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './starWarItem.css';
 
-const StarWarItem = ({ postData }) => (
+const StarWarItem = ({ postData }) => {
+    let ObjEntries = Object.entries(postData);
+    console.log(ObjEntries)
+    return (
+
     <div className="StarWarItem-container">
         <div className="thumbnail">
-
+            <img src={postData.thumbnailUrl} />
+            <span><b>{postData.username}</b></span>
         </div>
         <div className="image">
-          
+            <img src={postData.imageUrl} />
         </div>
-        <div>
-        {postData? Object.values(postData).map((values, i) => (
-            <div className="comment" key={i}>
-            <div className="comment-user"><b>{values}</b></div>
-          </div>)
-          ) : null}
-      </div>
+        <div className="Frame">
+        {/* {postData? postData.map((StarWarItem, i) => (
+                    <div className="comment" key={i}>
+                        <div className="comment-user"><b>{StarWarItem.birth_year}</b></div>
+                    </div>)
+                ) : null} */}
+        { ObjEntries.map(item => {
+            return ( <div className="Line">
+                        <div className='desc'>{item[0]}:</div><div className='val'>{item[1]}</div>
+                    </div>
+            )
+                }
+            )
+        }
+        </div>
 
     </div>
-);
+    );}
 
 export default StarWarItem;
