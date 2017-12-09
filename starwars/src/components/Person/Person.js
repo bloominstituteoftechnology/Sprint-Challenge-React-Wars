29ee0eb71e4eda48e3e932708f03be5c02c02cea
Ting Wang <felix.wang.1026@gmail.com>
2017-12-08 19:34:42 -0500

    Modified the styles

--- starwars/src/components/Person/Person.js
+++ starwars/src/components/Person/Person.js
@@ -1,33 +1,41 @@
-import React, { Component } from 'react';
-import './Person.css';
-import Homeworld from '../Homeworld/Homeworld';
-import Species from '../Species/Species';
+import React, { Component } from "react";
+import "./Person.css";
+import Homeworld from "../Homeworld/Homeworld";
+import Species from "../Species/Species";
 
 class Person extends Component {
   constructor() {
     super();
     this.state = {
-      person: [],
+      person: []
     };
   }
 
   componentDidMount() {
-    this.setState({ person: this.props.person, homeworld: this.props.person.homeworld, species: this.props.person.species[0] });
+    this.setState({
+      person: this.props.person,
+      homeworld: this.props.person.homeworld,
+      species: this.props.person.species[0]
+    });
   }
 
   render() {
     return (
       <div className="Person">
-        <div>Name: {this.state.person.name}</div>
-        <div>Birth Of Year: {this.state.person.birth_year}</div>
-        <div>Eye Color: {this.state.person.eye_color}</div>
-        <div>Gender: {this.state.person.gender}</div>
-        <div>Hair Color: {this.state.person.hair_color}</div>
-        <div>Height: {this.state.person.height}</div>
-        <Homeworld home={this.props.person.homeworld}/>
-        <div>Mass: {this.state.person.mass}</div>
-        <div>Skin Color: {this.state.person.skin_color}</div>
-        <Species race={this.props.person.species}/>
+        <div>
+          <strong>{this.state.person.name}</strong>
+        </div>
+        <div className="Features">
+          <div>Birth Of Year: {this.state.person.birth_year}</div>
+          <div>Eye Color: {this.state.person.eye_color}</div>
+          <div>Gender: {this.state.person.gender}</div>
+          <div>Hair Color: {this.state.person.hair_color}</div>
+          <div>Height: {this.state.person.height}</div>
+          <div>Mass: {this.state.person.mass}</div>
+          <div>Skin Color: {this.state.person.skin_color}</div>
+        </div>
+        <Homeworld home={this.props.person.homeworld} />
+        <Species race={this.props.person.species} />
       </div>
     );
   }
