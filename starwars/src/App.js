7fb9af16a023a3738768ebcc3c54dd6d4ca2ad59
cgroov <30381292+groov1234@users.noreply.github.com>
2018-01-12 18:19:47 -0500

    API data/custom font imported - basic css setup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CharacterData } from './components/CharacterData.js';
 
 class App extends Component {
   constructor() {
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharacterData people={this.state.starwarsChars} />
       </div>
     );
   }
---
+++ starwars/src/components/CharacterData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CharacterData.css'
+
+export const CharacterData = (props) => {
+  return (
+    <div className="CharBox">
+      {props.people.map(starwarsChars => {
+        return (
+          <div className="Chars">
+            <div className="Char-Name">{starwarsChars.name}</div>
+            <div className="Char-Height">{starwarsChars.height}</div>
+            <div className="Char-Weight">{starwarsChars.mass}</div>
+            <div className="Char-BirthYear">{starwarsChars.birth_year}</div>
+            <div className="Char-Gender">{starwarsChars.gender}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
