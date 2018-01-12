613a726159af6fb05d69a1832474a42f74463fb2
Perry Ahern <pahern@gmail.com>
2018-01-12 18:38:00 -0500

    More info added, backing up before refactoring

--- starwars/src/components/Character/Character.js
+++ starwars/src/components/Character/Character.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Character.css';
 
 const Character = (props) => {
+  const data = props.charData;
+
   return (
     <div className='Character'>
-      <div className='charName'>{props.charData.name}</div>
+      <div className='charName'>{data.name}</div>
       <div className='charData'>
-        Born: {props.charData.birth_year}
+        <div>Born: {data.birth_year} on <a href={data.homeworld} target='_blank' >homeworld</a></div>
+        <div>Gender: {data.gender}; Species: <a href={data.species[0]} target='blank'>Click</a></div>
+        <div>Height: {data.height}; Mass: {data.mass}</div>
+        <div><span>Eyes:</span> {data.eye_color}; Hair: {data.hair_color}; Skin: {data.skin_color}</div>
       </div>
     </div>
   )
