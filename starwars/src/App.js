d3e72de349996100edbe1bf15ca8f4c13e349ef1
Samuel Cha <sunchipssam@gmail.com>
2018-01-17 01:17:33 -0800

    implemented vehicles and starships

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      playerPictures: {},
+      vehiclePictures: {},
+      starshipPictures: {},
     };
   }
   componentDidMount() {
@@ -23,10 +26,45 @@ class App extends Component {
       .catch(err => {
         throw new Error(err);
       });
-  }
 
+    const playerPictures = {
+      'Luke Skywalker': 'https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg/revision/latest?cb=20170927034529',
+      'C-3PO': 'https://vignette.wikia.nocookie.net/starwars/images/7/7e/ThreepioTFA-Fathead.png/revision/latest?cb=20161109035240',
+      'R2-D2': 'https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png/revision/latest?cb=20161108040914',
+      'Darth Vader': 'https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/revision/latest?cb=20130621175844',
+      'Leia Organa': 'https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png/revision/latest?cb=20171224004147',
+      'Owen Lars': 'https://vignette.wikia.nocookie.net/starwars/images/e/eb/OwenCardTrader.png/revision/latest?cb=20171108050140',
+      'Beru Whitesun lars': 'https://vignette.wikia.nocookie.net/starwars/images/c/cc/BeruCardTrader.png/revision/latest?cb=20170713063118',
+      'R5-D4': 'https://vignette.wikia.nocookie.net/starwars/images/c/cb/R5-D4_Sideshow.png/revision/latest?cb=20160809033145',
+      'Biggs Darklighter': 'https://vignette.wikia.nocookie.net/starwars/images/0/00/BiggsHS-ANH.png/revision/latest?cb=20130305010406',
+      'Obi-Wan Kenobi': 'https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg/revision/latest?cb=20111115052816',
+    }
+
+    this.setState({ playerPictures })
+
+    const vehiclePictures = {
+      'Snowspeeder': 'https://vignette.wikia.nocookie.net/starwars/images/4/44/T-47_Speeder_DICE.png/revision/latest?cb=20151106063152',
+      'Imperial Speeder Bike': 'https://vignette.wikia.nocookie.net/starwars/images/6/64/74zSpeederbike-NEGVV.png/revision/latest?cb=20170410035749',
+      'Tribubble bongo': 'https://vignette.wikia.nocookie.net/starwars/images/d/d8/Bongo.jpg/revision/latest?cb=20080430201913',
+    }
+
+    this.setState({ vehiclePictures })
+
+    const starshipPictures = {
+      'X-wing': 'https://vignette.wikia.nocookie.net/starwars/images/b/b2/X-wing_2_Fathead.png/revision/latest?cb=20161004003758',
+      'Imperial shuttle': 'https://vignette.wikia.nocookie.net/starwars/images/a/a9/Imperial_shuttle.png/revision/latest?cb=20150827042115',
+      'TIE Advanced x1': 'https://vignette.wikia.nocookie.net/starwars/images/1/1d/Vader_TIEAdvanced_SWB.png/revision/latest?cb=20160915042032',
+      'Jedi starfighter': 'https://vignette.wikia.nocookie.net/starwars/images/7/7a/Jsf_duo2.jpg/revision/latest?cb=20060706121723',
+      'Trade Federation cruiser': 'https://vignette.wikia.nocookie.net/starwars/images/4/47/InvisibleHandStarboard-FF.png/revision/latest?cb=20160906045609',
+      'Naboo star skiff': 'https://vignette.wikia.nocookie.net/starwars/images/d/df/Naboo_star_skiff_1.png/revision/latest?cb=20130212042348',
+      'Jedi Interceptor': 'https://vignette.wikia.nocookie.net/starwars/images/a/a6/Eta-2_Interceptor.png/revision/latest?cb=20130613124413',
+      'Belbullab-22 starfighter': 'https://vignette.wikia.nocookie.net/starwars/images/3/3e/Soulless_One2_TCW.jpg/revision/latest?cb=20090311020627,'
+    }
+
+    this.setState({ starshipPictures })
+  }
   render() {
-    console.log(this.state.starwarsChars)
+    // console.log(this.state)
     return (
       <div className="App">
         <h1 className="Header">
@@ -39,6 +77,9 @@ class App extends Component {
                 key={i}
                 id={i}
                 player={playerData}
+                picture={this.state.playerPictures[playerData.name]}
+                vehiclesPictures={this.state.vehiclePictures}
+                starshipsPictures={this.state.starshipPictures}
               />
             )
           })}
--- starwars/src/Player/Player.js
+++ starwars/src/Player/Player.js
@@ -6,25 +6,52 @@ class Player extends Component {
 		super(props);
 		this.state = {
 			player: {},
-			// name: '',
+			name: '',
+			picture: '',
+			species: {},
 			birthYear: '',
-			// gender: '',
+			mass: '',
+			height: '',
+			gender: '',
 			hairColor: [],
 			hairColorStyle: [],
 			eyeColor: [],
 			eyeColorStyle: [],
 			skinColor: [],
 			skinColorStyle: [],
-
-			// vehicles: [],
+			homeworld: {},
+			vehicles: [],
+			starships: [],
 		}
 	}
 
 	componentDidMount() {
 		const player = this.props.player
-		this.setState({ player: player });
+		
+		this.setState({ player });
 
-		this.setState({ birthYear: this.formatBirthYear(player.birth_year) })
+		this.setState({ name: player.name });
+
+		this.setState({ picture: this.props.picture });
+
+		fetch(player.species)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ species: data })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+
+		this.setState({ birthYear: this.formatBirthYear(player.birth_year) });
+
+		this.setState({ mass: player.mass });
+
+		this.setState({ height: player.height });
+
+		this.setState({ gender: player.gender });
 
 		const hairColor = this.parseColor(player.hair_color);
 		const hairColorStyle = Array.isArray(hairColor) ? hairColor.map(color => {
@@ -35,7 +62,7 @@ class Player extends Component {
 				borderRadius: '50%',
 				height: '10px',
 				width: '10px',
-				'margin-right': '2px',
+				marginRight: '2px',
 			}
 		}) : 'none';
 
@@ -48,7 +75,7 @@ class Player extends Component {
 				borderRadius: '50%',
 				height: '10px',
 				width: '10px',
-				'margin-right': '2px',
+				marginRight: '2px',
 			}
 		}) : 'none';
 
@@ -61,7 +88,7 @@ class Player extends Component {
 				borderRadius: '50%',
 				height: '10px',
 				width: '10px',
-				'margin-right': '2px',
+				marginRight: '2px',
 			}
 		}) : 'none';
 
@@ -73,6 +100,50 @@ class Player extends Component {
 			skinColor,
 			skinColorStyle,
 		});
+
+		fetch(player.homeworld)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ homeworld: data });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+
+    // console.log(player.vehicles);
+    const vehiclesData = [];
+    player.vehicles.forEach((vehicleAPI) => {
+			fetch(vehicleAPI)
+	      .then(res => {
+	        return res.json();
+	      })
+	      .then(data => {
+	        vehiclesData.push(data);
+	      })
+	      .catch(err => {
+	        throw new Error(err);
+	      });
+    })
+
+    this.setState({ vehicles: vehiclesData });
+
+    const starshipsData = [];
+    player.starships.forEach((starshipAPI) => {
+    	fetch(starshipAPI)
+	      .then(res => {
+	        return res.json();
+	      })
+	      .then(data => {
+	        starshipsData.push(data);
+	      })
+	      .catch(err => {
+	        throw new Error(err);
+	      });
+    })
+
+    this.setState({ starships: starshipsData })
 	}
 
 	parseColor(hairColorArr) {
@@ -115,52 +186,104 @@ class Player extends Component {
 	// const eyeColor = { 'backgroundColor': formatColor(player.eye_color), borderStyle: 'solid', borderWidth: '1px', borderRadius: '50%', height: '10px', width: '10px' }
 	// const skinColor = { 'backgroundColor': formatColor(player.skin_color), borderStyle: 'solid', borderWidth: '1px', borderRadius: '50%', height: '10px', width: '10px' }
 	render() {
+		// console.log(this.state.homeworld)
 		// const hairColorStyle = formatColor(player.hair_color) !== player.hair_color ? { 'backgroundColor': formatColor(player.hair_color), borderStyle: 'solid', borderWidth: '1px', borderRadius: '50%', height: '10px', width: '10px' } :
-		console.log(this.state.hairColor)
-		console.log(this.state.hairColorStyle)
+		// console.log(this.state.hairColor)
+		// console.log(this.state.hairColorStyle)
+		// console.log(this.state.name);
+		// console.log(this.state.player);
+		// console.log(this.state.starships)
 		return (
 			<div className="PlayerCard">
 				<h3 className="PlayerCard-Header">
-					{this.state.player.name}
+					{this.state.name}
 				</h3>
 
-				<div className="PlayerInfo">
-					<div className="PlayerInfo__descriptions">
-						<div className="PlayerInfo__boxR">born: </div>
-						<div className="PlayerInfo__boxR">mass: </div>
-						<div className="PlayerInfo__boxR">height: </div>
-						<div className="PlayerInfo__boxR">gender: </div>
-						<div className="PlayerInfo__boxR">hair: </div>
-						<div className="PlayerInfo__boxR">eyes: </div>
-						<div className="PlayerInfo__boxR">skin: </div>
+				<div className="PlayerCard-body">
+					<div className="PlayerInfo">
+						<div className="PlayerInfo__descriptions">
+							<div className="PlayerInfo__boxR">species: </div>
+							<div className="PlayerInfo__boxR">sentient: </div>
+							<div className="PlayerInfo__boxR">born: </div>
+							<div className="PlayerInfo__boxR">home: </div>
+							<div className="PlayerInfo__boxR">mass: </div>
+							<div className="PlayerInfo__boxR">height: </div>
+							<div className="PlayerInfo__boxR">gender: </div>
+							<div className="PlayerInfo__boxR">hair: </div>
+							<div className="PlayerInfo__boxR">eyes: </div>
+							<div className="PlayerInfo__boxR">skin: </div>
+						</div>
+
+						<div className="PlayerInfo__values">
+							<div className="PlayerInfo__boxL">{String(this.state.species.name).toLowerCase()}</div>
+							<div className="PlayerInfo__boxL">
+								{this.state.species.designation === 'sentient' ? 'yes' : 'no'}
+							</div>
+							<div className="PlayerInfo__boxL">{this.state.birthYear}</div>
+							<div className="PlayerInfo__boxL">{this.state.homeworld.name}</div>
+							<div className="PlayerInfo__boxL">{this.state.mass} kg</div>
+							<div className="PlayerInfo__boxL">{this.state.height} cm</div>
+							<div className="PlayerInfo__boxL">{this.state.gender}</div>
+							<div className="PlayerInfo__boxL">
+								{Array.isArray(this.state.hairColorStyle) ? this.state.hairColorStyle.map((hairStyle, i) => {						
+									return (
+										<div key={i} style={hairStyle}></div>
+									) 
+								}) : <div>{this.state.hairColor}</div>}
+							</div>
+							<div className="PlayerInfo__boxL">
+								{Array.isArray(this.state.eyeColorStyle) ? this.state.eyeColorStyle.map((eyeStyle, i) => {						
+									return (
+										<div key={i} style={eyeStyle}></div>
+									) 
+								}) : <div>{this.state.eyeColor}</div>}
+							</div>
+							<div className="PlayerInfo__boxL">
+								{Array.isArray(this.state.skinColorStyle) ? this.state.skinColorStyle.map((skinStyle, i) => {						
+									return (
+										<div key={i} style={skinStyle}></div>
+									) 
+								}) : <div>{this.state.skinColor}</div>}
+							</div>
+						</div>
 					</div>
 
-					<div className="PlayerInfo__values">
-						<div className="PlayerInfo__boxL">{this.state.birthYear}</div>
-						<div className="PlayerInfo__boxL">{this.state.player.mass} kg</div>
-						<div className="PlayerInfo__boxL">{this.state.player.height} cm</div>
-						<div className="PlayerInfo__boxL">{this.state.player.gender}</div>
-						<div className="PlayerInfo__boxL">
-							{Array.isArray(this.state.hairColorStyle) ? this.state.hairColorStyle.map((hairStyle, i) => {						
-								return (
-									<div style={hairStyle}></div>
-								) 
-							}) : <div>{this.state.hairColor}</div>}
-						</div>
-						<div className="PlayerInfo__boxL">
-							{Array.isArray(this.state.eyeColorStyle) ? this.state.eyeColorStyle.map((eyeStyle, i) => {						
-								return (
-									<div style={eyeStyle}></div>
-								) 
-							}) : <div>{this.state.eyeColor}</div>}
-						</div>
-						<div className="PlayerInfo__boxL">
-							{Array.isArray(this.state.skinColorStyle) ? this.state.skinColorStyle.map((skinStyle, i) => {						
-								return (
-									<div style={skinStyle}></div>
-								) 
-							}) : <div>{this.state.skinColor}</div>}
-						</div>
+					<div className="PlayerPicture">
+						<img src={this.state.picture} alt="player" />
+					</div>
+				</div>
+
+				<div className="PlayerInfo--detailed">
+					<div className="Vehicle--header">
+						vehicles:
+					</div>
+					<div className="PlayerVehicles">
+						{this.state.vehicles ? this.state.vehicles.length > 0 ? this.state.vehicles.map((vehicle, i) => {
+							// console.log(this.state.vehicles)
+							return (
+								<div key={i} className="PlayerInfo--detailed-item">
+									<div className="VehicleText">{String(vehicle.name).toLowerCase()}</div>
+									<img className="VehiclePicture" src={this.props.vehiclesPictures[vehicle.name]} alt='vehicle' />
+								</div>
+							)
+						}) : 'none' : null}
+					</div>
+				</div>
+
+				<div className="PlayerInfo--detailed">
+					<div className="Vehicle--header">
+						starships:
+					</div>
+					<div className="PlayerVehicles">
+						{this.state.starships ? this.state.starships.length > 0 ? this.state.starships.map((starship, i) => {
+							// console.log(this.state.starships)
+							return (
+								<div key={i} className="PlayerInfo--detailed-item">
+									<div className="VehicleText">{String(starship.name).toLowerCase()}</div>
+									<img className="VehiclePicture" src={this.props.starshipsPictures[starship.name]} alt='starship' />
+								</div>
+							)
+						}) : 'none' : null}
 					</div>
 				</div>
 
