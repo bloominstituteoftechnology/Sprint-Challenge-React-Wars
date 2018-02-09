0be5d54f98ec0acf612dad8f8363fc76348bd830
Smithface <kylewibblesmith@gmail.com>
2018-02-09 11:40:00 -0500

    display black box for each item in array
    got the basic layout down, need to insert proper info into container.js

---
+++ starwars/src/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Container.css';
+
+function Container(props) {
+  return(
+    <div className="container">
+    hola
+    </div>
+  );
+}
+
+export default Container;
