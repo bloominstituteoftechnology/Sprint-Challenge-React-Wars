653af0b4b3e230a09444f00ece74274c85740bcc
Charlie Sparks <csparks@antioch.edu>
2018-02-09 11:01:22 -0800

    getting started on building the cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
   constructor() {
---
+++ starwars/src/components/cards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cards(props) {
+  return (
+    <div className="cards">
+      <div className="cards__row1">
+
+      </div>
+    </div>
+    )
+}
