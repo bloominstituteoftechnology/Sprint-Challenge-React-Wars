4ade04fd685a5f54179fa79a999ff9f8a484f903
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-09 12:48:48 -0600

    Bold Title Update
    change information categories to bold

--- starwars/src/components/cardblock/cardblock.js
+++ starwars/src/components/cardblock/cardblock.js
@@ -11,11 +11,11 @@ class CardBlock extends Component {
       <CardBlockStyle>
         <h2>{this.props.data.name}</h2>
         <img src='https://images.unsplash.com/photo-1486736065690-45e0655d038a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8417003192c1520308b460e9ae6ab4c4&auto=format&fit=crop&w=1050&q=80' alt='blank background' />
-        <p>Gender: {this.props.data.gender}</p>
-        <p>Height: {this.props.data.height} cm</p>
-        <p>Weight: {this.props.data.mass} kg</p>
-        <p>Hair: {this.props.data.hair_color}</p>
-        <p>Complexion: {this.props.data.skin_color}</p>
+        <p><strong>Gender:</strong> {this.props.data.gender}</p>
+        <p><strong>Height:</strong> {this.props.data.height} cm</p>
+        <p><strong>Weight:</strong> {this.props.data.mass} kg</p>
+        <p><strong>Hair:</strong> {this.props.data.hair_color}</p>
+        <p><strong>Complexion:</strong> {this.props.data.skin_color}</p>
       </CardBlockStyle>
     );
   }
