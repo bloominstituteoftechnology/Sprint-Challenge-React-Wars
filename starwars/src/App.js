19005bafb9e2cc48197e2c47e36de90475311c9d
Christopher Beards <beardscm@gmail.com>
2018-02-09 13:52:36 -0500

    Initial Designs-W.I.P

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -25,20 +25,21 @@ class App extends Component {
   }
   render() {
     const data =this.state.starwarsChars.map(character =>  
+       <div className="container"> 
         <div className="character_card">
-          <h1 className="name">{character.name}</h1>
-          <h2 className="height">{character.height}</h2>
-          <p className="hair_color">{character.hair_color}</p>
-          <p className="skin_color">{character.skin_color}</p>
-          <p className="birth">{character.birth_year}</p>
-          <p className="gender">{character.gender}</p>
-          <p className="homeworld">{character.homeworld}</p>
-          <p className="films">{character.films}</p>
-          <p className="gender">{character.gender}</p>
-          <p className="species">{character.species}</p>
-          <p className="vehicles">{character.vehicles}</p>
-          <p className="starships">{character.starships}</p>
+          <p className="name">{character.name}</p>
+          <p className="height"><strong>Height: </strong>{character.height}</p>
+          <p className="hair_color"><strong>Hair Color: </strong>{character.hair_color}</p>
+          <p className="skin_color"><strong>Skin Color: </strong>{character.skin_color}</p>
+          <p className="birth"><strong>Date of Birth: </strong>{character.birth_year}</p>
+          <p className="gender"><strong>Gender: </strong>{character.gender}</p>
+          <p className="homeworld"><strong>Homeworld: </strong>{character.homeworld}</p>
+          <p className="films"><strong>Films: </strong>{character.films}</p>
+          <p className="species"><strong>Species: </strong>{character.species}</p>
+          <p className="vehicles"><strong>Vehicles: </strong>{character.vehicles}</p>
+          <p className="starships"><strong>Starships: </strong>{character.starships}</p>
           </div> 
+        </div>
         )
 
     return (
