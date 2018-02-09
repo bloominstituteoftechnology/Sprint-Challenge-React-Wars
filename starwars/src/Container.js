5ae84d278d42924f08d56d461507e85adcb9edf7
Smithface <kylewibblesmith@gmail.com>
2018-02-09 12:06:59 -0500

    inserted all useful info

--- starwars/src/Container.js
+++ starwars/src/Container.js
@@ -4,7 +4,31 @@ import './Container.css';
 function Container(props) {
   return(
     <div className="container">
-    hola
+      <div className="container__topRow">        {/* top row */}
+        <div>Star Wars</div>
+        <div>Character License</div>
+      </div>
+      <div className="container__middleRow">        {/* middle row */}
+        <div></div>
+        <div>          {/* center start */}
+          <div>Name: {props.charInfo.name}</div>
+          <div>YOB: {props.charInfo.birth_year}</div>
+          <div>
+            <div>Gender: {props.charInfo.gender}</div>
+            <div>Eye Color: {props.charInfo.eye_color}</div>
+            <div>Hair Color: {props.charInfo.hair_color}</div>
+            <div>Skin: {props.charInfo.skin_color}</div>
+            <div>Height:{props.charInfo.height}</div>
+            <div>Mass :{props.charInfo.mass}</div>
+          </div>
+        </div>          {/* center end */}
+        <div></div>
+      </div>
+      <div className="container__bottomRow">        {/* bottom row */}
+        <div>created: {props.charInfo.created}</div>
+        <div></div>
+        <div>edited: {props.charInfo.edited}</div>
+      </div>
     </div>
   );
 }
