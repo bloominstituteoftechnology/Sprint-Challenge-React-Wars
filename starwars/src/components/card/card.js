376e2379c0af1c2ccdacd8a63089ec991c76eb44
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-09 14:12:13 -0500

    Changed Design a Bit
    card.js --- Two class names for double/triple item rows instead
    
    card.js --- Two classes, added borders, increased image size.

--- starwars/src/components/card/card.js
+++ starwars/src/components/card/card.js
@@ -20,15 +20,13 @@ function Card(props) {
             <h1>{props.character.name}
             </h1>
             <div className="Card-Info-Block">
-                <div className="Card-Info-Item">Gender: {props.character.gender}</div>
-                <div className="Card-Info-Item">Born: {props.character.birth_year}</div>
-                <br /><br />
-                <div>Skin Color: {props.character.skin_color}</div>
-                <div>Eye Color: {props.character.eye_color}</div>
-                <div>Hair Color: {props.character.hair_color}</div>
-                <br /><br />
-                <div className="Card-Info-Item">Height: {props.character.height}</div>
-                <div className="Card-Info-Item">Mass: {props.character.mass}</div>
+                <div className="Card-Info-Item-Double">Gender: {props.character.gender}</div>
+                <div className="Card-Info-Item-Double">Born: {props.character.birth_year}</div>
+                <div className="Card-Info-Item-Triple">Skin Color: {props.character.skin_color}</div>
+                <div className="Card-Info-Item-Triple">Eye Color: {props.character.eye_color}</div>
+                <div className="Card-Info-Item-Triple">Hair Color: {props.character.hair_color}</div>
+                <div className="Card-Info-Item-Double">Height: {props.character.height}</div>
+                <div className="Card-Info-Item-Double">Mass: {props.character.mass}</div>
             </div>
         </div>
     );
