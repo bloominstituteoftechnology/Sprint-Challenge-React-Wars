680b71e3ccc3b6b9b2aa92c1ae4c92b8e5009aad
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-09 12:32:44 -0500

    Updated Card Style
    App.js --- 99% I just tried something and then deleted it in here...
    
    card.js --- Added class names.
    
    card.css --- Added classes, hover effect.

--- starwars/src/components/card/card.js
+++ starwars/src/components/card/card.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
+import './card.css'
 
 function Card(props) {
     return (
-        <div>
+        <div className="Card">
             <h1>{props.character.name}</h1>
-            <div>Gender: {props.character.gender}</div>
-            <div>Born: {props.character.birth_year}</div>
-            <div>Skin Color: {props.character.skin_color}</div>
-            <div>Eye Color: {props.character.eye_color}</div>
-            <div>Hair Color: {props.character.hair_color}</div>
-            <div>Height: {props.character.height}</div>
-            <div>Weight: {props.character.mass}</div>
+            <div className="Card-Info-Block">
+                <div className="Card-Info-Item">Gender: {props.character.gender}</div>
+                <div className="Card-Info-Item">Born: {props.character.birth_year}</div>
+                <br /><br />
+                <div>Skin Color: {props.character.skin_color}</div>
+                <div>Eye Color: {props.character.eye_color}</div>
+                <div>Hair Color: {props.character.hair_color}</div>
+                <br /><br />
+                <div className="Card-Info-Item">Height: {props.character.height}</div>
+                <div className="Card-Info-Item">Weight: {props.character.mass}</div>
+            </div>
         </div>
     );
 }
