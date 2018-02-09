33ada89528a5878a075791a234cc494c849cca94
Mephestys <mephestys@gmail.com>
2018-02-09 11:21:59 -0700

    Challenge completed, going to keep poking at it for a while. - Currently, InfoCards receive index values as a key, not optimal. - App passes data to InfoCard, which renders that data. - App creates an array of InfoCards and renders them. - Each InfoCard highlights when hovered over. - No console errors.

--- starwars/src/components/InfoCard/InfoCard.js
+++ starwars/src/components/InfoCard/InfoCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import "./InfoCard.css";
 
 const InfoCard = props => {
   const character = props.character;
 
   return (
-    <div>
-      {character.name}
+    <div className="InfoCard">
+      Name: {character.name} <br />
+      Gender: {character.gender} <br />
+      Skin Color: {character.skin_color} <br />
+      Eye Color: {character.eye_color} <br />
+      Hair Color: {character.hair_color} <br />
+      Birth Year: {character.birth_year} <br /> 
     </div>
   )
 }
