cd93b6651c1f7fb7aff1eb661739af0438fb46f0
rcs784 <rcstinson@hotmail.com>
2018-02-09 17:40:17 -0500

    Russell Stinson CS7 Sprint Challenge Complete with Extra Features

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Character from './Character';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
       starwarsChars: []
     };
   }
-  componentDidMount() {
+
+  componentWillMount() {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -25,8 +27,16 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div>
         <h1 className="Header">React Wars</h1>
+        <div className="App">
+          {this.state.starwarsChars.map((char, i) => {
+            return <Character index={i} name={char.name} height={`${char.height} cm`} weight={`${char.mass} kg`} hair_color={char.hair_color}
+              skin_color={char.skin_color} eye_color={char.eye_color} birth_year={char.birth_year} gender={char.gender} 
+                homeworld={char.homeworld} films={JSON.stringify(char.films)} species={JSON.stringify(char.species)} 
+                  vehicles={JSON.stringify(char.vehicles)} starships={JSON.stringify(char.starships)} />
+          })}
+        </div>
       </div>
     );
   }
