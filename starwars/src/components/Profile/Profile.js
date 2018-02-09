5a1fc13a879d7b5427749a4f36ee2673eee89d45
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-09 11:42:16 -0600

    add mass to profile

--- starwars/src/components/Profile/Profile.js
+++ starwars/src/components/Profile/Profile.js
@@ -4,13 +4,14 @@ import './Profile.css';
 const Profile = (props) => {
   return (
     <li className='profile'>
-      <div className="profile__info">Name: {props.profile.name}</div>
-      <div className="profile__info">Birth Year: {props.profile.birth_year}</div>
-      <div className="profile__info">Gender: {props.profile.gender}</div>
-      <div className="profile__info">Height: {props.profile.height}</div>
-      <div className="profile__info">Skin: {props.profile.skin_color}</div>
-      <div className="profile__info">Hair: {props.profile.hair_color}</div>
-      <div className="profile__info">Eye Color: {props.profile.eye_color}</div>
+      <div>Name: {props.profile.name}</div>
+      <div>Birth Year: {props.profile.birth_year}</div>
+      <div>Gender: {props.profile.gender}</div>
+      <div>Height: {props.profile.height}</div>
+      <div>Mass: {props.profile.mass}</div>
+      <div>Skin: {props.profile.skin_color}</div>
+      <div>Hair: {props.profile.hair_color}</div>
+      <div>Eye Color: {props.profile.eye_color}</div>
     </li>
   )
 }
