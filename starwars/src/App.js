223a4b088479f49237e25148385af9b89e0dbb42
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-02-10 00:38:57 +0500

    finished displaying info on the screen and its styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -23,13 +23,35 @@ class App extends Component {
         throw new Error(err);
       });
   }
+  
+
+  
   render() {
+    //console.log(chars);
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="container">
+          <div className="characters">
+            {this.state.starwarsChars.map(char => {
+              return (
+                <div key={char.created} className="allChars">
+                  <div className="info">Name: {char.name}</div>
+                  <div className="info">Birth Year: {char.birth_year}</div>
+                  <div className="info">Gengder: {char.gender}</div>
+                  <div className="info">Height: {char.height}</div>
+                  <div className="info">Eye color: {char.eye_color}</div>
+                  <div className="info">Skin Color: {char.skin_color}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
