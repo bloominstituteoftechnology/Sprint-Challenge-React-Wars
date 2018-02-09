b64f078e771407cb1ef8368c3d2fa7d194748db6
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-09 11:31:18 -0800

    Kinda star-wars-y styling, ugly as sin though

--- starwars/src/Data.js
+++ starwars/src/Data.js
@@ -13,12 +13,12 @@ class Data extends React.Component {
         return (
           <div key={char.created} className='char'>
             <div className='char-name'> {char.name} </div>
-            <div className='char-bd'> <span>Birth Year:</span> {char.birth_year} </div>
-            <div className='char-gender'> <span>Gender:</span> {char.gender} </div>
-            <div className='char-hair'> <span>Hair Color:</span> {char.hair_color} </div>
-            <div className='char-height'> <span>Height:</span> {char.height}{'cm'} </div>
-            <div className='char-mass'> <span>Mass:</span> {char.mass}{'kg'} </div>
-            <div className='char-skin'> <span>Skin Color:</span> {char.skin_color} </div>
+            <div className='char-bd attrib'> <span>Birth Year:</span> {char.birth_year} </div>
+            <div className='char-gender attrib'> <span>Gender:</span> {char.gender} </div>
+            <div className='char-hair attrib'> <span>Hair Color:</span> {char.hair_color} </div>
+            <div className='char-height attrib'> <span>Height:</span> {char.height}{'cm'} </div>
+            <div className='char-mass attrib'> <span>Mass:</span> {char.mass}{'kg'} </div>
+            <div className='char-skin attrib'> <span>Skin Color:</span> {char.skin_color} </div>
           </div>
             );
       })}
