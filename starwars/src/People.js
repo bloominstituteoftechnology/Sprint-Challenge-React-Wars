ef06f12eaa42305d42015b7f2086472a8675d693
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-09 12:40:04 -0500

    Styling Updates
    Added a few extra styling changes to each character box

--- starwars/src/People.js
+++ starwars/src/People.js
@@ -2,38 +2,44 @@ import React from 'react';
 
 const styles = {
   display: 'flex',
-  width: '50%',
+  textAlign:'center',
+  width: '33%',
+  maxWidth: '350px',
   flexDirection: 'column',
+  flexWrap:'wrap',
   border: '1px solid black',
   margin: '20px',
+  padding:'15px 0',
+  background: '#C3C3C3',
+  lineHeight: '150%',
 }
 
 function People ({character}) {
   return (
     <div className="character" style={styles}>
       <div className="name">
-        Name: {character.name}
+        <strong>Name:</strong> {character.name}
       </div>
       <div className="birth_year">
-        Birth Year: {character.birth_year}
+        <strong>Birth Year:</strong> {character.birth_year}
       </div>
       <div className="gender">
-        Gender: {character.gender}
+        <strong>Gender:</strong> {character.gender}
       </div>
       <div className="eye_color">
-        Eye Color: {character.eye_color}
+        <strong>Eye Color:</strong> {character.eye_color}
       </div>
       <div className="hair_color">
-        Hair Color: {character.hair_color}
+        <strong>Hair Color:</strong> {character.hair_color}
       </div>
       <div className="height">
-        Height: {character.height}
+        <strong>Height:</strong> {character.height}
       </div>
       <div className="mass">
-        Mass: {character.mass}
+        <strong>Mass:</strong> {character.mass}
       </div>
       <div className="skin_color">
-        Skin Color: {character.skin_color}
+        <strong>Skin Color:</strong> {character.skin_color}
       </div>
     </div>
   )
