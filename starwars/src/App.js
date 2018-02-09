76fc832ec53c1dff56d2613b64502fffdcfe3f19
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-09 13:43:51 -0500

    pulled data from api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -27,6 +27,28 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <ul>
+          {this.state.starwarsChars.map((character, i) => {
+            return (
+              <table>
+                <tr>
+                  <th>Character Name</th>
+                  <th>Gender</th>
+                  <th>Hair Color</th>
+                  <th>Eye Color</th>
+                  <th>Height</th>
+                </tr>
+                <tr key={i}>
+                  <td>{character.name}</td>
+                  <td>{character.gender}</td>
+                  <td>{character.hair_color}</td>
+                  <td>{character.eye_color}</td>
+                  <td>{character.height}</td>
+                </tr>
+              </table>
+            )
+          })}
+        </ul>
       </div>
     );
   }
