b7f694688367261e686b36f32e875e6edbf60967
kait-schorr <kaitlynschorr@gmail.com>
2018-03-09 12:24:03 -0600

    Styling and centered header, noticed the cards moved outside of container when expanded

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import CharacterComponent from "./components/CharacterComponent";
 
 class App extends Component {
@@ -28,12 +28,18 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <Container>
-          <h1 className="Header">React Wars</h1>
-          {this.state.starwarsChars.map(char => {
-            return <CharacterComponent key={char.count} char={char} />;
-          })};
+      <div className="app-wrapper">
+        <Container className="app-content">
+          <Row>
+            <Col sm={12} className="text-center">
+              <h1 className="Header">React Wars</h1>
+            </Col>
+          </Row>
+          <Row>
+            {this.state.starwarsChars.map(char => {
+              return <CharacterComponent key={char.count} char={char} />;
+            })};
+          </Row>
         </Container>
       </div>
     );
--- starwars/src/components/CharacterComponent.js
+++ starwars/src/components/CharacterComponent.js
@@ -1,25 +1,52 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Card, CardTitle, CardBody, CardText } from "reactstrap";
+import {
+  Collapse,
+  Button,
+  Card,
+  CardTitle,
+  CardBody,
+  CardText,
+  Col
+} from "reactstrap";
+
+import "./CharacterComponents.css";
 
 class CharacterComponent extends Component {
+  constructor() {
+    super();
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
   render() {
     return (
-      <Card>
-        <CardBody>
-          <CardTitle>{this.props.char.name}</CardTitle>
-          <CardText>Birth Year: {this.props.char.birth_year}</CardText>
-          <CardText>Height: {this.props.char.height}</CardText>
-          <CardText>Mass: {this.props.char.mass}</CardText>
-          <CardText>Hair Color: {this.props.char.hair_color}</CardText>
-          <CardText>Skin Color: {this.props.char.skin_color}</CardText>
-          <CardText>Eye Color: {this.props.char.eye_color}</CardText>
-          <CardText>Gender: {this.props.char.gender}</CardText>
-          <CardText>Home World: {this.props.char.homeworld}</CardText>
-          <CardText>Films: {this.props.char.films}</CardText>
-          <CardText>Species: {this.props.char.species}</CardText>
-        </CardBody>
-      </Card>
+      <Col sm={4}>
+        <Card className="charCards m-3">
+          <CardBody>
+            <CardTitle>{this.props.char.name}</CardTitle>
+
+            <Collapse isOpen={this.state.collapse}>
+              <CardText>Birth Year: {this.props.char.birth_year}</CardText>
+              <CardText>Height: {this.props.char.height}</CardText>
+              <CardText>Mass: {this.props.char.mass}</CardText>
+              <CardText>Hair Color: {this.props.char.hair_color}</CardText>
+              <CardText>Skin Color: {this.props.char.skin_color}</CardText>
+              <CardText>Eye Color: {this.props.char.eye_color}</CardText>
+              <CardText>Gender: {this.props.char.gender}</CardText>
+              <CardText>Home World: {this.props.char.homeworld}</CardText>
+              <CardText>Films: {this.props.char.films}</CardText>
+              <CardText>Species: {this.props.char.species}</CardText>
+            </Collapse>
+            <Button className="" onClick={this.toggle}>
+              Read More
+            </Button>
+          </CardBody>
+        </Card>
+      </Col>
     );
   }
 }
