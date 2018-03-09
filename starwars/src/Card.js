85903bfcb1cf0064f70f8ec1df4c4b215f5f5cfe
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-09 11:03:21 -0800

    updated font color and font weight

--- starwars/src/Card.js
+++ starwars/src/Card.js
@@ -6,9 +6,15 @@ const Card = props => {
     <div className="Char-Wrapper">
       <div className="Card">
         <h3 className="CharName">{props.char.name}</h3>
-        <p className="CharInfo">Height: {props.char.height}</p>
-        <p className="CharInfo">Eye Color: {props.char.eye_color}</p>
-        <p className="CharInfo">Birth Year: {props.char.birth_year}</p>
+        <p className="CharInfo">
+          <span>Height: </span> {props.char.height}
+        </p>
+        <p className="CharInfo">
+          <span>Eye Color: </span> {props.char.eye_color}
+        </p>
+        <p className="CharInfo">
+          <span>Birth Year: </span> {props.char.birth_year}
+        </p>
       </div>
     </div>
   );
