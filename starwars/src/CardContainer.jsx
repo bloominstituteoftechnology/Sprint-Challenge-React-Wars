4e2b182c61f46426e12fcf8035d332b6b2c0b37f
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-03-09 10:57:20 -0800

    character list displayed

---
+++ starwars/src/CardContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardContainer.css';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+  const CardContainer = props => {
+    const card = props.char;
+    
+
+  };
+  
+  export default CardContainer;
