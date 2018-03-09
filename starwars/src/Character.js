730cc866f291fe4202496ff8281906c8b737487b
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-03-09 17:12:25 -0500

    fix hair color property render

--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -6,17 +6,13 @@ export default class Character extends Component {
         super(props);
         this.state = {
             character: {},
-            // films: [],
         };
-        // So that when showFriendsHandler(obj) is passed down as a prop, it will be in scope???
-        // this.showFilmsHandler = this.showFilmsHandler.bind(this)
     }
+
     componentDidMount() {
         this.setState({character: this.props.character})
     }
-    // showFriendsHandler(obj) {
-    //     console.log(obj);
-    // }
+
     render() {
         return (
             <div className = "Character">
@@ -24,7 +20,7 @@ export default class Character extends Component {
                 <div className="birth">Born in {this.state.character.birth_year}</div>
                 <div className="eye_color">Eye Color: {this.state.character.eye_color}</div>
                 <div className="gender">Gender: {this.state.character.gender}</div>
-                <div className="hair_color">Gender: {this.state.character.gender}</div>
+                <div className="hair_color">Hair Color: {this.state.character.hair_color}</div>
                 <div className="height">Height: {this.state.character.height}</div>
                 <div className="mass">Mass: {this.state.character.mass}</div>
                 <div className="skin_color">Skin Color: {this.state.character.skin_color}</div>
