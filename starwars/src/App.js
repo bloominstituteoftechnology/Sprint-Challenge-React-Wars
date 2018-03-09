e44f62d88b8d5a835ec2285f25fbd2b1dee2500a
Randy Wilson <phoenixrr2113@gmail.com>
2018-03-09 17:50:51 -0500

    fixed background issue

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -37,39 +37,39 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1 className="Header">React Wars</h1>
-				{this.state.starwarsChars.map((chars) => {
-					return (
-						<Container>
-              <Row>
-                <Col md="3">
-							    <Card>
-								    <CardHeader tag="h3">{chars.name}</CardHeader>
-								    <CardBody>
-									    <CardTitle className="text-muted">
-										    Birth Year: {chars.birth_year}
-									    </CardTitle>
-									    <CardTitle className="text-muted">
-										    Eye Color: {chars.eye_color}
-									    </CardTitle>
-									    <CardTitle className="text-muted">
-										    Hair Color: {chars.hair_color}
-									    </CardTitle>
-									    <CardTitle className="text-muted">
-										    Gender: {chars.gender}
-									    </CardTitle>
-									    <CardTitle className="text-muted">
-										    Height: {chars.height}
-									    </CardTitle>
-									    <CardTitle className="text-muted">
-										    Mass: {chars.mass}
-									    </CardTitle>
-								    </CardBody>
-							    </Card>
+        <Container>
+          <Row>
+				    {this.state.starwarsChars.map((chars) => {
+					    return (
+                <Col md="4">
+                  <Card>
+                    <CardHeader tag="h3">{chars.name}</CardHeader>
+                    <CardBody>
+                      <CardTitle className="text-muted">
+                        Birth Year: {chars.birth_year}
+                      </CardTitle>
+                      <CardTitle className="text-muted">
+                        Eye Color: {chars.eye_color}
+                      </CardTitle>
+                      <CardTitle className="text-muted">
+                        Hair Color: {chars.hair_color}
+                      </CardTitle>
+                      <CardTitle className="text-muted">
+                        Gender: {chars.gender}
+                      </CardTitle>
+                      <CardTitle className="text-muted">
+                        Height: {chars.height}
+                      </CardTitle>
+                      <CardTitle className="text-muted">
+                        Mass: {chars.mass}
+                      </CardTitle>
+                    </CardBody>
+                  </Card>
                 </Col>
-              </Row>
-						</Container>
-					);
-				})}
+              );
+            })}
+          </Row>
+        </Container>
 			</div>
 		);
 	}
