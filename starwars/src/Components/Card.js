05eb33a2018231b23b5a762a9f8acc5f64de763d
astro11 <aquila.strother@asu.edu>
2018-03-09 13:40:27 -0500

    propTypes and updated card.js

--- starwars/src/Components/Card.js
+++ starwars/src/Components/Card.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Card, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import './Card.css'
+import PropTypes from "prop-types";
 
 const CardImgs = (props) => {
   const post = props.wars;
   return (
-    <div>   
-      <div>        
-        <Card body inverse style={{ borderColor: '#333' }}>
-          <CardTitle>Known As: {post.name}</CardTitle>
+    <div className="starWars">
+      <Card body inverse style={{ backgroundColor: '#3498db', padding: '30px', alignItems: 'center', borderColor: '#fff' }}>       
+          <CardTitle>Name: {post.name}</CardTitle>
           <CardSubtitle>Birth Year: {post.birth_year}</CardSubtitle>
-          <CardText>Height: {post.height}</CardText>          
-        </Card>
-      </div>
-    </div>    
-  )
-}
+          <CardSubtitle>Height: {post.height}</CardSubtitle>
+          <CardSubtitle>Gender: {post.gender}</CardSubtitle>
+          <CardSubtitle>Eye Color: {post.eye_color}</CardSubtitle>
+          <CardSubtitle>Skin Color: {post.skin_color}</CardSubtitle>               
+      </Card>
+    </div>
+  );
+};
+
+CardImgs.defaultProps = {
+  cardImg: PropTypes.shape({
+    name: "Luke"
+  })
+};
+
+CardImgs.propTypes = {
+  cardImg: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    height: PropTypes.string,    
+    hair_color: PropTypes.string
+  })
+};
+    
 
 export default CardImgs;
