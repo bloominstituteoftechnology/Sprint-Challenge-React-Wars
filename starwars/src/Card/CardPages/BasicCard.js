405ad1d6d39c5b7470f012c507d4ca6a7576cd62
Brett Sullivan <opticalfalicy@gmail.com>
2018-03-09 13:03:02 -0600

    Sprint Complete

---
+++ starwars/src/Card/CardPages/BasicCard.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// const BasicCard = props => {
+//     const { basicData } = props;
+//     return(
+//     <div>
+//     {basicData.map(basic =>{
+//         return(
+//             <div>
+//              <div className="Basic__Name">{basic.name}</div>
+//              <div className="Basic__Birth">{basic.birth_year}</div>
+//             </div>
+//         )
+
+//     })}
+//     </div>    
+//   )
+// }
+
+// export {BasicCard};
