30433ec1580a5a57ee1b1d199f004a0cdb3dd94e
Khepf <jeremy1975@gmail.com>
2018-03-09 11:50:42 -0500

    mapped initial non-subarray data

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Hello from Card</h3>
+      </div>
+    );
+  }
+};
+
+export default Card;
