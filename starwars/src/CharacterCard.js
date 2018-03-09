17a706981a5728415fffc253e23a70dd443dce36
Robert Rak <robertrak@hotmail.com>
2018-03-09 17:50:23 -0500

    Prettify cards further

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Card, CardTitle, CardBody, CardText } from 'reactstrap';
+import { Card, CardTitle, CardBody, CardText, CardHeader, CardFooter } from 'reactstrap';
 import './CharacterCard.css';
 
 const CharacterCard = props => {
   return (
     <div className="CharacterCard">
       <Card>
-        <CardBody>
+          <CardHeader>
           <CardTitle>{props.character.name}</CardTitle>
-        </CardBody>
+        </CardHeader>
         <CardBody>
         <CardText>Height: {props.character.height}</CardText>
         <CardText>Mass: {props.character.mass}</CardText>
         <CardText>Hair color: {props.character.hair_color}</CardText>
         <CardText>Skin color: {props.character.skin_color}</CardText>
         </CardBody>
+        <CardFooter></CardFooter>
       </Card>
     </div>
   )
