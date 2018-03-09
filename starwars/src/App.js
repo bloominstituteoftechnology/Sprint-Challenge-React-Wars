ab8fb6b28c430d36ef41fd715b4da7cc4320bfb9
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-09 12:20:09 -0600

    More revision, nothing works

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import CharCard from './Card/CharCard';
 
 class App extends Component {
--- starwars/src/Card/CharCard.js
+++ starwars/src/Card/CharCard.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
-import CharList from '../List/CharList';
 
 const CharCard = (props) => {
   return (
     <Row>
       <Col sm="6">
         <Card body>
-          <CardTitle>Special Title Treatment</CardTitle>
+          <CardTitle>{this.name}</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         </Card>
       </Col>
