2edf51699a7c0cc3e076b763e51048b370e00032
Robert Rak <robertrak@hotmail.com>
2018-03-09 16:52:41 -0500

    Clean up

--- starwars/src/CharacterList.js
+++ starwars/src/CharacterList.js
@@ -4,7 +4,7 @@ import './CharacterList.css';
 
 const CharacterList = props => {
   return (
-    <div className="CharacterListContainer row flex-wrap space-around">
+    <div className="CharacterListContainer">
     {props.characters.map(character => {
       return <CharacterCard key={character.name} character={character} />;
     })}
