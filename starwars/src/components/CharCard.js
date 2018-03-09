7bf9b329d2b9b7024a4ea4c7cd733305d09b6855
Nick Coultas <nncoultas@yahoo.com>
2018-03-09 09:52:32 -0800

    Cards are in place working on styling

---
+++ starwars/src/components/CharCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './CharCard.css';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+
+
+const CharCard = (props) => {
+    return (
+        <div>
+            <Card>
+                <CardBody>
+                <CardTitle>{props.char.name}</CardTitle>
+                <CardText>Born: {props.char.birth_year}</CardText>
+                <CardText>Gender: {props.char.gender}</CardText>
+                <CardText>Hair Color: {props.char.hair_color}</CardText>
+                <CardText>Weight: {props.char.mass}</CardText>
+                <CardText>Height: {props.char.height}</CardText>
+                </CardBody>
+            </Card> 
+        </div>
+    )
+}
+
+export default CharCard;
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
