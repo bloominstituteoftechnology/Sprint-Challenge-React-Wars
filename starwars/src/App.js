281ac28b623f8817bd43b5fe228978f574780827
unknown <adfaris@gmail.com>
2018-03-09 12:11:12 -0500

    Finished the QA

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -12,21 +12,35 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
-      .then(res => {
+    const myStarWars = fetch('https://swapi.co/api/people')
+      myStarWars.then(res => {
         return res.json();
       })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
+      .then(starwarsChars => {
+        this.setState({ starwarsChars: starwarsChars.results });
       })
       .catch(err => {
         throw new Error(err);
       });
   }
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {this.state.starwarsChars.map(starwarchar => {
+          return(
+          <div>
+              <h3> {starwarchar.name}</h3> 
+              <p> {starwarchar.birth_year}</p>
+              <p> {starwarchar.created}</p>
+              <p> {starwarchar.eyecolor} </p>
+              {/* <p> {starwarchar.films} </p> */}
+              <p> {starwarchar.gender} </p>
+              <p> {starwarchar.hair_color} </p>
+
+          </div>
+        );})};
       </div>
     );
   }
