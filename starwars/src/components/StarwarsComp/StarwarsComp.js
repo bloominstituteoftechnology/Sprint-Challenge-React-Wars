0ed0f6f701267445ade04381da24713691fc2a87
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-03-09 15:40:39 -0800

    Managed to retrieve data inside child component using fetch api for species array

--- starwars/src/components/StarwarsComp/StarwarsComp.js
+++ starwars/src/components/StarwarsComp/StarwarsComp.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import './StarwarsComp.css'
 
-const StarwarsComp = (props) => {
+class StarwarsComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      species: []
+    };
+  }
   
+  componentDidMount() {
+    console.log(this.props.characters.species);
+    const [ spec ] = this.props.characters.species;
+    console.log(spec);
+    
+    fetch(spec)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      this.setState({ species: data });
+      //console.log(data.results);
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+    //console.log(this.state)
+  }
+  
+  render() {
+    //console.log(this.state);
     return (
       <div className="characters">
-        <div>Name: {props.characters.name}</div>
-        <div>Birth date: {props.characters.birth_year}</div>
-        <div>Gender: {props.characters.gender}</div>
-        <div>Weight: {props.characters.mass}</div>
-        <div>Height: {props.characters.height}</div>
-        <div>Skin color: {props.characters.skin_color}</div>
-        <div>Eyes: {props.characters.eye_color}</div>
+        <div>Name: {this.props.characters.name}</div>
+        <div>Birth date: {this.props.characters.birth_year}</div>
+        <div>Gender: {this.props.characters.gender}</div>
+        <div>Weight: {this.props.characters.mass}</div>
+        <div>Height: {this.props.characters.height}</div>
+        <div>Skin color: {this.props.characters.skin_color}</div>
+        <div>Eyes: {this.props.characters.eye_color}</div>
+        <div>Species: {this.state.species.name}</div>
       </div>
     );
+  }
+  
 }
 
 
