867094d55b6645299ac88a4007bc8cb386c3ddde
Cesar Mejia Leiva <cmejia@gmail.com>
2018-03-09 20:45:50 -0800

    still working on putting local images into bootstap cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import StarWarsCard from './components/StarWarsCard/StarWarsCard'
 
+const images = [ 
+  './components/StarWarsCard/images/luke_skywalker.jpeg',
+  './components/StarWarsCard/images/C-3PO.jpeg',
+  './components/StarWarsCard/images/R2-D2.jpeg',
+  './components/StarWarsCard/images/darth_vader.jpeg',
+  './components/StarWarsCard/images/darth_vader.jpeg',
+  './components/StarWarsCard/images/leio_organa.jpeg',
+  './components/StarWarsCard/images/owen_lars.jpeg',
+  './components/StarWarsCard/images/beru_lars.jpeg',
+  './components/StarWarsCard/images/R5-D4.jpeg',
+  './components/StarWarsCard/images/biggs_darklighter.jpeg',
+  './components/StarWarsCard/images/obi-wan_kenobi.jpeg',
+]
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
     };
   }
+
   componentDidMount() {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
@@ -20,12 +35,20 @@ class App extends Component {
       .then(data => {
         this.setState({ starwarsChars: data.results });
       })
+      .then( () => {
+        const { starwarsChars } = this.state;
+        console.log(starwarsChars);
+        starwarsChars.map( (obj, index) => {
+          return obj.image = images[index];
+        })
+      })
       .catch(err => {
         throw new Error(err);
       });
   }
   
   render() {
+    console.log(this.state.starwarsChars);
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
--- starwars/src/components/StarWarsCard/StarWarsCard.js
+++ starwars/src/components/StarWarsCard/StarWarsCard.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import './StarWarsCard.css'
 import PropTypes from 'prop-types';
-import img from './images/luke_skywalker.jpeg'
 
 const StarWarsCard = (props) => {
+  // src=require('./images/luke_skywalker.jpeg')
   return (
     <div className="StarWarsCard">
       <Card className="CardContainer" >
-      <CardImg top width="100%" src={img} alt="Card image cap" />
+      <CardImg top width="100%" src={props.char.image} alt="Card image cap" />
         <CardBody>
           <CardTitle className="CharName">{props.char.name}</CardTitle>
-          <CardSubtitle className="VitalsData">Vital Data:</CardSubtitle>
+          <CardSubtitle className="BioStats">Bio Stats</CardSubtitle>
 
           <div className="CardBioContent">
-          <CardText><span>Birth Year:</span> {props.char.birth_year}</CardText>
-          <CardText><span>Gender</span> {props.char.gender}</CardText>
-          <CardText><span>Height:</span> {props.char.height} cm</CardText>
-          <CardText><span>Mass:</span> {props.char.mass} kg</CardText>
-          <CardText><span>Skin Color:</span> {props.char.skin_color}</CardText>
-          <CardText><span>Hair Color:</span> {props.char.hair_color}</CardText>
-          <CardText><span>Eye Color:</span> {props.char.eye_color}</CardText>
+          <CardText><span className="BioData">Birth Year:</span> {props.char.birth_year}</CardText>
+          <CardText><span className="BioData">Gender</span> {props.char.gender}</CardText>
+          <CardText><span className="BioData">Height:</span> {props.char.height} <span>cm</span></CardText>
+          <CardText><span className="BioData">Mass:</span> {props.char.mass} <span>kg</span></CardText>
+          <CardText><span className="BioData">Skin Color:</span> {props.char.skin_color}</CardText>
+          <CardText><span className="BioData">Hair Color:</span> {props.char.hair_color}</CardText>
+          <CardText><span className="BioData">Eye Color:</span> {props.char.eye_color}</CardText>
           </div>
           <Button className="btn-card">Learn More</Button>
         </CardBody>
@@ -29,7 +29,7 @@ const StarWarsCard = (props) => {
   )
 };
 
-StarWarsCard.propTypes ={
+StarWarsCard.propTypes = {
   char: PropTypes.shape({
     name: PropTypes.string,
     birth_year: PropTypes.string,
