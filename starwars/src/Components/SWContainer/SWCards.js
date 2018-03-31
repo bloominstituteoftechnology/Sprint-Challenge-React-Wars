4b660aaa4da70447dffb1c369e55aede85c98397
unknown <alexisreyes90@gmail.com>
2018-03-31 14:41:45 -0400

    Updated cards, functionality works, coding complete

--- starwars/src/Components/SWContainer/SWCards.js
+++ starwars/src/Components/SWContainer/SWCards.js
@@ -4,18 +4,20 @@ import { Card, CardText, CardBody, CardSubtitle } from 'reactstrap';
 
 const SWCards = props => {
     return (
-        <Card className="SWBody">
+        <Card className="Body">
             <CardBody>
                     <CardSubtitle className="cardSubtitle">
                         {props.name}
                     </CardSubtitle>
             </CardBody>
-            <CardBody>
-                <CardText className="text--Body">
-                    <div>Was Born in the year: {props.birth_year}</div>
-                    <div>Gender: {props.gender}</div>
-                </CardText>
-            </CardBody>
+                <CardBody>
+                    <CardText className="text--Body">
+                        <div>Was Born in the year: {props.birth_year}</div>
+                        <div>Gender: {props.gender}</div>
+                        <div>Height: {props.height}</div>
+                        <div>Skin-Color: {props.eye_color}</div>
+                    </CardText>
+                </CardBody>
         </Card>
     );
 };
