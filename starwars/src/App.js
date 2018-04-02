4c7723df05e6a07a2bd197e5ae87a135d1d237b8
Glenn-David Daniel <d2rd@outlook.com>
2018-04-01 19:47:17 -0700

    Layout with code reuse from ls-notes project.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -27,6 +27,77 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {/* PASTE LS-NOTES DIVS BELOW */}
+        <div className="Notes-panel">
+            <div className="View-header">
+                <h1>Star Wars Characters{this.props.pageHeader}</h1>
+            </div>
+            <div>
+              <ul>
+                <li className="Note-row">
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 0 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 1 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 2 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                </li>
+                <li className="Note-row">
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 3 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 4 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 5 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                </li>
+                <li className="Note-row">
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 6 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 7 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                  <div className="NoteItem">
+                  <div className="Note-header">
+                      <h3>Character 8 Name {this.props.title}</h3>
+                  </div>
+                  <div className="Note-body">Character details go here.</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>  
+        {/* END OF LS-NOTES DIVS */}
       </div>
     );
   }
