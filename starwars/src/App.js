e6dc59db1de93b2f55efcbce2c8218d3b68e9482
Glenn-David Daniel <d2rd@outlook.com>
2018-04-01 23:26:33 -0700

    WIP Refactor 1 working.  Need to clean up component rendering as multiple items

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,9 +8,10 @@ import './App.css';
               <ul>
                 <li className="Note-row">
                   <div className="NoteItem">
-                  <div className="Note-header">
-                      <h3>Character 0 Name {this.props.title}</h3>
-                  </div>
+                    <div className="Note-header">
+                        {/* <h3>{this.props.name}</h3> */}
+                        <h3>{props.name}</h3>
+                    </div>
                     <div className="Note-body">
                       <u>Physical Characteristics</u>
                       <p>Name: {props.name}</p>
@@ -24,20 +25,40 @@ import './App.css';
                     </div>
                   </div>
                   <div className="NoteItem">
-                  <div className="Note-header">
-                      <h3>Character 1 Name {this.props.title}</h3>
-                  </div>
-                  <div className="Note-body">Character details go here.</div>
+                    <div className="Note-header">
+                        <h3>Character 1 Name {this.props.title}</h3>
+                    </div>
+                    <div className="Note-body">
+                      <u>Physical Characteristics</u>
+                      <p>Name: {props.name}</p>
+                      <p>Species: {props.species}</p>
+                      <p>Gender: {props.gender}</p>
+                      <p>Height: {props.height}</p>
+                      <p>Mass: {props.mass}</p>
+                      <p>Skin_Color: {props.skin_color}</p>
+                      <p>Eye_color: {props.eye_color}</p>
+                      <p>Hair_Color: {props.hair_color}</p>
+                    </div>
                   </div>
                   <div className="NoteItem">
-                  <div className="Note-header">
-                      <h3>Character 2 Name {this.props.title}</h3>
-                  </div>
-                  <div className="Note-body">Character details go here.</div>
+                    <div className="Note-header">
+                        <h3>Character 2 Name {this.props.title}</h3>
+                    </div>
+                    <div className="Note-body">
+                      <u>Physical Characteristics</u>
+                      <p>Name: {props.name}</p>
+                      <p>Species: {props.species}</p>
+                      <p>Gender: {props.gender}</p>
+                      <p>Height: {props.height}</p>
+                      <p>Mass: {props.mass}</p>
+                      <p>Skin_Color: {props.skin_color}</p>
+                      <p>Eye_color: {props.eye_color}</p>
+                      <p>Hair_Color: {props.hair_color}</p>
+                    </div>
                   </div>
                 </li>
               </ul>
-            </div>
+      </div>
     )
   }
 class App extends Component {
@@ -75,9 +96,9 @@ class App extends Component {
             <div>
             var app = (
               <div>
-                <Character name=“Tchala“ species=“Wakandan“ gender=“Male“ height=“5’11“ mass=“196“ skin_color=“Brown“ eye_color=“Brown“ hair_color=“Black“ />
+                <Character name='Tchala'species='Wakandan' gender='Male' height='181' mass='89' skin_color='Brown' eye_color='Brown' hair_color='Black' />
 
-                <Character name=““ species=““ gender=““ height=““ mass=““ skin_color=““ eye_color=““ hair_color=““ />
+                <Character name='Suri' species='Wakandan' gender='Female' height='165' mass='50' skin_color='Brown' eye_color='Hazel' hair_color='Auburn' />
               </div>
 )
 
@@ -89,5 +110,5 @@ class App extends Component {
   }
 }
 
-ReactDOM.render(app, document.querySelector('#app'));
+// ReactDOM.render(app, document.querySelector('#app'));
 export default App;
