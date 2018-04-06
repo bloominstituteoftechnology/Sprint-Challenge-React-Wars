275cb12148b3fa32633ec4e91eb0d1a43821958a
totsuya <supertatortots@gmail.com>
2018-04-06 13:48:59 -0400

    setting up framework of the card

--- starwars/src/components/Card/Card.js
+++ starwars/src/components/Card/Card.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Card.css';
 //should display a list of cards with character info
 const Cards = props => {
     return (
+        <div className='cardContainer'>
         <div className='cardData'>
+        <div>
             {props.name}
+                </div> <div>
+            {props.birth_year}
+                </div> 
+            {props.gender}
+           <div>
+            {props.height}
+            </div>
+            </div>
         </div>
     );
 };
