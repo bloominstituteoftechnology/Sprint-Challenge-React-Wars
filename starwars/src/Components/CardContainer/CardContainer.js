a6c324da498034dd7514c4f772a0e477e0f40657
2940cristian <2940cristian@gmail.com>
2018-04-06 09:49:25 -0700

    implemented propTypes

--- starwars/src/Components/CardContainer/CardContainer.js
+++ starwars/src/Components/CardContainer/CardContainer.js
@@ -1,13 +1,13 @@
 import React from "react";
+import PropTypes from 'prop-types';
 const CardContainer = (props) => {
 
-    console.log(props.data);
     return(
-        <div>
+        <div className = "post__Container">
         {props.data.map(post => {
             return(
                 <div className="post">
-                    <div>{post.name}</div>
+                    <h2>{post.name}</h2>
                     <div>Height: {post.height}</div>
                     <div>Mass: {post.mass}</div>
                     <div>Birth year: {post.birth_year}</div>
@@ -22,4 +22,16 @@ const CardContainer = (props) => {
     )
 }
 
+CardContainer.propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        height: PropTypes.string.isRequired,
+        mass: PropTypes.string.isRequired,
+        birth_year: PropTypes.string.isRequired,
+        gender: PropTypes.string.isRequired,
+        hair_color: PropTypes.string.isRequired,
+        eye_color: PropTypes.string.isRequired,
+        skin_color: PropTypes.string.isRequired
+    }))
+};
 export default CardContainer;
