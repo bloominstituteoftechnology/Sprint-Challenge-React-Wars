7f22580248ef33bfc394f062489db25a9c9d85b6
Justin Hong <jhong2009@gmail.com>
2018-04-06 09:22:25 -0700

    installed bootstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   constructor() {
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div><Button color="danger">Danger!</Button></div>
       </div>
     );
   }
