899a4093c3982a44aba25056f2cfb08161021331
Natalie Schennum <natalieschennum@gmail.com>
2018-04-06 10:43:37 -0700

    :(

---
+++ star-wars/starwars/src/SWChars.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardTitle, CardBody } from 'reactstrap';
+
+
+// const SWChars = props => {
+//     return (
+//         <div className="SWChar-Wrapper" >
+//             {props.starwarsChars.map(results => (
+//                     <Card>
+//                         <CardBody key={results.id}>
+//                             <CardTitle>(`${results.name}`)</CardTitle>
+//                             <p>{results.homeworld} {results.species}</p>
+//                         </CardBody>
+//                     </Card>
+//             ))
+//             }
+//       </div >
+//     );
+//     };
+// const SWChars = props => {
+//     console.log("Props", props);
+//     // map over staeWarsChars return a dom elment for each name in list
+//     return <div >{props.starwarsChars.map(p => <div>{p.name}:   {p.species}</div>)}</div>;
+//   };
+
+// export default SWChars; 
