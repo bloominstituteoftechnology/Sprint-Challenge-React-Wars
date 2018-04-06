e9eec7dd99196f904a3cc67b0e0c73b9570461d8
K Haag <kh@Ks-MacBook-Pro.local>
2018-04-06 11:47:19 -0700

    React Sprint--Styled

--- starwars/src/Component/Container.js
+++ starwars/src/Component/Container.js
@@ -3,59 +3,47 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } fr
 
 const Container = props => {
     return(
-        <div>
-            {props.myContainer.map((myData, index) => 
-            <Card key={"contents" + index}>
-                <CardImg top width="100%" src={myData.url} alt={myData.name}/>
-
-                
-            <CardBody>
-                <CardTitle>
-                {myData.name}
-                </CardTitle>
-                <CardSubtitle>
-                    Information regarding {myData.name}:
-                </CardSubtitle>
-                <CardText>
-                    <div>
-                        Eye Color: {myData.eye_color}
-                    </div>
-                    <div>
-                        Birth Year: {myData.birth_year}
-                    </div>
-                    <div>
-                        Gender: {myData.gender}
-                    </div>
-                    <div>
-                        Hair Color: {myData.hair_color}
-                    </div>
-                    <div>
-                        Height: {myData.height}
-                    </div>
-                    <div>
-                        Mass: {myData.mass}
-                    </div>
-                    <div>
-                        Skin Color: {myData.skin_color}
-                    </div>
-                    {/* <div>
-                    for (let i = 0; i < {myData.films.length}; i++){
-                        if (myData.films[i] !== )
-                    }
-                    </div> */}
-                    <div>
-                        Films: {myData.films[0]}
-                    </div>
-
-                </CardText>
-
-            </CardBody>
+        <div className="container d-flex flex-wrap justify-content-around">
+        {props.myContainer.map((myData, index) => 
+            <Card className="Card col-5 d-flex border border-danger bg-light mb-5 rounded" key={"contents" + index}>
+                {/* <CardImg top width="100%" src={myData.url} alt={myData.name}/> */}
+                <CardBody className="Card__body text-left">
+                    <CardTitle className="Card__Title text-center font-weight-bold p-1 rounded-circle bg-dark text-white">
+                        {myData.name}
+                    </CardTitle>
+                    <CardSubtitle className="Card__Subtitle text-center pb-1">
+                        Key Information:
+                    </CardSubtitle>
+                    <CardText>
+                        <div>
+                            Eye Color: {myData.eye_color}
+                        </div>
+                        <div>
+                            Birth Year: {myData.birth_year}
+                        </div>
+                        <div>
+                            Gender: {myData.gender}
+                        </div>
+                        <div>
+                            Hair Color: {myData.hair_color}
+                        </div>
+                        <div>
+                            Height: {myData.height}
+                        </div>
+                        <div>
+                            Mass: {myData.mass}
+                        </div>
+                        <div>
+                            Skin Color: {myData.skin_color}
+                        </div>
+                        <div>
+                            Films: {myData.films[0]}
+                        </div>
+                    </CardText>
+                </CardBody>
             </Card>)}
-
         </div>
     )
 }
     
-
-
 export default Container;
