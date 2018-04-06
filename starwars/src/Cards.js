52703b0bc224cf13eb89d3c2a9b7892be0f2321b
tanking <tanking@github.com>
2018-04-06 12:47:16 -0400

    Added other data to display

--- starwars/src/Cards.js
+++ starwars/src/Cards.js
@@ -1,10 +1,20 @@
-// Use Birthyear, created, edited, eye_color, gender, hair_color, height, mass, name, skin_color
+// Use  name, birth_year, gender, height, mass, eye_color, hair_color, skin_color, created, edited, 
 
 import React from 'react';
+import './Cards.css';
 
 const Cards = props => {
     return (
-        <div> {props.name} </div>
+        <div className = 'container'> 
+            <p> {props.name} </p>
+            <p> {props.birth_year} </p>
+            <p> {props.gender} </p>
+            <p> Height: {props.height} </p>
+            <p> Weight: {props.mass} </p>
+            <p> Eye Color: {props.eye_color} </p>
+            <p> Hair Color: {props.hair_color} </p>
+            <p> Skin Color: {props.skin_color} </p>
+        </div>
     )
 }
 
