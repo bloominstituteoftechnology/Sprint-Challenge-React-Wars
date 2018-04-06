8d5e219b4c40cd5a0616f6240948998b1ffc8d28
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-06 12:06:08 -0400

    Robert Hogan. Additional work to Species.js file. Corrected issues with import statements.

--- starwars/src/components/CharacterCard/CharacterCard.js
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Species from '../Species/Species';
+import { Button } from 'reactstrap';
 
 
 class CharacterCard extends React.component {
@@ -19,6 +21,18 @@ class CharacterCard extends React.component {
         return (
             <div classname="CardContainer">
                 <h2>{this.state.details.name}</h2>
+                <p>Birth Year: {this.state.details.birth_year}</p>
+                <p>Height: {this.state.details.height}</p>
+                <p>Mass: {this.state.details.mass}</p>
+                <p>Skin Color: {this.state.details.skin_color}</p>
+                <p>Hair Color: {this.state.details.hair_color}</p>
+                <p>Eye Color: {this.state.details.eye_color}</p>
+                    <div className="Links">
+                        <Button className="Links--link1" href={this.state.details.homeworld}>Home World</Button>
+                        <Button className="Links--link2" href={this.state.details.species}>Species</Button>
+                        <Button className="Links--link3" href={this.state.details.starships}>Star Ships</Button>
+                        <Button className="Links--link4" href={this.state.details.vehicles}>Vehicles</Button>
+                    </div>
             </div>
         );
     }
---
+++ starwars/src/components/Species/Species.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Species.css'
+
+class Species extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            species: {}
+        };
+    }
+
+    componentDidMount() {
+        fetch(this.props.data)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ species: data.results });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
