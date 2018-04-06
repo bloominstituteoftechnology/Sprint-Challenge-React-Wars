347add4c733a2b407289d7f5be7553eaf41d15d1
unknown <bsa6.23.94@gmail.com>
2018-04-06 12:43:52 -0400

    mostly working on making it better

---
+++ starwars/src/Components/FilmStuff.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const Flo = (props) => {
+// return <div>{props.arrays.map(a => <div key={a.films + a.species + a.starships + a.vehicles} className="etcStuff">
+// <div>Appearances:{a.films} 
+// <br /> Species:     {a.species}
+// <br /> Starships:     {a.starships}
+// <br /> Vehicles:     {a.vehicles}
+// </div>
+//     </div>)}
+//     </div>
+// }
+// export default Flo;
---
+++ starwars/src/Components/NameBirth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Flo from './FilmStuff.js'
+
+const Nom = (props) => {
+    console.log("Props", props);
+    return <div className="Birthstuff"> {props.nbd.map(n => <div key={n.birth_year + n.name}> 
+    <div className="palText"> Name:    {n.name}    <br />  <br /> Year of Birth:        {n.birth_year}
+    <br /> <br />  Gender:     {n.gender}
+    <br /> <br />  Hair:       {n.hair_color}
+    <br /> <br />  Height:     {n.height}
+    <br /> <br />  Mass:       {n.mass}
+    <br /> <br /> Skin Tone:      {n.skin_color}
+    </div>
+    </div>
+    )}
+    </div>
+}
+
+export default Nom;
