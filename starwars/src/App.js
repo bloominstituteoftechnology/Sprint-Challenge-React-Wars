7ee6e9519d0af022e8bea69e2fa8c1a25bbc324e
Axhon <axhonruiz@gmail.com>
2018-04-06 12:08:22 -1000

    added linting and card

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Card from './components/Card'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       starwarsChars: []
-    };
+    }
   }
+
   componentDidMount() {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
     fetch('https://swapi.co/api/people')
       .then(res => {
-        return res.json();
+        return res.json()
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsChars: data.results })
       })
       .catch(err => {
-        throw new Error(err);
-      });
+        throw new Error(err)
+      })
   }
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {this.state.starwarsChars.map(character => (
+          <Card key={character.url} {...character} />
+        ))}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ starwars/src/components/Card/Card.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Card = props => <div className="Card--container">{props.name}</div>
+
+export { Card }
