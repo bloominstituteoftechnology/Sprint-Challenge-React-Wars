6154d4958341f9755d55ff65f57ff1901abe647e
Justin M <jekm321@gmail.com>
2018-04-06 19:32:58 -0400

    checkpoint for styling

--- starwars/src/components/CharSelector/CharSelector.js
+++ starwars/src/components/CharSelector/CharSelector.js
@@ -3,26 +3,40 @@ import Species from '../Species/Species';
 import Starships from '../Starships/Starships';
 import Vehicles from '../Vehicles/Vehicles';
 import Films from '../Films/Films';
+import './CharSelector.css';
+import { Container, Row, Col } from 'reactstrap';
 
 const CharSelector = props => {
     return (
-        <div>
+        <div className="container">
             {props.swChars.map((char, index) => (
-                <div key={char.created + index}>
-                    <h2>{char.name}</h2>
-                    <h4>{`Born: ${char.birth_year}`}</h4>
-                    <h5>{`Gender: ${char.gender}`}</h5>
-                    <h5>{`height: ${char.height}, mass: ${char.mass} `}</h5>
-                    <h5>Skin Color: {char.skin_color}</h5>
-                    <h5>Eye Color: {char.eye_color}</h5>
-                    <Species species={char.species}/>
-                    <Starships starships={char.starships}/>
-                    <Vehicles vehicles={char.vehicles}/>
-                    <Films films={char.films} />
-                    <a href={char.url}>Website</a>
-                    <p>HomePlanet: <img src={char.homeworld} alt='home planet'/></p>
-
-                    {/* <Char /> */}
+                <div className="container__card" key={char.created + index}>
+                    <Container>
+                        <Row>
+                            <Col className="text-primary"><h2>{char.name}</h2></Col>
+                        </Row>
+                        <Row className="text-info">
+                            <Col><h4>{`Born: ${char.birth_year}`}</h4></Col>
+                            <Col><h5>{`Gender: ${char.gender}`}</h5></Col>
+                            <Col><h5>{`height: ${char.height}, mass: ${char.mass} `}</h5></Col>
+                        </Row>
+                        <Row className="text-info">
+                            <Col><h5>Skin Color: {char.skin_color}</h5></Col>
+                            <Col><h5>Eye Color: {char.eye_color}</h5></Col>
+                        </Row>
+                        <Row>
+                            <Col><Species species={char.species} /></Col>
+                            <Col><Starships starships={char.starships} /></Col>
+                            <Col><Vehicles vehicles={char.vehicles} /></Col>
+                        </Row>
+                        <Row>
+                            <Col><Films films={char.films} /></Col>
+                        </Row>
+                        <Row>
+                            <Col><a href={char.url}>Website</a></Col>
+                            <Col><p>HomePlanet: <img src={char.homeworld} alt='home planet' /></p></Col>
+                        </Row>
+                    </Container>
                 </div>
             ))}
         </div>
---
+++ starwars/src/components/Films/CarTrial.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [];
+
+class CarTrial extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+    this.init();
+  }
+
+  init = () => {
+      this.props.items.films.map((film, index) => (
+          items.push({id: index, src: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTA2NzY5OTk1NTNeQTJeQWpwZ15BbWU3MDQ3MjAzNDk@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg', caption: `slide ${index}`})
+      ))
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          className="custom-tag"
+          tag="div"
+          key={item.id}
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+        >
+          <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <div>
+        <style>
+          {
+            `.custom-tag {
+                max-width: 100%;
+                height: 500px;
+                background: black;
+              }`
+          }
+        </style>
+        <Carousel
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+        >
+          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default CarTrial;
