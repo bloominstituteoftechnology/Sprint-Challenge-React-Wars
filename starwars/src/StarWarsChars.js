418af7a0fc1a4c9b5eb82e7bbd99c890f025d78b
David Alexander Morales <damorales89@gmail.com>
2018-04-06 17:22:58 -0400

    struggle bus styles... sigh

--- starwars/src/StarWarsChars.js
+++ starwars/src/StarWarsChars.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card, CardTitle, CardBody, CardText } from 'reactstrap';
+import { Card, CardTitle, CardBody, CardText, CardImg } from 'reactstrap';
 
 const StarWarsChars = props => {
     return (
         <Card key={props.char.name}>
             <CardBody>
                 <CardTitle className="char-name">{props.char.name}</CardTitle>
+                <CardImg src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg/revision/latest/scale-to-width-down/500?cb=20170927034529" alt="Luke"></CardImg>
                 <CardText>Birthdate: {props.char.birth_year}</CardText>
                 <CardText>Height: {props.char.height}</CardText>
                 <CardText>Mass: {props.char.mass}</CardText>
