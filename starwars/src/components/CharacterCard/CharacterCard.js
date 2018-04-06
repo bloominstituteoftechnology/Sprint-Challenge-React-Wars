39364bc3027fc5fe14808bbd475d128f0851ee34
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-06 14:24:36 -0400

    Robert Hogan. Last minute touches to styling. Project complete.

--- starwars/src/components/CharacterCard/CharacterCard.js
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -25,11 +25,13 @@ class CharacterCard extends React.Component {
                 <p className="birthday">Birth Year: {this.state.details.birth_year}</p>
                 <p className="height">Height: {this.state.details.height}</p>
                 <p className="mass">Mass: {this.state.details.mass}</p>
+                <p className="gender">Gender: {this.state.details.gender}</p>
                 <p className="skin">Skin Color: {this.state.details.skin_color}</p>
                 <p className="hair">Hair Color: {this.state.details.hair_color}</p>
                 <p className="eyes">Eye Color: {this.state.details.eye_color}</p>
                     <div className="Links">
                         <Button className="Links--link" href={this.state.details.homeworld}>Home World</Button>
+                        <Button className="Links--link" href={this.state.details.films}>Films</Button>
                         <Button className="Links--link" href={this.state.details.species}>Species</Button>
                         <Button className="Links--link" href={this.state.details.starships}>Star Ships</Button>
                         <Button className="Links--link" href={this.state.details.vehicles}>Vehicles</Button>
