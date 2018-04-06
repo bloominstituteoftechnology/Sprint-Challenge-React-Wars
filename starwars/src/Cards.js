89ec72962d952318a2a13185af1362f246c9b9ba
tanking <tanking@github.com>
2018-04-06 12:33:19 -0400

    Created Cards.js and mapped data array to display div of character names

---
+++ starwars/src/Cards.js
@@ -0,0 +1,11 @@
+// Use Birthyear, created, edited, eye_color, gender, hair_color, height, mass, name, skin_color
+
+import React from 'react';
+
+const Cards = props => {
+    return (
+        <div> {props.name} </div>
+    )
+}
+
+export default Cards;
