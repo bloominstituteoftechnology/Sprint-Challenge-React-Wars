e5a234083493bed0f8778ef784afbfcabba8db11
cgoez <goez2001@gmail.com>
2018-04-06 10:30:55 -0700

    structure changes, card styling

--- starwars/src/components/CharDetails/CharDetails.js
+++ starwars/src/components/CharDetails/CharDetails.js
@@ -1,20 +1,39 @@
 import React from "react";
+import "./CharDetails.css";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 const CharDetails = props => {
   console.log(props.chars);
 
   return (
-    <div>
+    <div className="charDetails">
       {props.chars.map(char => (
         <div>
-          <div>Name: {char.name}</div>
-          <div>Birth Year: {char.birth_year}</div>
-          <div>Gender: {char.gender}</div>
-          <div>Height: {char.height}</div>
-          <div>Mass: {char.mass}</div>
-          <div>Hair Color: {char.hair_color}</div>
-          <div>Eye Color: {char.eye_color}</div>
-          <div>Skin Color: {char.skin_color}</div>
+          <Card className="charCard">
+            <CardBody>
+              <CardTitle>
+                <div>{char.name}</div>
+              </CardTitle>
+              <CardText>
+                <div>Birth Year: {char.birth_year}</div>
+                <div>Gender: {char.gender}</div>
+                <div>Height: {char.height}</div>
+                <div>Mass: {char.mass}</div>
+                <div>Hair Color: {char.hair_color}</div>
+                <div>Eye Color: {char.eye_color}</div>
+                <div>Skin Color: {char.skin_color}</div>
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
         </div>
       ))}
     </div>
