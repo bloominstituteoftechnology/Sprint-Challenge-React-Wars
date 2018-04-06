f1f5813f23472a1bdecfabe8203cd9efaf717a14
Abraham Bueno <abueno@localhost.localdomain>
2018-04-06 14:04:53 -0500

    Completed project, but may need to work on styling and stretch.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from "./components/Title.js"
+import CardContainer from "./components/CardContainer"
+import CardLogic from "./components/CardLogic"
+
 
 class App extends Component {
   constructor() {
@@ -8,7 +12,7 @@ class App extends Component {
       starwarsChars: []
     };
   }
-  componentDidMount() {
+  componentDidMount(e) {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -22,14 +26,21 @@ class App extends Component {
       .catch(err => {
         throw new Error(err);
       });
+      // e.preventDefault();
+
   }
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+     
+        {/* <h1 className="Header">React Wars</h1> */}
+        <Title />
+        <CardLogic data={this.state.starwarsChars} />
+        <CardContainer />
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ starwars/src/components/CardContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, CardTitle, CardSubtitle, CardBody } from 'reactstrap';
+class CardContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <Card>
+                Card Component
+                </Card>
+            </div>
+        );
+    }
+};
+export default CardContainer;
