14b4480a282d0869b6214126194b981bb7fda0c2
Axhon <axhonruiz@gmail.com>
2018-04-06 12:24:14 -1000

    began styling card

--- starwars/src/components/Card/Card.jsx
+++ starwars/src/components/Card/Card.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import './Card.css'
+
+// birth_year: "19BBY"
+// created: "2014-12-09T13:50:51.644000Z"
+// edited: "2014-12-20T21:17:56.891000Z"
+// eye_color: "blue"
+// films: Array[5]
+// gender: "male"
+// hair_color: "blond"
+// height: "172"
+// homeworld: "https://swapi.co/api/planets/1/"
+// mass: "77"
+// name: "Luke Skywalker"
+// skin_color: "fair"
+// species: Array[1]
+// starships: Array[2]
+// url: "https://swapi.co/api/people/1/"
+// vehicles: Array[2]
 
 const Card = props => <div className="Card--container">{props.name}</div>
 
