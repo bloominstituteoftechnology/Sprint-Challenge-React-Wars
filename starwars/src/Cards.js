98abf2c4b75600bbdab67fe11cad22caa87cbad9
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-06 13:04:56 -0400

    Added styling to cards

--- starwars/src/Cards.js
+++ starwars/src/Cards.js
@@ -6,14 +6,16 @@ import './Cards.css';
 const Cards = props => {
     return (
         <div className = 'container'> 
-            <p> {props.name} </p>
-            <p> {props.birth_year} </p>
-            <p> {props.gender} </p>
-            <p> Height: {props.height} </p>
-            <p> Weight: {props.mass} </p>
-            <p> Eye Color: {props.eye_color} </p>
-            <p> Hair Color: {props.hair_color} </p>
-            <p> Skin Color: {props.skin_color} </p>
+            <div className = 'data'>
+                <p> Name: {props.name} </p>
+                <p> Birth Year:{props.birth_year} </p>
+                <p> Gender: {props.gender} </p>
+                <p> Height: {props.height} </p>
+                <p> Weight: {props.mass} </p>
+                <p> Eye Color: {props.eye_color} </p>
+                <p> Hair Color: {props.hair_color} </p>
+                <p> Skin Color: {props.skin_color} </p>
+            </div>
         </div>
     )
 }
