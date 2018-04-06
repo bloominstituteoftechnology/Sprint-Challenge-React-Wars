67b32a3626992d8709586125dc43e47603e032eb
Paul Fang <paulfang@pauls-mbp.home>
2018-04-06 11:02:40 -0700

    App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,35 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
-  componentDidMount() {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
+ import './App.css';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import CharacterList from './CharacterList.js';
+ 
+ class App extends Component {
+   state = {
+     starwarsChars: []
+   }
+ 
+   componentDidMount() {
     fetch('https://swapi.co/api/people')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
-    );
-  }
-}
+    .then(res => {
+      return res.json();
+    }
 
-export default App;
+    render() {
+      return (
+        <div className="App container">
++        <div><h1 className="Header">React Wars</h1></div>
++        <div> <CharacterList people={this.state.starwarsChars} /> </div>
+       </div>
+     );
+   }
+ }
+
+ export default App;
+
+    
