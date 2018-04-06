f066019ae35125b496fe50916c89c69ab740ac62
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-06 11:43:18 -0700

    finished project. added jsx styling directly in CharList and removed CharList.css file. Also added some flexbox to index.css

--- starwars/src/CharList.js
+++ starwars/src/CharList.js
@@ -5,13 +5,13 @@ const CharList = props => {
     return (
         <div className="character-wrapper">
         {props.starwarsChars.map((char, index) => (
-            <Card>
+                <Card style={{ width: '400px', marginBottom: "20px", boxShadow: "8px 8px 12px black", opacity: "0.8"}}>
                 <CardBody key={index + char.name}>
                     <CardTitle>{char.name}</CardTitle>
-                    <CardSubtitle style={{marginBottom: '0.5em'}}>{`Gender:----${char.gender.toUpperCase()}----`}</CardSubtitle>
-                    <CardSubtitle style={{ marginBottom: '0.5em' }}>{`Birth Year:----${char.birth_year}----`}</CardSubtitle>
-                    <CardSubtitle style={{ marginBottom: '0.5em' }}>{`Eye Color:----${char.eye_color.toUpperCase()}----`}</CardSubtitle>
-                    <CardSubtitle style={{ marginBottom: '0.5em' }}>{`Hair Color:----${char.hair_color.toUpperCase()}----`}</CardSubtitle>
+                    <CardSubtitle style={{marginBottom: '10px'}}>{`Gender:----${char.gender.toUpperCase()}----`}</CardSubtitle>
+                    <CardSubtitle style={{ marginBottom: '10px' }}>{`Birth Year:----${char.birth_year}----`}</CardSubtitle>
+                    <CardSubtitle style={{ marginBottom: '10px' }}>{`Eye Color:----${char.eye_color.toUpperCase()}----`}</CardSubtitle>
+                    <CardSubtitle style={{ marginBottom: '10px' }}>{`Hair Color:----${char.hair_color.toUpperCase()}----`}</CardSubtitle>
                     <CardSubtitle>{`Height:----${char.height}cm----`}</CardSubtitle>
                 </CardBody>
             </Card>
