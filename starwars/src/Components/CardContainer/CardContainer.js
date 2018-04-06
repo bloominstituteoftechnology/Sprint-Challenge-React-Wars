f2229d737eafe7b94b14daec09d32e71c209a6a3
2940cristian <2940cristian@gmail.com>
2018-04-06 09:03:45 -0700

    Passed data onto each card

--- starwars/src/Components/CardContainer/CardContainer.js
+++ starwars/src/Components/CardContainer/CardContainer.js
@@ -1,9 +1,23 @@
 import React from "react";
+const CardContainer = (props) => {
 
-const CardContainer = () => {
+    console.log(props.data);
     return(
-        <div className="post">
-
+        <div>
+        {props.data.map(post => {
+            return(
+                <div className="post">
+                    <div>{post.name}</div>
+                    <div>Height: {post.height}</div>
+                    <div>Mass: {post.mass}</div>
+                    <div>Birth year: {post.birth_year}</div>
+                    <div>Gender: {post.gender}</div>
+                    <div>Hair Color: {post.hair_color}</div>
+                    <div>Eye color: {post.eye_color}</div>
+                    <div>Skin Color: {post.skin_color}</div>
+                </div>
+            )
+        })}
         </div>
     )
 }
