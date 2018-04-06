bee4b5a22a75bb09f0c65914d1f68cf665f49d88
imsdhk <jappleleaf@gmail.com>
2018-04-06 12:22:24 -0400

    imported bootrap css in app.js & imported card from reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor() {
---
+++ starwars/src/Component/Characters.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
