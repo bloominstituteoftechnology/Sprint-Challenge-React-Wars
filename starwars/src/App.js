ed574826096ee706edd2de8718b79c050c8a81f6
Devon Bacon <devon.bacon@gmail.com>
2018-04-06 15:39:33 -0700

    Playing around with promises and fetch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,11 +9,21 @@ class App extends Component {
       starwarsChars: []
     };
   }
+  fetchHomeworldData(url, char) {
+    fetch(url)
+      .then(res => {
+        return res.json();
+      }).then(data => {
+        char.homeworldJSON = data;
+      }).catch(err => {
+        throw new Error(err);
+      });
+  }
   componentDidMount() {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch('https://swapi.co/api/people/')
       .then(res => {
         return res.json();
       }).then(data => {
@@ -26,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <Card chars={this.state.starwarsChars} fetchData={this.fetchData}/>
+        <Card state={this.state} fetchHomeworldData={this.fetchHomeworldData}/>
       </div>
     );
   }
--- starwars/src/Card.js
+++ starwars/src/Card.js
@@ -3,16 +3,34 @@ import React from 'react';
 const Card = props => {
   return (
     <div className="cards_container">
-      {props.chars.map((char, index) => (
-        <div className="card">
+      {props.state.starwarsChars.map((char, index) => (
+        <div className="card" key={`${char.name}${index}`}>
+          {props.fetchHomeworldData(char.homeworld, char)}
           <div className="card_personInfo">
-            <div className="personInfo_name"><strong>Name:</strong> {char.name}</div>
-            <div className="personInfo_gender"><strong>Gender:</strong> {char.gender}</div>
-            <div className="personInfo_height"><strong>Height:</strong> {char.height}</div>
-            <div className="personInfo_mass"><strong>Mass:</strong> {char.mass}</div>
-            <div className="personInfo_hairColor"><strong>Hair Color:</strong> {char.hair_color}</div>
-            <div className="personInfo_eyeColor"><strong>Eye Color:</strong> {char.eye_color}</div>
-            <div className="personInfo_homeworld"><strong>Homeworld:</strong> {}</div>
+            <div className="personInfo_name">
+              <strong>Name:</strong> {char.name}
+            </div>
+            <div className="personInfo_gender">
+              <strong>Gender:</strong> {char.gender}
+            </div>
+            <div className="personInfo_height">
+              <strong>Height:</strong> {char.height}
+            </div>
+            <div className="personInfo_mass">
+              <strong>Mass:</strong> {char.mass}
+            </div>
+            <div className="personInfo_hairColor">
+              <strong>Hair Color:</strong> {char.hair_color}
+            </div>
+            <div className="personInfo_eyeColor">
+              <strong>Eye Color:</strong> {char.eye_color}
+            </div>
+            <div className="personInfo_homeworld">
+              <strong>Homeworld:</strong> 
+              {console.log(this)} 
+              {console.log(char.homeworldJSON)}
+              {console.log(char)}
+            </div>
           </div>
           <div className="card_personPic"></div>
         </div>
