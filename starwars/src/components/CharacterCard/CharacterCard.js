d551ca070228b1b2085bb7a79bb52f1993c0dc4b
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-06 14:02:52 -0400

    Robert hogan. Project Completed. All requirements met.

--- starwars/src/components/CharacterCard/CharacterCard.js
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Species from '../Species/Species';
 import { Button } from 'reactstrap';
+import './CharacterCard.css'
 
 
 class CharacterCard extends React.Component {
@@ -20,18 +21,18 @@ class CharacterCard extends React.Component {
     render() {
         return (
             <div className="CardContainer">
-                <h2>{this.state.details.name}</h2>
-                <p>Birth Year: {this.state.details.birth_year}</p>
-                <p>Height: {this.state.details.height}</p>
-                <p>Mass: {this.state.details.mass}</p>
-                <p>Skin Color: {this.state.details.skin_color}</p>
-                <p>Hair Color: {this.state.details.hair_color}</p>
-                <p>Eye Color: {this.state.details.eye_color}</p>
+                <h2 className="name">{this.state.details.name}</h2>
+                <p className="birthday">Birth Year: {this.state.details.birth_year}</p>
+                <p className="height">Height: {this.state.details.height}</p>
+                <p className="mass">Mass: {this.state.details.mass}</p>
+                <p className="skin">Skin Color: {this.state.details.skin_color}</p>
+                <p className="hair">Hair Color: {this.state.details.hair_color}</p>
+                <p className="eyes">Eye Color: {this.state.details.eye_color}</p>
                     <div className="Links">
-                        <Button className="Links--link1" href={this.state.details.homeworld}>Home World</Button>
-                        <Button className="Links--link2" href={this.state.details.species}>Species</Button>
-                        <Button className="Links--link3" href={this.state.details.starships}>Star Ships</Button>
-                        <Button className="Links--link4" href={this.state.details.vehicles}>Vehicles</Button>
+                        <Button className="Links--link" href={this.state.details.homeworld}>Home World</Button>
+                        <Button className="Links--link" href={this.state.details.species}>Species</Button>
+                        <Button className="Links--link" href={this.state.details.starships}>Star Ships</Button>
+                        <Button className="Links--link" href={this.state.details.vehicles}>Vehicles</Button>
                     </div>
             </div>
         );
