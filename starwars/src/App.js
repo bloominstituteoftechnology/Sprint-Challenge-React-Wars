d68ac617d8f81c138c82748b1490d5435c017b7c
JacobLeonlyerla <umasour@yahoo.com>
2018-04-06 10:22:22 -0700

    added in the objects as a card and added styles, the styles need a lot more work

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainObject from './components/MainObject.js'
+import './components/MainObject.css'
 
 class App extends Component {
   constructor() {
@@ -25,9 +27,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
+     <MainObject {...this.state} />
     );
   }
 }
