7862b41855ebec799f8db469d639d29d16e9c61d
Devon Bacon <devon.bacon@gmail.com>
2018-04-06 09:46:39 -0700

    Created card componenet and added some style

---
+++ starwars/src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = props => {
+  return (
+    <div class="card">
+      <div></div>
+    </div>
+  )
+}
+
+export default Card;
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './Card.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
