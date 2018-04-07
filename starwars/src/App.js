c30ab25e5d4ba37fe5211e21b7d2603d39f250d5
Glenn-David Daniel <d2rd@outlook.com>
2018-04-07 13:08:07 -0700

    WIP: Added 'Link' import, added 'Character component.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import Character from './Character/Character';
 
 
 class App extends Component {
@@ -30,45 +31,50 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div>
+        <h2>TEXT SHOULD BE HERE</h2></div>
+        {/* <Character /> */}
         {/* PASTE LS-NOTES DIVS BELOW */}
-        
+        {/* {this.state.movies.map(movie => (
+          <CharacterCard key={movie.id} movie={movie} />
+        ))} */}
         {/* END OF LS-NOTES DIVS */}
       </div>
+    // return React.createElement('div', null, {className: App} 
     );
   }
 }
-
 // Create a character component
-  function CharacterCard({props}) {
-    const { name, species, gender, height, mass, skin_color, eye_color, hair_color, created, birth_year, homeworld, starships, vehicles, url } = props
-    return(
-      <div className="Note-row">
-        <div className="NoteItem">
-        <Link to={`/api/people/${props.name}`}>
-          <div className="Note-header"><h3>{props.name}</h3>
-          </div>
-        </Link>
-        <div className="Note-body">
-          <div className="Sub-Header">Physical Characteristics</div>
-            <p>Name: {props.name}</p>
-            <p>Species: {props.species}</p>
-            <p>Gender: {props.gender}</p>
-            <p>Height: {props.height}</p>
-            <p>Mass: {props.mass}</p>
-            <p>Skin_Color: {props.skin_color}</p>
-            <p>Eye_color: {props.eye_color}</p>
-            <p>Hair_Color: {props.hair_color}</p>
-          </div>
-          <div className="Sub-Header">Demographic Data</div>
-            <p>Created: {props.created}</p>
-            <p>Birth Year: {props.birth_Year}</p>
-            <p>Homeworld: {props.homeworld}</p>
-            <p>Starships: {props.starships}</p>
-            <p>Vehicles: {props.vehicles}</p>
-            <p>Url: {props.url}</p>
-          </div>
-      </div>
-    )
-  }
+  // function Character ({props}) {
+  //   const { name, species, gender, height, mass, skin_color, eye_color, hair_color, created, birth_year, homeworld, starships, vehicles, url } = props
+  //   return(
+  //     <div className="Note-row">
+  //       <div className="NoteItem">
+  //       <Link to={`/api/people/${props.name}`}>
+  //         <div className="Note-header"><h3>{props.name}</h3>
+  //         </div>
+  //       </Link>
+  //       <div className="Note-body">
+  //         <div className="Sub-Header">Physical Characteristics</div>
+  //           <p>Name: {props.name}</p>
+  //           <p>Species: {props.species}</p>
+  //           <p>Gender: {props.gender}</p>
+  //           <p>Height: {props.height}</p>
+  //           <p>Mass: {props.mass}</p>
+  //           <p>Skin_Color: {props.skin_color}</p>
+  //           <p>Eye_color: {props.eye_color}</p>
+  //           <p>Hair_Color: {props.hair_color}</p>
+  //         </div>
+  //         <div className="Sub-Header">Demographic Data</div>
+  //           <p>Created: {props.created}</p>
+  //           <p>Birth Year: {props.birth_Year}</p>
+  //           <p>Homeworld: {props.homeworld}</p>
+  //           <p>Starships: {props.starships}</p>
+  //           <p>Vehicles: {props.vehicles}</p>
+  //           <p>Url: {props.url}</p>
+  //         </div>
+  //     </div>
+  //   )
+  // }
 // ReactDOM.render(app, document.querySelector('#app'));
 export default App;
