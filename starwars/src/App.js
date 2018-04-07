7db8c986f3cf3b517fb3825675003fc3ba531b18
Glenn-David Daniel <d2rd@outlook.com>
2018-04-07 11:17:13 -0700

    WIP Refactor.  Last week's solution was totally wrong.  Never would have worked.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,62 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Create a character component
-  function Character(props) {
+  function CharacterCard({props}) {
+    const { name, species, gender, height, mass, skin_color, eye_color, hair_color, created, birth_year, homeworld, starships, vehicles, url } = props
     return(
-      <div>
-              <ul>
-                <li className="Note-row">
-                  <div className="NoteItem">
-                    <div className="Note-header">
-                        {/* <h3>{this.props.name}</h3> */}
-                        <h3>{props.name}</h3>
-                    </div>
-                    <div className="Note-body">
-                      <u>Physical Characteristics</u>
-                      <p>Name: {props.name}</p>
-                      <p>Species: {props.species}</p>
-                      <p>Gender: {props.gender}</p>
-                      <p>Height: {props.height}</p>
-                      <p>Mass: {props.mass}</p>
-                      <p>Skin_Color: {props.skin_color}</p>
-                      <p>Eye_color: {props.eye_color}</p>
-                      <p>Hair_Color: {props.hair_color}</p>
-                    </div>
-                  </div>
-                  <div className="NoteItem">
-                    <div className="Note-header">
-                        <h3>Character 1 Name {this.props.title}</h3>
-                    </div>
-                    <div className="Note-body">
-                      <u>Physical Characteristics</u>
-                      <p>Name: {props.name}</p>
-                      <p>Species: {props.species}</p>
-                      <p>Gender: {props.gender}</p>
-                      <p>Height: {props.height}</p>
-                      <p>Mass: {props.mass}</p>
-                      <p>Skin_Color: {props.skin_color}</p>
-                      <p>Eye_color: {props.eye_color}</p>
-                      <p>Hair_Color: {props.hair_color}</p>
-                    </div>
-                  </div>
-                  <div className="NoteItem">
-                    <div className="Note-header">
-                        <h3>Character 2 Name {this.props.title}</h3>
-                    </div>
-                    <div className="Note-body">
-                      <u>Physical Characteristics</u>
-                      <p>Name: {props.name}</p>
-                      <p>Species: {props.species}</p>
-                      <p>Gender: {props.gender}</p>
-                      <p>Height: {props.height}</p>
-                      <p>Mass: {props.mass}</p>
-                      <p>Skin_Color: {props.skin_color}</p>
-                      <p>Eye_color: {props.eye_color}</p>
-                      <p>Hair_Color: {props.hair_color}</p>
-                    </div>
-                  </div>
-                </li>
-              </ul>
+      <div className="Note-row">
+        <div className="NoteItem">
+        <Link to={`/api/people/${props.name}`}>
+          <div className="Note-header"><h3>{props.name}</h3>
+          </div>
+        </Link>
+        <div className="Note-body">
+          <div className="Sub-Header">Physical Characteristics</div>
+            <p>Name: {props.name}</p>
+            <p>Species: {props.species}</p>
+            <p>Gender: {props.gender}</p>
+            <p>Height: {props.height}</p>
+            <p>Mass: {props.mass}</p>
+            <p>Skin_Color: {props.skin_color}</p>
+            <p>Eye_color: {props.eye_color}</p>
+            <p>Hair_Color: {props.hair_color}</p>
+          </div>
+          <div className="Sub-Header">Demographic Data</div>
+            <p>Created: {props.created}</p>
+            <p>Birth Year: {props.birth_Year}</p>
+            <p>Homeworld: {props.homeworld}</p>
+            <p>Starships: {props.starships}</p>
+            <p>Vehicles: {props.vehicles}</p>
+            <p>Url: {props.url}</p>
+          </div>
       </div>
     )
   }
@@ -89,21 +61,7 @@ class App extends Component {
       <div className="App">
         <h1 className="Header">React Wars</h1>
         {/* PASTE LS-NOTES DIVS BELOW */}
-        <div className="Notes-panel">
-            <div className="View-header">
-                <h1>Star Wars Characters{this.props.pageHeader}</h1>
-            </div>
-            <div>
-            var app = (
-              <div>
-                <Character name='Tchala'species='Wakandan' gender='Male' height='181' mass='89' skin_color='Brown' eye_color='Brown' hair_color='Black' />
-
-                <Character name='Suri' species='Wakandan' gender='Female' height='165' mass='50' skin_color='Brown' eye_color='Hazel' hair_color='Auburn' />
-              </div>
-)
-
-            </div>
-          </div>  
+        
         {/* END OF LS-NOTES DIVS */}
       </div>
     );
