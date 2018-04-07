384a8518f7755753be53b39a0c8444984b593097
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-07 15:50:19 -0700

    minor changes to include new css and a second list imported from linkdisplay

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import LinkDisplay from './linkdisplay';
 
 const List = props => {
   console.log("props", props.data)
-  return( <div>{props.data.map((u, index) =>
+  return( <div>{props.data.map((u, index) => 
   <div className='card__container'><h1 key={u + index} className='card__container-names'>{u.name}</h1>
-  
-  <ul>  
-    <li>Born {u.birth_year}</li>
-    <li>Eyes {u.eye_color}</li>
-    <li>Gender {u.gender}</li>
-    <li>Skin Tone {u.skin_color}</li>
-    <li>HomeWorld {u.homeworld}</li>
+  <div className='list-box'>
+  <ul>
+    <li>mass</li>
+    <li>{u.mass}</li>
+    <li>height</li>
+    <li>{u.height}</li>
+    <li>Born</li>
+    <li>{u.birth_year}</li>
+    <li>Eyes</li>
+    <li>{u.eye_color}</li>
+    <li>Gender</li>
+    <li>{u.gender}</li>
+    <li>Skin Tone</li>
+    <li>{u.skin_color}</li>
   </ul>
   </div>
-  )}</div> )
+  <div className='list-box'>
+  <ul>  
+    <LinkDisplay />
+  </ul>
+  </div>
+  </div>
+)}</div> )
 }
   
 
@@ -49,6 +63,7 @@ class App extends Component {
         <h1 className="Header">React Wars</h1>
         
         <List data={this.state.starwarsChars} />
+        
         
       </div>
     );
