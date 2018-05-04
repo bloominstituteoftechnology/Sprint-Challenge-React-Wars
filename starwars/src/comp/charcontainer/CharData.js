6bc11c5d22870960d624d699eec82b9ad0f56068
Brandon Benefield <bsquared18@gmail.com>
2018-05-04 14:00:44 -0400

    refactor: moved presentation from `CharContainer` to new comp `CharData` `CharData` is now imported into `CharContainer`

---
+++ starwars/src/comp/charcontainer/CharData.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+const CharData = props => {
+  return(
+    <React.Fragment>
+
+      {
+        props.charData.map((char, ind) => {
+          return (
+            <div key={ ind + Date.now() } className="char-card-container__char-card">
+
+              <h2>{char.name}</h2>
+              <p><span>Gender:</span> {char.gender}</p>
+              <p><span>Birth Year:</span> {char.birth_year}</p>
+              <p><span>Eye Color:</span> {char.eye_color}</p>
+              <p><span>Hair Color:</span> {char.hair_color}</p>
+              <p><span>Height:</span> {char.height}</p>
+              <p><span>Weight:</span> {char.mass}</p>
+              <p><span>Skin Color:</span> {char.skin_color}</p>
+
+              <div>
+                <div>
+                  <p>Films</p>
+                </div>
+                <div>
+                  {
+                    char.films.map(film => {
+                      return (
+                        <p>{film}</p>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <p>Specie</p>
+                </div>
+                <div>
+                  {
+                    char.species.map(specie => {
+                      return (
+                        <p>{specie}</p>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <p>StarShips</p>
+                </div>
+                <div>
+                  {
+                    char.starships.map(starship => {
+                      return (
+                        <p>{starship}</p>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <p>Vehicles</p>
+                </div>
+                <div>
+                  {
+                    char.vehicles.map(vehicle => {
+                      return (
+                        <p>{vehicle}</p>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            
+            </div>
+          );
+        })
+      }
+      
+    </React.Fragment>
+  );
+}
+
+export default CharData;
