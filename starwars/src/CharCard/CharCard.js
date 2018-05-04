c44f1e9c1f07571b2067078e8e55e451ab3dffdd
JSNavey <jsnavey@gmail.com>
2018-05-04 15:29:29 -0400

    add a bit of style

--- starwars/src/CharCard/CharCard.js
+++ starwars/src/CharCard/CharCard.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { Card, CardHeader, CardFooter, CardBody, CardTitle, CardText } from 'reactstrap';
+import './CharCardStyle.css';
 
 const CharCard = props => {
     return (
-       <div className = 'col-4 boxes'>
+       <div className = 'row boxes'>
            {props.starwarsChars.map(person => {
                return (
+                   <div className = 'col-12 col-sm-6 col-md-3 card'>
                    <Card>
-                       <CardHeader>{person.name}</CardHeader>
+                       <CardHeader><h2>{person.name}</h2></CardHeader>
                        <CardBody>
                            <CardText>Birth Year: {person.birth_year}</CardText>
                            <CardText>Gender: {person.gender}</CardText>
                            <CardText>Height: {person.height}</CardText>
                            <CardText>Mass: {person.mass}</CardText>
                            <CardText>Eye Color: {person.eye_color}</CardText>
-                           <CardText>Hair Color: {person.hair_color}</CardText>                           
+                           <CardText>Hair Color: {person.hair_color}</CardText>    
+                           <CardFooter>Created: {person.created}</CardFooter> 
+                           <CardFooter>Edited: {person.edited}</CardFooter>                       
                        </CardBody>
                    </Card>
+                   </div>
                )
            })}
         </div>
