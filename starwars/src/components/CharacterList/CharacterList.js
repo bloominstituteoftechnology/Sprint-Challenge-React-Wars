78f2691104c9264cabe8549c622c8719c5e0cafe
Xang Thao <xangthao24@yahoo.com>
2018-05-04 11:14:09 -0700

    Delete background img added styles and completed task but not stretch

--- starwars/src/components/CharacterList/CharacterList.js
+++ starwars/src/components/CharacterList/CharacterList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
+import "./CharacterList.css";
 
 class CharacterList extends React.Component {
   constructor(props) {
@@ -11,21 +12,57 @@ class CharacterList extends React.Component {
     console.log(this.props.list[0]);
     console.log(this.props);
     return (
-      <div className ="container">
+      <div className ="container char-list">
         <div className = "row">
          {this.props.list.map((char, index) => {
            return (
-            <div className="card col-12 col-sm-6 col-md-4 col-lg-3" key={index}>
-             <Card>
+            <div key={index} className="card col-12 col-sm-6 col-md-4 col-lg-3">
+             <Card >
               <CardTitle>{char.name}</CardTitle>
                 <CardBody>
-                   <CardTitle>Atributes</CardTitle>
-                   <CardText>Height: {char.height}</CardText>
-                   <CardText>Mass: {char.mass}</CardText>
-                   <CardText>Hair Color: {char.hair_color}</CardText>
-                   <CardText>Skin Color: {char.skin_color}</CardText>
-                   <CardText>Eye Color: {char.eye_color}</CardText>
-                   <CardText>Birth Year: {char.birth_year}</CardText>
+                   <CardTitle>Attributes</CardTitle>
+                   <CardText>
+                    <div className="attr height">
+                      <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/1220594-200.png"/>
+                      <span>Height:</span>
+                      <span>{char.height}</span>
+                    </div>
+                    </CardText>
+                   <CardText>
+                    <div className="attr mass">
+                      <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/20317-200.png" />
+                      <span>Mass:</span>
+                      <span>{char.mass}</span>
+                    </div>
+                   </CardText>
+                   <CardText>
+                    <div className="attr hair">
+                      <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/543109-200.png" />
+                      <span>Hair Color:</span>
+                      <span>{char.hair_color}</span>
+                    </div>
+                   </CardText>
+                   <CardText>
+                    <div className="attr skin">
+                      <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/1335654-200.png" />
+                      <span>Skin Color:</span>
+                      <span>{char.skin_color}</span>
+                    </div>
+                   </CardText>
+                   <CardText>
+                    <div className="attr eye">
+                      <img src="https://static.thenounproject.com/png/204713-200.png" />
+                      <span>Eye Color:</span>
+                      <span>{char.eye_color}</span>
+                    </div>
+                   </CardText>
+                   <CardText>
+                    <div className="attr birth">
+                      <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/479966-200.png" />
+                      <span>Birth Year:</span>
+                      <span>{char.birth_year}</span>
+                    </div>
+                   </CardText>
                 </CardBody>
               </Card>
             </div>
