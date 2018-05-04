5cdb1c49cd75a0f844eb4f302fabad2c3c820602
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-04 16:28:49 -0400

    added container props

--- starwars/src/Chars.js
+++ starwars/src/Chars.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Card, CardText, CardBody, CardTitle, Row, Col } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle, Row, Col, Container } from 'reactstrap';
 import './CharList.css';
 
 const Chars = ({ CharacterData }) => {
     return (
-        <div className="Character-Card">
-            <Row>
-                <Col lg="6">
-                    <Card>
-                        <Card body>
-                            <CardTitle>{CharacterData.name}</CardTitle>
-                            <CardText>Height: {CharacterData.height}</CardText>
-                            <CardText>Mass: {CharacterData.mass}</CardText>
-                            <CardText>Hair Color: {CharacterData.hair_color}</CardText>
-                            <CardText>Skin Color: {CharacterData.skin_color}</CardText>
+        <container>
+            <div className="Character-Card">
+                <Row >
+                    <Col lg="6">
+                        <Card>
+                            <Card body outline color="secondary">
+                                <CardTitle>{CharacterData.name}</CardTitle>
+                                <CardText>Height: {CharacterData.height}</CardText>
+                                <CardText>Mass: {CharacterData.mass}</CardText>
+                                <CardText>Hair Color: {CharacterData.hair_color}</CardText>
+                                <CardText>Skin Color: {CharacterData.skin_color}</CardText>
+                            </Card>
                         </Card>
-                 </Card>
-                </Col>
-            </Row>
-        </div>        
+                     </Col>
+                </Row>
+            </div> 
+        </container>
+               
     )
 }
 
