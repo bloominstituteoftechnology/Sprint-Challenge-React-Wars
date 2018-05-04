2465ca8aba644a605ab0b43cde5f3a13ee72c6d8
jaklundun <govi.escudero@gmail.com>
2018-05-04 09:40:02 -0700

    Installed dependencies to start server

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header.js'
+
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       starwarsChars: []
     };
@@ -12,7 +14,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch('https://swapi.co/api/people/')
       .then(res => {
         return res.json();
       })
@@ -26,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <Header starwarsChars ={this.state.starwarsChars} />
       </div>
     );
   }
---
+++ starwars/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+
+const Header = (props) => {
+
+    return (
+        <div className="header">
+            hello
+        </div>
+    )
+
+}
+
+export default Header; 
