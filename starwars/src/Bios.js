3e28a3e7f9dd7cac6f0a7bc0a1c10b99bf3ba11b
David <AquilaVirtual>
2018-05-04 11:06:48 -0700

    Project done without strech and incomplete Answers.md

--- starwars/src/Bios.js
+++ starwars/src/Bios.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Card, CardBody, CardTitle} from 'reactstrap';
+import './Bios.css';
 
 const Bio = ({actor}) => {
     return(
-        <Card>
-            <CardTitle>{actor.name}</CardTitle>
-            <CardBody><img src={actor.url} /></CardBody>
+        <Card className="Bios-Card">
+            <CardTitle>Name: {actor.name}</CardTitle>
+            <CardBody>Birth year: {actor.birth_year}</CardBody>
+            <CardBody>Eye color: {actor.eye_color}</CardBody>
+            <CardBody>Created: {actor.created}</CardBody>
+            <CardBody>Gender: {actor.gender}</CardBody>
+            <CardBody>Height: {actor.height}</CardBody>
+            <CardBody>Skin color: {actor.skin_color}</CardBody>
+            {/* <CardBody><img src={actor.homeworld} /></CardBody>
+            <CardBody><img src={actor.starships.Array} /></CardBody> */}
         </Card>
     )
 }
--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Characters.css';
 import Bio from './Bios';
 
 const Characters = props => {
     return (
-        <div>
+        <div className="Chars-wrapper">
    {props.starwarsChars.map(actor =>{
     return <Bio key={actor.id} actor={actor}/>;
    })}        
