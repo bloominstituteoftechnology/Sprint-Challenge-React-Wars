7f59e284cd3a972de642b7093ebb18fc9ff88055
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-04 09:03:59 -0700

    set up components and stylesheets

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharsContainer from './components/CharsContainer/CharsContainer';
 
 class App extends Component {
   constructor() {
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharsContainer chars={this.state.starwarsChars}/>
       </div>
     );
   }
---
+++ starwars/src/components/CharsContainer/CharsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Character from '../Character/Character';
+
+const CharsContainer = props => {
+    return <div></div>
+}
+
+export default CharsContainer;
