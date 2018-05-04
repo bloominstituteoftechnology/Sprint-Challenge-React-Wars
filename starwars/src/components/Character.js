09d228711e809bffe82e32a51ed4618779101b07
Chris Honda <christopher.honda92@gmail.com>
2018-05-04 10:50:56 -0700

    Finished Answers.md; added some formatting through reactstrap card styles

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
-const Character = (props) => {
+
+const Character = props => {
     return(
-    <div>
-        <h3>{props.name}</h3>
-        <p>Height: {props.height}</p>
-        <p>Eye Color: {props.eye_color}</p>
-        <p>Birth Year: {props.birth_year}</p>
-        <p>Hair Color: {props.hair_color}</p>
-        <p>Gender: {props.gender}</p>
-        <p>Skin Color: {props.skin_color}</p>
-        <p>Mass: {props.mass}</p>
-
-    </div>
+        <Card>
+            <CardBody>
+                <CardTitle>
+                    <CardImg className="mb-3"  />
+                    <h3>{props.name}</h3>
+                </CardTitle>
+                        <div>
+                            <p>Height: {props.height}</p>
+                            <p>Eye Color: {props.eye_color}</p>
+                            <p>Birth Year: {props.birth_year}</p>
+                            <p>Hair Color: {props.hair_color}</p>
+                            <p>Gender: {props.gender}</p>
+                            <p>Skin Color: {props.skin_color}</p>
+                            <p>Mass: {props.mass}</p>
+                        </div>
+            </CardBody>
+        </Card>
     )}
 
 export default Character;
