674771b42e9357635da78d0c6a095c3ff6c08e85
dlinz7 <linzdiegoa@gmail.com>
2018-05-04 12:58:34 -0500

    Sprint can display character info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import PeopleList from './PeopleList';
 
 class App extends Component {
   constructor() {
@@ -27,6 +28,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <PeopleList starwarsChars={this.state.starwarsChars}
+        />
       </div>
     );
   }
