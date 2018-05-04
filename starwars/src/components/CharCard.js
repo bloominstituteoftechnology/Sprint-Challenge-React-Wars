5fe0c5b484c847e5cafce4379e475995326e4b23
sagardesai90 <saagardesai@gmail.com>
2018-05-04 13:02:14 -0500

    not done yet but getting closer

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import {Card, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 
+const CharCard = props => {
+    return (
+        <div>
+            <Card>
+                <CardBody>
+                    <CardTitle>{props.char.name}</CardTitle>
+                    <CardSubtitle>{props.char.birth_year}</CardSubtitle>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
 export default CharCard;
