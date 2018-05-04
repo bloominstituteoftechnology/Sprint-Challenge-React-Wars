8ffeaf5a7bf731ea9b41ecd76886e0f36d19d6c8
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-05-04 12:15:43 -0400

    Enviroment Setup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch('https://swapi.co/api/people/')
       .then(res => {
         return res.json();
       })
@@ -26,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <Header />
       </div>
     );
   }
