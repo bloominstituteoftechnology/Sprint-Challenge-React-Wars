a3d347dc4c5e3983a60a6eb65e21f0760b86ad58
CMVNK <Kopecky12112@gmail.com>
2018-05-04 12:56:04 -0500

    Revert "initial commit so I can do PR"
    This reverts commit 0aae4ec1b8e21ff0daf657bb73027c28939b4032.

--- starwars/src/App.test.js
+++ starwars/src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import results from './starWarsPersons';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
