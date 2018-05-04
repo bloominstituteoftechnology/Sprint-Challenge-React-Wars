1fb4621ffc8b11e174017fcae3d8311f38e4c17f
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-04 13:56:37 -0500

    added cards compnent that in theory should work.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cards from './cards';
+
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
+    fetch('https://swapi.co/api/people/')
       .then(res => {
         return res.json();
       })
@@ -23,10 +25,15 @@ class App extends Component {
         throw new Error(err);
       });
   }
+  
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <div id="titles"><div id="titlecontent">
+          <h3>Star Wars Characters</h3>
+              {/* trying to connect cards */}
+          <Cards chars={this.state.starwarsChars} /> 
+        </div></div>
       </div>
     );
   }
---
+++ starwars/src/cards.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// tried creating a reactstrap card but my CDN is not working for some reason
+const Cards = (props) => {
+    return (
+        <div>
+            {props.chars.map(char => { // mapping over characters in array
+                return (
+                    <div>
+                        <Card className="character">
+                            <div className='title'>
+                            {/* getting name of character for card name */}
+                            <p className="name">{char.name}</p> 
+                            {/* getting birth year of character  */}
+                            <p className="born">{char.birth_year}</p>
+                            </div>
+                            {/* getting the rest of the character infomation for the card as they map over each person */}
+                            <div>
+                                <p className="info"> 
+                                {char.skin_color},
+                                {char.gender}, 
+                                {char.eye_color}, 
+                                {char.hair_color}, 
+                                {char.height},
+                                {char.mass}
+                                </p>
+                            </div>
+                        </Card>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+export default Cards;
