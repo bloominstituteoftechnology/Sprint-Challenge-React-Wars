980e87590ff473b04c453d2fb778d9c962f04d04
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-04 10:16:29 -0700

    add content and styles

--- starwars/src/components/Character/Character.js
+++ starwars/src/components/Character/Character.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 const Character = props => {
-    return <div></div>
+    return (
+    <div className="character">
+        <Card>
+            <img top width="100%" src={`${window.location.origin}/images/${props.char.name.split(' ').join('-')}.jpg`} alt={`portrait of ${props.char.name}`} className="char-img"/>
+            <CardBody className="char-info">
+                <CardTitle className="char-name">{props.char.name}</CardTitle>
+                <CardSubtitle className="stats">STATS:</CardSubtitle>
+                <CardText>birth year: {props.char.birth_year}</CardText>
+                <CardText>eye color: {props.char.eye_color}</CardText>
+                <CardText>gender: {props.char.gender}</CardText>
+            </CardBody>
+        </Card>
+    </div>
+    )
 }
 
 export default Character;
--- starwars/src/components/CharsContainer/CharsContainer.js
+++ starwars/src/components/CharsContainer/CharsContainer.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Character from '../Character/Character';
 
 const CharsContainer = props => {
-    return <div></div>
+    return  (
+    <div className="chars-container">
+        {props.chars.map((char, i) => {
+            return <Character key={i} char={char}/>;
+        })}
+    </div>
+    )
 }
 
 export default CharsContainer;
