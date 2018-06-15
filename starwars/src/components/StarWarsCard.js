091bd17c8affd6583300d9a1c57b61023c1bece8
DirupT <iamcooled@gmail.com>
2018-06-15 11:22:46 -0500

    updated CardText to a div/updated margins

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
-  
+import './StarWars.css';
+import { Card, CardTitle, CardText } from 'reactstrap';
+
 const StarWarsCard = props => {
-    return(
-    <div className='row'>
-        {props.starwarsChars.map(data => {
-            return <Card className='card-container col-4' body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-            <CardTitle>{data.name}</CardTitle>     
-            <CardText>
-                <p>Birth Year: {data.birth_year}</p>
-                <p>Created: {data.created}</p>
-                <p>Edited: {data.edited}</p>
-                <p>Eye Color: {data.eye_color}</p>
-                <p>Gender: {data.gender}</p>
-                <p>Hair Color: {data.hair_color}</p>
-                <p>Height: {data.height}</p>
-                <p>Mass: {data.mass}</p>
-                <p>Skin Color: {data.skin_color}</p>
-                <p></p>
-                </CardText>
-            <Button>Button</Button>
-          </Card>
-        })}
-    </div>);
+    return (
+        <div className='row'>
+            {props.starwarsChars.map(data => {
+                return <Card key={Math.random()} className='card-container' body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                    <CardTitle>{data.name}</CardTitle>
+                    <div>
+                        <p>Birth Year: {data.birth_year}</p>
+                        <p>Created: {data.created}</p>
+                        <p>Edited: {data.edited}</p>
+                        <p>Eye Color: {data.eye_color}</p>
+                        <p>Gender: {data.gender}</p>
+                        <p>Hair Color: {data.hair_color}</p>
+                        <p>Height: {data.height}</p>
+                        <p>Mass: {data.mass}</p>
+                        <p>Skin Color: {data.skin_color}</p>
+                    </div>
+                </Card>
+            })}
+        </div>
+    );
 }
 
-export default StarWarsCard
+export default StarWarsCard;
