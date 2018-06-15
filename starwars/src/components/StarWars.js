a6d25d3ddaca4739fcf1469f6542a8dbf8c73a0d
cmok4290 <cmok4290@gmail.com>
2018-06-15 12:10:09 -0400

    added new file, StarWarsCard.js

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import StarWarsCard from './StarWarsCard';
 
 const starWars = props => {
-
-} 
+    return (
+        <div className="starwars"> {
+            props.starwarsChars.map((item, index) => {
+                return (
+                    <StarWarsCard
+                        birthYear={index}
+                    />                  
+                );
+            })
+        }
+        </div>
+    );
+};
 
 export default starWars;
---
+++ starwars/src/components/StarWarsCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const starWarsCard = props => {
+    return (
+        <div>{props.birthYear}</div>
+    );
+};
+
+export default starWarsCard;
