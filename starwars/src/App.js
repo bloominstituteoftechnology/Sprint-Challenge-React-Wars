56009767485b7651d57adfd79decaeb0add6d6dc
Erin Meredith <ekmeredith@att.net>
2018-06-15 17:02:04 -0400

    edits made AFTER watching solution video

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './components/CardComponent';
-import Button from './components/ButtonComponent';
+import CharList from './components/CharsList';
 
 class App extends Component {
   constructor() {
@@ -31,34 +30,17 @@ class App extends Component {
       });
   };
 
-displayCard = name => {
-  let char = this.state.starwarsChars.slice();
-  char = char.filter((character) => {
-    return character.name === name;
-  })
-}
 
   render() {
-    console.log(this.state.starwarsChars);
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <div className='container'>
-        <Card char={this.state.starwarsChars[0]} />
-        <Card char={this.state.starwarsChars[1]} />
-        <Card char={this.state.starwarsChars[2]} />
-        <Card char={this.state.starwarsChars[3]} />
-        <Card char={this.state.starwarsChars[4]} />
-        <Card char={this.state.starwarsChars[5]} />
-        <Card char={this.state.starwarsChars[6]} />
-        <Card char={this.state.starwarsChars[7]} />
-        <Card char={this.state.starwarsChars[8]} />
-        <Card char={this.state.starwarsChars[9]} />
-      </div>
+       
+        <CharList chars={this.state.starwarsChars} />
+
       </div>
     );
-  }
-}
+  }}
 
 export default App;
 
---
+++ starwars/src/components/CharsList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css';
+import Char from './Char';
+
+
+const CharList = props => {
+    return (
+        <div className='charlist'>
+            {props.chars.map(char => 
+                <Char swChar={char} />
+            )}
+    
+        </div>
+    )
+}
+
+export default CharList;
