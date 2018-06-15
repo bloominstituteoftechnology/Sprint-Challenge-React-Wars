e802cacf8587775c32bddf7107b305922a3a8023
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-15 11:28:50 -0700

    Added previous button for characters, still buggy

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -32,10 +32,21 @@ class App extends Component {
       });
   };
 
-  updateChars = event => {
+  nextChars = event => {
     // We only have 9 pages of character info
+    event.preventDefault();
     let newPage = this.state.page;
     newPage + 1 > 9 ? newPage = 1 : newPage++;
+    this.setState({page: newPage});
+    console.log(this.state.page);
+    this.getCharacters(`https://swapi.co/api/people/?page=${this.state.page}`);
+  }
+
+  prevChars = event => {
+    // We only have 9 pages of character info
+    event.preventDefault();
+    let newPage = this.state.page;
+    newPage + 1 < 1 ? newPage = 9 : newPage--;
     this.setState({page: newPage});
     console.log(this.state.page);
     this.getCharacters(`https://swapi.co/api/people/?page=${this.state.page}`);
@@ -45,7 +56,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <button onClick={this.updateChars}>next</button>
+        <button onClick={this.prevChars}>previous</button> <button onClick={this.nextChars}>next</button>
         <CardList charsList={this.state.starwarsChars} />
       </div>
     );
