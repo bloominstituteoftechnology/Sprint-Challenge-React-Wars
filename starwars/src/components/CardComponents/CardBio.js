81c284eec789211d4eb4d26df984b2be12222578
Simon <Simon_Freelance@hotmail.com>
2018-06-15 16:17:22 -0400

    Bio information rough draft completed

--- starwars/src/components/CardComponents/CardBio.js
+++ starwars/src/components/CardComponents/CardBio.js
@@ -5,7 +5,12 @@ const CardBio = (props) => {
     return (
         <div class='bio__container'>
             <h2 className='bio__name'>{props.name}</h2>
+            <h3 className='bio__info'>Bio Information</h3>
             <ul className='bio__list'>
+                <li className = 'bio__item'>Gender: {props.gender}</li>
+                <li className = 'bio__item'>Eye Color: {props.eye_color}</li>
+                <li className = 'bio__item'>Height: {props.height}</li>
+                <li className = 'bio__item'>Hair Color: {props.hair_color}</li>
             </ul>
         </div>
     );
--- starwars/src/components/CardComponents/CardContainer.js
+++ starwars/src/components/CardComponents/CardContainer.js
@@ -5,9 +5,13 @@ import CardSummary from './CardSummary';
 
 const CardContainer = (props) => {
     return (
-        <div className = 'card-container'>
-        <CardBio name={props.character.name} />
-        <CardSummary />
+        <div className='card-container'>
+            <CardBio name={props.character.name} 
+            gender={props.character.gender} 
+            eye_color={props.character.eye_color}
+            height={props.character.height}
+            hair_color={props.character.hair_color} />
+            <CardSummary />
         </div>
     );
 }
