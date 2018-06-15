84cb1d42d8a592a178583da255566cc79be0333f
nickolaussmith <nickolaussmith721@gmail.com>
2018-06-15 16:16:43 -0600

    Finished?

---
+++ starwars/src/components/Char.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './StarWars.css';
+const Char = props => {
+    return <div className='star-wars-card'> 
+        <p>Name: {props.swChar.name} </p>
+        <p>Height: {props.swChar.height} </p>
+        <p>Eye Color: {props.swChar.eye_color} </p>
+        <p>Gender: {props.swChar.gender} </p>
+        </div>; 
+
+
+}
+
+export default Char;
---
+++ starwars/src/components/CharList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Char from './Char';
+import './StarWars.css';
+const CharsList = props => {
+    return <div className='chars-wrapper'> {props.chars.map(char => <Char  key={char.name}swChar={char} />)} </div>;
+    
+
+};
+export default CharsList;
