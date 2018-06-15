4771e16479500ba35606a3895e368ef79ae9af11
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-15 14:03:54 -0400

    Finished basic information

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -5,6 +5,13 @@ const Card = props => {
   return (
     <div className="card">
       <h2>{props.character.name}</h2>
+      <p>Born: <time>{props.character.birth_year}</time></p>
+      <p>Gender: {props.character.gender}</p>
+      <p>Skin Color: {props.character.skin_color}</p>
+      <p>Hair Color: {props.character.hair_color}</p>
+      <p>Eye Color: {props.character.eye_color}</p>
+      <p>Height: {props.character.height}</p>
+      <p>Mass: {props.character.mass}</p>
     </div>
   );
 };
