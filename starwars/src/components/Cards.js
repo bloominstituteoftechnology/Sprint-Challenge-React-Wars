ec893bd8eb6708b97d926ff8aca43d2ed4829498
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-15 17:37:28 -0500

    completed component

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -3,31 +3,20 @@ import './StarWars.css';
 
 const Cards = props => {
     return (
-        <div className="col card">
-            <p>{`Name: ${props.data.name}`}</p>
-            <p>{`Birth Year: ${props.data.birth_year}`}</p>
-            <p>{`Gender: ${props.data.gender}`}</p>
-            <p>{`Height: ${props.data.height}`}</p>
-            <p>{`Mass: ${props.data.mass}kg`}</p>
-            <p>{`Skin Color: ${props.data.skin_color}`}</p>
-            <p>{`Hair Color: ${props.data.hair_color}`}</p>
+        <div className="card">
+            <div className="name">
+                <p>{props.character.name}</p>
+            </div>
+            <div className="stats">
+                <p>{`Birth Year: ${props.character.birth_year}`}</p>
+                <p>{`Gender: ${props.character.gender}`}</p>
+                <p>{`Height: ${props.character.height}cm`}</p>
+                <p>{`Mass: ${props.character.mass}kg`}</p>
+                <p>{`Skin Color: ${props.character.skin_color}`}</p>
+                <p>{`Hair Color: ${props.character.hair_color}`}</p>
+            </div>
         </div>
     );
-}
-
-Cards.defaultProps = {
-    data: {
-        birth_year: 'year',
-        created: 'time',
-        edited: 'time',
-        eye_color: 'color',
-        gender: 'gender',
-        hair_color: 'color',
-        height: 'height',
-        mass: '0',
-        name: 'name',
-        skin_color: 'color',
-    }
 }
 
 export default Cards;
