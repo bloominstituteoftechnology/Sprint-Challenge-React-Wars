2b48bc1cddaadffd651048b1c8c5ff39e1e74c2f
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-15 14:46:14 -0400

    final  styling

--- starwars/src/components/CharacterDetails.js
+++ starwars/src/components/CharacterDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CharacterDetails = props => {
     return (
         <div className="custom-card">
-            <h1>{props.character.name}</h1>
+            <h2>{props.character.name}</h2>
             <p>Birth Year: {props.character.birth_year} Gender: {props.character.gender}</p>
             <p>Height: {props.character.height} Mass: {props.character.mass}</p>
             <p>Hair Color: {props.character.hair_color} Eye Color: {props.character.eye_color} </p>
