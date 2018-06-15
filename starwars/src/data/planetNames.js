fff353d56ba66f643f5cebe4076d2d4a796a31f7
Kevin Brack <brackkevin@gmail.com>
2018-06-15 14:52:37 -0500

    Planet names and film names moved to separate data sets

---
+++ starwars/src/data/planetNames.js
@@ -0,0 +1,22 @@
+planetNames = [
+  {
+    index: 1,
+    url: "https://swapi.co/api/planets/1/",
+    name: "Tatooine"
+  },
+  {
+    index: 2,
+    url: "https://swapi.co/api/planets/20/",
+    name: "Alderaan"
+  },
+  {
+    index: 8,
+    url: "https://swapi.co/api/planets/20/",
+    name: "Naboo"
+  },
+  {
+    index: 20,
+    url: "https://swapi.co/api/planets/20/",
+    name: "Stewjon"
+  }
+];
