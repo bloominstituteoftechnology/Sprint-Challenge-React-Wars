5dcc662224b685f89bd5128a61620b157618d45d
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-06-15 19:51:06 +0100

    add little styling

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import StarWars from './StarWars';
+import './StarWars.css'
 
 const StarWarsList = (props) => {
     return (
-        <div>
+        <div className ="app-Wrapper">
             {props.starwarsChars.map((starwars , index ) => {
                 return <StarWars starWars ={starwars} key ={index} />
             })}
