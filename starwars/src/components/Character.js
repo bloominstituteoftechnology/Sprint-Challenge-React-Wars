425e20b85144f510d5571934be0645252c9de0ef
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-15 10:43:15 -0700

    fixes title

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,12 +4,17 @@ const handleClick = character => {
   alert(character.name);
 };
 
+const handleMouseEnter = character => {
+  console.log(character.name);
+};
+
 const Character = props => (
   <div>
     <img
       src={`${window.location.origin}/img/${props.img}.jpg`}
       alt={props.character.name}
       onClick={() => handleClick(props.character)}
+      onMouseEnter={() => handleMouseEnter(props.character)}
       title={props.character.name}
     />
   </div>
