f0346352c2a201a12d1f65511589223df60ad1a4
Jameson <jamesonbrown@neolivity.com>
2018-06-15 09:55:15 -0600

    Created base structure for Card.js and CardList.js

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            Character Card
+        </div>
+    );
+};
+
+export default Card;
---
+++ starwars/src/components/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = () => {
+    return (
+        <div>
+            <Card />
+        </div>
+    );
+};
+
+export default CardList;
