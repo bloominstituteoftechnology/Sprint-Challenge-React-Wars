3973cab8b60a479738a7bd727d92818f5bd9e554
LBWright <lwright.js@gmail.com>
2018-06-15 12:08:12 -0500

    adjusted button styling

--- starwars/src/elements/Button.js
+++ starwars/src/elements/Button.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 const Button = styled.button`
 	color: white;
 	background: #222;
-	opacity: .9;
+	opacity: .95;
 	width: 150px;
 	height: 50px;
 	align-self: center;
 	font-size: 20px;
 	margin: 10px 0;
-	border: none;
+	border:none;
 	border-radius: 5px;
 `
 
