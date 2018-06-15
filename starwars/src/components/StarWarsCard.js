16d0053061480d31ffaab0916e27f4c3f9a25957
cmok4290 <cmok4290@gmail.com>
2018-06-15 12:59:23 -0400

    items displayed on StarWarsCard.js

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -6,7 +6,21 @@ const starWarsCard = props => {
     return (
         <div className="starwars-card">
             {props.birthYear}
-            {/* {props.created} */}
+            {props.created}
+            {props.edited}
+            {props.eyeColor}
+            {props.films}
+            {props.gender}
+            {props.hairColor}
+            {props.height}
+            {props.homeworld}
+            {props.mass}
+            {props.name}
+            {props.skinColor}
+            {props.species}
+            {props.starships}
+            {props.url}
+            {props.vehicles}
         </div>
     );
 };
