c7f8b9d6d97ea2bea0abe5347c565b9c9ff2f628
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-15 13:41:34 -0400

    Second commit
    Added CharacterList.js and Character.js
    Fetched Name, Gender, Height, etc.
    Created cards
    Styled cards
    Added Starwars logo
    Changed background to cover

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterList from './CharacterList';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsCharacters: []
     };
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
-
-  getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch(URL)
+    fetch('https://swapi.co/api/people')
       .then(res => {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsCharacters: data.results });
       })
       .catch(err => {
         throw new Error(err);
       });
-  };
+  }
 
   render() {
 
-    let characters = this.state.starwarsChars
-
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
-        {characters.map(character =>
-        <h4 key={character.name}>{character.name}</h4>
-        )}
+          <h2 className="Header">React Wars</h2>
+        <CharacterList results={this.state.starwarsCharacters} />
       </div>
     );
   }
---
+++ starwars/src/CharacterList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Character from './Character';
+
+const CharacterList = props => {
+    return (
+        <div className="CardRows">
+            {props.results.map(character => {
+            return <Character key={character.id} character={character} />;
+        })}
+        </div>
+    )
+}
+
+export default CharacterList;
