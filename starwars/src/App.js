5922b704ddfb4fb5a2a6bbb95a01021b1f35b5a9
Kevin Brack <brackkevin@gmail.com>
2018-06-15 12:48:53 -0500

    Added necessary planets data-set

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -14,6 +14,29 @@ class App extends Component {
     };
   }
 
+  planetNames = [
+    {
+      index: 1,
+      url: "https://swapi.co/api/planets/1/",
+      name: "Tatooine"
+    },
+    {
+      index: 2,
+      url: "https://swapi.co/api/planets/20/",
+      name: "Alderaan"
+    },
+    {
+      index: 8,
+      url: "https://swapi.co/api/planets/20/",
+      name: "Naboo"
+    },
+    {
+      index: 20,
+      url: "https://swapi.co/api/planets/20/",
+      name: "Stewjon"
+    }
+  ];
+
   componentDidMount() {
     this.getCharacters("https://swapi.co/api/people/");
   }
