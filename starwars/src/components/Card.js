af3e1c491286a3debcf9d1e7196a9344f0641fdc
Nicky Chen <nicky.chen0@gmail.com>
2018-06-15 09:49:56 -0700

    finished mvp

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -5,13 +5,12 @@ const Card = props => {
     return (
         <div className="Card">
             <h3>{props.character.name}</h3> 
-            <p>{props.character.birth_year}</p>
-            <p>{props.character.gender}</p> 
-            <p>{props.character.eye_color}</p>
-            <p>{props.character.height}</p> 
-            <p>{props.character.height}</p>
-            <p>{props.character.mass}</p>
-            <p>{props.character.skin_color}</p>
+            <p style={{ color: 'green' }}>Birth Year: {props.character.birth_year}</p>
+            <p style={{ color: 'blue' }}>Gender: {props.character.gender}</p> 
+            <p style={{ color: 'red' }}>Eye Color: {props.character.eye_color}</p>
+            <p style={{ color: 'purple' }}>Height: {props.character.height}</p> 
+            <p style={{ color: 'orange' }}>Mass: {props.character.mass}</p>
+            <p>Skin Color: {props.character.skin_color}</p>
         </div>
     )
 }
