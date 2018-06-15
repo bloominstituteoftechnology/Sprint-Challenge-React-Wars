6cb9af2e1868c961bbbdf537205f80542bad48dd
Kevin Brack <brackkevin@gmail.com>
2018-06-15 17:12:12 -0500

    Reverted to full character list for now

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -2,34 +2,37 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 const CardContainer = props => {
-  console.log(props.currentSorted);
-  let currentSorted = props.currentSorted;
+  // console.log(props.currentSorted);
+  // let currentSorted = props.currentSorted;
 
-  const sorting = () => {
-    if (currentSorted === "All") {
-      props.characters.map(character => {
-        return <CharacterCard character={character} key={character.url} />;
-      });
-    } else {
-      let sortedCharacterList = [];
-      for (let i = 0; i < props.characters.length; i++) {
-        for (let j = 0; j < props.characters[i].films.length; j++) {
-          if (props.characters[i].films[j] === currentSorted) {
-            sortedCharacterList.push(props.characters[i]);
-          }
-        }
-      }
-      sortedCharacterList.map(character => {
-        return <CharacterCard character={character} key={character.url} />;
-      });
-    }
-  };
+  // const sorting = () => {
+  //   if (currentSorted === "all") {
+  //     props.characters.map(character => {
+  //       return <CharacterCard character={character} key={character.url} />;
+  //     });
+  //   } else {
+  //     let sortedCharacterList = [];
+  //     for (let i = 0; i < props.characters.length; i++) {
+  //       for (let j = 0; j < props.characters[i].films.length; j++) {
+  //         if (props.characters[i].films[j] === currentSorted) {
+  //           sortedCharacterList.concat(props.characters[i]);
+  //         }
+  //       }
+  //     }
+  //     sortedCharacterList.map(character => {
+  //       return <CharacterCard character={character} key={character.url} />;
+  //     });
+  //   }
+  // };
 
   return (
     <div>
       CARD CONTAINER
       {/* MAP THROUGH ALL CHARACTER CARDS SORT BY FILM */}
-      {sorting()}
+      {/* {sorting()} */}
+      {props.characters.map(character => (
+        <CharacterCard character={character} key={character.url} />
+      ))}
     </div>
   );
 };
