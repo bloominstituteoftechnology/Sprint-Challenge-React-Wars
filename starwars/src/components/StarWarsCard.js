c0a45a30e35a1b84d71c2a688d6e80abe0aa47ae
DirupT <iamcooled@gmail.com>
2018-06-15 10:52:47 -0500

    updated cards with data

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -5,9 +5,20 @@ const StarWarsCard = props => {
     return(
     <div className='row'>
         {props.starwarsChars.map(data => {
-            return <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-            <CardTitle>{data.name}</CardTitle>
-            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+            return <Card className='card-container col-4' body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+            <CardTitle>{data.name}</CardTitle>     
+            <CardText>
+                <p>Birth Year: {data.birth_year}</p>
+                <p>Created: {data.created}</p>
+                <p>Edited: {data.edited}</p>
+                <p>Eye Color: {data.eye_color}</p>
+                <p>Gender: {data.gender}</p>
+                <p>Hair Color: {data.hair_color}</p>
+                <p>Height: {data.height}</p>
+                <p>Mass: {data.mass}</p>
+                <p>Skin Color: {data.skin_color}</p>
+                <p></p>
+                </CardText>
             <Button>Button</Button>
           </Card>
         })}
