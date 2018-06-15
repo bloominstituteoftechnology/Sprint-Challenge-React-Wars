ea331689c9d1ed4b227ba47c5905338d0e60288f
Allen Hai <allenhai03@gmail.com>
2018-06-15 10:54:57 -0500

    create dynamic Eye component and import it into CharacterCard component

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Eye from './Eye'
 import './StarWars.css'
 
 const CharacterCard = ({char})=> (
-    <div className="character-card">
-        
+    <div className="character-card"> 
         <h1 className="character-name">{char ? char.name : 'Character Name'}</h1>
+        <Eye color={char ? char.eye_color : 'black'} />
     </div>
 )
 
