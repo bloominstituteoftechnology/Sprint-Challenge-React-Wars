72c21650801df44d24c0ef78018c0bed4e2a51ea
DirupT <iamcooled@gmail.com>
2018-06-15 12:27:36 -0500

    added number navigation to pagination system

--- starwars/src/components/StarWarsButtons.js
+++ starwars/src/components/StarWarsButtons.js
@@ -4,8 +4,17 @@ import './StarWars.css';
 const StarWarsButtons = props => {
 
     return (
-        <div>
+        <div className='starwars-buttons'>
             <button onClick={() => props.button(props.prevPage)}>Previous!</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=1')}>1</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=2')}>2</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=3')}>3</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=4')}>4</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=5')}>5</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=6')}>6</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=7')}>7</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=8')}>8</button>
+            <button onClick={() => props.button('https://swapi.co/api/people/?page=9')}>9</button>
             <button onClick={() => props.button(props.nextPage)}>Next!</button>
         </div>
     );
