ac4b38eb748ad8cdb8527f93e5770d1ec12eacab
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-06-22 10:54:14 -0700

    PR

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,18 +13,18 @@ class App extends Component {
     this.getCharacters('https://swapi.co/api/people/');
   }
 
-  getCharacters = URL => {
+  getCharacters = (URL) => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
     fetch(URL)
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         this.setState({ starwarsChars: data.results });
       })
-      .catch(err => {
+      .catch((err) => {
         throw new Error(err);
       });
   };
