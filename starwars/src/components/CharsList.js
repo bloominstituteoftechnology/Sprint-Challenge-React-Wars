f8cb00d3a6f8269db5e02b20a36a9241e396092b
Sania Parekh <sparekh@Sanias-MacBook.home>
2018-06-22 14:38:40 -0500

    added props for character fact cards and added styling for the cards

--- starwars/src/components/CharsList.js
+++ starwars/src/components/CharsList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Char from './Char';
-
+import './StarWars.css';
 const CharsList = props => {
-return <div>{props.chars.map(char => <Char swChar={char} />)}</div>;
+return <div className='wrapper'>{props.chars.map(char => <Char swChar={char} />)}</div>;
 };
 
 export default CharsList;
