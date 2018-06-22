ad421ee58ff55fbe40fba1e9e740c2a9f8b129fa
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-22 13:49:35 -0500

    attempts to do list & character

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
 
-const Character = props => {
-    return (
-        <div>
-        <h1>{props.SingleCharacter.name}</h1>
-        </div>
-    )
-}
-
-
-
+// const Character = props => {
+//     return (
+//         <div>
+//         <h1>{props.SingleCharacter.name}</h1>
+//         </div>
+//     )
+// }
 
 
 
 
-export default Character; 
+
+
+
+// export default Character; 
--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Character from './Character.js'
+// import Character from './Character.js'
 
 
 const StarWarsList = props => {
