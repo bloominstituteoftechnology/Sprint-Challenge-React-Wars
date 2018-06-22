2415c203d6738b404622ae5d8309e93ac047978a
sean chavez <seanchavez@seanchavezsMBP.home>
2018-06-22 09:53:01 -0700

    add new components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,5 +1,8 @@
 import React  from 'react';
 import './StarWars.css'
 
+const Character = props => {
+    return <div>{props.oneChar.name}</div>
+}
 
 export default Character
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,6 +1,20 @@
 import React  from 'react';
-import Character from './Character.js'
+import Character from './Character'
 import './StarWars.css'
 
+const CharacterCard = props => {
+  return (
+    <div>
+      {props.characters.map(char => {
+        return (
+          <Character
+            key={Math.random()}
+            oneChar={char}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CharacterCard
