f63ffe47d46bc1f5217e1761c4344ea4ebc9d203
Katie Gorbell <katiegorbell@gmail.com>
2018-06-22 12:58:41 -0500

    final push for Sprint

--- starwars/src/components/card.js
+++ starwars/src/components/card.js
@@ -5,10 +5,13 @@ import './StarWars.css'
 const Card = props => {
     return (
         <div className='card-body'>
-            <h1>{props.starwarsChar.name}</h1>
-            <span>Gender: {props.starwarsChar.gender}</span>
-            <span>Height: {props.starwarsChar.height}</span>
-            <span>Weight: {props.starwarsChar.weight}</span>
+            <h1 className='char-header'>{props.starwarsChar.name}</h1>
+            <div className='char-attributes'>
+                <span className='attribute'>Gender: {props.starwarsChar.gender}</span>
+                <span className='attribute'>Height: {props.starwarsChar.height}</span>
+                <span className='attribute'>Mass: {props.starwarsChar.mass}</span>
+                <span className='attribute'>Birth Year: {props.starwarsChar.birth_year}</span>
+            </div>
         </div>
     );
 }
