6b2af15e9f5b521b47c7964242e020124e424e11
Adrian Adames <adrianadames@gmail.com>
2018-06-24 21:07:45 -0400

    sprint challenge started

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Character from "./components/Character";
- 
+  
 class App extends Component {
   constructor() {
     super();
