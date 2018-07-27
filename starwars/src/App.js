24e7037e9db182ba98841e805159513fc7a423b1
David Pok <david.pok@gmail.com>
2018-07-27 11:13:40 -0700

    test

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   getCharacters = URL => {
-    console.log(data.results);
+    console.log();
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
