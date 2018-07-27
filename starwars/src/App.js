4ce86273555783e012a658135c306cf881854c42
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-27 13:55:57 -0400

    Added paging

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      prev: null,
+      next: null,
       starwarsChars: [],
       fonts: [
         {font: 'coruscant-common', active: true, display: 'Coruscant Common',},
@@ -29,16 +31,22 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
+    if(URL){
+      fetch(URL)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          this.setState({
+            starwarsChars: data.results,
+            prev: data.previous,
+            next: data.next,
+          });
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+    }
   };
 
   selectActiveCharacter = id => {
@@ -61,6 +69,10 @@ class App extends Component {
           {(this.state.activeChar === null) ?
             <CharacterList characters={this.state.starwarsChars} selectChar={this.selectActiveCharacter} /> :
             <CharacterDisplayFull character={this.state.activeChar} deselectChar={this.deselectActiveCharacter} /> }
+          <div className="paging-btns">
+            <div className="page-btn" onClick={() => this.getCharacters(this.state.prev)}>Previous</div>
+            <div className="page-btn" onClick={() => this.getCharacters(this.state.next)}>Next</div>
+          </div>
         </div>
       </div>
     );
