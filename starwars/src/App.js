b1a3cc5b38d1fe08eeeaccb63525380f3026667d
Carey <careycbaldwin@gmail.com>
2018-07-27 13:03:55 -0400

    installed dependencies and styled text

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -23,6 +24,7 @@ class App extends Component {
       })
       .then(data => {
         this.setState({ starwarsChars: data.results });
+        console.log("Characters", this.state.starwarsChars)
       })
       .catch(err => {
         throw new Error(err);
@@ -32,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Star Wars Characters</h1>
       </div>
     );
   }
