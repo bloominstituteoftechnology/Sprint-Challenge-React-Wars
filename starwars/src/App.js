6ac19f385cab959410f526b78b0866b4f3b1c0e9
adamcglee <adamcglee@gmail.com>
2018-07-27 15:21:57 -0700

    added in functions to remove back or next nav buttons that aren't valid because you are either on the first or last page.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
+    if(URL) {
     fetch(URL)
       .then(res => {
         return res.json();
@@ -31,6 +32,25 @@ class App extends Component {
       .catch(err => {
         throw new Error(err);
       });
+    }
+  };
+
+  hideNext = () => {
+    if(this.state.next === null) {
+      console.log("it's hidden!")
+      return 'right-btn-hidden'
+    } else {
+      return null;
+    }
+  };
+
+  hidePrevious = () => {
+    if(this.state.previous === null) {
+      console.log("it's hidden!")
+      return 'left-btn-hidden'
+    } else {
+      return null;
+    }
   };
 
   next = () => {
@@ -47,8 +67,8 @@ class App extends Component {
         <div className="sticky-header">
           <h1 className="Header">React Wars</h1>
           <div className="d-flex justify-content-center">
-            <div className="nav-btn left-button" onClick={this.previous}>Back</div>
-            <div className="nav-btn right-button" onClick={this.next}>Next</div>
+            <div className={`nav-btn left-btn ${this.hidePrevious()}`} onClick={this.previous}>Back</div>
+            <div className={`nav-btn right-btn ${this.hideNext()}`} onClick={this.next}>Next</div>
           </div>
         </div>
         <div className="core-content">
