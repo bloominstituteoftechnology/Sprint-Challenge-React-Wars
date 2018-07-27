4e3a1a26af3a15664153124e39bce95b3209caa2
tulf <timzulf@gmail.com>
2018-07-27 13:08:22 -0400

    managed to import react strap and get in!

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import WarCard from './components/StarCard'
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <WarCard />
       </div>
     );
   }
---
+++ starwars/src/components/StarCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './StarWars.css';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const WarCard = (props) => {
+  return(
+<h1> hello</h1>
+);
+}
+
+export default WarCard;
