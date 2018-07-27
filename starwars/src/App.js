09e25d89f5d6d5e5063baaf00c0160ee3ed2cf1a
Trevor <trevorfehrman@gmail.com>
2018-07-27 11:18:52 -0700

    Figured out the pagination.  Simple in retrospect.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,50 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CardList from "./components/CardList";
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			starwarsChars: [],
+			prev: null,
+			next: null
+		};
+	}
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
-  }
+	componentDidMount() {
+		this.getCharacters("https://swapi.co/api/people");
+	}
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-        console.log(data);
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
+	getCharacters = URL => {
+		// feel free to research what this code is doing.
+		// At a high level we are calling an API to fetch some starwars data from the open web.
+		// We then take that data and resolve it our state.
+		fetch(URL)
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				this.setState({
+					starwarsChars: data.results,
+					next: data.next,
+					prev: data.previous
+				});
+			})
+			.catch(err => {
+				throw new Error(err);
+			});
+	};
 
-  handleNext = event => {
+	handleNext = event => {};
 
-  }
-
-  render() {
-    return (
-      <div className="AppWrapper">
-        <h1 className="Header">React Wars</h1>
-        <CardList cardArray={ this.state.starwarsChars } />
-        {/* <button onClick={ this.getCharacters('https://swapi.co/api/people/?page=2') }>Next</button> */}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="AppWrapper">
+				<h1 className="Header">React Wars</h1>
+				<CardList cardArray={this.state.starwarsChars} />
+				<button className="Button" onClick={() => this.getCharacters(this.state.prev)}>Previous</button>
+				<button className="Button" onClick={() => this.getCharacters(this.state.next)}>Next</button>
+			</div>
+		);
+	}
 }
 
 export default App;
