f51c5fbf72b40b50c423da3a7f31cd4af277aaa3
MichaelBoegner <cmikeb@gmail.com>
2018-07-27 12:38:35 -0400

    Charles Boegner
    realized that lists has nothing to do with it. Looking back on documentation and previous code for accessing an object within an array. Something here I am missing.

--- starwars/src/components/DataItems.js
+++ starwars/src/components/DataItems.js
@@ -5,19 +5,7 @@ import './StarWars.css';
 const DataItems = props => {
   return(
     <div>
-       {props.data.name}
-       {props.data.skin_color}
-       {props.data.vehicles}
-       {props.data['birth-year']}
-       {props.data.created}
-       {props.data.edited}
-       {props.data.eye_color}
-       {props.data.films}
-       {props.data.gender}
-       {props.data.hair_color}
-       {props.data.height}
-       {props.data.homeworld}
-       {props.data.mass}
+{console.log(props)}
 
     </div>
      
