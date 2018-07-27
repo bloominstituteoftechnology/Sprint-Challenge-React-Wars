772497bbe6897c36847e6beee3496e68dd2c3401
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-27 12:14:00 -0700

    Major Changes - Displaying list of props.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,10 +4,18 @@ import './StarWars.css';
 const Character = props => {
     return (
     <div className="text-box">
-        <div className="image-container">
-            {/* <img className="my-image" src="http://worldwideinterweb.com/wp-content/uploads/2016/11/celebrities-in-star-wars.jpg" alt="Avatar"/> */}
-        </div>
+        {/* <div className="image-container">
+            <img className="my-image" src="http://worldwideinterweb.com/wp-content/uploads/2016/11/celebrities-in-star-wars.jpg" alt="Avatar"/>
+        </div> */}
         <h4><b>{props.mychar.name}</b></h4>
+        <ul>
+            <li>height: {props.mychar.height}</li>
+            <li>mass: {props.mychar.mass}</li>
+            <li>hair color: {props.mychar.hair_color}</li>
+            <li>skin color: {props.mychar.skin_color}</li>
+            <li>eye color: {props.mychar.eye_color}</li>
+            <li>birth_year: {props.mychar.birth_year}</li> 
+        </ul>
         {/* <p>{ JSON.stringify(props.mychar, null, 2) }</p> */}
     </div>
     );
