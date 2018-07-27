2b907d53e0b8f2599fd5f69c93065c41372712d8
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-27 12:15:25 -0400

    Display full character styling initial

--- starwars/src/components/CharacterDisplayFull.js
+++ starwars/src/components/CharacterDisplayFull.js
@@ -4,14 +4,14 @@ import './StarWars.css';
 const CharacterDisplayFull = props => {
   return(
     <div className="character-full">
-      <div className="character-name">Name: {props.character.name}</div>
+      <div className="character-name">Name: {props.character.name.toUpperCase()}</div>
       <div className="character-birthyear">Born: {props.character.birth_year}</div>
       <div className="character-height">Height: {props.character.height}</div>
       <div className="character-mass">Mass: {props.character.mass}</div>
-      <div className="character-gender">Gender: {props.character.gender}</div>
-      <div className="character-eye-color">Ocular Coloration: {props.character.eye_color}</div>
-      <div className="character-colors">Body Coloration: {props.character.skin_color}</div>
-      <div className="btn deslect-btn" onClick={props.deselectChar}> 'RETURN' </div>
+      <div className="character-gender">Gender: {props.character.gender.toUpperCase()}</div>
+      <div className="character-eye-color">Ocular Coloration: {props.character.eye_color.toUpperCase()}</div>
+      <div className="character-colors">Body Coloration: {props.character.skin_color.toUpperCase()}</div>
+      <div className="deselect-btn" onClick={props.deselectChar}> RETURN </div>
     </div>
   );
 };
