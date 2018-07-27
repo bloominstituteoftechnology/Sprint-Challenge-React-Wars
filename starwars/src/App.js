b8448e8254717c687ff64ef16f8d2a895e3f698e
LaikaFusion <newrad4@gmail.com>
2018-07-27 12:52:00 -0400

    Added page changing functionality

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      next: "",
+      previous: ""
     };
   }
 
@@ -19,20 +21,22 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
+    if(URL === null){
+      return;
+    }
     fetch(URL)
       .then(res => {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        console.log(data)
+        this.setState({ starwarsChars: data.results, next: data.next, previous: data.previous });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
-
   expand = (e) =>{
-
    e.target.nextElementSibling.classList.toggle('hidden');
   }
 
@@ -44,6 +48,11 @@ class App extends Component {
           // return JSON.stringify(Element);
           return <StarCard methods={this.expand} data={Element} />
         })}
+
+        <div className="buttonBar">
+          <button onClick={this.getCharacters.bind(this, this.state.previous)} className="navButton" id="previous">Previous Page</button>
+          <button onClick={this.getCharacters.bind(this, this.state.next)} className="navButton" id="next">Next Page</button>
+        </div>
       </div>
     );
   }
