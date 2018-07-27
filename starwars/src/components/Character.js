259780618ac9c8e41e8ff73a378cb040b66c0d5b
Charlie Winslow <cwinslow22@yahoo.com>
2018-07-27 12:04:30 -0500

    added a little more styling so that they look like actual cards
    pulled in some more character content

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Character.css';
 
 const Character = props => {
+    const { name, birth_year, eye_color, gender, hair_color, height, mass,  } = props.char;
     return (
         <div className="char-container" >
-            {props.char.name}
+            <h3>{name}</h3>
+            <p>Gender: {gender}</p>
+            <p>Height: {height}</p>
+            
         </div>
     );
 };
