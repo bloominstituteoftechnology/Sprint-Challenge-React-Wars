46872d75aafcc8fafe02a65be8270087749249db
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-27 14:25:01 -0700

    Major changes - Worked on the design of the page

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,14 +7,14 @@ const Character = props => {
         {/* <div className="image-container">
             <img className="my-image" src="http://worldwideinterweb.com/wp-content/uploads/2016/11/celebrities-in-star-wars.jpg" alt="Avatar"/>
         </div> */}
-        <h4><b>{props.mychar.name}</b></h4>
+        <h4 className="char-names"><b>{props.mychar.name}</b></h4>
         <ul className="unordered-list">
-            <li>height: {props.mychar.height}</li>
-            <li>mass: {props.mychar.mass}</li>
-            <li>hair color: {props.mychar.hair_color}</li>
-            <li>skin color: {props.mychar.skin_color}</li>
-            <li>eye color: {props.mychar.eye_color}</li>
-            <li>birth_year: {props.mychar.birth_year}</li> 
+            <li className="properties">height: {props.mychar.height}</li>
+            <li className="properties">mass: {props.mychar.mass}</li>
+            <li className="properties">hair color: {props.mychar.hair_color}</li>
+            <li className="properties">skin color: {props.mychar.skin_color}</li>
+            <li className="properties">eye color: {props.mychar.eye_color}</li>
+            <li className="properties">birth_year: {props.mychar.birth_year}</li> 
         </ul>
         {/* <p>{ JSON.stringify(props.mychar, null, 2) }</p> */}
     </div>
