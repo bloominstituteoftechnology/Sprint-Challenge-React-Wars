6cea78728f9e0ea29d4cd595a1d5b6c0f5e6e6fa
dasgma <dasgrigoma@gmail.com>
2018-07-27 15:32:58 -0700

    Added images for characters.

--- starwars/src/components/CharacterCard/CharacterCard.js
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -13,8 +13,8 @@ class CharacterCard extends Component {
 
         return (
             <div className={classes}>
-                <div className="profile-pic">{this.props.profilePicture}</div>
                 <div className="field name">{this.props.name}</div>
+                <div className="profile-pic">{this.props.profilePicture}</div>
                 <div className="field birthday">{this.props.birthday}</div>
                 <div className="field eye-color">{this.props.eyeColor}</div>
                 <div className="field gender">{this.props.gender}</div>
@@ -22,7 +22,6 @@ class CharacterCard extends Component {
                 <div className="field mass">{this.props.mass}</div>
                 <div className="field hair-color">{this.props.hairColor}</div>
                 <div className="field skin-color">{this.props.skinColor}</div>
-                
             </div>
           );
     }
