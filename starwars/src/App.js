1111f3874c8d59c31f31bb65f6d9d49e6e3b7e20
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-27 09:19:22 -0700

    add basic styling using reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,43 +3,41 @@ import './App.css';
 import StarCard from './components/Card.js';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			starwarsChars: []
+		};
+	}
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
-  }
+	componentDidMount() {
+		this.getCharacters('https://swapi.co/api/people');
+	}
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
+	getCharacters = URL => {
+		// feel free to research what this code is doing.
+		// At a high level we are calling an API to fetch some starwars data from the open web.
+		// We then take that data and resolve it our state.
+		fetch(URL)
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				this.setState({ starwarsChars: data.results });
+			})
+			.catch(err => {
+				throw new Error(err);
+			});
+	};
 
-  render() {
-    return (
-      <div className="background">
-        <div className="App">
-          <h1 className="Header">React Wars</h1>
-          <StarCard char={this.state.starwarsChars} />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1 className="Header">React Wars</h1>
+				<StarCard char={this.state.starwarsChars} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -15,7 +15,7 @@ const StarCard = (props) => {
                             <CardBody>
                                 <CardTitle>{character.name}</CardTitle>
                                 <CardSubtitle>{`Gender: ${character.gender}, Born on: ${character.birth_year}`}</CardSubtitle>
-                                <CardText>{`Appeared in ${character.films.length} films. ${character.name} has a height of ${character.height} cm.`}</CardText>
+                                <CardText>{`Appeared in ${character.films.length} films. ${character.name} has a height of ${character.height} cm. ${character.name} has ${character.starships.length} starships and ${character.vehicles.length} vehicles. ${character.name} has ${character.skin_color} skin color and ${character.eye_color} eyes.`}</CardText>
                                 <Button>More info</Button>
                             </CardBody>
                         </Card>
