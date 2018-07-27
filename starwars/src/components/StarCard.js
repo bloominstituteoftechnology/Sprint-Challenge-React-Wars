c46ac4fad570bdae14feb4d27d3f462c444b53a8
AI2EJAY <JHammett1314@gmail.com>
2018-07-27 12:55:23 -0500

    Pretty much stuck. I'm not having a very good time in react.

---
+++ starwars/src/components/StarCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StarCard = props => {
+    return (
+        <div className ="card-container">
+        <h1> hi {props.name}</h1>
+        </div>
+    )
+
+
+}
+export default StarCard;
