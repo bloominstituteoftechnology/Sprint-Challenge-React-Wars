ea35a7b9c13dfdb7810520daa7ce1fcb13d00bc8
szincone <sawyerzincone@gmail.com>
2018-07-27 10:11:40 -0600

    converted cm to display as feet and inches

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -3,17 +3,19 @@ import "./StarWars.css";
 
 const CharacterCard = props => {
   const { name } = props.starwarsArray;
+  const heightInFeetTotal = (props.starwarsArray.height * 0.393701) / 12;
+  const feet = Math.floor(heightInFeetTotal);
+  const inches = Math.round((heightInFeetTotal - feet) * 12);
   return (
-    <ul
+    <div
       // style={
       //   props.todoDataProp.completed ? { textDecoration: "line-through", textDecorationColor: "#FF220C"} : null
       // }
       // onClick={() => props.handleToggleComplete(props.todoDataProp.id)}
     >
-      <li>
-        <h4>{name}</h4>
-      </li>
-    </ul>
+        <h2>{name}</h2>
+        <h4>Height: {`${feet}' ${inches}"`}</h4>
+    </div>
   );
 };
 
