c3dc63c2231655857751d3b79d9fbdb9582d8f67
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-27 10:11:23 -0700

    add pagination ability

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import StarCard from './components/Card.js';
+import StarPagination from './components/Pagination.js';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			starwarsChars: []
+			starwarsChars: [],
+			page: 1
 		};
 	}
 
 	componentDidMount() {
 		this.getCharacters('https://swapi.co/api/people');
 	}
+
+	// componentDidUpdate(prevState) {
+	// 	if (this.state.starwarsChars !== prevState.starwarsChars) {
+	// 		this.getCharacters(`https://swapi.co/api/people/?page=${this.state.page}`)
+	// 	}
+	// }
 
 	getCharacters = URL => {
 		// feel free to research what this code is doing.
@@ -23,6 +31,7 @@ class App extends Component {
 				return res.json();
 			})
 			.then(data => {
+				console.log(data);
 				this.setState({ starwarsChars: data.results });
 			})
 			.catch(err => {
@@ -30,11 +39,32 @@ class App extends Component {
 			});
 	};
 
+	setPage = (event) => {
+		this.setState({ page: Number(event.target.innerText) }, () => this.getCharacters(`https://swapi.co/api/people/?page=${this.state.page}`))
+	}
+
+	prevPage = () => {
+		if (this.state.page > 1){
+			this.setState((prevState) => {
+				return {page: prevState.page - 1};
+			});
+		}
+	}
+
+	nextPage = () => {
+		if (this.state.page < 5) {
+			this.setState((prevState) => {
+				return { page: prevState.page + 1 };
+			});
+		}
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1 className="Header">React Wars</h1>
 				<StarCard char={this.state.starwarsChars} />
+				<StarPagination setPage={this.setPage} prevPage={this.prevPage} nextPage={this.nextPage} />
 			</div>
 		);
 	}
