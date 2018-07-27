48108d79933ba5fc7043c9f92158023f3f9b5fd3
MichaelBoegner <cmikeb@gmail.com>
2018-07-27 13:05:05 -0400

    Charles Boegner
    All non URL stats are appearing. Styling.

--- starwars/src/components/DataItems.js
+++ starwars/src/components/DataItems.js
@@ -5,7 +5,16 @@ import './StarWars.css';
 const DataItems = props => {
   return(
     <div>
-{console.log(props)}
+        
+       <p>Name: {props.data.name}</p>
+      <p>Skin Color: {props.data.skin_color}</p>
+       <p>Birth Year: {props.data['birth-year']}</p>
+       <p>Eye Color: {props.data.eye_color}</p>
+       <p>Gender: {props.data.gender}</p>
+       <p>Hair color: {props.data.hair_color}</p>
+       <p>Height: {props.data.height}</p>       
+       <p>Mass: {props.data.mass}</p>
+
 
     </div>
      
