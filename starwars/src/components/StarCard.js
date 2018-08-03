3ef6813f86829c19f6232518424b78716866b942
Cwright0428 <cwright0428@yahoo.com>
2018-08-03 12:29:36 -0500

    completely stuck

---
+++ starwars/src/components/StarCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './StarWars.css';
+
+let StarCard;
+
+StarCard = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default StarCard;
