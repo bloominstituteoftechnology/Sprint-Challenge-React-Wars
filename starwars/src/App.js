8b53d2a4b0a2651cee582da84ab6a44df7ba1eb8
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-03 12:47:49 -0400

    set up StarCards.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import StarCards from "./components/StarCards";
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters("https://swapi.co/api/people");
   }
 
   getCharacters = URL => {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <StarCards />
       </div>
     );
   }
---
+++ starwars/src/components/StarCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StarCards = props => {
+  return (
+    <div className="dataCards">
+      
+    </div>
+  );
+};
+
+export default StarCards
