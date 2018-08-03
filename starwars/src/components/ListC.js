68a3e3c9f6372fd3d9d9cdc29ca5cb2cf8a2f954
rverdi642 <rverdi642@gmail.com>
2018-08-03 16:43:02 -0500

    Now have data coming back...need to style
    Richard Verdier

--- starwars/src/components/ListC.js
+++ starwars/src/components/ListC.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import StarWarsC from './StarWarsC.js';
+import StarWars from  './StarWars.css';
 
 const ListC = props => {
     return ( <div>
-    <div> {props.ListC.name} </div>
-     <div> {props.ListC.gender} </div>
+    <div> Name: {props.ListC.name} </div>
+     <div>Gender: {props.ListC.gender} </div>
+    < div> Eye Color: {props.ListC.eye_color} </div>
+    < div> Height: {props.ListC.height} </div>
+    < div> Home World: {props.ListC.homeworld} </div>
      </div>
     );
     };
--- starwars/src/components/StarWarsC.js
+++ starwars/src/components/StarWarsC.js
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import ListC from './ListC.js';
-import StarWars from  './StarWars.css';
+// import StarWars from  './StarWars.css';
 
 const StarWarsC =  props =>  {
     return (
 
-        <div>
-            {props.List.map((list,i) => <ListC key={i} ListC={list}/>)}
+        <div className="container">
+            {props.List.map((list,i) => 
+                <ListC key={i} ListC={list}/>)}
         </div>
     );
 };          
