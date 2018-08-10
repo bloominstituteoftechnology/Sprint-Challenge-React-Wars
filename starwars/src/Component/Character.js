274f99e3a19d5d8639f5f68810c6460499c60527
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-08-10 00:44:30 -0700

    test: ensure Card displys on screen

--- starwars/src/Component/Character.js
+++ starwars/src/Component/Character.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import { Card, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
 
 const character = props => {
     return (
         <div>
-            Star Trek FTW!
+            <Row>
+                <Col sm="4">
+                    <Card body>
+                        <CardTitle>Special Title Treatment</CardTitle>
+                        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                        <Button>Go somewhere</Button>
+                    </Card>
+                </Col>
+                <Col sm="4">
+                    <Card body>
+                        <CardTitle>Special Title Treatment</CardTitle>
+                        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                        <Button>Go somewhere</Button>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };
