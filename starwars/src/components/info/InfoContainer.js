78b550445670e6efe8cd1a3688cf8b50c226fb19
Clint Kunz <clintkunz@gmail.com>
2018-08-31 11:42:28 -0600

    fixed links

--- starwars/src/components/info/InfoContainer.js
+++ starwars/src/components/info/InfoContainer.js
@@ -3,11 +3,10 @@ import './info.css';
 
 const InfoContainer = (props) => {
     return (
-        
         <div className="info-container">
             <div className="name">
-                <h1>Name: {props.starwarsChar.name}</h1>
-                <h2>Homeworld: <a href="{props.starwarsChar.homeworld}">{props.starwarsChar.homeworld}</a></h2>
+                <h1>Name: <a href={props.starwarsChar.url}>{props.starwarsChar.name}</a></h1>
+                <h2>Homeworld: <a href={props.starwarsChar.homeworld}>{props.starwarsChar.homeworld}</a></h2>
             </div>
             <div className="demographics">
                 <h4>
@@ -19,7 +18,7 @@ const InfoContainer = (props) => {
             <div className="films">
                 <ul>
                     {props.starwarsChar.films.map(film => 
-                        <li>Film: <a href="{film}">{film}</a></li>)}
+                        <li>Film: <a href={film}>{film}</a></li>)}
                 </ul>
             </div>
             
