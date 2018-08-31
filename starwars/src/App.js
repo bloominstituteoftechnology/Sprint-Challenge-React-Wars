b3ac04e5d64ae46fa01d579540f5ea977b5e734a
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-31 09:02:23 -0700

    linking DisplayCard component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import DisplayCard from './components/DisplayCard';
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <DisplayCard />
       </div>
     );
   }
---
+++ starwars/src/components/DisplayCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DisplayCard = props => {
+    return (
+        <div class ='wholecard'>
+        <h1>Who's Who of Combat</h1>
+        
+        </div>
+    );
+};
+
+export default DisplayCard;
