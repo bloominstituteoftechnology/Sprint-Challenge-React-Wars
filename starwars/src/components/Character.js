b56b955ef3f69fb24f67e550652d323934d4053c
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-31 12:23:09 -0400

    added properties to display, basic functionality goal complete

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,16 @@ import './StarWars.css';
 
 const Character = (props) => {
   return (
-    <li className = 'character' id = {props.character.created}></li>
+    <li className = 'character' id = {props.character.created}>
+      <h3>{props.character.name}</h3>
+        <p>{props.character.birth_year}</p>
+        <p>{props.character.gender}</p>
+        <p>{props.character.hair_color}</p>
+        <p>{props.character.eye_color}</p>
+        <p>{props.character.skin_color}</p>
+        <p>{props.character.height}</p>
+        <p>{props.character.mass}</p>
+    </li>
   );
 }
 
