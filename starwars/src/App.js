d424bf644c56fe20d336634bf35cb62f556065ba
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-31 12:53:25 -0500

    SC WIP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+import SWC from "./components/Card";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      input: ""
     };
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters("https://swapi.co/api/people/");
   }
 
   getCharacters = URL => {
@@ -29,10 +32,52 @@ class App extends Component {
       });
   };
 
+  handlechange = event => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <Form>
+          <FormGroup>
+            <Label for="cardsearch">Search Characters</Label>
+            <Input
+              type="text"
+              name="search"
+              id="csipt"
+              placeholder="Search"
+              handlechange={this.handlechange}
+              value={this.state.input}
+            />
+          </FormGroup>
+        </Form>
+        <div className="cardlist-container">
+          {this.state.starwarsChars.map(item => (
+            <SWC
+              key={Math.random()}
+              birth_year={item.birth_year}
+              created={item.created}
+              edited={item.edited}
+              eye_color={item.eye_color}
+              films={item.films}
+              gender={item.gender}
+              hair_color={item.hair_color}
+              height={item.height}
+              homeworld={item.homeworld}
+              mass={item.mass}
+              name={item.name}
+              skin_color={item.skin_color}
+              species={item.species}
+              starships={item.starships}
+              url={item.url}
+              vehicles={item.vehicles}
+            />
+          ))}
+        </div>
       </div>
     );
   }
---
+++ starwars/src/components/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+
+const SWC = props => {
+  return (
+    <div className="swc">
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardTitle>{props.name}</CardTitle>
+          <CardSubtitle>{props.homeworld}</CardSubtitle>
+          <CardText>
+            {props.starships}
+          </CardText>
+          <Button>Collect</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default SWC;
