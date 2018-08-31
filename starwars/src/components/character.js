e843cd894fad47abbf61a243025bd5921ec57d07
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-31 16:52:52 -0700

    1st Stab at Styling.

--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -1,22 +1,28 @@
 // import React, { Component } from 'react';
 import React from 'react';
-
+import './StarWars.css';
 const Character = props => {
   // console.log(props.data);
   return (
   
-  <div>
+  <div className= "card">
   
-  {props.data.name}
-  {props.data.birth_year}
-  {props.data.gender}
-  {props.data.hair_color}
-  {props.data.eye_color}
-  {props.data.skin_color}
-  {props.data.height}
-  {props.data.mass}
-  {props.data.created}
-  {props.data.edited}
+  <h2>NAME:{props.data.name}</h2>
+
+  <div className= "stats">
+    <p>{props.data.birth_year}</p>
+    <p>{props.data.gender}</p>
+    <p>{props.data.hair_color}</p>
+    <p>{props.data.eye_color}</p>
+    <p>{props.data.skin_color}</p>
+    <p>{props.data.height}</p>
+    <p>{props.data.mass}</p>
+  </div>
+  <div>
+    <p>{props.data.created}</p>
+    <p>{props.data.edited}</p>
+
+  </div>
 
   </div>
 
