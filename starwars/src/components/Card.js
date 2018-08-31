83c09f16dc6d889968f94caba1506b4cac9e37fe
Timothy Hoang <timh1203@gmail.com>
2018-08-31 10:57:45 -0500

    Add card properties

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,7 +4,14 @@ import './Card.css'
 const Card = (props) => {
   return (
     <div className="Card">
-      <h1>{props.char.name}</h1>
+      <h1 className="charName">{props.char.name}</h1>
+      <p><span className="charProperty">Born:</span> {props.char.birth_year}</p>
+      <p><span className="charProperty">Gender:</span> {props.char.gender}</p>
+      <p><span className="charProperty">Mass:</span> {props.char.mass}kg</p>
+      <p><span className="charProperty">Height:</span> {props.char.height}m</p>
+      <p><span className="charProperty">Eye Color:</span> {props.char.eye_color}</p>
+      <p><span className="charProperty">Hair Color:</span> {props.char.hair_color}</p>
+      <p><span className="charProperty">Skin Color:</span> {props.char.skin_color}</p>
     </div>
   )
 }
