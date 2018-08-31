0a32d968dd6b6443939caff7c846bdbfbb13e988
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-08-31 14:29:26 -0400

    Added the rest of the data to StarWarsData.js

--- starwars/src/components/StarWarsData.js
+++ starwars/src/components/StarWarsData.js
@@ -5,7 +5,11 @@ const StarWarsData = props => {
     <div className="data-card">
             Name: {props.starwarsChars.name} <br/>
        Birth Year: {props.starwarsChars.birth_year} <br/>
-            Gender: {props.starwarsChars.gender}
+            Gender: {props.starwarsChars.gender}<br/>
+            Eye-Color: {props.starwarsChars.eye_color}<br />
+            Hair-Color: {props.starwarsChars.hair_color}<br />
+            Height: {props.starwarsChars.height}<br />
+            Skin-Color: {props.starwarsChars.skin_color}<br />
     </div>
     );
 };
