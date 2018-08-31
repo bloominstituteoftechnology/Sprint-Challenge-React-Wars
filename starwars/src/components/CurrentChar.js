37f62b8a41d11c23008ad2e065ab43a59512189f
decagondev <decagondevelopment@gmail.com>
2018-08-31 17:40:59 +0100

    (SPRINT : STRUCTURE) : created structure for the CurrentChar component

--- starwars/src/components/CurrentChar.js
+++ starwars/src/components/CurrentChar.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const CurrentChar = props => {
+  // create a clickable element that can call the currentChar method passedto it via props
+  return (
+    <div className="current-char" onClick={() => props.currentChar(props.char.name)}>
+      {props.char.name}
+    </div>
+  );
+};
+
+export default CurrentChar;
