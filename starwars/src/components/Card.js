05a8e3ba47aec66a015846b58324169da8e7bb28
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-31 13:00:31 -0400

    finished card file that documents each attribute of the starwars character

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -2,5 +2,22 @@ import  React from 'react';
 import ReactDOM from 'react-dom'
 import './StarWars.css'
 
-
-export default Card;
+function Card (props){
+    const data = props.data
+    return (
+        <div className="card">
+            <h1>{data.name}</h1>
+            <div>
+                <p><strong>Birth Year:</strong> {data.birth_year}</p>
+                <p><strong>Gender:</strong> {data.gender}</p>
+                <p><strong>Eye Color:</strong> {data.eye_color}</p>
+                <p><strong>Hair Color:</strong> {data.hair_color}</p>
+                <p><strong>Height:</strong> {data.height}</p>
+                <p><strong>Mass:</strong> {data.mass}</p>
+                <p><strong>Skin Color:</strong> {data.skin_color}</p>
+            </div>
+        </div>
+    )
+}
+ export default Card; 
+     
