91a5df766351cec8fe32fdcea082bde49fda226c
Timothy Hoang <timh1203@gmail.com>
2018-08-31 11:58:24 -0500

    Add fix for page 0

--- starwars/src/components/Pagination.js
+++ starwars/src/components/Pagination.js
@@ -4,7 +4,7 @@ import './Card.css'
 const Pagination = (props) => {
   return (
     <div className="Pagination">
-      <button onClick={(e) => props.getCharacters(props.previousPage ? props.previousPage : null)}>Previous Page</button>
+      <button onClick={(e) => props.getCharacters(props.previousPage ? props.previousPage : "https://swapi.co/api/people")}>Previous Page</button>
       <button onClick={(e) => props.getCharacters(props.nextPage)}>Next Page</button>
     </div>
   )
