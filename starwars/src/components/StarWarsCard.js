f58345848a2d5989f970a0ae909e6fbadbca4cde
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-08-31 11:38:02 -0400

    Created App Structure: -Made card and data components. - linked comps to App.js

---
+++ starwars/src/components/StarWarsCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import StarWarsData from './StarWarsData';
+
+const StarWarsCard = props => {
+    return (
+        <div>
+            <StarWarsData/>
+        </div>
+    );
+};
+
+
+export default StarWarsCard;
---
+++ starwars/src/components/StarWarsData.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const StarWarsData = props => {
+    return (
+        <div>
+            Star Wars Data
+        </div>
+    );
+};
+
+
+export default StarWarsData;
