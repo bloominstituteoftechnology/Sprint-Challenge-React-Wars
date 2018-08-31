0142665c125bb1b82ea654f47fa9ac776b8ad828
unknown <cornielleandres@gmail.com>
2018-08-31 15:03:43 -0400

    Create basic pagination system.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      nextPage: '',
+      previousPage: '',
+      prevClass: ['display-none'],
+      nextClass: ['display-none']
     };
   }
 
@@ -24,10 +28,45 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsChars: data.results, nextPage: data.next, previousPage: data.previous });
       })
       .catch(err => {
         throw new Error(err);
+      });
+  };
+
+  getNewPage = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ starwarsChars: data.results, nextPage: data.next, previousPage: data.previous });
+      })
+      .catch(err => {
+        if (!this.state.previousPage) {
+          this.setState({ prevClass: [] }, () => {
+            setTimeout(() => {
+              this.setState((prevState) => {
+                prevState.prevClass.push('display-none');
+
+                return { prevClass: prevState.prevClass };
+              });
+            }, 1000);
+          });
+        } else if (!this.state.nextPage) {
+          this.setState({ nextClass: [] }, () => {
+            setTimeout(() => {
+              this.setState((prevState) => {
+                prevState.nextClass.push('display-none');
+
+                return { nextClass: prevState.nextClass };
+              });
+            }, 1000);
+          });
+        } else {
+          throw new Error(err);
+        }
       });
   };
 
@@ -35,6 +74,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <button onClick = { () => this.getNewPage(this.state.previousPage) }>PREVIOUS</button>
+        <button onClick = { () => this.getNewPage(this.state.nextPage) }>NEXT</button>
+        <div className={ this.state.prevClass.join(" ") }>There is no previous page</div>
+        <div className={ this.state.nextClass.join(" ") }>There is no next page</div>
         <StarWars 
           starwarsChars = { this.state.starwarsChars } 
         />
