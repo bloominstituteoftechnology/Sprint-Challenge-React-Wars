26111a1c78718390bba2c7027fad492cf9aaa139
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-31 12:28:07 -0400

    basic card style, added attribute descriptions to character info

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,13 +5,13 @@ const Character = (props) => {
   return (
     <li className = 'character' id = {props.character.created}>
       <h3>{props.character.name}</h3>
-        <p>{props.character.birth_year}</p>
-        <p>{props.character.gender}</p>
-        <p>{props.character.hair_color}</p>
-        <p>{props.character.eye_color}</p>
-        <p>{props.character.skin_color}</p>
-        <p>{props.character.height}</p>
-        <p>{props.character.mass}</p>
+        <p>Birth year: {props.character.birth_year}</p>
+        <p>Gender: {props.character.gender}</p>
+        <p>Hair color: {props.character.hair_color}</p>
+        <p>Eye color: {props.character.eye_color}</p>
+        <p>Skin color: {props.character.skin_color}</p>
+        <p>Height: {props.character.height}</p>
+        <p>Mass: {props.character.mass}</p>
     </li>
   );
 }
