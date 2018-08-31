318f66583336bb9cfe81b1ac52d974cf1b0f28a6
verydecent <wnjhwng@gmail.com>
2018-08-31 09:27:17 -0700

    fixed syntax bug

--- starwars/src/components/StarwarsData.js
+++ starwars/src/components/StarwarsData.js
@@ -5,7 +5,7 @@ import Character from './Character';
 function StarwarsData(props) {
     return (
         <div>
-            {props.charInfoList.map((element, index ) => <Character key={i} traits={element} />)}
+            {props.charInfoList.map((element, index ) => <Character key={index} traits={element} />)}
         </div>
     );
 }
