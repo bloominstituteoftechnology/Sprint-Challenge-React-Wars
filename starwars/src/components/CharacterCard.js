eaeb38ea13c15a985978d4207803145550b899f5
iAmAdamReid <adam@adamreid.me>
2018-08-31 11:37:09 -0700

    more styling

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -7,13 +7,13 @@ const CharacterCard = props => {
     return (
         <div className = 'card' id={props.count}>
             <h2>{props.char.name}</h2>
-            <p>Birth Year: {props.char.birth_year}</p>
-            <p>Gender: {props.char.gender}</p>
-            <p>Height: {props.char.height}</p>
-            <p>Eye Color: {props.char.eye_color}</p>
-            <p>Hair Color: {props.char.hair_color}</p>
-            <p>Skin Color: {props.char.skin_color}</p>
-            <p>Weight: {props.char.mass}</p>
+            <p><strong>Birth Year:</strong> {props.char.birth_year}</p>
+            <p><strong>Gender:</strong> {props.char.gender}</p>
+            <p><strong>Height:</strong> {props.char.height}</p>
+            <p><strong>Eye Color:</strong> {props.char.eye_color}</p>
+            <p><strong>Hair Color:</strong> {props.char.hair_color}</p>
+            <p><strong>Skin Color:</strong> {props.char.skin_color}</p>
+            <p><strong>Weight:</strong> {props.char.mass}</p>
         </div>
     )
 }
