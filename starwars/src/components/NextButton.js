db3b54fe1e0b04ac4ec52a0ba890e33509dee75f
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-31 13:16:22 -0500

    pagination

---
+++ starwars/src/components/NextButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NextButton = props => {
+    return (
+        <button onClick={props.onClick ? props.onClick:null} >Next</button>
+    );
+}
+
+export default NextButton;
