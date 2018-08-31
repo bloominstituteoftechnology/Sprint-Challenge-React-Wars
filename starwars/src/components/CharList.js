5a2bfa7fc41fa0e51567fe7e3d067b80aee69e35
decagondev <decagondevelopment@gmail.com>
2018-08-31 17:37:45 +0100

    (SPRINT : STRUCTURE) : created structure for the CharList component

--- starwars/src/components/CharList.js
+++ starwars/src/components/CharList.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const CharList = props => {
+  // map over the characters and teturn the element data
+  return (
+    <div>
+
+      {props.chars.map((char, index) => {
+        return <CurrentChar char={char} key={index} currentChar={props.currentChar} />;
+      })};
+    </div>
+  );
+};
+
+export default CharList;
