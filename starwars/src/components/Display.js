dee0f8c5d5ba05ccab4b764f1aecd92ae13f3e1b
Brandon White <bwhite06@stac.edu>
2018-08-31 21:14:42 -0400

    cards created

--- starwars/src/components/Display.js
+++ starwars/src/components/Display.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import './StarWars.css'
 
 function Display(props) {
   console.log('props', props);
   return (
-  <div>
+  <div className= "list-elements">
     <ul>
-      <li>Name:{props.height.name}</li>
-      <li>Height:{props.height.height}</li>
-      <li>Weight:{props.height.mass}kg</li>
-      <li>Hair Color:{props.height.hair_color}</li>
+      <li>Name: {props.height.name}</li>
+      <li>Height: {props.height.height}</li>
+      <li>Weight: {props.height.mass}kg</li>
+      <li>Hair Color: {props.height.hair_color}</li>
     </ul>
 
       </div>
