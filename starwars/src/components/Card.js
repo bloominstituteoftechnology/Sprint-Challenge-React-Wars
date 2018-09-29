d976261e70d024c3eb4d443841cf23a6b1374fa6
Matthew Meitl <darksamus6@gmail.com>
2018-09-29 13:38:03 -0500

    displaying all data required unstyled

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -10,6 +10,17 @@ const Card = (props) => {
     return(
         <div>
             {props.value.name}
+            {props.value.gender}
+            {props.value.birth_year}
+            {props.value.height}
+            {props.value.mass}
+            {props.value.hair_color}
+            {props.value.eye_colors}
+            {props.value.skin_color}
+            {props.value.created}
+            {props.value.edited}
+            
+
         </div>
 
     )
