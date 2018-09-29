8313f6a96e5c7c5d42178ba785f9918bcb24f524
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-29 14:54:01 -0400

    added import for CharInfo.js

--- starwars/src/components/charCard.js
+++ starwars/src/components/charCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CharInfo from './CharInfo.js';
 
 const CharCard = (props) => {
   return (
     <div>
       <h3>{props.character.name}</h3>
+      <CharInfo facts={props.character} />
     </div>
   )
 }
