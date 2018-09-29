be0d1ff21534e4f4f9303ec6818532a1699b3c4d
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-29 14:27:02 -0400

    completed the main tasks

--- starwars/src/components/card/card/card.js
+++ starwars/src/components/card/card/card.js
@@ -4,10 +4,12 @@ import Bio from './bio/characterBio';
 
 export default (props) => {
   return (
-    <div>
+    <div className='cardContainer'>
       <h2>{props.char.name}</h2>
       <Data data={props.char}/>
-      <Bio bio={props.char}/>
+      {/* <Bio bio={props.char}/> */}
     </div>
   )
 }
+
+//todo --- > add the bio back into the card, to show vehicles and other arrays.
--- starwars/src/components/card/cardsList.js
+++ starwars/src/components/card/cardsList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Card from './card/card'
+import '../StarWars.css'
 
 export default (props) => {
    return (
-      <div>
+      <div className='cardsContainer'>
       {props.cardList.map((card) => <Card char={card}/> )}
       </div>
    )
