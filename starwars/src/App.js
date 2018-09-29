aa803fdd8a1165444690af5d1930156c61e892b5
Brandon <brandon.g.elzy@gmail.com>
2018-09-29 14:06:49 -0500

    Brandon G. Elzy - Built card component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './components/card/card.js';
 import './App.css';
 
 class App extends Component {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {this.state.starwarsChars.length > 0 ? <Card data={this.state.starwarsChars[0]} /> : null}
       </div>
     );
   }
