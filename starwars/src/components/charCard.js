e761c8eaa4810f5ec2e4bb5c7e1596fd4654d20a
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-29 15:40:19 -0400

    added css className to CharCard component

--- starwars/src/components/charCard.js
+++ starwars/src/components/charCard.js
@@ -3,7 +3,7 @@ import CharInfo from './CharInfo.js';
 
 const CharCard = (props) => {
   return (
-    <div>
+    <div className='charCard'>
       <h3>{props.character.name}</h3>
       <CharInfo facts={props.character} />
     </div>
