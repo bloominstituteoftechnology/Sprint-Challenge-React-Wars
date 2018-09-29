98a9607dadc380cc585cea23a16fa5ae672be5d8
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-29 14:04:59 -0400

    added import for CharList.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import CharList from './components/CharList.js';
+
 class App extends Component {
   constructor() {
     super();
@@ -33,6 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharList characters={this.state.starwarsChars} />
       </div>
     );
   }
