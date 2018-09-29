1f4550912563ad4064cf0dc0ca8519016a5d6f27
Bruce Cabanayan <bcabanayan@Bruces-MBP.lan>
2018-09-29 09:32:23 -0700

    Created character list and character card components

---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const CharacterCard = (props) => {
+    return (
+        <h1>CharacterCard Test</h1>
+    )
+}
+
+export default CharacterCard;
+
+
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+
+const CharacterList = (props) =>{
+    return (
+        <div> 
+            <h1>Test</h1>
+            <CharacterCard />
+        </div>
+    )
+}
+
+export default CharacterList;
