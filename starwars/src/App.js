587fc26c770b6daf886070b80000b44c4a8d5d58
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-29 12:59:39 -0400

    Sorted cards by name

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,7 +35,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        {this.state.starwarsChars.map((char)    =>  (
+        {this.state.starwarsChars.sort(
+            function(a, b)  {
+                let aName = a.name.toUpperCase();
+                let bName = b.name.toUpperCase();
+                if(aName < bName)   {
+                    return -1;
+                }   else if (aName > bName) {
+                    return 1;
+                }   else {
+                    return 0;
+                }
+            }
+        ).map((char)    =>  (
             <Card name={char.name} height={char.height} mass={char.mass} eye_color={char.eye_color}
                 hair_color={char.hair_color} skin_color={char.skin_color} gender={char.gender} key={char.created}/>
         ))}
