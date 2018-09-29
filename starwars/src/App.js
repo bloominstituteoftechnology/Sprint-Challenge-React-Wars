daaa25b2325249ba2009eda509afe35e1ed54666
Marc de la Torre <mdelatorre1314@gmail.com>
2018-09-29 09:18:08 -0700

    initial PR

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <h2 className="Characters">Characters:</h2>
       </div>
     );
   }
