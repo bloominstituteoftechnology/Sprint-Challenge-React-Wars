4c00b617ce983399a86bb5bf8f67362f8491bf6d
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-09-29 11:16:28 -0600

    Working on Styling. Added bootstrap and reactstrap.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Card, CardTitle, CardText } from 'reactstrap';
 
 const Character = (props) => {
     return ( 
         <div className="individual-character">
-            {props.character.name}
-            {props.character.gender}
-            {props.character.birth_year}
-            {props.character.height}
-            {props.character.eye_color}
-            {props.character.hair_color}
-            {props.character.skin_color}
+                <Card>
+                    <CardTitle>{props.character.name}</CardTitle>
+                        <CardText>
+                            <div className="character-information-1">
+                                <p>Gender: {props.character.gender}</p>
+                                <p>Birth Year: {props.character.birth_year}</p>
+                                <p>Height: {props.character.height}</p>
+                            </div>
+                            <div className="character-information-2">
+                                <p>Eye Color:{props.character.eye_color}</p>
+                                <p>Hair Color: {props.character.hair_color}</p>
+                                <p>Skin Color: {props.character.skin_color}</p>
+                            </div>
+                        </CardText>
+                </Card>
         </div>
      );
 }
