fe507e783b1cfb2960a8fd013d464819c1ccf192
Mitchell Robles <mitchellr92@live.com>
2018-09-29 11:02:59 -0700

    added some more styling

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,16 +4,24 @@ import './StarWars.css';
 const Card = (props) => {
     return (
         <div className='card'>
-            <p>Hello there!</p>
-            <div><p>Name</p>{props.character.name}</div>
-            <div><p>Birth year</p>{props.character.birth_year}</div>
-            <div><p>Home world</p>{props.character.homeworld}</div>
-            <div><p>Gender</p>{props.character.gender}</div>
-            <div><p>Height</p>{props.character.height}</div>
-            <div><p>Mass</p>{props.character.mass}</div>
-            <div><p>Skin color</p>{props.character.skin_color}</div>
-            <div><p>Eye color</p>{props.character.eye_color}</div>
-            <div><p>Hair color</p>{props.character.hair_color}</div>
+            <h1>Hello there!</h1>
+            <div className='character-attributes'>
+                <div className='attribute'><p>Name</p>{props.character.name}</div>
+                <div className='attribute'><p>Birth year</p>{props.character.birth_year}</div>
+                {/* <div className='attribute'><p>Home world</p>{props.character.homeworld}</div> */}
+                <div className='attribute'><p>Gender</p>{props.character.gender}</div>
+                <div className='attribute'><p>Height</p>{props.character.height}</div>
+                <div className='attribute'><p>Mass</p>{props.character.mass}</div>
+                <div className='attribute'><p>Skin color</p>{props.character.skin_color}</div>
+                <div className='attribute'><p>Eye color</p>{props.character.eye_color}</div>
+                <div className='attribute'><p>Hair color</p>{props.character.hair_color}</div>
+                {/* <div className='attribute'><p>Species</p>{props.character.species}</div> */}
+                {/* <div className='attribute'><p>Starships</p>{props.character.starships}</div> */}
+                {/* <div className='attribute'><p>Vehicles</p>{props.character.vehicles}</div> */}
+                {/* <div className='attribute'><p>Films</p>{props.character.films}</div> */}
+                <div className='attribute'><p>Created</p>{props.character.created}</div>
+                <div className='attribute'><p>Edited</p>{props.character.edited}</div>
+            </div>
         </div>
     )
 }
