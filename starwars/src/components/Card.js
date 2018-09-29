2848fd02ef4b0f2866b636be10967e5db89b3bb9
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-29 12:30:10 -0600

    Cards created

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,7 +4,14 @@ import './StarWars.css';
 const Card = props => {
     return (
         <div className="card">
-            {props.character.name}
+            <h1>{props.character.name}</h1>
+            <p>{props.character.gender}</p>
+            <p>{props.character.birth_year}</p>
+            <p>{props.character.height}</p>
+            <p>{props.character.mass}</p>
+            <p>{props.character.skin_color}</p>
+            <p>{props.character.hair_color}</p>
+            <p>{props.character.eye_color}</p>
         </div>
     )
 }
