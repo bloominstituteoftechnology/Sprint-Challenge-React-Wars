bcf9c064ed0451e596a2e4916c13b6c0e8bfe8a3
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-29 15:11:57 -0400

    removed props that are urls

--- starwars/src/components/CharInfo.js
+++ starwars/src/components/CharInfo.js
@@ -4,9 +4,7 @@ const CharInfo = (props) => {
   return (
     <div className='charInfo'>
       <ul>
-        <li>{props.facts.homeworld}</li>
         <li>{props.facts.gender}</li>
-        <li>{props.facts.species}</li>
         <li>{props.facts.birth_year}</li>
         <li>{props.facts.height} cm</li>
         <li>{props.facts.mass} kg</li>
