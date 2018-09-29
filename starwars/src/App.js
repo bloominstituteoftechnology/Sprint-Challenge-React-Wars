f112adce4f595888884c6ec221a4693ef2ff7c8e
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-29 14:05:45 -0400

    Changed the App.js to fetch more character data and display it accordingly

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,23 +13,39 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters(['https://swapi.co/api/people', 'https://swapi.co/api/people/?page=2', 'https://swapi.co/api/people/?page=3', 'https://swapi.co/api/people/?page=4', 'https://swapi.co/api/people/?page=5', 'https://swapi.co/api/people/?page=6', 'https://swapi.co/api/people/?page=7', 'https://swapi.co/api/people/?page=8']);
   }
 
   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
+    const characters = [];
+    let merged = [];
+    let count = 0;
+
+    URL.forEach(url => {
+      fetch(url)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          characters.push(data.results);
+          merged = [].concat.apply([], characters);
+          count++;
+          if (URL.length === count) {
+            addCharacters(merged);
+          }
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+    });
+
+    const addCharacters = charactersArray => {
+      console.log(charactersArray);
+      this.setState({ starwarsChars: charactersArray });
+    }  
   };
 
   showCharacter = length => {
