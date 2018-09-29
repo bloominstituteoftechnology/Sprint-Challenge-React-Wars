40c904a45a56fcf1164b453f7f46c36f81ea58ac
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-29 14:29:10 -0400

    added a character bio file so I can later on add the remaing data that have URLs

--- starwars/src/components/card/card/bio/characterBio.js
+++ starwars/src/components/card/card/bio/characterBio.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import List from './films/filmsList'
 
 export default (props) => {
   return (
     <div>
       <ul>
-        <li></li>
+        <li>Films: <List filmList ={props.bio.films}/></li>
         <li></li>
         <li></li>
         <li></li>
