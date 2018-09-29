0932ae09ccd7827ce72a4f4c8644518b14712ae8
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-29 12:18:25 -0400

    Create basic Card component and import to App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './components/Card.js'
 import './App.css';
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
       })
       .then(data => {
         this.setState({ starwarsChars: data.results });
+        console.log(this.state)
       })
       .catch(err => {
         throw new Error(err);
