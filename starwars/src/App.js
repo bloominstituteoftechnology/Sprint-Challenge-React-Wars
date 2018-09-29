15be2dcf3152e061dcbcf03c1b7add88a0856893
unknown <jerrardsmith215@gmail.com>
2018-09-29 13:48:33 -0400

    Jerrard Smith updatd

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   constructor() {
     super();
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -3,17 +3,28 @@ import React from "react";
 const Character = props => {
     return (
         <div className="card">
-            <div className="card_header">{props.char.name}</div>
+            <h2 className="card_header">{props.char.name}</h2>
             
             <div className="card_content">
-                <h2 className="charDOB">Date of birth: {props.char.birth_year}</h2>
-                <h2 className="charSex">{props.char.gender}</h2>
+                <h3 className="charDOB">Date of birth: {props.char.birth_year}</h3>
+                <h3 className="charSex">{props.char.gender}</h3>
 
-                    <div className="card-content_extra">
-                        Eye color: {props.char.eye_color}
-                        
-                    </div>
+                <div className="card-content_info">
+                    Species: {props.char.species}
+                    From: {props.char.homeworld}
+                    Ship(s): {props.char.starships}
+
+                    Skin: {props.char.skin_color}
+                    Eye color: {props.char.eye_color}
+                    Hair: {props.char.hair_color}
+                    Mass: {props.char.mass}
+                    Vehicle(s): {props.char.vehicles}
+                </div>
+
+                <h3 className="charMovies">Featured in: {props.char.films}</h3>
             </div> 
         </div>
     );
 };
+
+export default Characters;
