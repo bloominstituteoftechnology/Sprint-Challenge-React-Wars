bc9f707ed4d019cc5294fcaa8e3223d45bc03b36
Denise Escobar <descobar1490@gmail.com>
2018-09-29 13:08:34 -0500

    Added more item to each card
    -I have more chracter information displaying and I have a button to open the card. The information below the button will not be visable until it is clicked

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -3,7 +3,15 @@ import React from "react";
 const Card = (props) =>{
     return(
         <div>
-            {props.name}
+            <p>{props.name}</p>
+            <button>Open</button>
+            <ul>
+                <li>Born: {props.birth_year}</li>
+                <li>Eye Color: {props.eye_color}</li>
+                <li>Gender: {props.gender}</li>
+                <li>Hair Color: {props.hair_color}</li>
+                <li>Height: {props.height}</li>
+            </ul>
         </div>
     )
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -6,7 +6,13 @@ const Character = (props) =>{
         <div>
             {props.starwarsChars.map(item =>{
                 return(
-                        <Card name={item.name}/>
+                        <Card 
+                        name={item.name}
+                        birth_year={item.birth_year}
+                        eye_color={item.eye_color}
+                        gender={item.gender}
+                        hair_color={item.hair_color}
+                        height={item.height}/>
                 )
             })}
         </div>
