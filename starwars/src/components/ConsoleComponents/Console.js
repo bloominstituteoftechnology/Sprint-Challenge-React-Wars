9c4fc260f6594f19466c59130ea133375e732a89
Jennifer King <jenbellpgh@gmail.com>
2018-09-29 15:21:19 -0400

    Put information into rows

--- starwars/src/components/ConsoleComponents/Console.js
+++ starwars/src/components/ConsoleComponents/Console.js
@@ -11,22 +11,28 @@ const Console = (props) => {
     
     return(
         <div className='console'>
-            <div>Name: {props.name}</div>
-            <div>Height: {props.height}</div>
-            <div>Mass: {props.mass}</div>
-            <div>Hair Color: {props.hair_color}</div>
-            <div>Skin Color: {props.skin_color}</div>
-            <div>Eye Color: {props.eye_color}</div>
-            <div>Birth Year:{props.birth_year}</div>
-            <div>Gender: {props.gender}</div>
-            <div>Homeworld: {props.homeworld}</div>
-            <div>Species: </div>
-            <div>Starships: </div>
-            <div>Vehicles: </div>
-            <div>Films: </div>
-            <div>Created: {props.created}</div>
-            <div>Edited: {props.edited}</div>
-            <div>URL: {props.url}</div>
+            <h2>{props.name}</h2>
+            <div className='charRow'>
+                <div>Gender: {props.gender}</div>
+                <div>Birth Year:{props.birth_year}</div>
+                <div>Height: {props.height}</div>
+                <div>Mass: {props.mass}</div>
+            </div>
+            <div className='charRow'>
+                <div>Hair Color: {props.hair_color}</div>
+                <div>Skin Color: {props.skin_color}</div>
+                <div>Eye Color: {props.eye_color}</div>
+                <div>Species: </div>
+            </div>
+            <div className='charRow'>Homeworld: {props.homeworld}</div>
+            <div className='charRow'>Starships: </div>
+            <div className='charRow'>Vehicles: </div>         
+            <div className='charRow'>Films: </div>
+            <div className='charRow'>
+                <div>Created: {props.created}</div>
+                <div>Edited: {props.edited}</div>
+            </div>
+            <div className='charRow'>URL: {props.url}</div>
         </div>
     );
 };
