5ee44b2c52efb3cb17e0a74bd6eb78b44cac8939
meganfontenot <meganfontenot517@yahoo.com>
2018-09-29 11:52:38 -0500

    fixed card layout

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,20 +4,16 @@ import CardDates from './CardDates';
 
 const Card = props => {
     return (
-        <div className="card">
-            <div className="card__front">
-                <h1>{props.character.name}</h1>
-                <p><strong>Birth Year: </strong>{props.character.birth_year}</p>
-                <p><strong>Gender: </strong>{props.character.gender}</p>
-            </div>
-            <div className="card__back">    
-                <p><strong>Height: </strong>{props.character.height}</p>
-                <p><strong>Mass: </strong>{props.character.mass}</p>
-                <p><strong>Hair Color: </strong>{props.character.hair_color}</p>
-                <p><strong>Eye Color: </strong>{props.character.eye_color}</p>
-                <p><strong>Skin Color: </strong>{props.character.skin_color}</p>
-                <CardDates character={props.character} />
-            ls</div>
+        <div className="custom-card">
+            <h1>{props.character.name}</h1>
+            <p><strong>Birth Year: </strong>{props.character.birth_year}</p>
+            <p><strong>Gender: </strong>{props.character.gender}</p>
+            <p><strong>Height: </strong>{props.character.height}</p>
+            <p><strong>Mass: </strong>{props.character.mass}</p>
+            <p><strong>Hair Color: </strong>{props.character.hair_color}</p>
+            <p><strong>Eye Color: </strong>{props.character.eye_color}</p>
+            <p><strong>Skin Color: </strong>{props.character.skin_color}</p>
+            <CardDates character={props.character} />
         </div>
     );
 };
