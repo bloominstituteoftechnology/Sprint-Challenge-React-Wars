ca8c155db88dc2e5c432bc3b5da6b0da343e7d96
Joseph Mercado <jmercado@Josephs-MacBook-Air.local>
2018-09-29 20:19:12 -0400

    made changes. Still working on issues.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -32,7 +32,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Star Wars</h1>
+        <div >
+          {this.state.starwarsChars.map((item) => {
+            return (
+              <div className = 'star-wars-card' key = {item.edited}>
+                <p className = 'name'>{item.name}</p>
+                <div className = 'info'>
+                  <p><u><b>Gender:</b></u> {item.gender}</p>
+                  <p><u><b>Birth Year:</b></u> {item.birth_year}</p>
+                  <p><u><b>Height(cm)</b></u>: {item.height}</p>
+                  <p><u><b>Weight(kg)</b></u>: {item.mass}</p>
+                  <p><u><b>Hair Color:</b></u> {item.hair_color}</p>
+                  <p><u><b>Eye Color:</b></u> {item.eye_color}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
