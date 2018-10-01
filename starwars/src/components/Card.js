af9e4cbe2cc7802951fd9ff78d25b0b0b1ecc33f
Lambda_School_Loaner_26 <lambda_school_loaner_26@Lambda-School-Loaner-26s-MacBook-Pro.local>
2018-10-01 18:01:17 -0500

    Card Component created

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div key={this.props.name} className="Card-body">
+                <h2>{this.props.name}</h2>
+                <div className="card-content">
+                    <h3>Info</h3>
+                    <p>Birth Year: {this.props.birthYear}</p>
+                    <p>Home World: {this.props.homeWorld}</p>
+                    <p>Gender: {this.props.gender}</p>
+                    <p>hair Color: {this.props.hairColor}</p>
+                    <p>Height {this.props.height}</p>
+                    <p>Weigth: {this.props.weigth}</p>
+                    <p>Films: {this.props.films}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card;
