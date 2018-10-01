dc2ebd1365847b7ce289368a3b13772bfe105723
Clem <stacy.clem@e-hps.com>
2018-10-01 08:35:03 -0700

    Made the cards flip for better results.

--- starwars/src/components/Starwars.js
+++ starwars/src/components/Starwars.js
@@ -8,17 +8,21 @@ render() {
 return (
 
 <div className="card-content">
-    <img src="https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8xNzkvMDkyL2kwMi9TVEFSV0FSUy1DTEFTU0lGSUVELmpwZw==" alt="" />
-
+    <div className='front'> 
     <h1>Starwars</h1>
-    <h2>Character: {this.props.chars.name}</h2>
+    <h2>Character<br /> 
+    <p />
+    {this.props.chars.name}</h2>
+    </div> 
+    <div className='back'>
+    <img src="https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8xNzkvMDkyL2kwMi9TVEFSV0FSUy1DTEFTU0lGSUVELmpwZw==" alt="" />
     <p>Skin: {this.props.chars.skin_color} {' '}</p>
         <p>Eye Color: {this.props.chars.eye_color}{' '} </p>
         <p>Hair: {this.props.chars.hair_color}{''}</p>
         <p>Height: {this.props.chars.height}{' '}</p>
         <p>Birthday: {this.props.chars.birth_year}{' '}</p>
         <p>Weight: {this.props.chars.mass} {' '} </p>
-
+      </div>
 </div>
 
 )
