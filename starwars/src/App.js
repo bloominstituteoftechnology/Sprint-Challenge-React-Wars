7456dd51909d039c802a2f74bfbaf95de4d3a317
decagondev <decagondevelopment@gmail.com>
2018-10-01 13:07:20 +0100

    (REFACTORS) : refactored the code to jsx and in to folders for components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import CharList from './components/CharList';
-import CharCard from './components/CharCard';
+import React from "react";
+import "./App.css";
+import CharList from "./components/CharList";
+import CharCard from "./components/CharCard";
 
 class App extends React.Component {
   constructor() {
@@ -15,7 +15,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters("https://swapi.co/api/people/");
   }
 
   getCharacters = URL => {
@@ -24,38 +24,44 @@ class App extends React.Component {
         return res.json();
       })
       .then(data => {
-        console.log(data); 
-        this.setState({ starwarsChars: data.results, nextPage: data.next, prevPage: data.previous});
+        console.log(data);
+        this.setState({
+          starwarsChars: data.results,
+          nextPage: data.next,
+          prevPage: data.previous
+        });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
- // show the next page of data
+  // show the next page of data
   showNextPage = event => {
-    if(this.state.nextPage !== null) {
+    if (this.state.nextPage !== null) {
       const newData = this.getCharacters(this.state.nextPage);
       console.log(newData);
     }
-  }
+  };
 
   // show the previous page of data
   showPrevPage = event => {
-    if(this.state.prevPage !== null) {
+    if (this.state.prevPage !== null) {
       const newData = this.getCharacters(this.state.prevPage);
       console.log(newData);
     }
-  }
+  };
 
   showCurrentChar = name => {
-    const currentChar = this.state.starwarsChars.find(char => char.name === name);
+    const currentChar = this.state.starwarsChars.find(
+      char => char.name === name
+    );
     console.log(currentChar);
     this.setState({ currentChar: currentChar });
-  }
+  };
 
   returnToList = event => {
-    this.setState({ currentChar: null })
-  }
+    this.setState({ currentChar: null });
+  };
 
   render() {
     if (this.state.currentChar === null) {
@@ -63,16 +69,26 @@ class App extends React.Component {
         <div className="container">
           <h1 className="Header">React Wars</h1>
           <div className="charList">
-            <CharList chars={this.state.starwarsChars} showCurrentChar={this.showCurrentChar} showNextPage={this.showNextPage} showPrevPage={this.showPrevPage} nextPage={this.state.nextPage} prevPage={this.state.prevPage} />
+            <CharList
+              chars={this.state.starwarsChars}
+              showCurrentChar={this.showCurrentChar}
+              showNextPage={this.showNextPage}
+              showPrevPage={this.showPrevPage}
+              nextPage={this.state.nextPage}
+              prevPage={this.state.prevPage}
+            />
           </div>
         </div>
       );
     } else {
-        return ( 
-          <div className="container">
-            <CharCard returnToList={this.returnToList} character={this.state.currentChar}/>
-          </div>
-        );
+      return (
+        <div className="container">
+          <CharCard
+            returnToList={this.returnToList}
+            character={this.state.currentChar}
+          />
+        </div>
+      );
     }
   }
 }
