258b81aa7e875898ff42fc6fcfb82220fc6c7a19
hraya <hraya@lssu.edu>
2018-09-30 22:05:17 -0500

    Completed MVP
    Need to complete styling then try a strecth goal

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,14 +4,17 @@ import './StarWars.css';
 const Character = (props) => {
     return (
         <div className="card-container">
-            <div className="char-name">{props.obj.name}</div>
-            <div className="char-bday">{props.obj.birth_year}</div>
-            <div className="char-eye-color">{props.obj.eye_color}</div>
-            <div className="char-gender">{props.obj.gender}</div>
-            <div className="char-hair-color">{props.obj.hair_color}</div>
-            <div className="char-height">{props.obj.height}</div>
-            <div className="char-mass">{props.obj.mass}</div>
-            <div className="char-skin">{props.obj.skin_color}</div>
+            <h1>Lets find out about {props.obj.name}!</h1>
+            <div className="attributes">
+                <div className="char-name atrribute">{props.obj.name}</div>
+                <div className="char-bday atrribute">DOB: {props.obj.birth_year}</div>
+                <div className="char-eye-color atrribute">Eye Color: {props.obj.eye_color}</div>
+                <div className="char-gender atrribute">Gender: {props.obj.gender}</div>
+                <div className="char-hair-color atrribute">Hair Color: {props.obj.hair_color}</div>
+                <div className="char-height atrribute">Height: {props.obj.height}</div>
+                <div className="char-mass atrribute">Mass: {props.obj.mass}</div>
+                <div className="char-skin atrribute">Skin Type: {props.obj.skin_color}</div>
+            </div>
         </div>
     )
 }
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -5,7 +5,7 @@ import Character from './Character';
 const CharacterList = (props) => {
     return (
         <div className="list-container">
-            <div>{props.value.map(item => <Character obj={item} key={item.created}/>)}</div>    
+            <div>{props.value.map(item => <Character classNmae="character-comp" obj={item} key={item.created}/>)}</div>    
         </div>
     )
 }
