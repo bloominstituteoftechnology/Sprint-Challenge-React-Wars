fb31b77334848d16661e8516e92bedbd422d4efb
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-04 16:55:18 -0500

    files connected

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Characters = props => {
+  return (
+    <div>
+      straight from characters
+    </div>
+  )
+}
+
+export default Characters;
---
+++ starwars/src/components/CharactersList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Characters from './Characters';
+
+const CharacterList = () => {
+  return (
+    <div>
+    straight out of Character list
+    <Characters />
+    </div>
+  )
+}
+
+export default CharacterList;
