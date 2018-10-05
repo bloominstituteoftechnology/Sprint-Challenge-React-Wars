43cef1c3ed5a1f2c3f0569048f98d68b0d8bfab8
Lee Len Formento <leebatef@gmail.com>
2018-10-05 12:22:56 -0400

    Added a nav

---
+++ starwars/src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ const Nav = props => {
+   return (
+    <div className='nav'>
+    <button class="hamburger">&#9776;</button>
+    </div>
+  );
+}
+export default Nav;
