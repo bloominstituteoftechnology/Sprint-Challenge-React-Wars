8b66180462f0a53ca3114a724a6b94be75cd784b
frankbenevento <fb.mentoslabs@gmail.com>
2018-10-05 08:48:15 -0700

    added card and header

---
+++ starwars/src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './Card.css';
+
+
+
+const Card = props =>{
+    return(
+        <div className='card'>
+            <div className='card-header'></div>
+            <div className='card-body'></div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ starwars/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+const Header = props =>{
+    return(
+        <div>
+            <p>{props.title}</p>
+        </div>
+    )
+}
+
+export default Header
