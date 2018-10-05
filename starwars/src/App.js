148884d26f43d4268499f9ef4b5001a201da4f61
Chad Jemmett <chad.jemmett@gmail.com>
2018-10-05 09:35:22 -0600

    answers completed.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Chad Jemmett's React Wars</h1>
+        {console.log(this.state.starwarsChars)}
       </div>
     );
   }
