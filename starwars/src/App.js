a2ba438bb47e58db64ea0f0466b16985c004836c
Unknown <zachary.arney@gmail.com>
2018-10-05 12:42:59 -0500

    Imported Character component and added some divs for styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import Character from './components/Character.js';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+  state = {
+    starwarsChars: [],
+  };
 
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people');
   }
 
-  getCharacters = URL => {
+  getCharacters = (URL) => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
     fetch(URL)
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         this.setState({ starwarsChars: data.results });
       })
-      .catch(err => {
+      .catch((err) => {
         throw new Error(err);
       });
   };
@@ -33,6 +31,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="list-container">
+          <div className="shadow-top" />
+          <div className="shadow-left" />
+          <div className="scroll-container">
+            {this.state.starwarsChars.map((character) => {
+              return <Character character={character} />;
+            })}
+          </div>
+        </div>
       </div>
     );
   }
