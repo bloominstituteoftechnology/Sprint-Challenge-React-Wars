9ac8140c5ed20aa43972117a67b545b349af7320
Sungyun Won <seanwon8@gmail.com>
2018-10-05 12:44:15 -0700

    mvp

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Character = props =>{
+    return(
+        <div>
+            {props.starwarsChars.name}<br></br>
+            {props.starwarsChars.birth_year}<br></br>
+            {props.starwarsChars.created}<br></br>
+            {props.starwarsChars.edited}<br></br>
+            {props.starwarsChars.eye_color}<br></br>
+            {props.starwarsChars.films}<br></br>
+            {props.starwarsChars.gender}<br></br>
+            {props.starwarsChars.hair_color}<br></br>
+            {props.starwarsChars.height}<br></br>
+            {props.starwarsChars.mass}<br></br>
+            {props.starwarsChars.skin_color}<br></br>
+            {props.starwarsChars.species}<br></br>
+            {props.starwarsChars.starships}<br></br>
+            {props.starwarsChars.vehicles}
+        </div>
+    )
+}
+
+export default Character
