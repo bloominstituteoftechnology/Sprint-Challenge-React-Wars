1d08a94abf4fdbb807ed91c87759b265bba2643f
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-05 13:36:27 -0400

    final

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -34,7 +34,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className = "container">
         <Character list={this.state.starwarsChars} />
+        </div>
       </div>
     );
   }
--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -11,7 +11,8 @@ function Character(props) {
             <p className ="bd">Birth Year: {char.birth_year}</p>
             <p className ="sc">Skin Color: {char.skin_color}</p>
             <p className ="ec">Eye Color: {char.eye_color}</p>
-            <p className ="hc">Birth Year: {char.birth_year}</p>
+            <p className ="hc">Hair Color: {char.hair_color}</p>
+            <p className ="height">Height: {char.height}</p>
             <p className ="gender">Gender: {char.gender}</p>
             <p className ="mass">mass: {char.mass}</p>
             <p className ="created">Creation Date: {char.created}</p>
