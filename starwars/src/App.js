87010ad2f2be63ec9039c6a85bbfbcba78f5c68e
Rahul Desai <rd825@georgetown.edu>
2018-10-05 12:18:22 -0400

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './components/card'
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="cards">
+          {this.state.starwarsChars.map((char, i) => <Card key={i} char={char}/>)}
+        </div>
       </div>
     );
   }
---
+++ starwars/src/components/card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let Card = props => {
+    return (
+        <div className='card'>
+            <h2>{props.char.name}</h2>
+            <p>A {props.char.gender === 'n/a' ? 'robot created' : props.char.gender + ' born'} in {props.char.birth_year === 'unknown' ? 'an unknown year' : props.char.birth_year}.</p>
+            <div className='traits'>
+                <p>{props.char.name} is {props.char.height}cm tall and weighs {props.char.mass}kg.</p>
+                <ul className="physicalTraits">
+                    <li>Hair Color: {props.char.hair_color === 'n/a' ? 'none' : props.char.hair_color}</li>
+                    <li>Eye Color: {props.char.eye_color}</li>
+                    <li>Skin Color: {props.char.skin_color}</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
