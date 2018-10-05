007a80fe5cdd9e37cdea0f8d8977781abdc4e95e
unknown <brandongrych@gmail.com>
2018-10-05 11:40:55 -0500

    Card design finished

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -5,13 +5,19 @@ const Card = props => {
     return (
         <div className="card">
             <h1>{props.character.name}</h1>
-            <p>{props.character.gender}</p>
-            <p>{props.character.birth_year}</p>
-            <p>{props.character.height}</p>
-            <p>{props.character.mass}</p>
-            <p>{props.character.skin_color}</p>
-            <p>{props.character.hair_color}</p>
-            <p>{props.character.eye_color}</p>
+            <div className="content-medium">
+                <p>Height: {props.character.height}</p>
+                <p>Mass: {props.character.mass}</p>
+            </div>
+            <div className="content-medium">
+                <p>Gender: {props.character.gender}</p>
+                <p>Birth Year: {props.character.birth_year}</p>
+            </div>
+            <div className="content-bottom">
+                <p>Skin Color: {props.character.skin_color}</p>
+                <p>Hair Color: {props.character.hair_color}</p>
+                <p>Eye Color: {props.character.eye_color}</p>
+            </div>
         </div>
     )
 }
