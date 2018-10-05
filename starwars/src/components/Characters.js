ed915b8507fb44038c39c1e5f1eac222ec132efc
Sungyun Won <seanwon8@gmail.com>
2018-10-05 11:38:41 -0700

    forgot to save

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,1 +1,18 @@
 import React from 'react'
+
+const Characters = props =>{
+    return(
+        <div>
+            {props.starwarsChars.birth_year}
+            {props.starwarsChars.created}
+            {props.starwarsChars.edited}
+            {props.starwarsChars.eye_color}
+            {props.starwarsChars.films}
+            {props.starwarsChars.gender}
+            {props.starwarsChars.hair_color}
+            {props.starwarsChars.height}
+            {props.starwarsChars.mass}
+            {props.starwarsChars.}
+        </div>
+    )
+}
