b7d6045222dd79e74a4c03850ba2a00595b995ea
BryanGf <briangf@uw.edu>
2018-10-05 08:58:45 -0700

    Cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './components/Card';
 
 class App extends Component {
   constructor() {
@@ -32,7 +33,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <Card 
+          starwarsChars={this.state.starwarsChars}
+        />
       </div>
     );
   }
