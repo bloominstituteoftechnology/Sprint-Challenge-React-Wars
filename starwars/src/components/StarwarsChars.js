edf2a29bc17040753dc46fce9f09d5e37e7aafff
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-10-05 11:51:19 -0600

    final

--- starwars/src/components/StarwarsChars.js
+++ starwars/src/components/StarwarsChars.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Character from './Character';
+import './StarWars.css';
 
 const StarwarsChars = props => {
     return (
-        <div>
+        <div className="container">
             {props.characters.map(character => <Character key={character.name} char={character} />)}
         </div>
     )
