79cd3011bb62bd7895e2d58dfb499cc5b35f721d
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-10-05 15:35:56 -0500

    minor styling updates

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -6,10 +6,12 @@ const Character = props => {
         <div className="character" id={props.index}>
             <h1>{props.character.name}</h1>
             <ul>
-                <li>{props.character.height}</li>
-                <li>{props.character.mass}</li>
-                <li>{props.character.hair_color}</li>
-                <li>{props.character.birth_year}</li>
+                <li>Height: {props.character.height}</li>
+                <li>Weight: {props.character.mass}</li>
+                <li>Hair Color: {props.character.hair_color}</li>
+                <li>Eye Color: {props.character.eye_color}</li>
+                <li>Birth Year: {props.character.birth_year}</li>
+                <li>Gender: {props.character.gender}</li>
 
             </ul>
         </div>
