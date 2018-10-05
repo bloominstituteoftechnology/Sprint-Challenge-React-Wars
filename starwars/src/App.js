400867151965a6d40fd3f45469c6ea38038fd26e
Michael Checo <checomichael2@gmail.com>
2018-10-05 11:52:44 -0400

    styled cards and imported files into main component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import StarWarsList from './components/StarWarsList';
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="StarWars">
+        <StarWarsList data={this.state.starwarsChars} />
+        </div>
       </div>
     );
   }
