496ad5b728a02e50b9868ba690a8e6551bdd3ab6
Angelo Deleon <rudy209@comcast.net>
2018-10-05 10:47:06 -0700

    fixed h2 on app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <h2> className="Characters">Characters:</h2>
+        <h2 className="Characters">Characters:</h2>
         <div className='Card'>
           <StarWarsContainer characters={this.state.starwarsChars} />
 
