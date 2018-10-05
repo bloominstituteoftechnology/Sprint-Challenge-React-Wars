c6ed551583b5e1157f39d8621c17075f4d62a3b7
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-05 15:09:08 -0700

    added a short circuit

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -6,9 +6,9 @@ const Card = (props) => {
             <h1>Name: {props.character.name}</h1>
             <h3>Homeworld: {props.character.homeworld}</h3>
             <h3>Film: {props.character.films[0]}</h3>
-            <h3 className={ props.character.gender === 'n/a' ? 'noGender' : 'gender'}>Gender: {props.character.gender}</h3>
-            <h3 className={ props.character.starships[0] ? 'ship' : 'noShip'}>StarShip: {props.character.starships[0]}</h3>
-            <h3 className={ props.character.vehicles[0] ? 'vehicle' : 'noVehicle'}>Vehicles: {props.character.vehicles[0]}</h3>
+            <h3 className={ props.character.gender === 'n/a' && 'noGender'}>Gender: {props.character.gender}</h3>
+            <h3 className={ !props.character.starships[0] && 'noShip'}>StarShip: {props.character.starships[0]}</h3>
+            <h3 className={ !props.character.vehicles[0] && 'noVehicle'}>Vehicles: {props.character.vehicles[0]}</h3>
         </div>
     );
 }
