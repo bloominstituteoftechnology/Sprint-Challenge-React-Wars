5d0b04ab2b8ba3008cb3ceb49f8ad5176cd10d24
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-05 12:49:07 -0500

    More css styling

--- starwars/src/components/Display.js
+++ starwars/src/components/Display.js
@@ -6,7 +6,7 @@ const Display = props => {
     return (
         <div className="card-container">
             <h2>{props.characters.name}</h2>
-            <p className="content">{props.characters.birth_year}</p>
+            <p className="content">Birth Year: {props.characters.birth_year}</p>
             <p className="content">{props.characters.created}</p>
             <p className="content">{props.characters.edited}</p>
             <p className="content">{props.characters.eye_color}</p>
@@ -14,12 +14,12 @@ const Display = props => {
             <p className="content">{props.characters.gender}</p>
             <p className="content">{props.characters.hair_color}</p>
             <p className="content">{props.characters.height}</p>
-            <p className="content">{props.characters.homeworld}</p>
+            <p className="content url">{props.characters.homeworld}</p>
             <p className="content">{props.characters.mass}</p>
             <p className="content">{props.characters.skin_color}</p>
-            <p className="content">{props.characters.species}</p>
-            <p className="content">{props.characters.starships}</p>
-            <p className="content">{props.characters.vehicles}</p>
+            <p className="content url">{props.characters.species}</p>
+            <p className="content url">{props.characters.starships}</p>
+            <p className="content url">{props.characters.vehicles}</p>
         </div>
     );
 }
