417f00f8f6c5584fe308f4aba60b087815b86809
Stublink <chrisrey95@gmail.com>
2018-10-05 12:18:58 -0400

    Basic Styling

--- starwars/src/components/PersonInfo.js
+++ starwars/src/components/PersonInfo.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './StarWars.css';
 
 const PersonInfo = ({person}) => {
     return (
         <div className="person">
           {/* <img src={person.url} alt=""/> */}
-          {person.name}
-          {person.birth_year}
-          {person.eye_color}
-          {person.hair_color}
-          {person.height}
-          {person.mass}
+          <h2>{person.name}</h2>
+          <h3>Birth Year: {person.birth_year}</h3>
+          <h3>Height: {person.height}</h3>
+          <h3>Weight: {person.mass}kgs</h3>
+          <h3>Eye Color: {person.eye_color}</h3>
+          <h3> Hair Color: {person.hair_color} </h3>
         </div>
     );
 }
