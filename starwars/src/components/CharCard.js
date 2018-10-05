457c3f532fc0c98cd8168bb157f29282699eccfb
lola <lev@lolas-air.lan>
2018-10-05 13:56:42 -0400

    Added unit for mass

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -13,7 +13,7 @@ const CharCard = ({char, ind}) => {
                     <h2 className='feature'>Number of films: {char.films.length}</h2>
                 </div>
                 <div className='appearance'>Appearance
-                    <h2 className='feature'>Mass: {char.mass}</h2>
+                    <h2 className='feature'>Mass: {char.mass}kg</h2>
                     <h2 className='feature'>Eye color: {char.eye_color}</h2>
                     <h2 className='feature'>Hair color: {char.hair_color}</h2>
                     <h2 className='feature'>Skin color: {char.skin_color}</h2>
