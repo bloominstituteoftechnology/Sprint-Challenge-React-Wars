6e516408b8abacd030dbd0d61e72b8936e7cc465
Rahul Desai <rd825@georgetown.edu>
2018-10-05 13:06:37 -0400

    Added comments

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,6 +5,7 @@ import Card from './components/card'
 class App extends Component {
   constructor() {
     super();
+    // I added next and previous to state to store my URLs for pagination.
     this.state = {
       url: 'https://swapi.co/api/people',
       starwarsChars: [],
@@ -17,6 +18,7 @@ class App extends Component {
     this.getCharacters(this.state.url);
   }
 
+  // Here are my two pagination functions. There is some error handling included to prevent the site from crashing if the user clicks on a button that will not lead anywhere.
    previousPage = () => {
     if (this.state.previous !== null) {
       this.getCharacters(this.state.previous);
@@ -40,6 +42,7 @@ class App extends Component {
       .then(data => {
         console.log(data);
         this.setState({ starwarsChars: data.results, next: data.next, previous: data.previous });
+        // I updated what gets added to state here, so that I could easily access the next and previous URLs.
       })
       .catch(err => {
         throw new Error(err);
@@ -50,13 +53,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <div className="pageButtons">
+
+        {/* this creates next and previous buttons on the top of the page */}
+        <div className="pageButtons"> 
           <button onClick={this.previousPage}>Previous</button>
           <button onClick={this.nextPage}>Next</button>
         </div>
+        
+        {/* this maps through the data to create character bio cards */}
         <div className="cards">
           {this.state.starwarsChars.map((char, i) => <Card key={i} char={char}/>)}
         </div>
+        
+        {/* this creates next and previous buttons on the bottom of the page */}
         <div className="pageButtons">
           <button onClick={this.previousPage}>Previous</button>
           <button onClick={this.nextPage}>Next</button>
