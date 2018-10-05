c30312679a07a181562ca95411165dba8cea9d3c
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-05 13:05:46 -0400

    created more components

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Card extends React.Component {
+
+    render() {
+        return (
+            <div className="card">
+            </div>
+        );
+    }
+}
+
+export default Card;
