6269f7c33f69050106b69f6dbd1d5172cfb366bb
rytwalker <rytwalker@gmail.com>
2018-10-05 13:51:45 -0400

    Added a few new styles to cute dogs dot com

--- cute-dogs-dot-com/src/App.js
+++ cute-dogs-dot-com/src/App.js
@@ -44,6 +44,15 @@ class App extends Component {
         {this.state.dogs.map((dog, i) => {
           return <Image dog={dog} key={i} id={i} />;
         })}
+        <footer className="Footer">
+          &copy; 2018 CuteDogs.com{' '}
+          <span>
+            Disclaimer: CuteDogs.com might be a real other site, but this is
+            just a test for educational purposes. I am just fetching data with
+            Fetch and React so if that is exciting to you, thank you. If you
+            need anything else at all, I mean at all, email me.{' '}
+          </span>
+        </footer>
       </div>
     );
   }
