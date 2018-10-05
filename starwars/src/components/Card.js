38e4aacb8e5bc1f8952826f19400c11d3f2a2dc9
Axolotl <samuelmachat@gmail.com>
2018-10-05 13:01:49 -0400

    added all non-link data to cards

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './StarWars.css'
 
-const Card = ({ char }) => (
+const Card = ({
+  char: {
+    name,
+    birth_year,
+    eye_color,
+    gender,
+    hair_color,
+    height,
+    mass,
+    skin_color
+  }
+}) => (
   <div className="card">
-    <p>{char.name}</p>
+    <p>{name}</p>
+    <p>Born: {birth_year}</p>
+    <p>Eye color: {eye_color}</p>
+    <p>Gender: {gender}</p>
+    <p>Hair color: {hair_color}</p>
+    <p>Height: {height}</p>
+    <p>Mass: {mass}</p>
+    <p>Skin color: {skin_color}</p>
   </div>
 )
 
