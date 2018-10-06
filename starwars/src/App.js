fb8cb7453f27f70b1ee6ad05538f68f2ed1f0e12
Courtney Buratto <you@example.com>
2018-10-06 11:17:50 -0700

    FIX (fixed CharCard import)

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <CharCard starwarsChars = {this.state.starwarsChars} />
+        <CharCard starwarsChars = {this.state.starwarsChars} /> 
       </div>
     );
   }
