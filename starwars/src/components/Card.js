128fbe7e679231fad01522ce2c1ef079bba352fc
David Reichardt <davereichardt@gmail.com>
2018-10-06 13:03:51 -0400

    Started getting info displayed for characters

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const StarWarsCharacters = (props) => {
+    return (
+        <div>
+            {props.starwarsChars.map(character => {
+                return (
+                    <div className="person" key={character.toString()}>
+                        <h1>{character.name}</h1>
+                        <p>Gender: {character.gender}</p>
+                        <p>Birth Year: {character.birth_year}</p>
+                        <p>Eye Color: {character.eye_color}</p>
+                        <p>Skin Color: {character.skin_color}</p>
+                        <p>Hair Color: {character.hair_color}</p>
+                        <p>Height: {character.height}</p>
+                        <p>Mass: {character.mass}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default StarWarsCharacters;
