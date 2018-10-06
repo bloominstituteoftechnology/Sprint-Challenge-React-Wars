4bdca3edd462b173432599bca5249c8cccd6149c
Victor G <victorgordian103@gmail.com>
2018-10-06 09:37:45 -0700

    Finish my Character js which grabs the states info

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,2 +1,15 @@
 import React from 'react'
 
+const Character = (props) => {
+    return(
+        <div data-id={props.obj.created}>
+            <h1>{props.obj.name}</h1>
+            <p>gender : {props.obj.gender}</p>
+            <p>year of birth: {props.obj.birth_year}</p>
+            <p>hair color: {props.obj.hair_color}</p>
+            <p>eye color: {props.obj.eye_color}</p>
+        </div>
+    )
+}
+
+export default Character
