17a680d890c488c0e562f4fdd4cdc47e2e516c09
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-06 14:53:01 -0400

    changes

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,14 +7,74 @@ import NextBackButton from './components/NextBackButton.js';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      starwarsChars: []
+    this.state =    {
+      starwarsChars: [
+           /* {
+            "name": "Luke Skywalker", 
+            "height": "172", 
+            "mass": "77", 
+            "hair_color": "blond", 
+            "skin_color": "fair", 
+            "eye_color": "blue", 
+            "birth_year": "19BBY", 
+            "gender": "male", 
+            "homeworld": "https://swapi.co/api/planets/1/", 
+            "films": [
+                "https://swapi.co/api/films/2/", 
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/3/", 
+                "https://swapi.co/api/films/1/", 
+                "https://swapi.co/api/films/7/"
+            ], 
+            "species": [
+                "https://swapi.co/api/species/1/"
+            ], 
+            "vehicles": [
+                "https://swapi.co/api/vehicles/14/", 
+                "https://swapi.co/api/vehicles/30/"
+            ], 
+            "starships": [
+                "https://swapi.co/api/starships/12/", 
+                "https://swapi.co/api/starships/22/"
+            ], 
+            "created": "2014-12-09T13:50:51.644000Z", 
+            "edited": "2014-12-20T21:17:56.891000Z", 
+            "url": "https://swapi.co/api/people/1/"
+        }, 
+        {
+            "name": "C-3PO", 
+            "height": "167", 
+            "mass": "75", 
+            "hair_color": "n/a", 
+            "skin_color": "gold", 
+            "eye_color": "yellow", 
+            "birth_year": "112BBY", 
+            "gender": "n/a", 
+            "homeworld": "https://swapi.co/api/planets/1/", 
+            "films": [
+                "https://swapi.co/api/films/2/", 
+                "https://swapi.co/api/films/5/", 
+                "https://swapi.co/api/films/4/", 
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/3/", 
+                "https://swapi.co/api/films/1/"
+            ], 
+            "species": [
+                "https://swapi.co/api/species/2/"
+            ], 
+            "vehicles": [], 
+            "starships": [], 
+            "created": "2014-12-10T15:10:51.357000Z", 
+            "edited": "2014-12-20T21:17:50.309000Z", 
+            "url": "https://swapi.co/api/people/2/"
+        },*/
+      ]
       
-    };
+    }; 
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+   this.getCharacters("http://swapi.co/api/people");
   }
 
   getCharacters = URL => {
@@ -23,25 +83,28 @@ class App extends Component {
     // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
+        console.log(res);
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars : data.results });
+       this.setState({ starwarsChars : data.results });
       })
       .catch(err => {
+        console.log(err);
         throw new Error(err);
       });
   };
 
   render() {
+    
+    console.log(this.state.starwarsChars);
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
 
-        
-        {this.state.starwarsChars.map((stars, i) => 
-          <StarDisplay key={i} char={char} /> 
-        )}
+        <StarDisplay  
+            starwarsChars =  {this.state.starwarsChars}
+        />
 
         <NextBackButton />
 
--- starwars/src/components/StarDisplay.js
+++ starwars/src/components/StarDisplay.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import './StarWars.css';
+import FilmDisplay from './FilmDisplay';
+
 
 const StarDisplay = (props) => {
 	return (
 	    <div>
-	    	{props.starwarsChars.map(stars, i => (
-	    		
-	    		<div>	
-						<h1>Name : {stars[i].name} </h1>
-			    		<p>Height  : {stars[i].height} </p>
-			    		<p>Mass  : {stars[i].mass} </p>
-			    		<p>Hair-color  : {stars[i].hair_color} </p>
-				    	<p>Eye-color  : {stars[i].eye_color} </p>
-				    	<p>Birth-year  : {stars[i].birth_year} </p>
-				    	<p>Gender  : {stars[i].gender} </p>
-				    	<p>Homeworld  : {stars[i].homeworld} </p>
-				    	<p>Films  : {stars[i].films} </p>
-				    	<p>Species  : {stars[i].species} </p>
-				    	<p>Vehicles  : {stars[i].vehicles} </p>
-				    	<p>Starships  : {stars[i].starships} </p>
-				    	<p>Created  : {stars[i].created} </p>
-				    	<p>Edited  : {stars[i].edited} </p>
-				    	<p>URL  : {stars[i].url} </p>
-			   	</div>   
+	    	{props.starwarsChars.map((stars, i) => (
+	    		<div>
+	    			<h2> Name :  {stars.name} </h2>
+			    	<p> Height  :  {stars.height} </p>
+			    	<p> Mass  : {stars.mass} </p>
+			    	<p> Hair-color  :  {stars.hair_color} </p>
+				   	<p> Eye-color  :  {stars.eye_color} </p>
+				   	<p> Birth-year  :  {stars.birth_year} </p>
+				   	<p> Gender  :  {stars.gender} </p>
+				   	<p> Homeworld  :  {stars.homeworld} </p>
+				   	 
+				   	<FilmDisplay props = {stars.films} />
 
-			))}
+				   	<p> Species  :  {stars.species} </p>
+				   	<p> Vehicles  :  {stars.vehicles} </p>
+				   	<p> Starships  :  {stars.starships} </p>
+				   	<p> Created  :  {stars.created} </p>
+				   	<p> Edited  :  {stars.edited} </p>
+				   	<p> URL  :  {stars.url} </p>	
+				</div>   
+			))}	
 		</div>  	
 	);
 }
 
 
 export default StarDisplay;
+
