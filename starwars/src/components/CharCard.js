9e7a2746c3f7746ccfeaa1b58ce33e4d03553d0f
Courtney Buratto <you@example.com>
2018-10-07 15:02:36 -0700

    STYLES (began styling with object variables)

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Char from './Char';
 
 const CharCard = props => {
+    const cardWrapper = {
+        display: "flex",
+        flexWrap: "wrap",
+    }
+    const card = {
+        border: "1px solid black",
+        borderBottom: "10px solid black",
+        borderRight: "20px solid black",
+        borderTop: "2px solid black",
+        width: "40%",
+        margin: "auto"
+    }
+    
     return (
-        <div>
+        <div style = {cardWrapper}>
             {props.starwarsChars.map(char => {
                 return (
-                    <div className = "card"> 
+                    <div style ={card} >
                         <h2>{char.name}</h2>
                         <Char char ={char}/> 
-                    </div>               
+                    </div>           
                 )
             })}
         </div>
