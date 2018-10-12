b521696610b168de3e6d1938d4f52479a66e32d3
nickkruger <ngkruger@gmail.com>
2018-10-12 13:46:35 -0600

    Added more character traits

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Character = props => {
     return (
         <div>
-            <p>{props.char.name}</p>
+            <h2>{props.char.name}</h2>
+            <p>Born in: {props.char.birth_year}</p>
+            <p>Gender: {props.char.gender}</p>
+            <p>Eye Color: {props.char.eye_color}</p>
+            <p>Hair Color: {props.char.hair_color}</p>
         </div>
     )
 }
