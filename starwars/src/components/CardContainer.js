ceea979e5cb1f1258666d03da85007da867a8fab
jaklundun <govi.escudero@gmail.com>
2018-10-12 09:13:19 -0700

    Built container component named CardContainer to hold Starwars character cards yet to be built.

---
+++ starwars/src/components/CardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardContainer = props => {
+    return (
+        <div>
+            <h1 className="Header">React Wars</h1>
+        </div>
+    )
+}
+
+export default CardContainer;
