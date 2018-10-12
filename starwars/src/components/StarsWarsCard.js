52663b0f5509a6d1ec0deca11c436599baeab8a1
Corey Gumbs <cgumbs@Coreys-MacBook-Pro-3.local>
2018-10-12 12:33:18 -0400

    [updated] added charcater card info

--- starwars/src/components/StarsWarsCard.js
+++ starwars/src/components/StarsWarsCard.js
@@ -5,9 +5,14 @@ const StarWarsChars = props => {
         <div>
             <h1 onClick={() => {props.card(props.index)}}>{props.characters.name}</h1>
             <div className="character-card">
+            <p>Birth Year: {props.characters.birth_year}</p>
+            <p>Gender: {props.characters.gender}</p>
+            <p>Height: {props.characters.height}</p>
+            <p>Mass: {props.characters.mass}</p>
+            <p>Hair Color: {props.characters.hair_color}</p>
+            <p>Eye Color:{props.characters.eye_color}</p>
+            {/* <p>Flims: {props.characters.films}</p> */}
             </div>
-            
-
         </div>
     )
 }
