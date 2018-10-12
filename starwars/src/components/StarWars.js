da3387c42494116973b93c5a74e865f6bdd76ff9
Jake Thomas <jmaxt12@gmail.com>
2018-10-12 10:54:10 -0600

    All info displayed. Working on styling

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './StarWars.css';
+
+const Starwars = (props) => {
+    return (
+        <div>
+            {props.starwarsChars.map((info, index) => {
+                return (
+                    <div className = 'character-info'>
+                        <h2 key={index}>{info.name}</h2>
+                        <p key={index}>Birth Year: {info.birth_year}</p>
+                        <p key={index}>Eye Color: {info.eye_color}</p>
+                        <p key={index}>Hair Color: {info.hair_color}</p>
+                        <p key={index}>Skin Color: {info.skin_color}</p>
+                        <p key={index}>Birth Year: {info.birth_year}</p>
+                        <p key={index}>Gender: {info.gender}</p>
+                        <p key={index}>Height: {info.height}</p>
+                        <p key={index}>Mass: {info.mass}</p>
+                    </div>
+                    
+                )
+            })}
+          
+        </div>
+    )
+}
+export default Starwars;
