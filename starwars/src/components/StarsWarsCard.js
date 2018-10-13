a77f53cc26a06e196dab920999b6181f1808d8d3
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-13 00:05:40 -0400

    [completed] styling completed

--- starwars/src/components/StarsWarsCard.js
+++ starwars/src/components/StarsWarsCard.js
@@ -21,17 +21,17 @@ class StarWarsChars extends React.Component{
 
     render(){
         return(
-            <div>
-                <h1 onClick={() => this.helper()}>{this.props.characters.name}</h1>
+            <div className="card-container">
+                <h1 className="character-title" onClick={() => this.helper()}>{this.props.characters.name}</h1>
                 {this.state.isCollapse ? null : 
                 <div className={this.props.characters.style}>
-                    <p>Birth Year: {this.props.characters.birth_year}</p>
-                    <p>Gender: {this.props.characters.gender}</p>
-                    <p>Height: {this.props.characters.height}</p>
-                    <p>Mass: {this.props.characters.mass}</p>
-                    <p>Hair Color: {this.props.characters.hair_color}</p>
-                    <p>Eye Color:{this.props.characters.eye_color}</p>
-                    {/* <p>Flims: {props.characters.films}</p> */}
+                    <p className="char-attr"><span className="attr">Born: </span>{this.props.characters.birth_year}</p>
+                    <p className="char-attr"><span className="attr">Gender:</span> {this.props.characters.gender}</p>
+                    <p className="char-attr"><span className="attr">Height: </span>{this.props.characters.height}</p>
+                    <p className="char-attr"><span className="attr">Mass/Weight: </span>{this.props.characters.mass}</p>
+                    <p className="char-attr"><span className="attr">Hair Color: </span>{this.props.characters.hair_color}</p>
+                    <p className="char-attr"><span className="attr">Eye Color:</span>{this.props.characters.eye_color}</p>
+                    <p className="char-attr"><span className="attr">Skin Color:</span>{this.props.characters.skin_color}</p>
                 </div>
                 }
             </div>
