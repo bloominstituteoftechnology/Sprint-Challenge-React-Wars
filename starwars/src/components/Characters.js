42544c5b5e8b45bdc2ff2adf7ae4de3f480c7919
Grace <gracekang.93@gmail.com>
2018-11-09 14:55:11 -0800

    finished MVP?

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,33 +1,25 @@
-import React, {Fragment, Component} from 'react';
+import React from 'react';
 import './StarWars.css';
 import Card from './Card';
 
-class Characters extends Component {
-    constructor(props) {
-        super();
-
-    }
-
-    render() {
-        return (
-            <Fragment>
-                {this.props.charactersArray.map(x => <Card
-                    name={x.name}
-                    gender={x.gender}
-                    birthYear={x['birth_year']}
-                    eyeColor={x['eye_color']}
-                    hairColor={x['hair_color']}
-                    height={x.height}
-                    mass={x.mass}
-                    species={x.species}
-                    homeWorld={x.homeworld}
-                    starShips={x.starships}
-                    films={x.films}
-                />)}
-            </Fragment>
-        );
-    }
-
+function Characters(props) {
+    return (
+        <div className="row">
+            {props.charactersArray.map(x => <Card
+                name={x.name}
+                gender={x.gender}
+                birthYear={x['birth_year']}
+                eyeColor={x['eye_color']}
+                hairColor={x['hair_color']}
+                height={x.height}
+                mass={x.mass}
+                species={x.species}
+                homeWorld={x.homeworld}
+                starShips={x.starships}
+                films={x.films}
+            />)}
+        </div>
+    );
 }
 
 export default Characters;
