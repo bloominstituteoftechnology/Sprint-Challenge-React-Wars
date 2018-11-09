e14782d2698dbe6d3c38345d7d4825c2fd2e5c05
wcolts2000 <wcolts2000@gmail.com>
2018-11-09 10:18:25 -0800

    added key attribute to mapped character cards

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const Character = (props) => {
-  console.log("PROPS", props.charsOnProps.starwarsChars);
   return (
     <div>
       {props.charsOnProps.starwarsChars.map((char, index) => {
         return (
-          <div className="charContainer">
+          <div className="charContainer" key={index+1} >
             <h3 style={{color: "darkred", textTransform: "uppercase"}}>{char.name}</h3>
             <h5 style={{color: "#121212"}}>Born: {char.birth_year} <br />
                 Height: {char.height} <br />
