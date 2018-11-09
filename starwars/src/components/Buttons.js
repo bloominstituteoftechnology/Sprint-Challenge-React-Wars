3e631a2e81ab19dcf5b5a13346d4b52922e4aac4
cbtsao47 <cbtsao47@gmail.com>
2018-11-09 12:59:03 -0600

    add hover animation

--- starwars/src/components/Buttons.js
+++ starwars/src/components/Buttons.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Buttons = (props) =>{
     return(
         <button className = {props.className} onClick={()=> props.previous == null ? alert('beginning of page') : props.clickHandler(props.previous)}>Go Back</button>
+        
     );
 }
 export default Buttons;
