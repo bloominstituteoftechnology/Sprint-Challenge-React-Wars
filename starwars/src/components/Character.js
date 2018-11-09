cdc9fe7a47639a765cd887feed2a09d5bc0f5044
steelworx <eileencuevas62@gmail.com>
2018-11-09 13:35:18 -0500

    added styling to app

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import './character.css';
 
 const Character = props => {
     return(
-        <div>
-            <h3>{props.singleChar.name}</h3>
-            <p>YEAR OF BIRTH: {props.singleChar.birth_year}</p>
-            <p>EYE COLOR: {props.singleChar.eye_color}</p>
-            <p>HAIR COLOR: {props.singleChar.hair_color}</p>
-            <p>SKIN COLOR: {props.singleChar.skin_color}</p>
-            <p>HEIGHT: {props.singleChar.height} CM</p>
-            <p>MASS: {props.singleChar.mass} KG</p>
-            <p>GENDER: {props.singleChar.gender}</p>
-            <p>DATA CREATED: {props.singleChar.created}</p>
-            <p>LAST EDITED: {props.singleChar.edited}</p>
+        <div className='character-data'>
+            <h2>{props.singleChar.name}</h2>
+            <div className='info'>
+                <div className='character-img'>IMAGE HERE</div>
+                <div className='info-container'>
+                    <p className='category'>YEAR OF BIRTH:</p>
+                    <p>{props.singleChar.birth_year}</p>
+                    <div className='info-details'>
+                        <div className='info-detail'>
+                            <div className='individual-detail'>
+                                <p className='category'>EYE COLOR:</p>
+                                <p>{props.singleChar.eye_color}</p>
+                            </div>
+                            <div className='individual-detail'>
+                                <p className='category'>HAIR COLOR:</p>
+                                <p>{props.singleChar.hair_color}</p>
+                            </div>
+                            <div className='individual-detail'>
+                                <p className='category'>SKIN COLOR:</p>
+                                <p>{props.singleChar.skin_color}</p>
+                            </div>
+                            <div className='individual-detail'>
+                                <p className='category'>HEIGHT:</p>
+                                <p>{props.singleChar.height} cm</p>
+                            </div>
+                            <div className='individual-detail'>
+                                <p className='category'>MASS:</p>
+                                <p>{props.singleChar.mass} kg</p>
+                            </div>
+                            <div className='individual-detail'>
+                                <p className='category'>GENDER:</p>
+                                <p>{props.singleChar.gender}</p>
+                            </div>
+                        </div>
+                        <div className='info-created-edited'>
+                            <p className='category'>DATA CREATED:</p>
+                            <p>{props.singleChar.created}</p>
+                            <p className='category'>LAST EDITED:</p>
+                            <p>{props.singleChar.edited}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Character from './Character';
+import './characterCard.css';
 
 const CharacterCard = props => {
     return(
-        <div>
+        <div className='character-card'>
             {props.starwarsChars.map(currentChar => (
                 <Character singleChar={currentChar}/>
             ))}
