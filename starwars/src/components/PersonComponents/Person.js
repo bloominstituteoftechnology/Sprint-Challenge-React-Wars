5e5104433121459ffed05ba271490848211cad09
Trai Lynne <trailynne@outlook.com>
2018-11-09 11:52:11 -0700

    Added code to PersonMeasurements.js Imported and called in Person.js	Properly rendering data in P tags

--- starwars/src/components/PersonComponents/Person.js
+++ starwars/src/components/PersonComponents/Person.js
@@ -3,12 +3,17 @@ import "./Person.css";
 
 // Components
 import PersonLooks from "./PersonLooks";
+import PersonMeasurements from "./PersonMeasurements";
 
 const Person = props => {
   return (
     <div className="person">
       <h2>{props.person.name}</h2>
+      <h3>
+        {props.person.gender} - {props.person.birth_year}
+      </h3>
       <PersonLooks person={props.person} />
+      <PersonMeasurements person={props.person} />
     </div>
   );
 };
--- starwars/src/components/PersonComponents/PersonMeasurements.js
+++ starwars/src/components/PersonComponents/PersonMeasurements.js
@@ -1,2 +1,18 @@
 import React from "react";
 import "./Person.css";
+
+const PersonMeasurements = props => {
+  return (
+    <div className="person-measure">
+      <p>
+        <strong>Height:</strong> {props.person.height}
+      </p>
+
+      <p>
+        <strong>Mass:</strong> {props.person.mass}
+      </p>
+    </div>
+  );
+};
+
+export default PersonMeasurements;
