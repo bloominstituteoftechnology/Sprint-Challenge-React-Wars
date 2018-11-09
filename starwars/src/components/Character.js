c9d9cee79dd18ad208df008120774a91da41405a
BrandCam <BrandCam>
2018-11-09 11:04:08 -0600

    character cards JSX wrote

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Character(props){
-   return <h2>im a character</h2>
+   return (
+    <div className='char-card'>
+        <h2>{props.character.name}</h2>
+        <li>Gender : {props.character.gender}</li>
+        <li>Hair color : {props.character.hair_color}</li>
+        <li>Birth Year : {props.character.birth_year}</li>
+        <li>Eye color : {props.character.eye_color}</li>
+    </div>
+   )
 }
 
 export default Character
