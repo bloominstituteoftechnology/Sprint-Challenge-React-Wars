24489e7a2c8d043452233e615f081785ab26352d
BrandCam <BrandCam>
2018-11-09 10:49:07 -0600

    displaying something for every index in API array

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Character(props){
+   return <h2>im a character</h2>
+}
+
+export default Character
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Character from './Character'
+
+function CharacterList(props){
+    return (
+        <div className='char-list'>
+            {props.starwarsChars.map(
+                character => <Character
+                character = {character}
+                />
+            )}
+        </div>
+    )
+}
+
+export default CharacterList;
