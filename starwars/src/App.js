bf772447e4e673a31e8598521268db1b5e9eb9ad
wcolts2000 <wcolts2000@gmail.com>
2018-11-09 11:14:35 -0800

    added some styling before lunch on buttons for pagination, still need to make functional

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,23 +13,25 @@ class App extends Component {
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people');
   }
-
+  
   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
     fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data)
+      this.setState({ starwarsChars: data.results });
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
   };
-
+  
+  
   render() {
     return (
       <div className="App">
@@ -37,6 +39,11 @@ class App extends Component {
         <WarsContainer
           starwarsChars={this.state.starwarsChars}
         />
+        <div className="pagination-container" >
+          <button className="pagination">Previous Page</button>
+          <button className="pagination">Next Page</button>
+        </div>
+        <h6>All data recieved from <a href="https://swapi.co/api/people">StarWars API</a></h6>
       </div>
     );
   }
