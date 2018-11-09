b3fb6e16a6dc1c1a0fe9bd3a78a3ae5810e560b9
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-09 15:01:34 -0500

    "cssupdate, MVPcomplete"

--- starwars/src/components/SWCard.js
+++ starwars/src/components/SWCard.js
@@ -1,29 +1,33 @@
 import React from 'react';
-// import './SymbolsBoxs.css';
+import './SWCard.css';
 
 const SWCard = (props) => {
     return (
-    <div className="SWCard">
-            <div>
-                <div className = 'SWCard d'>
-                    <h2>{props.it.name}</h2>
+    <div className="Container">
+            <div className = 'SWCard' >
+                <div className = 'SWCard d w'>
+                    <h2 className="white">{props.it.name}</h2>
                 </div>
                 <div className='SWCard d' >
+                <h4>Gender: {props.it.gender}</h4>
+                <h4>Mass: {props.it.mass}</h4>
 
-                <h4>Birth Year: {props.it.birth_year}</h4>
-                <h4>Eye Color: {props.it.eye_color}</h4>
+                
+                
                 <h4>Height: {props.it.height}</h4>
 
                 </div>
                 <div className='SWCard d' >
-                <h4>Gender: {props.it.gender}</h4>
+                <h4>Eye Color: {props.it.eye_color}</h4>
                 <h4>Hair Color: {props.it.hair_color}</h4>
-                <h4>Mass: {props.it.mass}</h4>
+                
                 <h4>Skin Color: {props.it.skin_color}</h4>
-
-
-
                 <h4>{props.it.created}</h4>
+                <h4>Birth Year:{props.it.birth_year}</h4>
+
+
+
+                
 
                 </div >
                 <div>
