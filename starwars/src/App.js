7aa0be532bafbb97579a938c1675ae7e96ac2e4f
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-09 09:16:40 -0700

    Built up a character list to grab the information from the given URL, built up a character card to display the information. Got it rendering in my App.js .

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CharacterList from "./components/CharacterList";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      currentIndex: 0
     };
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters("https://swapi.co/api/people");
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
         return res.json();
@@ -33,6 +32,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div>
+          <CharacterList
+            chars={this.state.starwarsChars}
+            currentIndex={this.state.currentIndex}
+          />
+        </div>
       </div>
     );
   }
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./StarWars.css";
+
+const CharacterCard = props => {
+  return (
+    <div className="card" id={props.count}>
+      <h2>{props.char.name}</h2>
+      <p>
+        <strong>Birth Year:</strong> {props.char.birth_year}
+      </p>
+      <p>
+        <strong>Gender:</strong> {props.char.gender}
+      </p>
+      <p>
+        <strong>Height:</strong> {props.char.height}
+      </p>
+      <p>
+        <strong>Weight:</strong> {props.char.mass}
+      </p>
+      <p>
+        <strong>Skin Color:</strong> {props.char.skin_color}
+      </p>
+      <p>
+        <strong>Eye Color:</strong> {props.char.eye_color}
+      </p>
+      <p>
+        <strong>Hair Color:</strong> {props.char.hair_color}
+      </p>
+    </div>
+  );
+};
+
+export default CharacterCard;
