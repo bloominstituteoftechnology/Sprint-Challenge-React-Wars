17caba7524e8aa886ac22859c19b41270aac21bd
Trai Lynne <trailynne@outlook.com>
2018-11-09 11:45:05 -0700

    Added code to PersonLooks.js Imported and called in Person.js Properly rendering data in P tags Correceted spelling in PersonMeasurements.js file name

--- starwars/src/components/PersonComponents/Person.js
+++ starwars/src/components/PersonComponents/Person.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Person.css";
 
+// Components
+import PersonLooks from "./PersonLooks";
+
 const Person = props => {
   return (
     <div className="person">
       <h2>{props.person.name}</h2>
+      <PersonLooks person={props.person} />
     </div>
   );
 };
--- starwars/src/components/PersonComponents/PersonLooks.js
+++ starwars/src/components/PersonComponents/PersonLooks.js
@@ -1,2 +1,22 @@
 import React from "react";
 import "./Person.css";
+
+const PersonLooks = props => {
+  return (
+    <div className="looks">
+      <p>
+        <strong>Hair Color:</strong> {props.person.hair_color}
+      </p>
+
+      <p>
+        <strong>Skin Color:</strong> {props.person.skin_color}
+      </p>
+
+      <p>
+        <strong>Eye Color:</strong> {props.person.eye_color}
+      </p>
+    </div>
+  );
+};
+
+export default PersonLooks;
