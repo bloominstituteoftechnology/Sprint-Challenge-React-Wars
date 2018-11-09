b696e7e7dd56a986025a25c60cff6e3ff8576b90
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-09 14:07:05 -0500

    Trying to get  fetch working

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch(URL)
+    fetch("https://swapi.co/api/people/")
       .then(res => {
         return res.json();
       })
