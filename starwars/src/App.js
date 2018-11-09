a5a6ee5f503323adf4706fe5b75d93377fd7fe28
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-09 14:09:47 -0500

    Troubleshooting

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch("https://swapi.co/api/people/")
+    fetch(URL)
       .then(res => {
         return res.json();
       })
