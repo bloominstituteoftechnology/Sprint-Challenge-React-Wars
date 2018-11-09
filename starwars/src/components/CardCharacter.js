25dd9fdb9e68aa4e5ffe7414ca34fc008871c6cb
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-09 08:59:39 -0800

    mvp completed

--- starwars/src/components/CardCharacter.js
+++ starwars/src/components/CardCharacter.js
@@ -5,15 +5,15 @@ function CardCharacter(props) {
   console.log(props.character, 'in card character')
   return (
     <div className={`character`}>
-      <img src={'https://banner2.kisspng.com/20180206/vtq/kisspng-luke-skywalker-anakin-skywalker-drawing-star-wars-luke-skywalker-transparent-background-5a7a21bb24d333.5712049415179534671508.jpg'}/>
+      <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIMrXCq8FXWe7yBfw0fE3uhsU4xo3pnSIInGUXWTgXUVICytoxlA'}/>
       <h1 className={`character-name`}>{props.character.name}</h1>
       <p>birth year: {props.character.birth_year}</p>
       <p>eye color: {props.character.eye_color}</p>
-      <p>height: {props.character.height}</p>
-      <p>gender: {props.character.gender}</p>
       <p>hair color: {props.character.hair_color}</p>
-      <p>mass: {props.character.mass}</p>
       <p>skin color: {props.character.skin_color}</p>
+      <p>gender: {props.character.gender}</p>
+      <p>height: {props.character.height}</p>
+      <p>mass: {props.character.mass}</p>
 
     </div>  
   )
