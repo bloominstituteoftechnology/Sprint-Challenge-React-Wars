37ccf66ecded2ef1b9c534ee0bd373e6b66d12fc
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-12 20:06:44 -0500

    "?"

--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -2,15 +2,19 @@ import React from 'react';
 import SWCard from './SWCard';
 
 const CardList = props => {
-    return(
+        return (
 
-        <React.Fragment>
-            {props.monkeys.map((eachOneInMap, index) => (
-            <SWCard key={eachOneInMap.created} it={eachOneInMap} /> ))}
+                <
+                React.Fragment > {
+                    props.monkeys.map((eachOneInMap, index) => ( <
+                            SWCard key = { eachOneInMap.created }
+                            it = { eachOneInMap }
+                            /> ))}
 
-        </React.Fragment>
-    );
-};
+                            <
+                            /React.Fragment>
+                        );
+                    };
 
 
-export default CardList;
+                    export default CardList;
