b270250654bbef87dc3d2800fadb8c6ad0d09da2
Joseph Eastman <josephryan96@gmail.com>
2018-11-16 10:48:11 -0800

    React Wars MVP

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './StarWars.css';
+
+const StarWars = props => {
+    return (
+        <div star-wars-char>
+        <p>Name: {props.char.name}</p>
+        <p>Gender: {props.char.gender}</p>
+        <p>Skin Color: {props.char.skin_color}</p>
+        <p>Eye Color: {props.char.eye_color}</p>
+        <p>Birth Year: {props.char.birth_year}</p>
+        <p>Mass: {props.char.mass}</p>
+        <p>Height: {props.char.height}</p>
+        <p>Creation: {props.char.created}</p>
+        <p>Edited: {props.char.edited}</p>
+        </div>
+    )
+}
+
+export default StarWars;
---
+++ starwars/src/components/StarWarsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StarWars from './StarWars';
+
+const StarWarsList = props => {
+    return (
+        <div char-list>
+            {props.starwarsList.map((char) =>
+                <StarWars char={char}/>
+                )}
+        </div>
+    )
+};
+
+export default StarWarsList;
