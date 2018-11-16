a5e4cfb65a1ba1fe5c817b0f0606fe6617c2e0b1
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-16 12:11:23 -0500

    attempts

---
+++ starwars/src/CharacterList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Characters from './Characters';
+
+// const CharacterList = props => {
+//     return (
+//       <div>
+//         {props.Characters.map(characterInMap => (
+//           <Characters
+//             key={characterInMap.starwarsChars}
+//             characterOnProps={characterInMap}
+//           />
+//         ))}
+//       </div>
+//     );
+//   };
+
+// export default CharacterList;
+
---
+++ starwars/src/Characters.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Characters = props => {
+    return <h2>{props.charactersOnProps.getCharacters}</h2>;
+  };
+
+export default Characters;
