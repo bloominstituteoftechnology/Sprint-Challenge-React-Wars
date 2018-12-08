651599fa71d1d1f401e49de058a33116b4aff80b
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-12-08 13:28:51 -0500

    data rendered, styling commences

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -29,10 +30,35 @@ class App extends Component {
       });
   };
 
+  
+
   render() {
+    const starwarsChars = this.state.starwarsChars.map(el => {
+      return <article className="charCard"> 
+        <h2>{el.name}</h2>
+        <h3>Birth Year<h4>{el.birth_year}</h4></h3>
+        <h3>Created<h4>{el.created}</h4></h3>
+        <h3>Edited<h4>{el.edited}</h4></h3>
+        <h3>Eye Color<h4>{el.eye_color}</h4></h3>
+        {/* <h3>Films<ul>{[el.films]}</ul></h3> */}
+        <h3>Gender<h4>{el.gender}</h4></h3>
+        <h3>Hair Color<h4>{el.hair_color}</h4></h3>
+        <h3>Height<h4>{el.height}</h4></h3>
+        <h3>Homeworld<h4>{el.homeoworld}</h4></h3>
+        <h3>Mass<h4>{el.mass}</h4></h3>
+        <h3>Species<h4>{el.species}</h4></h3>
+        <h3>Skin Color<h4>{el.skin_color}</h4></h3>
+        {/* <h3>Starships<ul>{el.starships}</ul></h3> */}
+        <h3>Vehicles<ul>{el.vehicles}</ul></h3>
+      </article>
+
+    });
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div>
+          <div className= "charCard">{starwarsChars}</div>
+        </div>
       </div>
     );
   }
