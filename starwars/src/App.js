158d524bf7e01f495f5160795e680ce50a871dc5
Leila <nb.leila10@gmail.com>
2018-12-08 12:49:22 -0600

    Add pagination system that will allows to load the next page of data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      nextUrl: "",
+      prevUrl: ""
     };
   }
 
@@ -23,18 +25,36 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ 
+          starwarsChars: data.results ,
+          nextUrl: data.next,
+          prevUrl: data.previous
+        });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
+  getNext = () => {
+    if(this.state.nextUrl) {
+      this.getCharacters(this.state.nextUrl);
+    }
+  }
+
+  getPrev = () => {
+    if(this.state.prevUrl) {
+      this.getCharacters(this.state.prevUrl);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
         <CharList chars={this.state.starwarsChars}/>
+        <button onClick={this.getNext}>Next</button>
+        <button onClick={this.getPrev}>Previous</button>
       </div>
     );
   }
--- starwars/src/components/Char.js
+++ starwars/src/components/Char.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 const Char = (props) => {
-  const films = props.character.films;
-const filmsDisplay = films.map(film => <li>{film}</li>);
   return (
     <div className="character">
-      <div><h3>Name</h3>: {props.character.name}</div>
-      <div><h3>Gender</h3>: {props.character.gender}</div>
-      <div><h3>Hair Color</h3>: {props.character.hair_color}</div> 
-      <div><h3>Skin Color</h3>: {props.character.skin_color}</div>       
-      <div><h3>Height</h3>: {props.character.height}</div> 
-      <div><h3>Mass</h3>: {props.character.mass}</div> 
-      <div><h3>Birth year</h3>: {props.character.birth_year}</div>
-      <div><h3>Eye Color</h3>: {props.character.eye_color}</div>
+      <div className="info"><h3>Name</h3>: {props.character.name}</div>
+      <div className="info"><h3>Gender</h3>: {props.character.gender}</div>
+      <div className="info"><h3>Hair Color</h3>: {props.character.hair_color}</div> 
+      <div className="info"><h3>Skin Color</h3>: {props.character.skin_color}</div>       
+      <div className="info"><h3>Height</h3>: {props.character.height}</div> 
+      <div className="info"><h3>Mass</h3>: {props.character.mass}</div> 
+      <div className="info"><h3>Birth year</h3>: {props.character.birth_year}</div>
+      <div className="info"><h3>Eye Color</h3>: {props.character.eye_color}</div>
     </div>
   );
 };
