ba1103782d3475b1692321e034153b4c60e85ff3
Trip1701 <bj2336@email.vccs.edu>
2018-12-08 12:49:55 -0500

    Data Mapped to Card Components

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -10,9 +10,12 @@ class Card extends React.Component {
       <div className="card-container__card">
         <img src="#" alt="placeholder" />
         <div className="card__bio">
-          <p><strong>NAME:</strong> {this.props.name}</p>
-          <p>test</p>
-          <p>test</p>
+          <p><strong>NAME:</strong> {this.props.state.name}</p>
+          <p><strong>GENDER:</strong> {this.props.state.gender}</p>
+          <p><strong>YOB:</strong> {this.props.state.birth_year}</p>
+
+          <p><strong>HEIGHT:</strong> {this.props.state.height}</p>
+          <p><strong>MASS:</strong> {this.props.state.mass}</p>
         </div>
       </div>
     );
--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -8,10 +8,11 @@ class CardContainer extends React.Component {
 
   render() {
     console.log(this.props.state[0])
+    const cards = this.props.state.map(character => <Card state={character}/>)
     return(
       <div className='card-container'>
         <h2>DataBase ID Card</h2>
-        <Card />
+        {cards}
       </div>
     );
   }
