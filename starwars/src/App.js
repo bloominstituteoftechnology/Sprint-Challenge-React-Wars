f9cd5d02071e4f4b2f645e3e9d49779dd420ca8e
Andrew Yurlov <yurlovandrew@gmail.com>
2018-12-08 10:12:53 -0800

    mvp complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters('https://swapi.co/api/people');
   }
 
   getCharacters = URL => {
@@ -28,7 +28,7 @@ class App extends Component {
   };
 
   render() {
-    const { starwars } = this.state;
+    const { starwars } = this.state.starwarsChars;
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,11 +3,21 @@ import './StarWars.css';
 
 const Character = (props) => {
   const { character } = props;
-  console.log(props);
 
   return (
     <div className="card">
       <h3>{character && character.name}</h3>
+      <p>{character && character.birth_year}</p>
+      <p>{character && character.hair_color}</p>
+      <p>{character && character.eye_color}</p>
+      <p>{character && character.gender}</p>
+      <p>{character && character.mass}</p>
+      <p>{character && character.skin_color}</p>
+      <p>{character && character.created}</p>
+      <p>{character && character.edited}</p>
+      <p>{character && character.height}</p>
+      <p>{character && character.homeworld}</p>
+      <p>{character && character.url}</p>
     </div>
   );
 }
