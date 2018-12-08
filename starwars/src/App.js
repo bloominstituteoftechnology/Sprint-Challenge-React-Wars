191c1926203e03f5be858c33c320bb773b2a773e
jrizza88 <jetorres88@gmail.com>
2018-12-08 12:46:28 -0500

    imported and exported necessary files

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/characterComponent/CharacterCard';
+import './components/characterComponent/CharacterData';
 
 class App extends Component {
   constructor() {
@@ -33,6 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+
       </div>
     );
   }
