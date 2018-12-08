0543b2215224a42aedc9898c9d879eeda4bba77a
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-12-08 12:33:53 -0600

    MVP Complete, with simply styling.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,15 +3,17 @@ import './StarWars.css';
 
 const Character = (props) => {
   return (
-    <div>
-      <div>Name: {props.chars.name}</div>
-      <div>D.O.B.: {props.chars.birth_year}</div>
-      <div>Gender: {props.chars.gender}</div>
-      <div>Height: {props.chars.height}</div>
-      <div>Mass: {props.chars.mass}</div>
-      <div>Hair Color: {props.chars.hair_color}</div>
-      <div>Eye Color: {props.chars.eye_color}</div>
-      <div>Skin Color: {props.chars.skin_color}</div>
+    <div className={props.chars.name === 'Darth Vader' ? 'darth' : 'char-card'}>
+      <div className='name'>{props.chars.name}</div>
+      <div className='char-data'>
+        <div>D.O.B.: {props.chars.birth_year}</div>
+        <div>Gender: {props.chars.gender}</div>
+        <div>Height: {props.chars.height}</div>
+        <div>Mass: {props.chars.mass}</div>
+        <div>Hair Color: {props.chars.hair_color}</div>
+        <div>Eye Color: {props.chars.eye_color}</div>
+        <div>Skin Color: {props.chars.skin_color}</div>
+      </div>
     </div>
   );
 };
