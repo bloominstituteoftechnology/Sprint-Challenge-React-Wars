92b3cbcd70c7786cf9f0d428a4f37cf15b0cd621
ThomasUtsey <tomutsey@yahoo.com>
2018-12-08 14:42:57 -0500

    midlevel push

--- starwars/src/components/CharacterBuild.js
+++ starwars/src/components/CharacterBuild.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import DisplayWindow from './DisplayWindow'
 const CharacterBuild = (props) => {
   return(
-<div>
+<ul>
       {props.character.map(character => {
         
         // console.log(props.crossOut);
         return <DisplayWindow><div>{character.name}</div></DisplayWindow>
+
+        return <DisplayWindow><div>{character.birth_year}</div></DisplayWindow>
         
     })}
-    </div>
+    </ul>
   )
 
 }
