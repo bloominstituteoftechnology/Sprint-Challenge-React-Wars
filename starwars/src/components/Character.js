da7df8be4ea8ad5d7143dff28b2ee6a48efde45c
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-12-08 11:20:29 -0700

    added styles to components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import './Character.css'
 
 class Character extends React.Component {
   render() {
     return (
       <div className="character">
-        <h1>{this.props.name}</h1>
+        <h2>{this.props.name}</h2>
+        <p>Birth Year: {this.props.birthYear}</p>
+        <p>Gender: {this.props.gender}</p>
+        <p>Eye Color: {this.props.eyeColor}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+        <p>Height: {this.props.height}</p>
+        <p>Mass: {this.props.mass}</p>
+        <p>Skin Color: {this.props.skinColor}</p>
       </div>
     
     )
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -5,18 +5,19 @@ class CharacterCard extends React.Component {
   render() {
     // console.log(this.props.characters)
     return (
-      <div className="character">
-        {this.props.characters.map(character => <Character 
+      <div className="character-container">
+        {this.props.characters.map(character => 
+        <Character 
           name={character.name}  
           key={character.created}
-          birthYear={character.birthyear}
+          birthYear={character.birth_year}
           eyeColor={character.eye_color}
           gender={character.hair_color}
           hairColor={character.hair_color}
           height={character.height}
           mass={character.mass}
           skinColor={character.skin_color}
-          />)}
+        />)}
       </div>
     )
   }
