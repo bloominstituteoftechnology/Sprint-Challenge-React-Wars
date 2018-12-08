28dc3e4c46ec4960cf7e8f40c6b26c0749f201a5
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-12-08 11:41:16 -0600

    Building a Container

--- starwars/src/components/StarContainer.js
+++ starwars/src/components/StarContainer.js
@@ -1,3 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import StarContent from "./StarContent";
+import StarHeader from "./StarHeader";
 
 
+const StarContainer = () => {
+  return (
+    <div className="banner-wrapper">
+      <StarHeader />
+      <StarContent />
+    </div>
+  );
+};
+
+export default StarContainer;
