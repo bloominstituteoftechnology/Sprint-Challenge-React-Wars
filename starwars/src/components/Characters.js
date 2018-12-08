a91083f3fc1aab56eb6de321e7ed245d740f0fb1
Bao Pham <ryanvuepham@gmail.com>
2018-12-08 12:19:26 -0600

    finished Characters.js. working on .map over array

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Characters = props => {
+    return (
+        <div>
+            <h2>{}</h2>
+            <p>Birth Year:</p>
+            <p>Created:</p>
+            <p>Edited:</p>
+            <p>Eye Color:</p>
+            <p>Gender:</p>
+            <p>Hair Color:</p>
+            <p>Height:</p>
+            <p>Mass:</p>
+            <p>Skin Color:</p>
+        </div>
+    )
+}
+
+export default Characters;
