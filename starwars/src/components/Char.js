a1a330c5ed4d5beeef62bec1afdeddcce8d9732b
Leila <nb.leila10@gmail.com>
2018-12-08 13:03:00 -0600

    Change styling to show two cards in one row

--- starwars/src/components/Char.js
+++ starwars/src/components/Char.js
@@ -5,12 +5,11 @@ const Char = (props) => {
     <div className="character">
       <div className="info"><h3>Name</h3>: {props.character.name}</div>
       <div className="info"><h3>Gender</h3>: {props.character.gender}</div>
+      <div className="info"><h3>Eye Color</h3>: {props.character.eye_color}</div>
       <div className="info"><h3>Hair Color</h3>: {props.character.hair_color}</div> 
       <div className="info"><h3>Skin Color</h3>: {props.character.skin_color}</div>       
       <div className="info"><h3>Height</h3>: {props.character.height}</div> 
-      <div className="info"><h3>Mass</h3>: {props.character.mass}</div> 
       <div className="info"><h3>Birth year</h3>: {props.character.birth_year}</div>
-      <div className="info"><h3>Eye Color</h3>: {props.character.eye_color}</div>
     </div>
   );
 };
