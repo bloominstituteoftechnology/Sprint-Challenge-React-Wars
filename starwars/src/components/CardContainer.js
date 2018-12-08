e55714fe63a5d881937e71a5c45bfae3a4840f79
brettmadrid <brettmadrid9@gmail.com>
2018-12-08 10:39:32 -0800

    formatted output to 5 cards per row, using flex for responsiveness

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './StarWars.css'
 
 const CardContainer = props => {
   return (
-    <div>
+    <div className="container">
       {props.charArray.map(char => (
-        <div className="cardContainer">
-          <h3>Name: {char.name}</h3>
+        <div className="card">
+          <h3>{char.name}</h3>
           <p>Height: {char.height}</p>
           <p>Mass: {char.mass}</p>
           <p>Hair Color: {char.hair_color}</p>
@@ -13,8 +14,8 @@ const CardContainer = props => {
           <p>Eye Color: {char.eye_color}</p>
           <p>Birth Year: {char.birth_year}</p>
           <p>Gender: {char.gender}</p>
-          <p>Created: {char.created}</p>
-          <p>Last Edited: {char.edited}</p>
+          <p>Created: {char.created.slice(0,10)}</p>
+          <p>Last Edited: {char.edited.slice(0,10)}</p>
         </div>
       ))}
     </div>
