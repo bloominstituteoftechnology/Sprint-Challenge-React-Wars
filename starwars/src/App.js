111c14b8619eafad041defbaef10ced908044e31
iradsanchez <ira.d.sanchez@gmail.com>
2018-12-08 12:37:20 -0700

    MVP complete - Ira Sanchez

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,7 +33,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">
+          React Wars
+          <h2>by Ira Sanchez</h2>
+        </h1>
         <CharacterList characters={this.state.starwarsChars} />
       </div>
     );
