b712ce34c7836485e2b8c1d6e984ef56733cc882
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-12-08 14:06:07 -0600

    added button to toggle collapse.

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -3,10 +3,10 @@ import "./StarWars.css";
 
 const StarCard = props => {
   return (
-      <div className='cardContainer'>
+      <div className='starContainer'>
       {props.data.map(character => {
           return (
-           <div className='card'> 
+           <div className='starCard'> 
            <h1>{character.name}</h1>
            <p>Gender: {character.gender}</p>
            <p>Height: {character.height}cm</p>
@@ -14,11 +14,17 @@ const StarCard = props => {
            <p>Skin Color: {character.skin_color}</p>
            <p>Eye Color: {character.eye_color}</p>
            <p>Birth Year: {character.birth_year}</p>
+           <button onClick={(e) => this.handleClick(e)}>
+        Click me
+      </button>
            </div>
           )
       })}
+      
       </div>
   )
 }
+
+
 
 export default StarCard;
