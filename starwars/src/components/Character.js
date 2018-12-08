a8749865984eac2f1d298d0f1615bfbec7cde247
James Wigdor <jpwigdor@gmail.com>
2018-12-08 13:26:39 -0500

    Make Each Character Have Their Own Card
    i never imported the .css files! that solved it.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './StarWars.css';
 
 const Character = (props) => {
   return (
@@ -12,6 +13,5 @@ const Character = (props) => {
     </div>
   )
 }
-
 
 export default Character;
