275590892229548552518c77e35c669d889736e5
Jason Cook <the.root.user@gmail.com>
2018-12-08 13:11:00 -0500

    Add basic physical distinction of items

--- starwars/src/components/AvatarStats.js
+++ starwars/src/components/AvatarStats.js
@@ -7,16 +7,18 @@ const AvatarStats = props => {
       {props.data.map(item => {
         return (
           <div className="avatarStatBlock">
-            <p>Name: {item.name}</p>
-            <p>Birth year: {item.birth_year}</p>
-            <p>Gender: {item.gender}</p>
-            <p>Eye color: {item.eye_color}</p>
-            <p>Hair color: {item.hair_color}</p>
-            <p>Skin color: {item.skin_color}</p>
-            <p>Height: {item.height}</p>
-            <p>Mass: {item.mass}</p>
-            <p>Created: {item.created}</p>
-            <p>Edited: {item.edited}</p>
+            <p className="renderData">Name: {item.name}</p>
+            <hr />
+            <p className="renderData">Birth year: {item.birth_year}</p>
+            <p className="renderData">Gender: {item.gender}</p>
+            <p className="renderData">Eye color: {item.eye_color}</p>
+            <p className="renderData">Hair color: {item.hair_color}</p>
+            <p className="renderData">Skin color: {item.skin_color}</p>
+            <p className="renderData">Height: {item.height}</p>
+            <p className="renderData">Mass: {item.mass}</p>
+            <hr />
+            <p className="renderData">Created: {item.created}</p>
+            <p className="renderData">Edited: {item.edited}</p>
           </div>
         );
       })}
