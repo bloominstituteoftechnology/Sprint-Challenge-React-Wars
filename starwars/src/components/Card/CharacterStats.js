136d290683bd4cba5441df832a27111cd5a75f6e
Jonathan Dorety <kyullog@gmail.com>
2018-12-08 14:23:40 -0500

    Complete MVP and styling

--- starwars/src/components/Card/CharacterStats.js
+++ starwars/src/components/Card/CharacterStats.js
@@ -4,6 +4,7 @@ import "../StarWars.css";
 const CharacterStats = props => {
   return (
     <div className="character-stats">
+      <span>Gender: {props.info.gender}</span>
       <span>Birth Year: {props.info.birth_year}</span>
       <span>Height: {props.info.height}</span>
       <span>Mass: {props.info.mass}</span>
