5dc92f410ca1c4a3140d8a5112ebc21c1137f07b
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-12-08 11:44:54 -0700

    added more styles to the hover effects for the character cards.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,14 +5,19 @@ class Character extends React.Component {
   render() {
     return (
       <div className="character">
+        <div className="character-line-top"></div>
         <h2>{this.props.name}</h2>
-        <p>Birth Year: {this.props.birthYear}</p>
-        <p>Gender: {this.props.gender}</p>
-        <p>Eye Color: {this.props.eyeColor}</p>
-        <p>Hair Color: {this.props.hairColor}</p>
-        <p>Height: {this.props.height}</p>
-        <p>Mass: {this.props.mass}</p>
-        <p>Skin Color: {this.props.skinColor}</p>
+        <div className="character-line-bottom"></div>
+        <div className="character-facts">
+          <p>Birth Year: {this.props.birthYear}</p>
+          <p>Gender: {this.props.gender}</p>
+          <p>Eye Color: {this.props.eyeColor}</p>
+          <p>Hair Color: {this.props.hairColor}</p>
+          <p>Height: {this.props.height}</p>
+          <p>Mass: {this.props.mass}</p>
+          <p>Skin Color: {this.props.skinColor}</p>
+        </div>
+        
       </div>
     )
   }
