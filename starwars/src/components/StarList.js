6c1f268aea7dc49ba43611c300b3a871be2405ae
Andrew Yurlov <yurlovandrew@gmail.com>
2018-12-08 10:49:44 -0800

    refractored code

--- starwars/src/components/StarList.js
+++ starwars/src/components/StarList.js
@@ -3,10 +3,11 @@ import Character from './Character';
 import './StarWars.css';
 
 const StarList = (props) => {
+  const { starwars } = props;
 
   return (
     <div>
-      {props.starwars.map(character =>
+      {starwars.map(character =>
         <Character
           character={character}
           key={character.created}
