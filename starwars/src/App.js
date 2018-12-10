39c2cb8445e1278d8340add0c497dbc582a19745
dickwinters <coding1965@gmail.com>
2018-12-09 18:31:47 -0600

    finished pagination and simple styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {SingleCharacter} from './components/charsIter.js';
 import {SingleCharacterAttr} from './components/charsIter.js';
+import {PageButton} from './components/pageButton.js';
 
 class App extends Component {
   constructor() {
@@ -9,12 +10,14 @@ class App extends Component {
     this.state = {
       starwarsChars: [],
       currentItem: '',
+      dataNext: undefined,
+      dataPrev: undefined,
     };
   }
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
+  componentDidMount = () => {
+    this.getCharacters('https://swapi.co/api/people/?=page2&page=1');
+  };
 
   getCharacters = URL => {
     // feel free to research what this code is doing.
@@ -26,10 +29,21 @@ class App extends Component {
       })
       .then(data => {
         let thedata = data;
+        console.log(thedata);
         // this.setState({starwarsChars: data.results});
         this.setState(prevState => {
           const data = thedata.results;
           return {starwarsChars: data};
+        });
+
+        this.setState(prevState => {
+          const dNext = thedata.next;
+          return {dataNext: dNext};
+        });
+
+        this.setState(prevState => {
+          const dPrev = thedata.previous;
+          return {dataPrev: dPrev};
         });
       })
       .catch(err => {
@@ -42,6 +56,15 @@ class App extends Component {
     this.setState(prevState => {
       return {currentItem: target};
     });
+  };
+
+  buttonClick = event => {
+    if (event.target.innerText === 'Previous') {
+      return this.getCharacters(this.state.dataPrev);
+    }
+    if (event.target.innerText === 'Next') {
+      return this.getCharacters(this.state.dataNext);
+    }
   };
 
   render() {
@@ -58,6 +81,7 @@ class App extends Component {
           state={this.state.starwarsChars}
           name={this.state.currentItem}
         />
+        <PageButton button={this.buttonClick} />
       </div>
     );
   }
