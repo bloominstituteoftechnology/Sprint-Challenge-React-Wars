a1fb233c85a83a2acdc5026c9c59746029c7481c
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-12-15 14:01:08 -0500

    Add styling for character cards

--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Character from './Character';
 
 class CardList extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.data.starwarsChars.map(e => <div>{e.name}</div>)}
+      <div className="characterCard">
+        {this.props.data.starwarsChars.map(e => <Character name={e.name} gender={e.gender} />)}
       </div>
     );
   }
