051a4f808fc02d0606b344cb10f2f0978f3cbab3
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-12-15 11:04:16 -0800

    working on polishing project

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CreateWantedPosters from './components/wanted';
 
 class App extends Component {
   constructor() {
@@ -32,7 +33,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Bounty Hunter Board</h1>
+        <CreateWantedPosters
+          infoNeeded = {this.state.starwarsChars}
+        />
       </div>
     );
   }
---
+++ starwars/src/components/wanted.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Poster from './poster';
+
+import './StarWars.css';
+
+/*received as props:
+    infoNeeded = {this.state.starwarsChars}
+*/
+
+
+  function CreateWantedPosters(props) {  
+      console.log(props);
+        return(
+            <div className="wantedCard">
+                
+                {props.infoNeeded.map(obj => {
+                    return <Poster  
+                        id={obj.created}
+                        name={obj.name}
+                        skin={obj.skin_color}
+                        gender={obj.gender}
+                        hair={obj.hair_color}
+                        eyes={obj.eye_color}  
+                        height={obj.height}
+                        weight={obj.mass}
+                        born={obj.birth_year}          
+                    />
+                })}
+            </div>
+        )
+    };
+
+export default CreateWantedPosters;
