7afa270ff59a2b5b904e06bcb6ec331dfd642f86
Ken Barlow <kenbrlow@gmail.com>
2018-12-17 21:31:41 -0800

    Quickly finished displaying and styling some data for react wars.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,7 +4,11 @@ import './Character.css';
 const Character = props => {
     return (
         <div className="MainChar">
-            <h1>{props.characterData.name}</h1>
+            <h1 className="MainName">{props.characterData.name}</h1>
+            <p className="MainStat">Birth Year: {props.characterData.birth_year}</p>
+            <p className="MainStat">Eye Color:{props.characterData.eye_color}</p>
+            <p className="MainStat">Gender:{props.characterData.gender}</p>
+            <p className="MainStat">Mass:{props.characterData.mass}</p>
         </div>
     );
 };
