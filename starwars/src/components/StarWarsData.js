a9676eaefe02ab43cbed4af1589310cc138e8cc7
amadison17 <angelmadison2015@gmail.com>
2019-01-03 21:43:38 -0500

    updated version of data

--- starwars/src/components/StarWarsData.js
+++ starwars/src/components/StarWarsData.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './StarWars.css';
 
-const Makers = props => {
+const Chars = props => {
     return(
-        <div className="makers-container">
-            <h1 class="makers-name">Name:{props.Makers.name}</h1>
-            <p class="makers-birth_year">Birth year:{props.Makers.birth_year}</p>
-            <p class="makers-height">Height:{props.Makers.height}</p>
-            <p class="makers-gender">Gender:{props.Makers.gender}</p>
-            <p class="makers-eye_color">Eye color:{props.Makers.eye_color}</p>
+        <div className="chars-container">
+            <h1 className="chars-name">Name:{props.Chars.name}</h1>
+            <p className="chars-birth_year">Birth year:{props.Chars.birth_year}</p>
+            <p className="chars-height">Height:{props.Chars.height}</p>
+            <p className="chars-gender">Gender:{props.Chars.gender}</p>
+            <p className="chars-eye_color">Eye color:{props.Chars.eye_color}</p>
         </div>
     )
 }
 
-export default Makers;
+export default Chars;
