0b5721ad1ea11734814e7430f33103cf090dc516
amadison17 <angelmadison2015@gmail.com>
2019-01-06 16:46:45 -0500

    updated lines 7 to 11

--- starwars/src/components/StarWarsData.js
+++ starwars/src/components/StarWarsData.js
@@ -4,11 +4,11 @@ import './StarWars.css';
 const Chars = props => {
     return(
         <div className="chars-container">
-            <h1 className="chars-name">Name:{props.Chars.name}</h1>
-            <p className="chars-birth_year">Birth year:{props.Chars.birth_year}</p>
-            <p className="chars-height">Height:{props.Chars.height}</p>
-            <p className="chars-gender">Gender:{props.Chars.gender}</p>
-            <p className="chars-eye_color">Eye color:{props.Chars.eye_color}</p>
+            <h1 className="chars-name">Name:{props.chars.name}</h1>
+            <p className="chars-birth_year">Birth year:{props.chars.birth_year}</p>
+            <p className="chars-height">Height:{props.chars.height}</p>
+            <p className="chars-gender">Gender:{props.chars.gender}<p>
+            <p className="chars-eye_color">Eye color:{props.chars.eye_color}</p>
         </div>
     )
 }
