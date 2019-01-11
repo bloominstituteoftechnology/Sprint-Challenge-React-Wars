229dfe4c88f833ce2b70d60e00878ac6ce2f896d
Nolan Allen <nolan.r.allen@gmail.com>
2019-01-11 12:38:25 -0600

    Initial commit; reached MVP  functionality, need to finish questions since I forgot about them, need to work on more CSS afterward

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import StarWarsList from './components/StarWarsList'
+
 class App extends Component {
   constructor() {
     super();
@@ -33,6 +35,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <StarWarsList
+          chars={this.state.starwarsChars}
+        />
       </div>
     );
   }
