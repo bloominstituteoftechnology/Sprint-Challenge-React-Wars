a079d763962f6c786a187c0cbd7952708a9e8469
Jeff15113 <itsmejeffery@gmail.com>
2019-01-11 15:06:46 -0500

    added starwars info component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import StarWars from './components/StarWars'
 
 class App extends Component {
   constructor() {
@@ -29,10 +30,13 @@ class App extends Component {
       });
   };
 
+
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <StarWars characterList={this.state.starwarsChars} />
       </div>
     );
   }
