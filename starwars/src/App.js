24c530fe1fe5bbfd2df523a2667c415a1f17bc73
rosilioroman <rosilioroman@gmail.com>
2019-01-12 12:55:58 -0700

    Added readability to Answers.md. Started work on App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,6 +33,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <h2>Star Wars Characters</h2>
+        <ul className="CharacterList">
+
+        </ul>
       </div>
     );
   }
