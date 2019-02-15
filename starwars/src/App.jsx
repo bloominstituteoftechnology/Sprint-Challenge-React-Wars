464a3a89a1a5e8b809292d03cd3e6a735ca2cd83
jmendez44 <jenuel.mendez@gmail.com>
2019-02-15 12:38:42 -0500

    created a character component for charact list

--- starwars/src/App.jsx
+++ starwars/src/App.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+const Character = props => {
+  const {birth_year, created, edited, eye_color, films, gender, hair_color, height, homeworld, mass, name, skin_color, species, starships, url, vehicle} = props.charProp;
+  return (
+    <div>
+      <h4>Character: {name} </h4>
+      <ul>
+        <li>Birth Year: {birth_year}</li>
+      </ul>
+    </div>
+  )
+}
+
+
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       starwarsChars: []
+      
     };
   }
+
+
 
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people');
@@ -33,7 +51,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+
+
+        {this.state.starwarsChars.map((i) => <Character charProp={i} />
+        )}
       </div>
+
     );
   }
 }
