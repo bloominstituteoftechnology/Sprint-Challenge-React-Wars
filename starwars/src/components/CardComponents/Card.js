bfdd98e0f890a47463cf5411596ff2296daa38a8
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-16 11:58:12 -0800

    descturctured props

--- starwars/src/components/CardComponents/Card.js
+++ starwars/src/components/CardComponents/Card.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import './cardContainer.css'
 
-const Card = props => {
+const Card = ({char: {name, height, mass, eye_color, hair_color, skin_color, birth_year, created, edited}}) => {
     return (
          <div className="cardList">
          <h1>Character Card:</h1>
-           <p><strong>Name:</strong> {props.swItem.name}</p> 
-           <p><strong> Height:</strong> {props.swItem.height}</p>
-           <p><strong> Mass:</strong> {props.swItem.mass}</p>
-           <p><strong> Eye Color:</strong> {props.swItem.eye_color}</p>
-           <p><strong> Hair Color:</strong> {props.swItem.hair_color}</p>
-           <p><strong> Skin Color:</strong> {props.swItem.skin_color}</p>
-           <p><strong> Birth Year:</strong> {props.swItem.birth_year}</p>
-           <p><strong> Created:</strong> {props.swItem.created}</p>
-           <p><strong> Edited:</strong> {props.swItem.edited}</p>
-          
-       
+           <p><strong>Name:</strong> {name}</p> 
+           <p><strong> Height:</strong> {height}</p>
+           <p><strong> Mass:</strong> {mass}</p>
+           <p><strong> Eye Color:</strong> {eye_color}</p>
+           <p><strong> Hair Color:</strong> {hair_color}</p>
+           <p><strong> Skin Color:</strong> {skin_color}</p>
+           <p><strong> Birth Year:</strong> {birth_year}</p>
+           <p><strong> Created:</strong> {created}</p>
+           <p><strong> Edited:</strong> {edited}</p>
         </div>
     )
 }
