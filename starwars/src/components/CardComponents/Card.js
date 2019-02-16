b3c7470cd8a21c35636bc8818b8775c303693a5c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-16 11:41:36 -0800

    removed gender

--- starwars/src/components/CardComponents/Card.js
+++ starwars/src/components/CardComponents/Card.js
@@ -5,8 +5,7 @@ const Card = props => {
     return (
          <div className="cardList">
          <h1>Character Card:</h1>
-           <p><strong>Name:</strong> {props.swItem.name}</p>
-           <p><strong> Gender:</strong> {props.swItem.gender}</p>
+           <p><strong>Name:</strong> {props.swItem.name}</p> 
            <p><strong> Height:</strong> {props.swItem.height}</p>
            <p><strong> Mass:</strong> {props.swItem.mass}</p>
            <p><strong> Eye Color:</strong> {props.swItem.eye_color}</p>
