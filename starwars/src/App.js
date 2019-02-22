515f71dd47baf4df118abb270376eec9f5f72121
Dustin Myers <dustinmyers@users.noreply.github.com>
2019-02-21 17:40:22 -0700

    Update App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters('https://swapi.co/api/people/');
   }
 
   getCharacters = URL => {
