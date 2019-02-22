d0074706dc374b88e900fd2aff17ff895ae595c9
jkaddu <jkaddu@yahoo.com>
2019-02-22 08:53:23 -0800

    component created

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -2,5 +2,18 @@ import React from 'react';
 import '../StarWars.css';
 
 const Wars = props => {
-    
+    return (
+        <div className='container'>
+            {props.characters.map(person => {
+                return (
+                    <div className='info'>
+                        <h2>{person.name}</h2>
+                        <p>{person.birth_year}</p>
+                        <p>{person.gender}</p>
+                        <p>{person.height}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
