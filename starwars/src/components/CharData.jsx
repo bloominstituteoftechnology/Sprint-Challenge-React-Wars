59af92328aa388749463397e46010e5d6a106550
Nathan <s6nculve@gmail.com>
2019-03-08 20:52:27 -0800

    Added pictures to CharData.jsx. Running into error. Troubleshooting.

--- starwars/src/components/CharData.jsx
+++ starwars/src/components/CharData.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './StarWars.css';
+import beru from './imgs/Beru.jpg';
+import biggs from './imgs/Biggs.jpg';
+import c3po from './imgs/C3PO.jpeg';
+import leia from './imgs/Leia.jpg';
+import luke from './imgs/Luke.jpg';
+import obiwan from './imgs/Obiwan.jpg';
+import owen from './imgs/Owen.jpg';
+import r2d2 from './imgs/R2D2.jpg';
+import r5d4 from './imgs/R5D4.jpeg';
+import vader from './imgs/Vader.jpeg';
+
 
 const Character = (props) => {
+    let pics = [
+      beru,
+      biggs,
+      c3po,
+      leia,
+      luke,
+      obiwan,
+      owen,
+      r2d2,
+      r5d4,
+      vader
+    ];
+
     return (
         <div className="character">
             <h2>{props.character.name}</h2>
+            <img src={pics[props.id]} />
             <ul>
                 <li>Gender: {props.character.gender}</li>
                 <li>Height: {props.character.height}</li>
