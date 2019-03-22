d370358d3cf5cdde7897f407a794146894a6e9eb
Sean Doyle <sdoylelambda@gmail.com>
2019-03-22 11:53:09 -0400

    StarwarsCharacters added.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import StarwarsCharacters from './components/StarwarsCharacters'
 
 class App extends Component {
   constructor() {
@@ -32,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <StarwarsCharacters />
       </div>
     );
   }
---
+++ starwars/src/components/StarwarsCharacters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StarwarsCharacters() {
+    return (
+        <div>
+            <h1>React Wars</h1>
+        </div>
+    );
+}
+
+export default StarwarsCharacters;
