5883cae8a7d734ef6853cc2f16e2f6775388cbe2
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-03-22 10:34:51 -0500

    Getting close to right setup

---
+++ starwars/src/CharInfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const CharInfo = props => {
+    return (
+        <div className="CharacterWrapper">
+            <h1 className="MainName">{props.characterData.name}</h1>
+            <p className="Charstats">Birth Year: {props.characterData.birth_year}</p>
+            <p className="CharStats">Eye Color:{props.characterData.eye_color}</p>
+            <p className="CharStats">Gender:{props.characterData.gender}</p>
+            <p className="CharStats">Mass:{props.characterData.mass}</p>
+        </div>
+    );
+};
+
+export default CharInfo;
