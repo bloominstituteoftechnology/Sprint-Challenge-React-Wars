2c3586d95d23ce0e6d3fe2f67cda0949bce44beb
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-26 10:31:35 -0700

    functional, except images throwing error

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -6,6 +6,8 @@ export default function Character(props) {
   return (
     <div className="character box">
       <h3>Name: {props.character.name}</h3>
+      <p>Born in: {props.character.birth_year}</p>
+      <img className="image" src={props.character.homeworld} alt="character Homeworld" />
     </div>
   );
 }
--- starwars/src/components/StarwarsCharList.js
+++ starwars/src/components/StarwarsCharList.js
@@ -4,8 +4,8 @@ import Character from "./Character";
 export default function StarwarsCharList(props) {
   return (
     <div>
-      {props.characters.map((character, index) => (
-        <Character key={index} character={character} />
+      {props.characters.map((character, birth_year, homeworld, index) => (
+        <Character key={index} character={character} birth_year={birth_year} homeworld={homeworld} />
       ))}
     </div>
   );
