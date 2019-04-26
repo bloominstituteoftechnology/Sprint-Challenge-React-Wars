547eb3dcbd31fc8391deb9ac49754c7b0ab02b87
greenhornsr <srollins11279@gmail.com>
2019-04-26 09:39:33 -0700

    retrieved attributes from swchars array of objects, added styling to each list element for presentation

--- starwars/src/components/StarWarChars.js
+++ starwars/src/components/StarWarChars.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import './StarWars.css'
+
 
 const StarWarChars = (props) => {
     return (
-        <ol>
-            {props.starwarsChars.map(char => <li>{char.name}</li>) }
-        </ol>
+        <section className="sw-list-container">
+            {props.starwarsChars.map((char, index) => 
+            <ul className="ul-names">
+                <li key={index} className="char-names">{char.name}</li>
+                <ul>
+                    <li className="char-att"><strong>Height: </strong>{char.height}</li>
+                    <li className="char-att"><strong>Mass: </strong>{char.mass}</li>
+                    <li className="char-att"><strong>Hair Color: </strong>{char.hair_color}</li>
+                    <li className="char-att"><strong>Skin Color: </strong>{char.skin_color}</li>
+                    <li className="char-att"><strong>Eye Color: </strong>{char.eye_color}</li>
+                    <li className="char-att"><strong>Birth Year: </strong>{char.birth_year}</li>
+                    <li className="char-att"><strong>Gender: </strong>{char.gender}</li>
+                </ul>
+            </ul>) }
+        </section>
     )
 }
 
