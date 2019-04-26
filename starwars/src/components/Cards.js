ff554df3f033eb2306bdfe67f7fecd41d2fa9f0e
Manju KC <Manju KC>
2019-04-26 14:56:24 -0400

    fixed some error

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -2,20 +2,20 @@ import React from "react"
 import './StarWars.css';
 
 
-const Cards = props => {
+const Cards = ({ data }) => {
     return (
         <div className="cards">
-            <h2 className="title">{props.data.name}</h2>
-            <p className="listItem"><strong>Gender</strong>:{props.data.gender}</p>
-            <p className="listItem"><strong>Hair Color</strong>:{props.data.hair_color}</p>
-            <p className="listItem"><strong>Eye Color</strong>:{props.data.eye_color}</p>
-            <p className="listItem"><strong>Height</strong>:{props.data.height}</p>
-            <p className="listItem"><strong>Skin Color</strong>:{props.data.skin_color}</p>
-            <p className="listItem"><strong>Date of Birth</strong>:{props.data.birth_year}</p>
-            <p className="listItem"><strong>Species</strong>:{props.data.species}</p>
-            <p className="listItem"><strong>Edited</strong>:{props.data.edited}</p>
-            <p className="listItem"><strong>Created</strong>:{props.data.created}</p>
-            <p className="listItem"><strong>URL</strong>:{props.data.url}</p>
+            <h2 className="title">{data.name}</h2>
+            <p className="listItem"><strong>Gender</strong>:{data.gender}</p>
+            <p className="listItem"><strong>Hair Color</strong>:{data.hair_color}</p>
+            <p className="listItem"><strong>Eye Color</strong>:{data.eye_color}</p>
+            <p className="listItem"><strong>Height</strong>:{data.height}</p>
+            <p className="listItem"><strong>Skin Color</strong>:{data.skin_color}</p>
+            <p className="listItem"><strong>Date of Birth</strong>:{data.birth_year}</p>
+            <p className="listItem"><strong>Species</strong>:{data.species}</p>
+            <p className="listItem"><strong>Edited</strong>:{data.edited}</p>
+            <p className="listItem"><strong>Created</strong>:{data.created}</p>
+            <p className="listItem"><strong>URL</strong>:{data.url}</p>
 
         </div>
     )
