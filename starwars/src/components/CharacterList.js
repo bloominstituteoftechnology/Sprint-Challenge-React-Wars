326d09f9a80c7167cdeb9d0d482fb889292471f5
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-26 14:23:02 -0700

    Update CharacterList.js

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -18,7 +18,7 @@ Example:
 
 This method translates to:
 
-itemsofPropsListFromParent.mapEvaluates(eachItemofPropsListFromParent [passes the data into =>] <theChildComponent key={unique.id} newObjectLiteral={willBeEachItemFromMap}) />
+itemsofPropsListFromParent.mapEvaluates(eachItemofPropsListFromParent [passes the data into =>] <theChildComponent key={unique.id} newItems={willBeEachItemFromMap}) />
 
 Summarization:
 
