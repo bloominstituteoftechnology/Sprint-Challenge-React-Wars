58c23364e9cd3d9c733d19c69161382c81ca738d
greenhornsr <srollins11279@gmail.com>
2019-04-26 14:29:27 -0700

    cleaned up swmap file

--- starwars/src/components/Method1/SWChar.js
+++ starwars/src/components/Method1/SWChar.js
@@ -6,15 +6,15 @@ const SWChar = (props) => {
     return (
         <section className="sw-list-container2">
             <ul className="ul-names2">
-                <li className="char-names2">{props.name}</li>
+                <li className="char-names2">{props.SWChar.name}</li>
                 <ul>
-                    <li className="char-att2"><strong>Height: </strong>{props.height}</li>
-                    <li className="char-att2"><strong>Mass: </strong>{props.mass}</li>
-                    <li className="char-att2"><strong>Hair Color: </strong>{props.haircolor}</li>
-                    <li className="char-att2"><strong>Skin Color: </strong>{props.skincolor}</li>
-                    <li className="char-att2"><strong>Eye Color: </strong>{props.eyecolor}</li>
-                    <li className="char-att2"><strong>Birth Year: </strong>{props.birthyear}</li>
-                    <li className="char-att2"><strong>Gender: </strong>{props.gender}</li>
+                    <li className="char-att2"><strong>Height: </strong>{props.SWChar.height}</li>
+                    <li className="char-att2"><strong>Mass: </strong>{props.SWChar.mass}</li>
+                    <li className="char-att2"><strong>Hair Color: </strong>{props.SWChar.hair_color}</li>
+                    <li className="char-att2"><strong>Skin Color: </strong>{props.SWChar.skin_color}</li>
+                    <li className="char-att2"><strong>Eye Color: </strong>{props.SWChar.eye_color}</li>
+                    <li className="char-att2"><strong>Birth Year: </strong>{props.SWChar.birth_year}</li>
+                    <li className="char-att2"><strong>Gender: </strong>{props.SWChar.gender}</li>
                 </ul>
             </ul>
         </section>
--- starwars/src/components/Method1/SWMap.js
+++ starwars/src/components/Method1/SWMap.js
@@ -4,20 +4,11 @@ import '../StarWars2.css'
 
 
 const SWMap = (props) => {
-    console.log(props)
+    // console.log(props)
     return (
         <section className="m2container2">
             {props.starwarsChars.map((char) => (
-                <SWChar 
-                name={char.name} 
-                height={char.height}
-                mass={char.mass}
-                haircolor={char.hair_color}
-                skincolor={char.skin_color}
-                eyecolor={char.eye_color}
-                birthyear={char.birth_year}
-                gender={char.gender}
-                />
+                <SWChar SWChar={char}/>
             ))}
         </section>
     )
