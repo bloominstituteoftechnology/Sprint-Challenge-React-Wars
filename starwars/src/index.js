56079775346ec3084816f6cb55165d6efcb1f979
Moses Samuel <mosamuel98@gmail.com>
2019-04-26 11:10:57 -0700

    provide error handling: wrap entire app in try/catch

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,80 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import App from './App';
+import Whoops from './components/Whoops';
+
+
+
+// Create a client engine instance
+const engine = new Styletron();
+
+try {
+    // Provide the engine to the app
+    render(
+    <StyletronProvider value={engine}>
+      <App />
+    </StyletronProvider>
+  );
+
+  console.warn('countdown till explosion: 5 seconds', )
+  blowUpAppAfter(5000) // this app will crash in 5 seconds
+  
+} catch(e) {
+    try {
+        console.error(e)
+        render(<App />, document.getElementById('backup'));
+    }  catch(e) {
+        console.error(e)
+        render(<Whoops status={501} />, document.getElementById('fireSquad'))
+    }
+}
+
+
+function blowUpAppAfter(ms) {
+    return setTimeout(tick, ms)
+}
+
+function tick(countdown) {
+    var counter = countdown;
+    var nuke = setInterval(function(){
+        console.log(counter);
+        counter--
+
+        switch(counter) {
+            case 3000:
+            case 3: 
+            console.warn('3')
+
+            case 2000:
+            case 2: 
+            console.warn('1')
+
+            case 1000:
+            case 1: 
+            console.warn('1')
+
+            default: 
+                console.log('eminent death approaching...')
+        }
+        
+        if (counter === 0) {
+            console.error("Welp, this is the end. Goodbye.");
+            clearInterval(nuke);
+
+            console.log('*sees the light*')
+
+            throw new Error('blowing up your app...')
+        }
+    }, 1000);   
+}
+
+
+
+
+
+
