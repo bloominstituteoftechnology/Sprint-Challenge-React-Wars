5ce176765f460c8b9fdc54afe440404ebe94114e
Lambda_School_Loaner_96 <lambda_school_loaner_96@Lambda-School-Loaner-96s-MacBook-Pro.local>
2019-05-10 13:03:37 -0400

    App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      starwarsChars:CharsList
       starwarsChars: [
         {
           birth_year: "placeholder"
@@ -191,6 +192,38 @@ class App extends Component {
     };
   }
 
+  addStarwarsChars = e =>{
+    e.preventDefault();
+    this.setState({
+      starwarsChars: [
+        ...this.state. starwarsChars,
+        {
+          birth_year: this.state.birth_year,
+          created:this.state.created,
+          edited: this.state.edited,
+          eye_color: this.state.eye_color,
+          films:this.state.films,
+          gender: this.state.gender,
+          hair_color:this.state.hair_color,
+          height:this.state.height,
+          homeworld: this.state.homeworld,
+          mass:this.state.mass,
+          name: this.state.name,
+          skin_color: this.state.skin_color,
+          species: this.state.species,
+          starships: this.state.starships,
+          url: this.state.url,
+          vehicles: this.state.vehicles,
+        }
+      ]
+    });
+  }
+
+  changeHandler = e => {
+    console.log(e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people/');
   }
@@ -215,6 +248,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {this.state.starwarsChars.map(CharsList =>(
+          <CharsList charsList={CharsList}/>
+        ))}
       </div>
     );
   }
