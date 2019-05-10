4fea21d922469b4f6a7321dd5f36243d82b59e70
Cory Hedeen <hedeencory@gmail.com>
2019-05-10 16:48:03 -0400

    Flex-box struggles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Eye Color</h1>
       <div className="names">
         <div>{this.state.starwarsChars.map(char => <EyeColor eyes={char} />)}</div>
       </div>
