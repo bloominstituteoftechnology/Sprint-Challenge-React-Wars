b5fba9591117de8356ed9b22718cd05be650743d
Lambda_School_Loaner_96 <lambda_school_loaner_96@Lambda-School-Loaner-96s-MacBook-Pro.local>
2019-05-10 12:23:45 -0400

    StarwarsChars

--- starwars/src/components/StarwarsChars.js
+++ starwars/src/components/StarwarsChars.js
@@ -2,10 +2,32 @@ import React from "react";
 
 const StarwarsChars = props => {
     return (
-        <StarwarsChars: Array[10]/>
-        <input onSubmit={props.addChar}/>
+        <input StarwarsChars={props.addChar}
+        type="text"
+         value={props.name}
+        placeholder="name"
+        name="name"
+        birth_year= "placeholder"
+        created="placeholder"
+        edited="placeholder"
+        eye_color="placeholder"
+        films="placeholder"
+        gender="placeholder"
+        hair_color="placeholder"
+        height="placeholder"
+        homeworld="placeholder"
+        mass="placeholder"
+        name="placeholder"
+        skin_color="placeholder"
+        species="placeholder"
+        starships="placeholder"
+        url="placeholder"
+        vehicles="placeholder"
+        />
 
     );
 }
 
 export default StarwarsChars;
+
+
