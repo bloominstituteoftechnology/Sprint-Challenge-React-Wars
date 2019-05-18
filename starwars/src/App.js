9325fc542f0bffe770a3a09fd9f82a381e2c0508
shian simms <ssimmswork@gmail.com>
2019-05-17 22:47:23 -0400

    commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,40 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
+    constructor() {
+        super();
+        this.state = {
+            starwarsChars: [];
+        };
+    }
+
+    componentDidMount() {
+        this.getCharacters('https://swapi.co/api/people/');
+    }
+
+    getCharacters = URL => {
+        // feel free to research what this code is doing.
+        // At a high level we are calling an API to fetch some starwars data from the open web.
+        // We then take that data and resolve it our state.
+        fetch(URL)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ starwarsChars: data.results });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
     };
-  }
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
-
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            h1 className = "Header" > React Wars < /h1> < /
+            div >
+        );
+    }
 }
 
 export default App;
