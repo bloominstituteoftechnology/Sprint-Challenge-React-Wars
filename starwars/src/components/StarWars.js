696bc7670142bd9c5a0ccfb85a2110a8762f69ba
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-05-19 16:04:44 -0400

    more styling

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -8,9 +8,9 @@ function StarWars(props){
       <div className="character">
               <h1>{props.character.name}</h1>
               <ul>
-              <li><strong>Mass:</strong> {props.character.mass}</li>
-              <li><strong>Gender:</strong>{props.character.gender}</li>
-              <li><strong>Skin Color:</strong>{props.character.skin_color}</li>
+              <li><strong>Mass: </strong> {props.character.mass}</li>
+              <li><strong>Gender: </strong>{props.character.gender}</li>
+              <li><strong>Skin Color: </strong>{props.character.skin_color}</li>
               </ul>       
       </div> 
     );
