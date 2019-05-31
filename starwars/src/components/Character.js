0ab4ecfa37c72678141d1cddd3c1b57aecae28c2
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-05-31 12:27:18 -0500

    Update Character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,11 @@
 import  React  from "react";
-import React from "react";
+
 
 function Character(props) {
   return (
     <div className="character-card">
       <div className="character-info">
-        <h3>{props.char.name}</h3>
+       <p><strong>Name:</strong>{props.char.name} </p>
         <p>
           <strong>Height:</strong> {props.char.height}
         </p>
@@ -27,75 +27,3 @@ function Character(props) {
 }
 
 export default Character;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const Character = props => {
-  return(
-    <div className="Character container">
-      <div className="row">
-        <div className="nine columns">
-          <h3>{props.character.name}</h3>
-          <p>Birth Year: {props.character.birth_year}</p>
-          <p>Gender: {props.character.gender}</p>
-          <p>Hair Color: {props.character.hair_color}</p>
-          <p>Height: {props.character.height}</p>
-          <p>Mass: {props.character.mass}</p>
-          <p>Skin Color: {props.character.skin_color}</p>
-          <p>Eye Color: {props.character.birth_year}</p>
-          <ul>
-            {props.character.films.map(film => (
-              <li film={film}>{`Films: ${film}`}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default Character*/
