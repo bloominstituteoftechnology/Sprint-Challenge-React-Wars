28493d77e84cb5b4172ee1963373a351f0299c26
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-05-31 11:49:37 -0500

    Update App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ChararcterList from "./components/CharacterList";
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters("https://swapi.co/api/people/");
   }
 
   getCharacters = URL => {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <ChararcterList starwarsChars={this.state.starwarsChars} />
       </div>
     );
   }
