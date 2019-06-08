1b92ffab36ef4447afa1b775f3a7a1c881eb5198
heuzin <matheusqs.arq@gmail.com>
2019-06-07 20:30:10 -0400

    added more properties and style to the page

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -4,7 +4,11 @@ import "./StarWars.css"
 function Characters(props) {
     return (
         <div className="characters"> 
-            <p>{props.charactersInfo.name}</p>
+            <p>Name: {props.charactersInfo.name}</p>
+            <p>Birth Year: {props.charactersInfo.birth_year}</p>
+            <p>Gender: {props.charactersInfo.gender}</p>
+            <p>Height: {props.charactersInfo.height}</p>
+            <p>Mass: {props.charactersInfo.mass}</p>
         </div>
     )
 }
