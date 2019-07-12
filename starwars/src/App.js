da3d01fbbcddf5b2ce825a8d6d2cf8115bd37769
Jose Montero Jr <josemariodev@gmail.com>
2019-07-12 11:15:34 -0500

    connected to api using axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,41 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+function App() {
+  const [character, setCharacter] = useState();
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people')
+      .then(res => console.log(res.data.results))
+      .catch(err => console.log(err));
+  }, []);
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
-    );
-  }
+  return <div className='App'>
+    
+  </div>;
 }
 
 export default App;
