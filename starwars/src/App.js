cc166678dd2b0e5e4b29c21520671f0af3feca26
Jose Montero Jr <josemariodev@gmail.com>
2019-07-12 11:56:53 -0500

    passed props, mapping over data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Characters from './components/Character';
 
 function App() {
-  const [character, setCharacter] = useState();
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people')
-      .then(res => console.log(res.data.results))
+      .get('https://swapi.co/api/people/')
+      .then(res => setCharacters(res.data.results))
       .catch(err => console.log(err));
   }, []);
 
-  return <div className='App' />;
+  return (
+    <div className='App'>
+      <Characters characters={characters} />
+    </div>
+  );
 }
 
 export default App;
---
+++ starwars/src/components/Character.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Character = props => {
+  return (
+    <div>
+      <h1>{props.character.name}</h1>
+    </div>
+  );
+};
+
+export default Character;
