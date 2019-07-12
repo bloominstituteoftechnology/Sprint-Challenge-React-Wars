7d6187c431b920fb4c3a17375350da1875d5388c
Dave Irwin <david@redirwin.com>
2019-07-12 08:52:16 -0700

    Imported axios. Sucessfully pulling data. Building display.

--- starwars/src/components/GetChars.js
+++ starwars/src/components/GetChars.js
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 // import styled from 'styled-components'
 
-export default function GetChars() {
-  const [charList, changeCharList] = useState();
+import DisplayChars from "./DisplayChars";
 
-  return <p>In GetChars.</p>;
+export default function GetChars() {
+  const [charList, updateCharList] = useState();
+  const [pageStart, updatePageStart] = useState(1);
+  console.log(charList);
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/?page=${pageStart}`)
+      .then(res => updateCharList(res.data))
+
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+      charList.map(char=>{
+          <DisplayChar charList={charList}/>
+      })
+  )>;
 }
