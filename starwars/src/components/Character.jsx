c591440a95da408ad7e9f470ee31221ffd5afe90
Michael Harris <mhguitar1997@gmail.com>
2019-07-12 08:54:25 -0700

    Add character component code

--- starwars/src/components/Character.jsx
+++ starwars/src/components/Character.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Character = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h3>Name: {character.name}</h3>
+      <h5>Gender: {character.gender}</h5>
+      <h5>Born: {character.birth_year}</h5>
+      <div>
+        <p>
+          Height: {character.height} - Mass:{" "}
+          {character.mass}
+        </p>
+        <p>
+          Hair Color: {character.hair_color} - Eye-Color:{" "}
+          {character.eye_color}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Character;
