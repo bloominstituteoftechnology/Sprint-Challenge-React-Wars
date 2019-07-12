fcb6904a7e164efbe5949e9d5fc406f5c337fafd
Michael Harris <mhguitar1997@gmail.com>
2019-07-12 08:45:50 -0700

    Create character list component and views folder

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
-const [state, setState] = useState([])
-
-
+  const [state, setState] = useState([]);
   const getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
         return res.json();
@@ -20,15 +15,12 @@ const [state, setState] = useState([])
         throw new Error(err);
       });
   };
-
-  useEffect(() => getCharacters('https://swapi.co/api/people/'), [])
-
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
-    );
-  
-}
+  useEffect(() => getCharacters("https://swapi.co/api/people/"), []);
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+    </div>
+  );
+};
 
 export default App;
---
+++ starwars/src/views/CharacterList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CharacterList;
