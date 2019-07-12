e1a478ca2e244a6890ecfa9373624ec155699ceb
Michael Harris <mhguitar1997@gmail.com>
2019-07-12 09:49:39 -0700

    Install test renderer

--- starwars/src/views/CharacterList.js
+++ starwars/src/views/CharacterList.js
@@ -4,13 +4,19 @@ import { Grid, Dimmer, Loader } from "semantic-ui-react";
 
 const CharacterList = ({ characters, loading }) => {
   return (
-    <Dimmer active={loading}>
-      <Grid container divided centered>
-        {characters.map((char, i) => (
-          <Character character={char} key={i} />
-        ))}
-      </Grid>
-    </Dimmer>
+    <>
+      {loading ? (
+        <Dimmer active={loading}>
+          <Loader size="huge">Loading</Loader>
+        </Dimmer>
+      ) : (
+        <Grid container divided centered>
+          {characters.map((char, i) => (
+            <Character character={char} key={i} />
+          ))}
+        </Grid>
+      )}
+    </>
   );
 };
 
