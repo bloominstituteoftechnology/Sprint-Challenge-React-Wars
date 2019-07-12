8fd83afa093fd719fdc63cd69a4877ff500aeaa5
Dave Irwin <david@redirwin.com>
2019-07-12 09:30:40 -0700

    All characters with data successfully displaying.

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -3,5 +3,16 @@ import React from "react";
 
 export default function CharCard(props) {
   console.log(props.char);
-  return "In CharCard";
+  return (
+    <div className="character-card">
+      <h2 className="heading">{props.char.name}</h2>
+      <div className="stats">
+        <h3>Stats</h3>
+        <p>Born: {props.char.birth_year}</p>
+        <p>Gender: {props.char.gender}</p>
+        <p>Height: {props.char.height}</p>
+        <p>Weight: {props.char.weight}</p>
+      </div>
+    </div>
+  );
 }
