383e440e8989168fd8debf4fdf31a4444ea92838
Michael Harris <mhguitar1997@gmail.com>
2019-07-12 09:53:25 -0700

    Add snapshot testing

--- starwars/src/__tests__/Character.js
+++ starwars/src/__tests__/Character.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from './App';
+import Character from '../components/Character';
 
 describe('<App />', () => {
   it('should match snapshot', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<Character />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
--- starwars/src/__tests__/CharacterList.test.js
+++ starwars/src/__tests__/CharacterList.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from './App';
+import CharacterList from '../views/CharacterList';
 
 describe('<App />', () => {
   it('should match snapshot', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<CharacterList />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
