6f548334b5e876dafabf1991e38b4d1a08773aad
Michael Harris <mhguitar1997@gmail.com>
2019-07-12 09:43:51 -0700

    Dimmer loading state

--- starwars/src/views/CharacterList.js
+++ starwars/src/views/CharacterList.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Character from '../components/Character';
-import {Grid, Dimmer, Loader} from 'semantic-ui-react'
+import React from "react";
+import Character from "../components/Character";
+import { Grid, Dimmer, Loader } from "semantic-ui-react";
 
-const CharacterList = ({characters}) => {
-    return (
-        <Grid container divided centered>
-            {characters.map((char, i) => <Character character={char} key={i} />)}
-        </Grid>
-    );
+const CharacterList = ({ characters, loading }) => {
+  return (
+    <Dimmer active={loading}>
+      <Grid container divided centered>
+        {characters.map((char, i) => (
+          <Character character={char} key={i} />
+        ))}
+      </Grid>
+    </Dimmer>
+  );
 };
 
 export default CharacterList;
