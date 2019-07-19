4c02aa8fb8d2a24adef78e8cb6c298526f5f1c59
doanmade <43007082+doanmade@users.noreply.github.com>
2019-07-19 12:11:11 -0500

    got data showing on the page

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,13 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import StarWarsCard from "./StarWarsCard";
+const MainCard = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
-const StarWars = () => {
+function StarWars() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(res => {
+        console.log("it's working:", res.data);
+        setData(res.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
-      <StarWarsCard />
+      <MainCard>
+        <h1 className="Header">React Wars</h1>
+
+        {data.map(data => {
+          return (
+            <StarWarsCard
+              key={data.created}
+              name={data.name}
+              birth={data.birth_year}
+              gender={data.gender}
+              eye={data.eye_color}
+              hair={data.hair_color}
+              height={data.height}
+              mass={data.mass}
+              skin={data.skin_color}
+            />
+          );
+        })}
+      </MainCard>
     </div>
   );
-};
+}
 
 export default StarWars;
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,11 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 
-const StarWarsCard = () => {
+const Body = styled.div`
+  max-width: 300px;
+  background-color: white;
+
+  border-radius: 25px;
+  padding-bottom: 20px;
+`;
+const Header = styled.div`
+  background-color: gray;
+  color: white;
+  padding: 20px;
+  border-radius: 25px 25px 0px 0px;
+`;
+
+function StarWarsCard(props) {
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <div className="characters">
+      <Body>
+        <Header>
+          <h2>Name: {props.name}</h2>
+        </Header>
+        <p>Gender: {props.gender}</p>
+
+        <p>Eye-Color: {props.eye}</p>
+        <p>Hair-Color: {props.hair}</p>
+        <p>Height: {props.height}cm</p>
+        <p>Mass: {props.mass}kg</p>
+        <p>Skin Color: {props.skin}</p>
+        <p>Birth Year: {props.birth}</p>
+        <p>HomeWorld: {props.home}</p>
+      </Body>
     </div>
   );
-};
+}
 
 export default StarWarsCard;
