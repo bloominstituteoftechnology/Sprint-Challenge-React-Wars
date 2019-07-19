d6b89762352f8466720d980411a0b0b8a51a9ef5
Kelly M <moreira.kd@outlook.com>
2019-07-19 17:33:01 -0400

    other props

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
 
+
+
+
+
 function CharacterCard(props) {
 console.log('props', props)
+
+
+
+
+
     return(
 
         <div>
             <img src={props.character.image} />
             <p>name: {props.character.name}</p>
-            <p>status: {props.character.status}</p>
+            <p>height: {props.character.height}</p>
+            <p>mass: {props.character.mass}</p>
+            <p>gender: {props.character.gender}</p>
+            <p>hair color: {props.character.hair_color}</p>           
+            
         </div>
     )
 }
