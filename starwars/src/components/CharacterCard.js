ffaff076f8b0a61eb495b1f5fdb1995d66628894
johnschneider1 <johnlawschneider@gmail.com>
2019-07-19 14:33:12 -0500

    adjusting styles

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,20 +1,32 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 const CharacterCard = ({ characters }) => (
-  <Card>
+  <Card.Group>
     {characters.map(character => {
       return (
         <Card key={character.url}>
           <Card.Content header={character.name} />
+          <Card.Meta>May the Force be with you</Card.Meta>
           <Card.Content> Height: {character.height}</Card.Content>
           <Card.Content> Hair-Color: {character.hair_color}</Card.Content>
           <Card.Content> DOB: {character.birth_year}</Card.Content>
+
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button basic color="#522D96">
+                DaForce
+              </Button>
+              <Button basic color="#0A0613">
+                DarkSide
+              </Button>
+            </div>
+          </Card.Content>
         </Card>
       );
     })}
-  </Card>
+  </Card.Group>
 );
 
 export default CharacterCard;
--- starwars/src/components/NewStyles.js
+++ starwars/src/components/NewStyles.js
@@ -3,9 +3,13 @@ import styled from "styled-components";
 export const WrapperDiv = styled.div`
   font-family: cursive;
   text-align: center;
-  background-image: url(https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80);
+  background-image: url(https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=696&q=80);
   background-blend-mode: hue;
   color: #2199e8;
 `;
 
 export const NewH1 = styled.h1``;
+
+// https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=696&q=80
+
+// https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80
