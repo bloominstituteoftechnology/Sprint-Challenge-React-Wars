5295ae7328b628eb46b752ce67e4999710862a25
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-03 12:03:48 -0500

    get character list data from swapi

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState('');
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+      .then(res => {
+        setData(res.data.results)
+      })
+      .catch(err => {
+        console.log(`Unable to retrieve data: ${err}`)
+      })
+  }, []);
 
   return (
     <div className="App">
