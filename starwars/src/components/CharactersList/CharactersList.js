5cbf8b61293dc6f424576740d8c79d935d164b4b
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-03 12:43:04 -0500

    import CharacterCard and render

--- starwars/src/components/CharactersList/CharactersList.js
+++ starwars/src/components/CharactersList/CharactersList.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 
+import CharacterCard from './CharacterCard';
+
 export default function(props) {
-    const [data] = useState(Array.from(props.data));
-    console.log('CharactersList.js: ', props.data);
     return(
         <Grid>
             <Grid.Column>
                 {!props.isLoading? 
                     props.data.map(character => {
-                        return <h2>{character.name}</h2>
+                        return <CharacterCard character={character} />
                     }) : null
                 }
             </Grid.Column>
