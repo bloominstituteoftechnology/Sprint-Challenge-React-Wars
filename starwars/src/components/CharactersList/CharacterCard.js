ebb54428c84593d58942ef75a81838b765d79526
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-03 13:15:09 -0500

    fetch and use characters homeworld data

--- starwars/src/components/CharactersList/CharacterCard.js
+++ starwars/src/components/CharactersList/CharacterCard.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'semantic-ui-react';
+import axios from 'axios';
 
 export default function(props) {
-    const character = props.character;
+    const [character] = useState(props.character);
+    const [homeWorld, setHomeWorld] = useState('');
+
+    useEffect(() => {
+        axios.get(character.homeworld)
+            .then(res => setHomeWorld(res.data.name))
+    }, []);
 
     return(
         <Card style={{margin: '2rem'}}>
@@ -10,7 +17,7 @@ export default function(props) {
                 <Card.Header>{character.name}</Card.Header>
                 <Card.Meta>Height: {character.height}</Card.Meta>
                 <Card.Meta>Weight: {character.mass}</Card.Meta>
-                <Card.Description>A {character.species} weighing {character.mass}</Card.Description>
+                <Card.Description>This character appears in {character.films.length} Star Wars films. Originating from {homeWorld} in {character.birth_year}, {character.name} is awesome!</Card.Description>
             </Card.Content>
         </Card>
     )
