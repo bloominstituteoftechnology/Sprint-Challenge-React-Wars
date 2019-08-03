221fbdcba06cacc1749574d9558632318412a788
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-03 12:41:28 -0500

    add character variable for easy access

--- starwars/src/components/CharactersList/CharacterCard.js
+++ starwars/src/components/CharactersList/CharacterCard.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 export default function(props) {
+    const character = props.character;
+
     return(
         <Card>
             <Card.Content>
-                <Card.Header>{props.name}</Card.Header>
-                <Card.Meta>{props.homeworld}</Card.Meta>
-                <Card.Description>A {props.species} weighing {props.mass}</Card.Description>
+                <Card.Header>{character.name}</Card.Header>
+                <Card.Meta>{character.homeworld}</Card.Meta>
+                <Card.Description>A {character.species} weighing {character.mass}</Card.Description>
             </Card.Content>
         </Card>
     )
