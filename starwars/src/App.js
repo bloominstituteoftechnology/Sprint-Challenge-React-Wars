481daee91390be9d5b033b0a17d067f09ffae6e6
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-03 12:42:36 -0500

    added isLoading state to fix render bug

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [data, setData] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -17,6 +18,7 @@ const App = () => {
     axios.get('https://swapi.co/api/people/')
       .then(res => {
         setData(res.data.results)
+        setIsLoading(false);
       })
       .catch(err => {
         console.log(`Unable to retrieve data: ${err}`)
@@ -26,7 +28,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <CharactersList data={data} />
+      <CharactersList data={data} isLoading={isLoading} />
     </div>
   );
 }
