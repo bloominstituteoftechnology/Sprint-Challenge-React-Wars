6fb7b0de4da8dcecb05b059e8ad0ce743721c43b
Dave Kane <davelkane@gmail.com>
2019-08-04 22:27:59 -0500

    Added integration test (./cypress/integration/first_test.js), visits app, clicks next button, verifies info, clicks prev button, verifies info

--- starwars/cypress/integration/first_test.js
+++ starwars/cypress/integration/first_test.js
@@ -1,5 +1,19 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', function () {
   it('Visits the star wars app', function () {
     cy.visit('localhost:3000')
+
+    cy.contains('Home Planet')
+
+    cy.contains('Next').click()
+
+    cy.contains('Chewbacca')
+
+    cy.contains('Prev').click()
+
+    cy.contains('Luke')
+
+
   })
 })
