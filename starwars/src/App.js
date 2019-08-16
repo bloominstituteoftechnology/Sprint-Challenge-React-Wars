2bd0306b71288fb29423dc731e688f621d719fa7
Raajn Patel <rndpatel@gmail.com>
2019-08-16 08:59:12 -0700

    axios cals made. data is being acquired from api successfully .raajn-patel useEffect loop ended with adding empty input after useEffect.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,12 +5,38 @@ import axios from 'axios';
 import styled from "styled-components";
 
 const App = () => {
+    const [toon, setToon] = useState();
+    const [movie, setMovie] = useState();
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any.'
+
+    useEffect(() => {
+        // axios
+        //     .get('https://swapi.co/api/')
+        // .then((response) => {
+        //     console.log(response)
+        // });
+        axios
+            .get('https://swapi.co/api/people/')
+            .then((response) => {
+                for (let i = 0; i < 11; i++) {
+                    console.log(response.data.results[i]);
+                    setToon(response.data.results);
+                }
+                axios
+                    .get(`https://swapi.co/api/films/`)
+                    .then((response) => {
+                        for (let i = 0; i < 11; i++) {
+                            console.log(response.data.results[i]);
+                            setMovie(response.data.results);
+                        }
+                    })
+            })
+    }, []);
 
   return (
     <div className="App">
