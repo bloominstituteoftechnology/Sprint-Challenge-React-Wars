92340e1fd69c912a5d0c73daba7b4b4cdfe1d2a2
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-16 08:52:00 -0700

    buttons added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Character from "./components/Character";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Icon, Button } from "semantic-ui-react";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -24,7 +24,16 @@ const App = () => {
 
   return (
     <div className="App">
-      
+      <div className="buttons">
+      <Button icon labelPosition="left">
+        <Icon name="pause" />
+        Pause
+      </Button>
+      <Button icon labelPosition="right">
+        Next
+        <Icon name="right arrow" />
+      </Button>
+      </div>
       <Card.Group className="cardgroup">
         {results
           ? results.map((item, index) => {
