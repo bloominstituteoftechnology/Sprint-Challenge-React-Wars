a5772dfc104186432a107356bf08c032c62040d7
Raajn Patel <rndpatel@gmail.com>
2019-08-16 08:43:46 -0700

    Added yarn, semantic, styled, and axios. files built and ready to develop

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import axios from 'axios';
+// import 'semantic-ui-css/semantic.min.css';
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,6 +17,6 @@ const App = () => {
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
