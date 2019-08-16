6918ff253b3eb2d6f57da6760ef9d1a267e87dbe
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-16 08:56:49 -0700

    framework for dynamic page changes started

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -12,11 +12,11 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [page, setPage] = useState("https://swapi.co/api/people/?page=1");
+  const [page, setPage] = useState(1);
   const [results, setResults] = useState();
 
   useEffect(() => {
-    axios.get('https://swapi.co/api/people/?page=1').then(response => {
+    axios.get(`https://swapi.co/api/people/?page=${page}`).then(response => {
       console.log(response.data);
       setResults(response.data.results);
     });
