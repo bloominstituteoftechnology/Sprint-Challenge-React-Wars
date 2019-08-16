6ef3ab2383ee970ca2deacd68bef9c4354aec6ba
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-16 08:47:44 -0700

    refactor

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,39 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import axios from 'axios';
-import Character from './components/Character';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [page, setPage] = useState('https://swapi.co/api/people');
+  const [page, setPage] = useState("https://swapi.co/api/people");
   const [results, setResults] = useState();
 
-  useEffect(()=>{
-    axios
-    .get(page)
-    .then(response=>{
-      console.log(response.data.results)
+  useEffect(() => {
+    axios.get(page).then(response => {
+      console.log(response.data.results);
       setResults(response.data.results);
-    })
-
-  },[page]);
+    });
+  }, [page]);
 
   return (
     <div className="App">
       <Card.Group className="cardgroup">
-      {(results)? results.map((item,index)=>{
-        return <Character person={item} key={item.name} random={index} />
-      }) : "Loading..."}
+        {results
+          ? results.map((item, index) => {
+              return <Character person={item} key={item.name} random={index} />;
+            })
+          : "Loading..."}
       </Card.Group>
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-const Character = (props) => {
-    const person = props.person
+const Character = props => {
+  const person = props.person;
 
-    return ( 
-        <Card>
-        {/* <Image src={`https://picsum.photos/200?random=${props.index}`} wrapped ui={false} /> */}
-        <Card.Content>
-          <Card.Header>{person.name}</Card.Header>
-          <Card.Meta>Born in {person.birth_year}</Card.Meta>
-          <Card.Description>
-            <p>Eye Color: {person.eye_color}</p>
-            <p>Gender: {person.gender}</p>
-            <p>Height: {person.height}</p>
-            
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name='user' />
-            {`Appeared in ${person.films.length} film\(s\)`}
-          </a>
-        </Card.Content>
-      </Card>
-     );
-}
- 
+  return (
+    <Card>
+      {/* <Image src={`https://picsum.photos/200?random=${props.index}`} wrapped ui={false} /> */}
+      <Card.Content>
+        <Card.Header>{person.name}</Card.Header>
+        <Card.Meta>Born in {person.birth_year}</Card.Meta>
+        <Card.Description>
+          <p>Eye Color: {person.eye_color}</p>
+          <p>Gender: {person.gender}</p>
+          <p>Height: {person.height}</p>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          {`Appeared in ${person.films.length} film\(s\)`}
+        </a>
+      </Card.Content>
+    </Card>
+  );
+};
+
 export default Character;
