ce0981a13a44303d336afe317c934df0e5a0bb4f
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-30 19:25:46 -0400

    successfully called the api, having trouble getting the info displayed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
+import Person from './components/Person';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,22 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, updateData] = useState({});
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://swapi.co/api/people/")
+        .then(res => updateData(res.data));
+    }
+    fetchData();
+  }, []);
+  console.log(data);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Person data={data}/>
     </div>
   );
 }
