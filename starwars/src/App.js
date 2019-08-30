059c8ba443a8ff92a9c562fdfeae74509166a95a
spettigrew <sara.pettigrew5@gmail.com>
2019-08-30 17:29:57 -0600

    installed imports and ReadMe questions completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
