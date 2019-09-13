5a8c217981c233b54bcb629108cecfbba85071e1
Manju KC <kcmanjuthapa@gmail.com>
2019-09-13 12:22:08 -0400

    changed styling of component

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,16 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+
+const StarWarsChars = styled.div`
+width: 30%;
+background-color: #e8bcbebd;
+margin: 20px;
+`
+const UL = styled.ul`
+list-style-type: none;
+padding: 0;
+`
+const H1 = styled.li`
+font-size: 1.8rem;
+`
+
 
 function StarWarsChar(props) {
   return (
-    <div className="character">
-      <ul>
-        <li><h1>{props.char.name}</h1></li>
+    <StarWarsChars>
+      <UL>
+        <li><H1>{props.char.name}</H1></li>
         <li>Birth Year: {props.char.birth_year}</li>
         <li>Eye Color: {props.char.eye_color}</li>
         <li>Height: {props.char.height}</li>
         <li>Mass: {props.char.mass}</li>
-      </ul>
-    </div>
+      </UL>
+    </StarWarsChars>
   );
 }
 
