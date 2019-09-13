5bdf2703dd7dfe2ef4af04c49abe63b085e2ab60
Manju KC <kcmanjuthapa@gmail.com>
2019-09-13 12:42:10 -0400

    changed some styling and finished MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,34 +1,41 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import StarWarsChar from "./components/StarWarsChar"
-import './App.css';
+import styled from "styled-components";
+import StarWarsChar from "./components/StarWarsChar";
+import "./App.css";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
 const App = () => {
-  const [starWarschar, setStarWarsChar] = useState([])
+  const [starWarschar, setStarWarsChar] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   useEffect(() => {
-    axios.get(`https://swapi.co/api/people/`)
-    .then(resp => {
+    axios.get(`https://swapi.co/api/people/`).then(resp => {
       setStarWarsChar(resp.data.results);
       console.log(resp.data.results);
-    })
-
-  },[])
+    });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {starWarschar.map(char => {
-            return <StarWarsChar char={char} key={char.name} />
-          })}
-      </div>
+      <Div>
+        {starWarschar.map(char => {
+          return <StarWarsChar char={char} key={char.name} />;
+        })}
+      </Div>
+    </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,26 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const StarWarsChars = styled.div`
-width: 30%;
-background-color: #e8bcbebd;
-margin: 20px;
-`
+  width: 40%;
+  height: 300px;
+  background-color: #e8bcbebd;
+  margin: 50px;
+  color: white;
+  background-color: #df4e0b;
+  border: 5px solid grey;
+  border-radius: 50%;
+`;
 const UL = styled.ul`
-list-style-type: none;
-padding: 0;
-`
+  list-style-type: none;
+  padding: 0;
+  padding-left: 40px;
+`;
 const H1 = styled.li`
-font-size: 1.8rem;
-`
-
+  font-size: 1.8rem;
+  margin-top: 100px;
+`;
 
 function StarWarsChar(props) {
   return (
     <StarWarsChars>
       <UL>
-        <li><H1>{props.char.name}</H1></li>
+        <li>
+          <H1>{props.char.name}</H1>
+        </li>
         <li>Birth Year: {props.char.birth_year}</li>
         <li>Eye Color: {props.char.eye_color}</li>
         <li>Height: {props.char.height}</li>
