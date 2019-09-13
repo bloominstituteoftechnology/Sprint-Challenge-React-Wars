bf7f28b93bfe6ed72e52d8db7d285969398996fd
kiyani <bamba.kiyani@gmail.com>
2019-09-13 09:46:31 -0400

    MVP not completed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {Card, ListGroup} from 'react-bootstrap'
 
 const People = ({name, mass, skinColor, height}) => {
     return (
-            <h1>Starwars Character</h1>
-            <h2>Name : {name}</h2>
-                <p> {mass}</p>
-                <p>: {skinColor}</p>
-                <p>: {height}</p>
+            <Card border="danger" bg ='info' style={{ width: '19rem'}}>
+                <Card.Header>Starwars Character</Card.Header>
+                <Card.Body>
+                <Card.Title>Name : {name}</Card.Title>
+                <ListGroup bg = 'danger' variant="flush">
+                    <ListGroup.Item>Mass: {mass}</ListGroup.Item>
+                    <ListGroup.Item>Skin Color: {skinColor}</ListGroup.Item>
+                    <ListGroup.Item>Height: {height}</ListGroup.Item>
+                </ListGroup>
+                </Card.Body>
+            </Card>
     )
 } 
 
-
-export default People;
+export default People
