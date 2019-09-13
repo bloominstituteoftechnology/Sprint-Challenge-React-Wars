d1f90726e26e6e013003c044ba5f872a5208a1cb
Manju KC <kcmanjuthapa@gmail.com>
2019-09-13 11:13:49 -0400

    added state and useEffect hook in App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,12 +3,19 @@ import React, { useState, useEffect }  from 'react';
 import './App.css';
 
 const App = () => {
+  const [char, setChar] = useState({})
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get(`"https://swapi.co/api/people/"`)
+    
+
+  },[])
 
   return (
     <div className="App">
