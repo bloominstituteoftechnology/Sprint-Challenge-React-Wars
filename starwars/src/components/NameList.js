2a1b5cead5f50669d9379613b4bd9d4191c90674
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-09-29 21:14:00 -0600

    added style

--- starwars/src/components/NameList.js
+++ starwars/src/components/NameList.js
@@ -3,15 +3,15 @@ import { Card, CardText, CardBody, CardTitle, CardImg, Col } from "reactstrap";
 
 export default function NameList(props) {
   return (
-    <Col>
+    <Col width="50%" >
       <Card className="listname" key={props.id}>
-          {/* <CardImg src="casey-horner-RmoWqDCqN2E-unsplash.jpg"/> */}
-            <CardBody className="card">
-            <CardTitle>Name: {props.name}</CardTitle>
-            <CardText>Sex: {props.gender}</CardText>
-            <CardText className="Text-Primary">Height: {props.height}</CardText>
-            <CardText>Hair Color: {props.hair_color}</CardText>
-            </CardBody>
+        {/* <CardImg src="casey-horner-RmoWqDCqN2E-unsplash.jpg"/> */}
+        <CardBody className="p-3 mb-2 bg-info text-white" width="50%" p="p-b">
+          <CardTitle>Name: {props.name}</CardTitle>
+          <CardText>Sex: {props.gender}</CardText>
+          <CardText className="Text-Primary">Height: {props.height}</CardText>
+          <CardText>Hair Color: {props.hair_color}</CardText>
+        </CardBody>
       </Card>
     </Col>
   );
