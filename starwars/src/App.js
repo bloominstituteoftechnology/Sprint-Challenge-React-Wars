6313b62f25427ebf37d50e4f275b02ebe2fe7c79
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-11 13:31:49 -0500

    box styles added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,32 +1,39 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import axios from "axios";
+import CharacterCard from "./components/CharacterCard";
+
+
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+
+
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-useEffect(() => {
-    axios
-      .get(`https://swapi.co/api/people`)
-      .then(response => {
-        console.log("title data",response.data);
-        
-        
-      })
-      .catch(error => {
-        console.log("The data was not returned", error);
-      });
-  }, []);
+
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+      
     </div>
+
+   <CharacterCard />
+ 
+
+
+    </div>
+
+
+
   );
 }
 
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+import styled from "styled-components";
+
+
+const Body = styled.section`
+display:flex;
+flex-wrap: wrap;
+margin: 10%;
+align-items: center;
+justify-content:center;
+
+`;
+
+const Box = styled.div`
+margin: 3%;
+text-align: center;
+border: none;
+box-shadow: 5px 10px gray;
+border-radius: 10px;
+`;
+
+const Title = styled.h1`
+color: black;
+`;
+
+const Text = styled(Title)`
+font-size: 1rem;
+`;
+
+const CharacterCard = () => {
+
+  const [character, setCharacters] =useState([{}]);
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people`)
+      .then(response => {
+        console.log("successful",response);
+        const arr = response.data.results;
+        setCharacters(arr);   
+      })
+      .catch(error => {
+        console.log("The data was not returned", error);
+
+      });
+  }, []);
+
+
+  return (
+    
+      <Body>
+   
+        {character.map ((item, index) =>{
+        return <Box key = {index}>
+       
+     
+                <Title>{item.name}</Title>
+                <Text>Gender: {item.gender}</Text>
+                <Text>Birth Year:{item.birth_year}</Text>
+                <Text>Height:{item.height}</Text>
+                <Text>Eye Color: {item.eye_color}</Text>
+            
+            
+
+            </Box> 
+         
+
+        })}
+     
+      </Body>
+   
+
+
+
+  );
+}
+
+export default CharacterCard;
+
