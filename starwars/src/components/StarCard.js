a8e5715af843bc6bd7831964e7f3a602c9831667
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-11 13:38:24 -0400

    had to turn my mahcine off and on for this to finaly work

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,32 +1,22 @@
-import React from 'react';
-import {Collapse, Button} from 'reactstrap';
+import React,{useState} from 'react';
 
-export class StarCard extends Component {
-    constructor(props) {
-      super(props);
-      this.toggle = this.toggle.bind(this);
-      this.state = { collapse: false };
-      this.name = props.name;
-      this.key = props.key;
-    }
-  
-    toggle() {
-      this.setState(state => ({ collapse: !state.collapse }));
-    }
-  
-    render() {
+
+export const StarCard = (props)=> {
+     
       return (
-        <div key = {this.key}>
-          <Button color="danger" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-          <Collapse isOpen={this.state.collapse}>
+        <div key = {props.key}>
+          <div>
             <div>
                 <p>
-                    {this.name}
+                    {props.name} weres the data
                 </p>
             </div>
-          </Collapse>
+          </div>
         </div>
       );
-    }
+    
   }
+
+
+
   
--- starwars/src/components/Starwars.js
+++ starwars/src/components/Starwars.js
@@ -2,23 +2,35 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import {CardHolder} from "../components/Styled";
 import {StarCard} from "../components/StarCard";
+import { Spinner } from 'reactstrap';
+
 export default function MakeCharacter(){
 
-    const [character,setChatacter] = useState([]);
-    useState(()=>{
-        axios.get("https://swapi.co/api/people/")
-        .then((res=>{
-            console.log(res.data.results);
-            setChatacter(res.data.results);
-        }))
-    },[])
-  
+ const [dude, setDude] = useState([]);
 
-    return(
-    <CardHolder>
-    {character.forEach((chara,index)=>(
-        key = {index}, name = {chara.name}
-    ))}
-    </CardHolder>
-    );
+ useEffect(()=>{
+     axios.get(`https://swapi.co/api/people/`)
+     .then((response)=>{
+         console.log(response.data.results);
+         setDude(response.data.results);
+         console.log(dude);
+     })
+ },[])
+
+ if(!dude){
+     return(
+         <div>
+             <Spinner color = "info"/>
+         </div>
+     )
+ }
+
+ return(
+     <CardHolder>
+         {dude.map((d,index)=>(
+             <StarCard key = {index} name = {d.name}/>
+         ))}
+     </CardHolder>
+ )
+
 }
