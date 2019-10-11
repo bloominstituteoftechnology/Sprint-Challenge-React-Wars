6c8a4b97e6e8dc19d6fbc5232caa115a3a3a79db
itsericfig <itsericfig@gmail.com>
2019-10-11 10:31:06 -0500

    Set up state and made api request with axios, retrieved data from Star Wars API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,9 +10,24 @@ const App = () => {
     // Try to think through what state you'll need for this app before starting. Then build out
     // the state properties here.
 
+    const [people, setPeople] = useState([]);
+
     // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
     // side effect in a component, you want to think about which state and/or props it should
     // sync up with, if any.
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people`)
+            .then(response => {
+                const people = response.data.results;
+                console.log("Star Wars Characters: ", people);
+                setPeople(people);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div className="App">
