a959fa083259339ace97c3eb302f21bfd6a2e217
Norma Bunton <te_nj25@hotmail.com>
2019-10-11 14:20:56 -0400

    yarn install

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,7 +4,20 @@ const CharacterCard = props => {
     return(
         <div className = "characters" key={props.key}>
             <h2>Character:{props.name}</h2>
-
+            
+            {/* const Buttton = styled.button`
+                padding:6px 10px;
+                margin: 5px;
+                border: none;
+                border-radius:3px;
+                color: white;
+                
+                ${props => (props.type === 'primary' ? `background: #2196f3;` :null)}
+                ${props => (props.type === 'success' ? `background: #2196f3;` :null)}
+                ${props => (props.type === 'danger' ? `background: #2196f3;` :null)}
+                ${props => (props.type === 'warning' ? `background: #2196f3;` :null)}
+                `; */}
+                
         </div>
     );
 };
