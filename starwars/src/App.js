1f9ad297fece7742408cbd17899ec1e38a09ac9d
itsericfig <itsericfig@gmail.com>
2019-10-11 11:24:08 -0500

    Applied some basic styling to components using styled-components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,20 @@
-// import React, useEffect, and useState
+// Import React, useEffect, and useState
 import React, { useEffect, useState } from "react";
-// import styled components
+// Import PersonCard Component
+import PersonCard from "./components/PersonCard";
+// Import Styled Components
 import styled from "styled-components";
-// import axios
+// Import Axios
 import axios from "axios";
 import "./App.css";
 
-import PersonCard from "./components/PersonCard";
+// Styled Components
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
 
 const App = () => {
     // Try to think through what state you'll need for this app before starting. Then build out
@@ -34,23 +42,25 @@ const App = () => {
     return (
         <div className="App">
             <h1 className="Header">React Wars</h1>
-            {people.map(person => {
-                return (
-                    <PersonCard
-                        key={person.name}
-                        name={person.name}
-                        height={person.height}
-                        mass={person.mass}
-                        hairColor={person.hair_color}
-                        skinColor={person.skin_color}
-                        eyeColor={person.eye_color}
-                        birthYear={person.birth_year}
-                        gender={person.gender}
-                        films={person.films.length}
-                        starships={person.starships.length}
-                    />
-                );
-            })}
+            <Container>
+                {people.map(person => {
+                    return (
+                        <PersonCard
+                            key={person.name}
+                            name={person.name}
+                            height={person.height}
+                            mass={person.mass}
+                            hairColor={person.hair_color}
+                            skinColor={person.skin_color}
+                            eyeColor={person.eye_color}
+                            birthYear={person.birth_year}
+                            gender={person.gender}
+                            films={person.films.length}
+                            starships={person.starships.length}
+                        />
+                    );
+                })}
+            </Container>
         </div>
     );
 };
--- starwars/src/components/PersonCard.jsx
+++ starwars/src/components/PersonCard.jsx
@@ -1,23 +1,36 @@
 // import React, useEffect, and useState
-import React, { useEffect, useState } from "react";
-// import styled components
+import React from "react";
+// import Styled Components
 import styled from "styled-components";
+
+const Card = styled.div`
+    width: 30%;
+    margin: 1%;
+    position: relative;
+    border-radius: 0.2rem;
+    background-color: whitesmoke;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
+    &:hover {
+        bottom: 0.5rem;
+        background-color: white;
+    }
+`;
 
 const PersonCard = props => {
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <h4>A little about "{props.name}"</h4>
+        <Card>
+            <h2>{props.name}</h2>
+            <h5>A little about "{props.name}"</h5>
             <p>Height: {props.height}</p>
             <p>Mass: {props.mass}</p>
             <p>Hair Color: {props.hairColor}</p>
             <p>Skin Color: {props.skinColor}</p>
             <p>Eye Color: {props.eyeColor}</p>
-            <p>{props.birthYear}</p>
+            <p>Birth Year: {props.birthYear}</p>
             <p>Gender: {props.gender}</p>
-            <p>Films: {props.films.length}</p>
-            <p>Starships: {props.starships.length}</p>
-        </div>
+            <p>Films: {props.films}</p>
+            <p>Starships: {props.starships}</p>
+        </Card>
     );
 };
 
