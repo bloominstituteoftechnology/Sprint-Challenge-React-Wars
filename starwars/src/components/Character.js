97025c9fcb6ad959bb3b3da0309567c81dc1e702
JChartier001 <JChartier001@gmail.com>
2019-10-11 10:20:34 -0500

    Retrieved data from API

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,2 +1,23 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 
+export default function Character() {
+const [characterInfo, setCharacterInfo] = useState([]);
+
+useEffect(() =>{
+axios
+.get('https://swapi.co/api/people')
+.then(response => {
+    console.log(response.data.results);
+    setCharacterInfo(response.data.results)
+})
+.catch(error => {
+    console.log('Data was not returned', error)
+})
+}, []);
+
+return (
+    null
+)
+
+}
