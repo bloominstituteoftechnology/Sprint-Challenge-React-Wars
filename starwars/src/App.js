47bf2364326c50c342747ee15292f21782f26842
itsericfig <itsericfig@gmail.com>
2019-10-11 10:18:10 -0500

    Initial commit, imported useEffect, useState, styled-components, and axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
+// import React, useEffect, and useState
+import React, { useEffect, useState } from "react";
+// import styled components
+import styled from "styled-components";
+// import axios
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+    // Try to think through what state you'll need for this app before starting. Then build out
+    // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+    // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+    // side effect in a component, you want to think about which state and/or props it should
+    // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <h1 className="Header">React Wars</h1>
+        </div>
+    );
+};
 
 export default App;
