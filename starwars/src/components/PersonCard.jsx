7101c575b3234e464d684647375deb14bb90d4d3
itsericfig <itsericfig@gmail.com>
2019-10-11 12:42:05 -0500

    Changed up the CSS, just different colors

--- starwars/src/components/PersonCard.jsx
+++ starwars/src/components/PersonCard.jsx
@@ -6,13 +6,16 @@ import styled from "styled-components";
 const Card = styled.div`
     width: 30%;
     margin: 1%;
+    color: yellow;
+    padding: 2rem 0;
     position: relative;
-    border-radius: 0.2rem;
-    background-color: whitesmoke;
+    border-radius: 0.5rem;
+    background-color: #111;
+    border: solid 3px yellow;
     box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
     &:hover {
         bottom: 0.5rem;
-        background-color: white;
+        text-shadow: 0px 2px 2px rgba(255, 255, 0, 0.5);
     }
 `;
 
