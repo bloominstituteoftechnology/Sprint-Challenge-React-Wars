276d7f580ff1c937a9fd6687676fa5ca09c584d3
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-11 14:16:28 -0500

    filter by item form rendered

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import './App.css';
 import CharacterCard from "./components/CharacterCard";
+import { CustomInput, Form, FormGroup, Label } from 'reactstrap';
+import styled from "styled-components";
 
+const Box = styled.div`
+margin: 3%;
+text-align: center;
+border: none;
+box-shadow: 5px 10px gray;
+border-radius: 10px;
+display: flex;
+width: 100%;
 
+`;
 
-
+const Box2 = styled.div`
+margin: 3%;
+text-align: center;
+display: flex;
+justify-content: center;
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-
-
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <div>
-      
-    </div>
+      <Box2>
+        <Form>
+      <FormGroup>
+        <Label for="Star-Wars-CheckBox">Search By:</Label>
+        <Box>
+          <CustomInput type="checkbox" id="exampleCustomCheckbox21"label="Name" />
+          <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Gender" />
+          <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="Birth Year" disabled />
+          <CustomInput type="checkbox" id="exampleCustomCheckbox4" label="Height" htmlFor="exampleCustomCheckbox4_X" disabled />
+          <CustomInput type="checkbox" id="exampleCustomCheckbox4" label="Eye Color" htmlFor="exampleCustomCheckbox5" disabled />
+          </Box>
+      </FormGroup>
+      </Form>
+
+
+    </Box2>
 
    <CharacterCard />
  
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-
 import styled from "styled-components";
 
 
