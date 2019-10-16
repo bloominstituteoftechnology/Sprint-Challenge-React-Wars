50245ea27094a0cffb30180ef3f138b65f1257af
Drazen <danstad2012@gmail.com>
2019-10-16 11:01:44 -0700

    now with functional components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,92 +1,50 @@
-import React from 'react';
-import './App.css';
-
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
-
-export default App;
-import React from 'react';
+import React , { useState , useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
-//import Character from './components/Character';
  
- 
-class Character extends React.Component {
-  state = {
-      name:'',
-      birth_year:'',
-      status:'',
-      id:0
-  };
-  
-  componentDidMount() {
- 
-      axios.get(`https://rickandmortyapi.com/api/character/`)
-      .then( res => {
-          const arr = res.data.results
-          this.setState({name: arr[this.props.id].name})
-          this.setState({status: arr[this.props.id].status})
-          console.log(this.state)
-          
-      })
-      .catch( err => {
-          console.log("Error:", err);
-      })
- 
-  }
-  render() {
-      return(
-      <div>
-          <h3></h3>
-          <h2>{this.props.id}:{this.state.name}</h2>
-          <p>{this.state.status}</p>
-      </div>)
-  }
-}
- 
- 
-function List(props) {
-  const nums = props.nums
-  console.log(nums)
-  const listItems = nums.map(i => 
-    <Character key={i.toString()} id={i} />
-    )
+
+function Character(props) {
+  const [name, setName] = useState('')
+
+  axios.get(`https://swapi.co/api/people/${props.item}`)
+  .then( res => {
+    const person = res.data
+    setName(person.name)
+  })
+  .catch( err => {
+    console.log("Error:", err);
+  })
+   
   return(
-    <div>
-      { listItems }
-    </div>
-  )
-    }
-  
-  
-  const nums = [1,2,3,4,5]
+    <li>
+        <h2>{name}</h2>
+    </li>)
+}
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
- 
+  
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
- 
+  const nums = [...Array(7).keys()];
+
+console.log(nums)
+  const listItems = nums.map((item, index) => {
+    return( <Character key={index} item={item+1}/> )
+  })
+  useEffect(() => {
+    console.log("Hello from the effect hook!");
+  },[])
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <List nums={nums}/>
-      <Character id={ 0 } />
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
- 
 export default App;
