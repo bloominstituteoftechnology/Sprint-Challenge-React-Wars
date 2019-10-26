deb8756f7ee60c9968e1dc5a67fa0d9a087a5d15
Samuel Kraus <S_read@aol.com>
2019-10-26 13:54:21 -0500

    Did some styling to the cards and created stylable variables

--- starwars/src/StarWarsCard.js
+++ starwars/src/StarWarsCard.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col, Button } from "reactstrap";
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+    
+    width: 100%;
+    margin: 0 auto;   
+    
+`;
 
 
-
-
+const Title = styled.h1`
+        font-size: 1.2rem;
+`;
 
 export default function StarWarsCard(props) {
 
     return(
         <Col xs='6' lg='4'>
-            <Card height='100%' body outline color = 'danger'  className='text-left'>
+            <StyledCard height='100%' body outline color = 'danger'  className='text-left'>
                 <CardBody height='100%'>
-                    <CardTitle className='text-center'>{props.name}</CardTitle>
-                    <CardSubtitle className='text-center'>{props.gender}</CardSubtitle>
-                   
+                    <Title className='text-center'>{props.name}</Title>
+                    <CardSubtitle className='text-center'>Gender: {props.gender}</CardSubtitle>
                     <CardText>Birth Year: {props.birthYear}</CardText>
                     <CardText>Eye Color: {props.eyes}</CardText>
                     <CardText>Hair Color: {props.hair}</CardText>
                     {/* <Button className='text-left'>More</Button> */}
                     <hr />
                 </CardBody>
-            </Card>
+            </StyledCard>
 
         </Col>
     )
