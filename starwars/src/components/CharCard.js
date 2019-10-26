6f01c403f5201164b70218c5245bea84a3e80f2c
vishalicious213 <vishalicious213@gmail.com>
2019-10-26 05:14:19 -0400

    cleaned up code. changed text colors on hover for more readability.

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -8,7 +8,13 @@ const StyledCard = styled.div`
     width: 13rem;
     margin: 1rem;
     :hover {
-        background: RGBA(119, 136, 153, .5)
+        background: RGBA(119, 136, 153, .5);
+        .charName {
+            color: yellow;
+        }
+        .stat {
+            color: lightyellow;
+        }
     }`;
 const StyledName = styled.h3`
 color: white;
@@ -18,37 +24,23 @@ color: gainsboro;
 text-shadow: 2px 2px black;`;
 
 const CharCard = (props) => {
-    // console.log("index: ", props.index);
-    console.log(props.name);
-    // console.log(props.gender);
-    // console.log(props.birth_year);
-    // console.log(props.hair_color);
-    // console.log(props.eye_color);
-    // console.log(props._skin_color);
-    // console.log(props.height);
-    // console.log(props.mass);
-    // console.log("");
 
-    return(
-        // <div>
-        //     <h2>CharCard Return</h2>
-        //     <p>{props.name}</p>
-        // </div>
-    
+    console.log(props.name);
+
+
+    return(    
         <StyledCard className="charCard">
             <StyledName className="charName">{props.name}</StyledName>
-            <StyledStat>Gender: {props.gender}</StyledStat>
-            <StyledStat>Birth Year: {props.birth_year}</StyledStat>
-            <StyledStat>Hair Color: {props.hair_color}</StyledStat>
-            <StyledStat>Eye Color: {props.eye_color}</StyledStat>
-            <StyledStat>Skin Color: {props.skin_color}</StyledStat>
-            <StyledStat>Height (cm): {props.height}</StyledStat>
-            <StyledStat>Weight (kg): {props.mass}</StyledStat>
+            <StyledStat className="stat">Gender: {props.gender}</StyledStat>
+            <StyledStat className="stat">Birth Year: {props.birth_year}</StyledStat>
+            <StyledStat className="stat">Hair Color: {props.hair_color}</StyledStat>
+            <StyledStat className="stat">Eye Color: {props.eye_color}</StyledStat>
+            <StyledStat className="stat">Skin Color: {props.skin_color}</StyledStat>
+            <StyledStat className="stat">Height (cm): {props.height}</StyledStat>
+            <StyledStat className="stat">Weight (kg): {props.mass}</StyledStat>
             <StyledStat>{``}</StyledStat>
         </StyledCard>
-    
     )
 }
-
 
     export default CharCard;
--- starwars/src/components/charGenerator.js
+++ starwars/src/components/charGenerator.js
@@ -9,33 +9,18 @@ const CharGenerator = () => { // make the results state
     useEffect(() => {
         axios.get("https://swapi.co/api/people/")
         .then(response => {
-        //   console.log(response);
-        // const swChars = response.data.results;
-
             setSWChars(response.data.results);
-        // console.log(swChars);
-        //   console.log(swChars[0]);
-        //   console.log(swChars[0][1]);
-        // console.log(response.data.results);
         })
         .catch(error => {
-        console.log("Error: ", error);
+            console.log("Error: ", error);
         })
     },[]) // useEffect
 
     return(
         swChars.map(char => {
-            // console.log(char.gender);
-            // console.log(char.name);
-            // console.log(CharCard(char));
-
             return CharCard(char);
         })
-    )
-
-    // console.log("outside");
-    // console.log(swChars);
+    ) // outside return
 } // CharGenerator
-
 
     export default CharGenerator;
