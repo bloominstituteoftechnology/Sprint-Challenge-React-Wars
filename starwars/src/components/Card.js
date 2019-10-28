35792ae3e3439879ad0f4472ef2f81a5bd3ef195
brianetaveras <taveras574@gmail.com>
2019-10-27 20:26:33 -0400

    passed prop down to card element. Added hover effect to each card

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -3,36 +3,62 @@ import styled from 'styled-components'
 
 const CardBox = styled.div`
     width: 200px;
-    height: 400px;
+    height: 300px;
     padding: 10px;
     background: white;
     border-radius: 20px;
     box-shadow: 10px 10px 20px black;
     margin: 10px;
+    overflow: hidden;
+
+    &:hover{
+        width: 400px;
+        height: 95%;
+        transition: 1s;
+        z-index: 1;
+    }
     
     `;
     
-    const CardImage = styled.img`
+    const CardImage = styled.div`
         width: 100%;
+        height: 100px;
         border-radius: 20px;
+        background: url(img/star-wars-logo.webp);
+        background-size: cover;
+        background-position: center;
     `; 
 
-const CardTitle = styled.span`
-    font-size: 24px;
-    text-align: center;
+    const CardTitle = styled.span`
+        font-size: 24px;
+        text-align: center;
 
-`;
+    `;
+
+    const CardInfo = styled.div`
+
+    `;
 
 
 const Card = (props)=>{
 
-    
+
 
     return (
         <div className="card">
             <CardBox>
                 <CardTitle>{props.char.name}</CardTitle>
-                <CardImage alt="star wars logo image" src="\img\star-wars-logo.webp" />
+                <CardImage alt="star wars logo image"/>
+                <CardInfo>
+                    <span><b>Preferred Gender:</b></span>
+                    <p>{props.char.gender}</p>
+                    <span><b>Birth Year:</b></span>
+                    <p>{props.char.birth_year}</p>
+                    <span><b>Height:</b></span>
+                    <p>{props.char.height}</p>
+                    <span><b>Eye Color:</b></span>
+                    <p>{props.char.eye_color}</p>
+                </CardInfo>
             </CardBox>
         </div>
     )
