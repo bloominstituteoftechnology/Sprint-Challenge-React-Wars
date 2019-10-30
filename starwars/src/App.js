d8c6a263e5c1f91b7bb8dbc298d13a2d80137b08
Shantay Grady <shantaygrady@gmail.com>
2019-10-29 23:12:03 -0400

    Creates state, useEffect hook

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,11 +5,21 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [character, setCharacter] = useState({});
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people/')
+      .then(res => {
+        setCharacter(res.data.results);
+      })
+      .catch(err => {
+        console.log("You have an error!", err);
+      });
+  }, []);
+  //.........................................Build out Renderer next....
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
