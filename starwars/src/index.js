9947da0876e15b5bf3a30ef3f3aea0d7a0e6d108
Hui <jeffrey.x.hui@gmail.com>
2019-11-08 08:05:37 -0800

    added parallax class to body in index.js

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+document.querySelector("body").classList.add("parallax");
 ReactDOM.render(<App />, document.getElementById('root'));
