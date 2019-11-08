e869307b651b5edc5cef58530d4ceccf5716e23f
jarrodskahill <jarrodskahill@gmail.com>
2019-11-08 09:17:16 -0800

    added more advanced styling, collapse with more information and prepared props

--- starwars/src/components/card.js
+++ starwars/src/components/card.js
@@ -1,22 +1,39 @@
 import React from "react"
 import {Card, CardImg, CardText, CardBody,
-CardTitle, CardSubtitle, Button} from 'reactstrap'
+CardTitle, CardSubtitle, Button, Collapse} from 'reactstrap'
 
 
 const StarWarsCard = props => {
+        const [isOpen, setIsOpen] = useState(false);
+      
+        const toggle = () => setIsOpen(!isOpen);
     return (
         <div>
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+        <CardTitle>{props.character.name}</CardTitle>
+        <CardSubtitle>Birth Year: {props.character.birth_year}</CardSubtitle>
+        <CardText>Gender:{props.character.gender}</CardText>
+        <CardText>Hair Color: {props.character.hair_color}</CardText>
+          <div>
+      <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>More Info</Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+          <CardText>Eye Color: {props.character.eye_color}</CardText>
+          <CardText>Mass: {props.character.mass}</CardText>
+          <CardText>Skin Color: {props.character.skin_color}</CardText>
+
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
         </CardBody>
       </Card>
     </div>     
 
         
     )
+    
 }
