f2ee7be533eae69066323137e3693587ee858910
jarrodskahill <jarrodskahill@gmail.com>
2019-11-08 09:00:40 -0800

    created a card.js for character cards and imported advanced styling from reactstrap

---
+++ starwars/src/components/card.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {Card, CardImg, CardText, CardBody,
+CardTitle, CardSubtitle, Button} from 'reactstrap'
+
+
+const StarWarsCard = props => {
+    return (
+        <div>
+      <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>     
+
+        
+    )
+}
