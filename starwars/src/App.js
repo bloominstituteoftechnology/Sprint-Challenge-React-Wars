f7f15806af9a2228e505a185fcc334d37bb63625
jarrodskahill <jarrodskahill@gmail.com>
2019-11-08 08:50:37 -0800

    added useEffect with axios to fetch data from api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [starWarsCharacter, setStarWars] = useState([]);
+  useEffect(()=>{
+  axios
+    .get(`https://swapi.co/api/people`)
+    .then(response => {
+      const starWarsCharacter = response.data.results;
+      console.log(starWarsCharacter);
+      setStarWars(starWarsCharacter);
+    })
+  },[]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
