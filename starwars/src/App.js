3f92339780a330920609c3acc93ad6a3b06c326c
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-11-22 22:39:13 -0500

    Test render component successful

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
+import Test from "./components/Test"
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [endpoint, setEndpoint] = useState ('https://swapi.co/api/people/')
+const [apiData, setApiData] = useState();
+ 
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+useEffect(() => {
+  axios
+  .get(endpoint)
+  .then(response => {
+    console.log(response.data);
+    setApiData(response.data);
+  })
+  .catch(err => console.log(err));
+}, [endpoint]); 
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Test/>
     </div>
   );
 }
---
+++ starwars/src/components/TransposeButtons.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TransposeButtons = (props) => {
+
+    return ( <
+        div className = "transposebuttons" >
+        <button>Next page</button>
+        <button>Prev page</button>
+
+
+        </div>
+    )
+}
+
+export default TransposeButtons; 
