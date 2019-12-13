7430bd1788ee9500f644b1feee16ac90017d4ada
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-13 10:23:07 -0800

    short answers

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Card, CardText, CardBody,CardTitle, CardSubtitle } from 'reactstrap';
 
 const CharCard = ({ name, year, films }) => {
   return (
-    <Card>
+    <Card maxWidth='325px'>
       <CardBody>
         <CardTitle>{name}</CardTitle>
         <CardSubtitle>Born: {year}</CardSubtitle>
