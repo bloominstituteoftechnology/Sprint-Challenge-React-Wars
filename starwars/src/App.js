6db61418bbba667afd982bab8daf2f10f663b49a
Jessica Wolff <jleahwolff@gmail.com>
2019-12-16 08:59:31 -0600

    I have the crawl working. Adding shining stars and API call in completed version.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,45 +1,92 @@
-import React, {useState, useEffect} from 'react';
+//import React, {useState, useEffect} from 'react';
 import './App.css';
-import StarCard from './components/StarCard';
+//import StarCard from './components/StarCard';
 import axios from 'axios';
-import styles from 'styled-components';
+//import styles from 'styled-components';
+import { gsap } from "gsap";
+import React, { Component, useState, useEffect } from "react";
+import { Power2, TimelineLite } from "gsap";
 
-const PageDiv = styles.div`
-display:flex;
-color:white;
-flex-wrap:wrap;
-align-items:center;
-justify-content: center;
 
-.Header{
-  width: 100%;
-  
+
+// const starApp = () => {
+//   const [starList, setStarList] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('https://swapi.co/api/people/')
+//     .then(res => {
+//         setStarList(res.data.results);
+//         console.log(res.data.results);
+//     })
+//     .catch(err => {
+//         console.log('Something is wrong', err);
+//     })
+// }, []);
+// }
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.intro = React.createRef();
+    this.content = React.createRef();
+  }
+  componentDidMount() {
+    const tl = new TimelineLite();
+    tl
+      .to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
+      .to(this.intro.current, 1.5, { opacity: 0 })
+      .to(this.content.current, 200, { top: "-120%" });
+  }
+
+//   componentDidMount() {
+//     fetch('https://swapi.co/api/people/')
+//     .then(res => {
+//       setStarList(res.data.results);
+//   })
+//   .catch(err => {
+//       console.log('Something is wrong', err);
+//   })
+// }, []);
+
+  render() {
+    
+    return (
+      <div className="container">
+        <section className="intro" ref={this.intro}>
+          <p>
+            A long time ago, in a galaxy far,<br /> far away....
+          </p>
+        </section>
+        <section className="crawl">
+        <div className="content" ref={this.content}>
+          <h1 className="title">Lambda School - WEB26, Jessica Wolff </h1>
+          <h2 className="subtitle">THE REACT WAR AWAKENS</h2>
+          <p>The Development Team Lead, Jess has vanished. In her absence, the sinister FUNCTIONAL BUG has risen from the ashes of the CI Tool and will not rest until the last developer has been destroyed.
+          </p>
+          <p>With the support of the QA TEAM, the Software Developer leads a brave RESISTANCE. He is desperate to find his Lead and gain her help in restoring peace and justice to the repository.
+          </p>
+        
+        </div>
+      </section>
+      {/* <section> */}
+        
+        {/* <p>{starList.map(e => (
+          <StarCard card={e} />
+          ))}</p> */}
+      {/* </section> */}
+      </div>
+    );
+  }
 }
-`
 
-const App = () => {
-  const [starList, setStarList] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://swapi.co/api/people/')
-    .then(res => {
-        setStarList(res.data.results);
-        console.log(res.data.results);
-        // setStarList(person);
-    })
-    .catch(err => {
-        console.log('Something is wrong', err);
-    })
-}, []);
-
-  return (
-    <PageDiv className="App">
-      <h1 className="Header">React Wars</h1>
-      {starList.map(e => (
-        <StarCard card={e} />
-      ))}
-    </PageDiv>
-  );
-}
+  // return (
+  //   <div className="App">
+  //     <h1 className="Header">React Wars</h1>
+      //  {starList.map(e => (
+      //    <StarCard card={e} />
+      // ))} 
+  //   </div>
+  // );
 
 export default App;
--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,44 +1,44 @@
 import React from "react";
-// import { Card, CardBody, CardTitle } from "reactstrap";
-import styles from 'styled-components';
+// // import { Card, CardBody, CardTitle } from "reactstrap";
+// import styles from 'styled-components';
+import { gsap } from "gsap";
 
-const CardStyle = styles.div`
-//display:flex;
-// align-items: center;
-// flex-wrap:wrap;
-// justify-content:center;
-// line-height: 1rem;
-//background: black;
+// const CardStyle = styles.div`
+// font-family: 'Oswald', sans-serif;
+// //display:flex;
+// // align-items: center;
+// // flex-wrap:wrap;
+// // justify-content:center;
+// // line-height: 1rem;
+// //background: black;
 
-.card{
-    //width:30%;
-    background: dodgerblue;
-    display:flex;
-    align-items: center;
-    flex-wrap:wrap;
-    justify-content:center;
-    margin: 2%;
+// // .card{
+// // //width:30%;
+// // background: dodgerblue;
+// // display:flex;
+// // align-items: center;
+// // flex-wrap:wrap;
+// // justify-content:center;
+// // margin: 2%;
 
-h2, h3, h4{
-    width: 90%;
-    line-height:1rem;
-    }
-}
+// // h2, h3, h4{
+// // width: 90%;
+// // line-height:1rem;
+// // }
+// // }
+// `
 
-
-
-`
-
-export default function StarCard(props) {
-// const {name} = props
-    return (
-        <CardStyle>
-            <div className="card">
-                <h2>{props.card.name} </h2>
-                <h3>DOB: {props.card.birth_year}</h3>
-                <h4>Height: {props.card.height}, Weight: {props.card.mass} </h4>
-                <h4>Eyes: {props.card.eye_color}, Hair: {props.card.hair_color}</h4>
-            </div>
-        </CardStyle>
-    );
-}
+// export default function StarCard(props) {
+// // const {name} = props
+//     return (
+//         <CardStyle>
+//             <div className="card">
+//                 {/* <h2>A long time ago in a galaxy far, far, away... </h2> */}
+//                 <h2>{props.card.name} </h2>
+//                 <h3>DOB: {props.card.birth_year}</h3>
+//                 <h4>Height: {props.card.height}, Weight: {props.card.mass} </h4>
+//                 <h4>Eyes: {props.card.eye_color}, Hair: {props.card.hair_color}</h4>
+//             </div>
+//         </CardStyle>
+//     );
+// }
