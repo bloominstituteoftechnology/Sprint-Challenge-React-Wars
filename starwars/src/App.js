8bc20723eac251d06ed0cc42ce0769dbdd6b8246
Justin A <jarata@gmail.com>
2019-12-16 08:57:52 -0800

    setting up state fetching data with useEffect checking data validity

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
+import CardList from "./components/CardList";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [swData, setSwData] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => {
+        console.log("Response:", res.data)
+        setSwData(res.data)
+      })
+  }, []);
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
