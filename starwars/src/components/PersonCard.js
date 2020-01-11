db6b0658be85d9bdcd3fb78617bbdcfdba5c042f
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-10 19:31:18 -0700

    Made the cards transparent. Tried to make them line up in rows. So far I can only get them to line up in a skinny column on the left

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardColumns, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 const PersonCard = props => {
     return (
-        <Card className="person-card">
-            <CardTitle className="name">Name: {props.name}</CardTitle>
-            <CardSubtitle className="height">
-                <p>Height: {props.height}</p>
-            </CardSubtitle>
-            <CardSubtitle className="mass">
-                <p>Mass: {props.mass}</p>
-            </CardSubtitle>
-            <CardSubtitle className="birth_year">
-                <p>Birth Year: {props.birth_year}</p>
-            </CardSubtitle>
-        </Card>
+        <CardColumns className="card-columns">
+            <Card style={{ backgroundColor: '#ffffff00', borderColor: '#333' }} className="person-card">
+                <CardTitle className="name">{props.name}</CardTitle>
+                <CardSubtitle className="height">
+                    <p>Height: {props.height}</p>
+                </CardSubtitle>
+                <CardSubtitle className="mass">
+                    <p>Mass: {props.mass}</p>
+                </CardSubtitle>
+                <CardSubtitle className="birth_year">
+                    <p>Birth Year: {props.birth_year}</p>
+                </CardSubtitle>
+            </Card>
+        </CardColumns>
     )
 
 };
