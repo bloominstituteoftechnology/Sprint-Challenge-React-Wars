82a1f732550e0674a9130cc6fd7d5b32086f771e
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-10 19:53:06 -0700

    Added drop down menus to show person info

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,20 +1,42 @@
-import React from "react";
-import { Card, CardColumns, CardTitle, CardSubtitle } from 'reactstrap';
+import React, { useState } from "react";
+import { Card, CardColumns, CardTitle, CardSubtitle, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const PersonCard = props => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+
     return (
+     
+
+
+
         <CardColumns className="card-columns">
             <Card style={{ backgroundColor: '#ffffff00', borderColor: '#333' }} className="person-card">
-                <CardTitle className="name">{props.name}</CardTitle>
-                <CardSubtitle className="height">
-                    <p>Height: {props.height}</p>
-                </CardSubtitle>
-                <CardSubtitle className="mass">
-                    <p>Mass: {props.mass}</p>
-                </CardSubtitle>
-                <CardSubtitle className="birth_year">
-                    <p>Birth Year: {props.birth_year}</p>
-                </CardSubtitle>
+                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                    <DropdownToggle caret>
+                        <CardTitle className="name">{props.name}</CardTitle>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem>
+                            <CardSubtitle className="height">
+                                <p>Height: {props.height}</p>
+                            </CardSubtitle>
+                        </DropdownItem>
+                        <DropdownItem>
+                            <CardSubtitle className="mass">
+                                <p>Mass: {props.mass}</p>
+                            </CardSubtitle>
+                        </DropdownItem>
+                        <DropdownItem>
+                            <CardSubtitle className="birth_year">
+                                <p>Birth Year: {props.birth_year}</p>
+                            </CardSubtitle>
+                        </DropdownItem>
+                    </DropdownMenu>
+                    
+                </Dropdown>
             </Card>
         </CardColumns>
     )
@@ -22,3 +44,6 @@ const PersonCard = props => {
 };
 
 export default PersonCard;
+
+
+    
