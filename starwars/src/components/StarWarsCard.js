52314937d18c1289856d50f634ebbd5d275b8d75
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-10 21:23:27 -0500

    finishing up on the styling

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Card} from 'reactstrap';
+import {Card, CardText} from 'reactstrap';
 import './StarWars.scss';
 import styled from 'styled-components';
 
 const Wrapper= styled.div`
-margin-right: 30%;
-margin-left: 30%;
+margin-right: 35%;
+margin-left: 35%;
 `
 
 
@@ -16,9 +16,9 @@ export default function StarWarsCard(props) {
     return (
         <Wrapper>
             <Card className='card'>
-            <p>Name: { props.person.name}</p>
-            <p>Gender: { props.person.gender}</p>
-            <p>A Feautured Film: { props.person.films[3]}</p>
+            <CardText className='text'>Name: { props.person.name}</CardText>
+            <CardText className='text'>Gender: { props.person.gender}</CardText>
+            <CardText className='text'>A Feautured Film: { props.person.films[0]}</CardText>
             </Card>
         </Wrapper>
     )
