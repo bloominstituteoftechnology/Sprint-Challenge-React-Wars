5aec28aacfca121a5978195be64215bb2d5de759
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-01-14 21:55:08 -0500

    -

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import InfoCard from './components/InfoCard';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people')
-  }
-
-  getCharacters = URL => {
-    fetch(URL)
+const App = () => {
+  const [character, starwarsChars] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people')
       .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results});
+        starwarsChars(res.data.results)
+        console.log(res.data.results)
       })
       .catch(err => {
-        throw new Error(err);
-      });
-  };
+        console.log('Error', err)
+      })
+  }, []);
+  
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+      <InfoCard starwarsChars={this.state.starwarsChars} />
+    </div>
+  );
 
-  render() {
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-        <InfoCard starwarsChars={this.state.starwarsChars} />
-      </div>
-    );
-  }
-
+  
+  
 }
 
 
