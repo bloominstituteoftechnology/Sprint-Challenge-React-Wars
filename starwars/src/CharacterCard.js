19f5ac6a725b31d81d1868ed5f96acb513c5d0aa
richknicks <rlynch@gmail.com>
2020-01-24 14:26:03 -0500

    final commit

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -4,13 +4,14 @@ const Card = styled.div`
 display: inline-block;
 background: lightgray;
 color: black;
-width: 200px;
+width: 25%;
 max-height: 300px;
-margin: 5%;
+margin:5%;
 padding: 15px 5px;
 opacity: 45%;
 overflow: hide;
 line-height: 1rem;
+box-shadow: 3px 3px 1px black;
 `;
 const H2Style = styled.h2`
 line-height: 2rem;
