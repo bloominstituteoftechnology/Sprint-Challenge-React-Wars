b40ea74ff93df5b7b2a2adbe101c87c76f9f329a
khashimovafiruza@gmail.com <khashimovafiruza@gmail.com>
2020-01-24 13:44:40 -0600

    starwars styling, but not completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SWCard from "./components/SWCard";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
---
+++ starwars/src/components/InfoCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Card = styled.div`
+    width: 100%;
+    margin: 2%;
+    background-color: #625B5A;
+    border-radius: 5%;
+`
+const SWName = styled.h3`
+    font-weight: bold;
+`
+
+const StyledInfo = styled.h5`
+    color: white;
+`
+
+
+const InfoCard = props => {
+return (
+ <Card>
+<SWName>{props.name}</SWName>
+<StyledInfo>Gender: {props.gender}</StyledInfo>
+<StyledInfo>Height: {props.height}cm</StyledInfo>
+<StyledInfo>Weight: {props.mass}kg</StyledInfo>            
+ </Card>
+    )
+}
+
+export default InfoCard
