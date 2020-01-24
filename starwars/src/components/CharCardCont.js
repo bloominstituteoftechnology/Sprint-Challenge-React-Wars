2b3a586fe30bb330a53a609ee083a0e3a44e2ab9
unknown <Dustin.Guillen14@yahoo.com>
2020-01-24 10:50:09 -0700

    MVP

--- starwars/src/components/CharCardCont.js
+++ starwars/src/components/CharCardCont.js
@@ -3,6 +3,13 @@ import CharacterCard from "./CharacterCard";
 import axios from "axios";
 import styled from "styled-components";
 
+const CardContainer = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
+
 const CharCardCont = () => {
   const [character, setCharacter] = useState([]);
 
@@ -19,15 +26,21 @@ const CharCardCont = () => {
   }, []);
 
   return (
-    <div>
+    <CardContainer>
       {character.map((person, index) => (
         <CharacterCard
           key={index}
           name={person.name}
+          birthyear={person.birth_year}
+          height={person.height}
+          mass={person.mass}
+          hair={person.hair_color}
+          eyes={person.eye_color}
+          sex={person.gender}
         />
       ))}
       ))
-    </div>
+    </CardContainer>
   );
 };
 
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,12 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 
+
+const Card = styled.div`
+
+    width: 30%;
+    background: rgba(225,225,225, 0.7);
+    padding: 4% 0;
+    border: 1px solid grey;
+    font-family: 'ZCOOL QingKe HuangYou', cursive;
+    font-size: 2rem;
+`; 
+
 const CharacterCard = props => {
 
   return (
-    <div>
+    <Card>
       <h2>{props.name}</h2>
-    </div>
+      <p>Year born: {props.birthyear}</p>
+      <p>
+        Height and weight: {props.height}cm, {props.mass}kg
+      </p>
+      <p>
+        Attributes: {props.hair} hair, with {props.eyes} eyes
+      </p>
+      <p>Gender: {props.sex}</p>
+    </Card>
   );
 };
 
