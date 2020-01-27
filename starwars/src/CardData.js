92f91de4b422768172a17e61cfa2bdf8d9496ded
Buddy Agyin <agyin3@gmail.com>
2020-01-27 14:22:47 -0700

    add pagination, add fetch films for characters

--- starwars/src/CardData.js
+++ starwars/src/CardData.js
@@ -3,30 +3,44 @@ import axios from 'axios';
 import StarWarsCard from './StarWarsCard';
 import {Container, Row} from 'reactstrap';
 
+
 export default function CardData () {
     
-    const [person, setPerson] = useState([]);
+    const [person, setPerson] = useState();
+    const [url, setUrl] = useState('https://swapi.co/api/people/')
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const starWarsData = await axios.get('https://swapi.co/api/people/');
-                console.log(starWarsData);
-                setPerson(starWarsData.data.results);
+                const starWarsData = await axios.get(url);
+                setPerson(starWarsData.data);
             }catch(err) {
                 console.log(err);
             }
         }
 
         fetchData();
-    }, [])
+    }, [url])
 
-    console.log(person);
+    const handleNext = () => {
+        if(person.next){
+            setUrl(person.next)
+        }
+    }
+
+    const handlePrevious = () => {
+        if(person.previous){
+            setUrl(person.previous)
+        }
+    }
+
 
     return (
         <Container>
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
             <Row>
-            {person.map((person, index) => {
+            {person && person.results.map((person, index) => {
                 return (
                 <StarWarsCard
                     key = {index}
@@ -36,6 +50,7 @@ export default function CardData () {
                     eyes = {person.eye_color}
                     birthYear = {person.birth_year}
                     species = {person.species}
+                    films = {person.films}
                 
                 />
                 )
--- starwars/src/StarWarsCard.js
+++ starwars/src/StarWarsCard.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col, Button } from "reactstrap";
+import axios from 'axios';
 
 export default function StarWarsCard(props) {
+
+    const [films, setFilms] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const promiseArray = props.films.map(url=> axios.get(url))
+        Promise.all( promiseArray )
+            .then(res => {
+                const filmDescriptions = res.map(film => film.data)
+                setFilms(filmDescriptions)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    },[props.films])
+
+    // Get Films using async/await
+
+    // useEffect(() => {
+    //     async function getFilms() {
+    //         const promiseArray = props.films.map(url=>axios.get(url));
+    //         try {
+    //             const filmDescriptions = (
+    //                 await Promise.all(promiseArray)
+    //               ).map(res=>res.data)
+    //               setFilms(filmDescriptions)
+    //         } catch(err) {
+    //             console.log(err)
+    //         }
+    //     }
+
+    //     getFilms()
+    // }, [props.films])
 
     return(
         <Col xs='6' lg='4'>
@@ -13,6 +48,7 @@ export default function StarWarsCard(props) {
                     <CardText>Birth Year: {props.birthYear}</CardText>
                     <CardText>Eye Color: {props.eyes}</CardText>
                     <CardText>Hair Color: {props.hair}</CardText>
+                    <CardText>Films: {loading ? 'Loading' : films && films.map(film => ` ${film.title} |`)}</CardText>
                     <Button className='text-left'>More</Button>
                 </CardBody>
             </Card>
