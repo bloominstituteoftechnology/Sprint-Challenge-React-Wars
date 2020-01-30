9621dabe31f25314753df3d1331d3bbc732d9f53
Kolade-Junaid <koladejun@yahoo.com>
2020-01-30 22:21:38 +0000

    Made some changes to the Charactercard.js file

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,31 +4,15 @@ import styled from "styled-components";
 
 const StyledCard = styled.div`
 display: flex;
-justify-content: center;
 flex-direction: column;
-flex-wrap: wrap;
-
 text-align:  center;
-align-items: center;
-
 margin: 0 auto;
-padding: 3rem;
 margin-bottom: 3rem;
-max-width: 1000px;
+max-width: 900px;
 
 background:linear-gradient(to right, #108dc7,#ef8e38);
-transition: ease-in-out 0.2s;
-
 color: ${props => props.inputColor || "#FF0000"};
 font-size: 1.5rem;
-
-
-&:hover {
-  transform: scale()(0.9);
-  transition: ease-in-out 0.11s;
-  box-shadow: 0.3em 0.3em 0.3em #DECBA4;
-  text-shadow: 0.5em 0.5em 0.5em #544a7d;
-}
 
 h2 {
   color:${props => props.inputColor ||  "#f4791f"};
@@ -40,8 +24,6 @@ h2 {
 h1 {
   text-align: center;
   margin: 0 auto;
-  
-
 }
 `;
 
