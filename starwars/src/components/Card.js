407e5decb90b41e5b8235f3f8dea10ebf02d2640
JW Nicholson <john.nicholson812@gmail.com>
2020-02-21 13:52:19 -0500

    MVP met

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,20 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
-const characterBlock = styled.div`
-background: #99f3eb;
+const CharCard = styled.div`
+  background: white;
+  color: black;
+ width:80%;
+ margin: auto;
+  max-height: 300px;
+  margin-botom: 20px;
 `;
 
 const Card = props => {
     console.log("Card js ", props)
 
     return(
-        <characterBlock>
+        <CharCard>
         <div  key={props.index}>
         <h2>Character: {props.name}</h2>
         <h3>Gender: {props.gender}</h3>
     </div>
-    </characterBlock>
+    </CharCard>
     )
     
 }
