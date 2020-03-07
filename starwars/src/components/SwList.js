17510310d742979e504b32fe00bae346adbc8e0d
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-03-06 20:16:12 -0500

    Added the SWlist component. Still need to get the API call to work.

--- starwars/src/components/SwList.js
+++ starwars/src/components/SwList.js
@@ -1,2 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+import SwCard from "./SwCard";
+
+function SwList() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/").then(response => {
+      console.log(response);
+      //   setCharacters(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {characters.map(character => {
+        return (
+          <SwCard
+            name={character.name}
+            birthYear={character.birth_year}
+            eyeColor={character.eye_color}
+          />
+        );
+      })}
+      ;
+    </div>
+  );
+}
+
+export default SwList;
