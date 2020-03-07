89ba891dda411f60260884c97c1add40caf3d910
Altyn Berdiyeva <aberdiyeva@LISAACADEMY.ORG>
2020-03-07 13:20:54 -0600

    my code is here

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Films from "./Films";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,19 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState({});
+  useEffect ( () => {
+    axios.get('http https://swapi.co/api/people/1/').then( res => {
+      setData(res.data);
+      console.log(res)
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      
+      <Films film={data.films}/>
     </div>
   );
 }
---
+++ starwars/src/Films.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Films = (props) => {
+    console.log(props)
+    return (
+        <div src = {props.film}></div> 
+    )
+}
+
+export default Films
