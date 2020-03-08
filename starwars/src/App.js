ec922b3eab9a2e8966a02cff7c412e9c6b3a4d2b
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-03-08 19:42:22 -0400

    Imported axios, useState, useEffect into App.js. Writing useState and effectState

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [character, setCharacter] = useState({})
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
