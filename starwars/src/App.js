5605dcf246ac8e85d3937d6dbf9fb52c6029666d
Xaver <xaverpinero@yahoo.com>
2020-03-08 21:28:46 -0400

    added axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { AxiosService } from "./AxiosSevice";
 
 const App = () => {
+  const [swItem, setSwItem] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    AxiosService.getSwData().then(swItem => {
+      setSwItem(swItem);
+      setLoading(false);
+    });
+  },[]);
+
+  if (loading) {
+    return <div>...Loading...</div>;
+  }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +27,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {swItem}
     </div>
   );
 }
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+//import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
