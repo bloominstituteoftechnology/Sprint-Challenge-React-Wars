81807c1b2828cd25a63e9640f991c325382f68a0
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-03-08 20:26:00 -0400

    Created character component file; successful axios call; mapping characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -12,9 +12,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+
+    axios 
+      .get('https://swapi.co/api/people/')
+      .then(res => setCharacter(res.data.results))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log('useState:', character)
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      
+      {/* <Characters />
+        {character.name} */}
+
+      character.map(characters => {
+        <>
+        <Characters 
+          character=
+        </>
+      })
+      
+
     </div>
   );
 }
---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { checkPropTypes } from 'prop-types';
+
+const Characters = () => {
+
+    return (
+        <div>
+
+        <p>Character: {props.character} </p>
+
+        </div>
+    )
+
+}
