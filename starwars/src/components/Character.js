0a3b19779761728cecb4ee7b886ef742c5b8beb7
avpimblesr <allan.pimble@gmail.com>
2020-03-10 18:29:08 -0400

    The code is done, I just have to answer the questions to complete MVP.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 const Character = (props) => {
+	const [popoverOpen, setPopoverOpen] = useState(false);
+
+	const toggle = () => setPopoverOpen(!popoverOpen);
 	return (
-		<div className = 'card'>
-			<h1>{props.character.name}</h1>
-      <p>Height: {props.character.height}cm</p>
-      <p>Eye Color: {props.character.eye_color}</p>
-      <p>Skin Color: {props.character.skin_color}</p>
+		// <div className="card">
+		// 	<h1>{props.character.name}</h1>
+		// 	<p>Height: {props.character.height}cm</p>
+		// 	<p>Eye Color: {props.character.eye_color}</p>
+		// 	<p>Skin Color: {props.character.skin_color}</p>
+		// </div>
+		<div>
+			<Button id="Popover1" type="button">
+			{props.character.name}
+			</Button>
+			<Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
+				<PopoverHeader>{props.character.name}</PopoverHeader>
+				<PopoverBody>Skin Color: {props.character.skin_color}</PopoverBody>
+			</Popover>
 		</div>
 	);
 };
