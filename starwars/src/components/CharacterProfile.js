bd3dd17a1d435577f9238986595860057a78a286
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-03-09 21:39:14 -0400

    Slight modification

--- starwars/src/components/CharacterProfile.js
+++ starwars/src/components/CharacterProfile.js
@@ -7,14 +7,23 @@ const CharacterProfile = (props) => {
       <Col sm="6">
         <Card body>
           <CardTitle>Name: {props.name}</CardTitle>
+          <br></br>
           <CardText>Height: {props.height}</CardText>
+          <br></br>
           <CardText>Mass: {props.mass}</CardText>
+          <br></br>
           <CardText>Hair Color: {props.hair_color}</CardText>
+          <br></br>
           <CardText>Skin Color: {props.skin_color}</CardText>
+          <br></br>
           <CardText>Eye Color: {props.eye_color}</CardText>
+          <br></br>
           <CardText>Birth Year: {props.birth_year}</CardText>
+          <br></br>
           <CardText>Gender: {props.gender}</CardText>
+          <br></br>
           <CardText>Homeworld: {props.homeworld}</CardText>
+          <br></br>
         </Card>
       </Col>
     </Row>
