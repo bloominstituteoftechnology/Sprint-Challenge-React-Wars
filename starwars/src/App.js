708a832283b6ccb938d0cb842799f07cabef24f8
unknown <locbgiang@gmail.com>
2020-03-20 09:52:24 -0700

    finished coding

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import PageBody from './PageBody.js';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,22 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/").then(response => {
+      //console.log(response.data.results);
+      setData(response.data.results)
+    }).catch(error => {
+      console.log('Error', error);
+    });
+  }, []);
+  //console.log(data);
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <PageBody
+        data = {data}
+      />
     </div>
   );
 }
