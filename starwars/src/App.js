b5dc58bc7c1c4c6c26f6b9cd2eda418e3866618f
Spencer_Vail <vail.spencer@gmail.com>
2020-04-17 08:24:57 -0700

    Some basic framework, imports added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,7 +9,10 @@ const App = () => {
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any.3
+
+  const apiUrl = 'https://swapi.py4e.com/api/people/';
+  const [person, setPerson] = useState();
 
   return (
     <div className="App">
