89d4b72e2d4c454f59b3af06a7b0b7b4c8b662d1
Spencer_Vail <vail.spencer@gmail.com>
2020-04-17 10:57:08 -0700

    MVP Done

---
+++ starwars/src/components/LoaderTool/LoaderTool.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const LoaderTool = ({}) => {
+    return(
+        <div>
+            <p>Loading....</p>
+         <img src='https://media.giphy.com/media/bR4poFy22rgUE/giphy.gif' alt='Loading'></img>
+        </div>
+    )
+}
+
+export default LoaderTool;
