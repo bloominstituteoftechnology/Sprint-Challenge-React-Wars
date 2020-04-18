f936378aaff9101aa10743712a8d72b05269e0e6
Taja House <ahlliahtaja@yahoo.com>
2020-04-17 21:03:26 -0700

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from '../src/components/Character';
+
 
 const App = () => {
+  const [characters, setCharacters]= useState([]);
+  // const [isHappy, setIsHappy]= useState(true);
+  console.log("Characters", characters[0])
+
+
+  useEffect(()=>{
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res=> setCharacters(res.data.results))
+    .catch(err=>console.log('Nope', err))
+  },[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,8 +24,15 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
+   <div className='App'>
       <h1 className="Header">Characters</h1>
+
+      <div className='character-container'>
+
+      {characters.map(character =>(
+        <Character key={character.id} name={character.name} img={character.image} gender={character.gender} status={character.status} species={character.species}/>
+      ))}   
+</div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background: #99f3eb;
+  color: black;
+  width: 200px;
+  max-height: 1300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  box-shadow: 2px 2px 2px grey;
+`;
+
+const CharacterImg = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: scale;
+  flex-shrink: 2;
+`;
+
+const Character = props =>{
+    return (
+             <Card>
+             <h2>{props.name}</h2>
+            <CharacterImg top width="10%" src={props.img} alt='Characters'/>
+            <p>{props.gender}</p>
+            <p>{props.status}</p>
+            <p>{props.species}</p>
+        </Card>
+           
+    )
+}
+
+export default Character;
