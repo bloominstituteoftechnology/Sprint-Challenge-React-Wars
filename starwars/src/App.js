df0b45901a30d4585c20c065fde760bd21ba4d6f
Brian Clayton <finalb@projectfinalboss.com>
2020-04-17 21:51:17 -0400

    Set up axios data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Character from "./components/Character";
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+const [swData, setSWData] = useState([]);
+
+
+useEffect(() => {
+
+
+  axios.get("https://swapi.py4e.com/api/people")
+  .then(res => console.log(res))
+  .catch(err => console.log("Luke, I am you're father...(Bad End)"))
+
+}, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +26,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {swData.map((people, index) => {
+
+          <Character key={index} type={people.results} />
+
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+
+import React from "react";
+
+
+
+function Character (props) {
+
+
+<section>{props.data.results}</section>
+
+
+}
+
+export default Character;
