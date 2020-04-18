e7ca1a4254b34b67691031409ef63f2548b882c0
workfromhome <workfromhome@Estebans-MacBook-Pro.local>
2020-04-18 10:31:33 -0700

    Was able to render data, create character component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character'
+import Axios from 'axios'
 import './App.css';
 
 const App = () => {
@@ -9,9 +11,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [rData, setRData] = useState([])
+
+  useEffect(()=>{
+    Axios
+    .get('https://swapi.py4e.com/api/people/')
+    
+    .then(response => {
+      console.log(response.data.results);
+      setRData(response.data.results);
+    
+    })
+    .catch(error => console.log("Error!", error))
+  },[])
+  
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className = 'row'>
+        {rData.map(data => {
+          return <Character name = {data.name} height = {data.height} mass = {data.mass} birth_year={data.birth_year} gender = {data.gender}/>
+        })}  
+      </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react'
+
+export default function Character(props) {
+
+    const {name, gender, height, mass, birth_year} = props;
+
+    return (
+        <div className = 'card'>
+            <h3><span className = 'yellow'>Name:</span> {name}</h3>
+            <h4><span className = 'yellow'>Gender:</span> {gender}</h4>
+            <h4><span className = 'yellow'>Birth:</span> {birth_year}</h4>
+            <h4><span className = 'yellow'>Height / Mass:</span> {height} in /{mass} kg</h4>
+            
+        </div>
+    )
+}
