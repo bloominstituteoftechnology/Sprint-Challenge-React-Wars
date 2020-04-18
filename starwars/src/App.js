51b06d81e0fcac4837cefa16ad6c576fe6d3b67d
workfromhome <workfromhome@Estebans-MacBook-Pro.local>
2020-04-18 11:26:13 -0700

    finished styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Character from './components/Character'
 import Axios from 'axios'
 import './App.css';
+import { Alert } from 'reactstrap';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +13,8 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [rData, setRData] = useState([])
+  const [rData, setRData] = useState([]);
+  const [films, setFilms] = useState([]);
 
   useEffect(()=>{
     Axios
@@ -24,14 +27,28 @@ const App = () => {
     })
     .catch(error => console.log("Error!", error))
   },[])
+
+  useEffect(()=>{
+    Axios
+    .get('https://swapi.py4e.com/api/films/')
+    
+    .then(response => {
+      console.log(response.data.results);
+      setFilms(response.data.results);
+    
+    })
+    .catch(error => console.log("Error!", error))
+  },[])
+
   
   
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <div className = 'row'>
+      <h1 className="Header">Star Wars Characters</h1>
+      <Alert color= 'warning'> In a Time Long Ago, In a Galaxy Far Far Away...</Alert>
+      <div className = 'xrow'>
         {rData.map(data => {
-          return <Character name = {data.name} height = {data.height} mass = {data.mass} birth_year={data.birth_year} gender = {data.gender}/>
+          return <Character name = {data.name} height = {data.height} mass = {data.mass} birth_year={data.birth_year} gender = {data.gender} eye_color = {data.eye_color} skin_color = {data.skin_color}/>
         })}  
       </div>
     </div>
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,16 +1,19 @@
 // Write your Character component here
 import React from 'react'
+import { Alert } from 'reactstrap';
 
 export default function Character(props) {
 
-    const {name, gender, height, mass, birth_year} = props;
+    const {name, gender, height, mass, birth_year, eye_color, skin_color} = props;
 
     return (
-        <div className = 'card'>
+        <div className = 'xcard'>
             <h3><span className = 'yellow'>Name:</span> {name}</h3>
             <h4><span className = 'yellow'>Gender:</span> {gender}</h4>
             <h4><span className = 'yellow'>Birth:</span> {birth_year}</h4>
-            <h4><span className = 'yellow'>Height / Mass:</span> {height} in /{mass} kg</h4>
+            <h4><span className = 'yellow'>Height/Mass:</span> {height}in/{mass}kg</h4>
+            <h4><span className = 'yellow'>Eye color/Skin Color:</span> {eye_color}/{skin_color} </h4>
+      
             
         </div>
     )
