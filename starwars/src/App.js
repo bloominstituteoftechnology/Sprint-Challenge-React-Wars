8a0fc231b3baf284227bbcf7ad003b252b5f6e89
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-04-19 20:57:50 -0700

    Mvp Completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import CharacterList from './components/CharacterList';
+import styled from 'styled-components';
+
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +15,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  const MainText = styled.h1`
+    font-family: 'Reenie Beanie', cursive;
+    color:  white;
+    font-weight: bold;
+    font-size: 60px;
+    text-shadow: 2px 2px black;
+    padding: 5px;
+    margin: 0;
+    `;
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <MainText className="Header">Characters</MainText>
+     <CharacterList />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+import { CharacterCard, Pictures, Text, CharacterContainer, NameText } from './CharacterStyles';
+
+const Character = props => {
+
+    return (
+        <CharacterContainer>
+    <CharacterCard>
+    
+        <NameText>{props.name}</NameText>
+        <Pictures src={props.pic} alt={props.name} />
+        <Text>Location: {props.location.name}</Text>
+        <Text>Status: {props.status}</Text>
+        
+    </CharacterCard>
+    </CharacterContainer>
+    )
+
+}
+
+export default Character;
