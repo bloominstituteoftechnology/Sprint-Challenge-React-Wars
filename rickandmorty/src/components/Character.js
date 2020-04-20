7b9e6a3ddbdbbf9c40a33d1f61cfcd50c92eac39
Elijah Atkins <elijahorama@gmail.com>
2020-04-19 22:05:26 -0600

    added basic navigation

--- rickandmorty/src/components/Character.js
+++ rickandmorty/src/components/Character.js
@@ -5,24 +5,51 @@ import CharacterCard from './CharacterCard';
 
 const Character = () => {
 
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState('https://rickandmortyapi.com/api/character/?page=1');
     const [characters, setCharacters] = useState([]);
+    const [info, setInfo] = useState([]);
+
+    const getPageNumber = (str='') =>{
+        if (str === ''){
+            return '';
+        }
+        return str.split('https://rickandmortyapi.com/api/character/?page=')
+    }
 
     useEffect(()=>{
         axios
-        .get(`https://rickandmortyapi.com/api/character/?page={page}`)
+        .get(page)
         .then(response => {
             setCharacters(response.data.results)
+            setInfo(response.data.info)
+
 
         })
         .catch(error => console.log("Error!", error))
-    }, []);
+    }, [page]);
     return (
+        <div className="holder">
+        <div className="nav">
+
+        <button onClick={()=>{
+            setPage(info.prev)
+        }}>Previous</button>
+<p>{getPageNumber(page)} of {info.pages}</p>
+        <button onClick={()=>{
+            setPage(info.next)
+        }}>Next</button>
+
+    </div>
         <div className="card-grid">
+
+            {console.log(info.next)}
+
 
             {characters.map(character =>(
                 <CharacterCard key={character.id} character={character} />
             ))}
+
+        </div>
         </div>
     )
 }
