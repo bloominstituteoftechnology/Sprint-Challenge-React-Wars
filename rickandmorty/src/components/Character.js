e5d675358d2131000314046cfd155103520fbc2f
Elijah Atkins <elijahorama@gmail.com>
2020-04-19 19:57:15 -0600

    adding axios support

--- rickandmorty/src/components/Character.js
+++ rickandmorty/src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from './CharacterCard';
+
+const Character = () => {
+
+    const [page, setPage] = useState(1);
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(()=>{
+        axios
+        .get(`https://rickandmortyapi.com/api/character/?page={page}`)
+        .then(response => {
+            console.log(response.data.results)
+            setCharacters(response.data.results)
+        })
+        .catch(error => console.log("Error!", error))
+    }, []);
+    return (
+        <div className="card-grid">
+        {characters.map(character => (
+        <Character key={character.id} character={character} />
+      ))}
+        </div>
+    )
+}
+export default Character;
--- rickandmorty/src/components/CharacterCard.js
+++ rickandmorty/src/components/CharacterCard.js
@@ -1,32 +1,33 @@
 import React from 'react';
 
-const CharacterCard = (props) =>{
+const CharacterCard = ({char}) =>{
+    console.log(char)
     return (
         <div className="card-container-out">
             <div className="card-container">
                 <div className="card-image">
-                    <div className={(props.results.status === "Dead") ? "dead" : "not-dead"}>
+                    <div className={(char.data.results.status === "Dead") ? "dead" : "not-dead"}>
                         <h1>DEAD</h1>
                     </div>
                     <div className="card-name">
-                        <h2>{props.results.name}</h2>
+                        <h2>{char.results.name}</h2>
                     </div>
                     <div className="card-content">
                         <div className="txt-wrapper">
                             <span>Species</span>
-                            <p>{props.results.species}</p>
+                            <p>{char.results.species}</p>
                         </div>
                         <div className="txt-wrapper">
                             <span>Gender</span>
-                            <p>{props.results.gender}</p>
+                            <p>{char.results.gender}</p>
                         </div>
                         <div className="txt-wrapper">
                             <span>Origin</span>
-                            <p>{props.results.orign.name}</p>
+                            <p>{char.results.orign.name}</p>
                         </div>
                         <div className="txt-wrapper">
                             <span>Last Location</span>
-                            <p>{props.results.location.name}</p>
+                            <p>{char.results.location.name}</p>
                         </div>
                     </div>
                 </div>
@@ -35,3 +36,4 @@ const CharacterCard = (props) =>{
         </div>
     )
 }
+export default CharacterCard;
