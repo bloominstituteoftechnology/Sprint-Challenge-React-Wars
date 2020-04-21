1788780564c93f6e91157992ca218ae26ebe95b0
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-04-20 19:32:41 -0500

    added answers to questions

--- starwars/src/components/Info-card.js
+++ starwars/src/components/Info-card.js
@@ -1,20 +1,27 @@
 import React from "react"
-import { Card, CardBody, CardTitle, CardText, Col } from "reactstrap"
+import styled from "styled-components"
+
+const Card = styled.div`
+    display: flex;
+    background: white;
+    border: 2px solid orange;
+    border-radius: 30px;
+    width: 200px;
+    margin-bottom: 20px;
+`
 
 const infoCard = props => {
 
-    return (
-    <Col>       
-        <Card className="soldCard">
-            <CardBody>
-                <CardTitle>{props.soldier.name}</CardTitle>
-                    <CardText>Gender: {props.soldier.gender}</CardText>
-                    <CardText>DOB: {props.soldier.birth_year}</CardText>
-                    <CardText>Hair color: {props.soldier.hair_color}</CardText>
-                    <CardText>Skin color: {props.soldier.skin_color}</CardText>
-            </CardBody>
+    return (       
+        <Card>
+            <div>
+                <h3>{props.soldier.name}</h3>
+                    <p>Gender: {props.soldier.gender}</p>
+                    <p>DOB: {props.soldier.birth_year}</p>
+                    <p>Hair color: {props.soldier.hair_color}</p>
+                    <p>Skin color: {props.soldier.skin_color}</p> 
+            </div>           
         </Card>
-    </Col>
     )
 }
 
