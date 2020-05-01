9f0a9b888940123ebc5dad525518ff825570fe6c
Ben Venker <bvemails@gmail.com>
2020-05-01 18:09:16 -0500

    set up basic routing

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <>
+      <Route exact path="/"></Route>
+      <Route path="/character/:id"></Route>
+      <Route path="/episode/:id"></Route>
+    </>
   );
-}
+};
 
 export default App;
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
