c36a28088a74196f10d0381d7f4d657e0cae6a60
Timur Mone <timur@yurtah.com>
2020-05-03 22:43:16 -0400

    done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,55 +1,49 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import styled from "styled-components";
+import StarWars from "./components/StarWars";
+import Header from "./components/Header";
+import { Container } from "semantic-ui-react";
+import Pages from "./components/Pagination";
+import "semantic-ui-css/semantic.min.css"
+import './App.css';
 
-import PersonContainer from "./components/PersonComponent";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pplPerPage,] = useState(4);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    const fetchPeople = async () => {
+      setLoading(true);
+      const res = await axios.get('https://swapi.co/api/people/')
+      setPeople(res.data.results)
+      setLoading(false);
+    }
+
+    fetchPeople();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPerson = currentPage * pplPerPage;
+  const indexOfFirstPerson = indexOfLastPerson - pplPerPage
+  const currentPeople = people.slice(indexOfFirstPerson, indexOfLastPerson);
+
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [people, setPeople] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://swapi.co/api/people/")
-      .then(response => {
-        setPeople(response.data.results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-  console.log("outside", people);
+const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
-  const AppContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    width: 95%;
-    margin: 0 auto;
-  `;
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <AppContainer>
-        {people.map(item => {
-          return (
-            <PersonContainer
-              name={item.name}
-              height={item.height}
-              mass={item.mass}
-              hair_color={item.hair_color}
-              skin_color={item.skin_color}
-              eye_color={item.eye_color}
-              birth_year={item.birth_year}
-              gender={item.gender}
-            />
-          );
-        })}
-      </AppContainer>
+      <Header />
+    <Container> 
+      <StarWars people={currentPeople} loading={loading}/>
+    </Container>
+    <Pages peoplePerPage={pplPerPage} totalPeople={people.length} paginate={paginate}/>
     </div>
   );
 };
