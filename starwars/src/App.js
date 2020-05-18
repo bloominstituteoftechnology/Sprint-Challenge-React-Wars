82c0e30fd3dfd612165e0e683fee8705030d7f0a
manuel vega <vega1340@gmail.com>
2020-05-18 12:59:41 -0700

    finished mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Cards from './components/Character'
+import styled from 'styled-components'
+
 
 const App = () => {
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [data, setData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+
+    .then((res) => {
+
+      setData(res.data.results)
+      
+    })
+
+    .catch((err) => {
+
+      console.log('Somethings Wrong!')
+
+    })
+
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters List!</h1>
+      <div>
+        <Cards data = {data} />
+      </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,62 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+// Write your Character component here
+
+const Cards = props => {
+    let {data} = props;
+
+    return (
+
+       <div>
+
+            { data.map(character => {
+                return (
+
+                    <StyledDiv>
+                        <StyledCard>
+
+                        <h3>Name: {character.name}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                        <h3>Status: {character.status}</h3>
+                        <h3>Spieces: {character.species}</h3>
+                        <h3>Mug Shot</h3>
+                        
+                        <StyledImg src={character.image}></StyledImg>
+                       
+
+                        </StyledCard>
+                    </StyledDiv>
+
+                )
+
+            })}
+            
+       
+       </div>
+
+    )
+
+}
+
+const StyledDiv = styled.div`
+
+flex-direction:column;
+ display: flex;
+ align-items:center;
+`;
+
+const StyledCard = styled.div`
+background-color:#8cc5d2c7;
+width: 50%;
+margin:2%;
+padding:2%;
+border: dotted 2px black;
+border-radius:50%;
+
+`;
+const StyledImg = styled.img`
+border: black solid 2px;
+border-radius:50%;
+`;
+export default Cards
