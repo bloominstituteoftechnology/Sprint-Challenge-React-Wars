f1392828c0aa76fff5206e7363f60c0ae3f2141f
JenVest2020 <jenvest2019@gmail.com>
2020-05-29 20:36:32 -0500

    installed npm,axios, and styled components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [person, setPerson] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people/')
+      .then(res => {
+        console.log('from App', res)
+        setPerson()
+      }, [])
+  })
 
   return (
     <div className="App">
