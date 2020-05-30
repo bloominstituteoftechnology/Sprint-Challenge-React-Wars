acf4bc00fb62ef614bb62cf9d7244dd7cc9a1f88
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-05-30 00:09:24 -0400

    changes

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,33 +2,36 @@ import React from "react"
 import {Card, CardTitle, Container, CardImg, Row, Col, CardSubtitle, CardText} from "reactstrap"
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-        padding: 4em;
-        background: papayawhip;
-    `;
+// const Wrapper = styled.div`
+//         padding: 4em;
+//         background: papayawhip;
+//     `;
 
 function Character(props){
     console.log(props.characterArray, "these are our props")
     
     return(
         
-        <div><Wrapper>
+        <div>
+        {/* <Wrapper> */}<Container>
         <Row>
         {props.characterArray.map ( (character) => {
             return(
                 <Col xs='5'>
-                <Card key={character.created}>
+                <Card key={character.created} body inverse color="info">
                     <CardTitle>{character.name}</CardTitle>
-                    <CardSubtitle>{character.gender}</CardSubtitle>
-                    <CardText>{character.height}</CardText>
-                    <CardText>{character.birth_year}</CardText>
-                    <CardText>{character.hair_color}</CardText>
-                    <CardText>{character.eye_color}</CardText>
+                    <CardSubtitle>Character Gender: {character.gender}</CardSubtitle>
+                    <CardSubtitle>Character Height: {character.height}</CardSubtitle>
+                    <CardSubtitle>Character Birth Year: {character.birth_year}</CardSubtitle>
+                    <CardSubtitle>Character Hair Color: {character.hair_color}</CardSubtitle>
+                    <CardSubtitle>Character Eye Color: {character.eye_color}</CardSubtitle>
                 </Card>
                 </Col>
             )
         })}
-        </Row> </Wrapper>
+        </Row> 
+        {/* </Wrapper> */}
+        </Container>
         </div>
        
     )
