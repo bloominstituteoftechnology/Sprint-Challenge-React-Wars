42c13e1be000389f3cc2f470a6a5dca69d4cd0a6
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-05-29 21:20:14 -0400

    original

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  //comment
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
