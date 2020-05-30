07bce5f7d34e4f30ff2a26e88e992991ecfbcd6c
kevin thew <kevthew@outlook.com>
2020-05-29 21:13:12 -0600

    finished with search bar stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+ const [data, setData] = useState([]);
+ const [searchTerm, setSearchTerm] = useState('');
+ const handleChange = e => {
+   setSearchTerm(e.target.value)
+ }
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(()=>{
+  axios
+  .get('https://swapi.py4e.com/api/people/')
+  .then(response =>{
+    console.log(response.data.results)
+    const initialArray = response.data.results
+    const filteredData = initialArray.filter( (character)=>{
+      return character.name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+    setData(filteredData);
+  });
+  },[searchTerm]);
+
+console.log(data);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <label>
+        Serach For Someone: <input type='text' name='search' value={searchTerm} onChange={handleChange}/>
+      </label>
+      <Character characterArray = {data}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from 'react';
+import {Card, CardTitle, CardSubtitle, CardBody, Container, Row, Col} from '../../node_modules/reactstrap'
+
+function Character(props){
+    return(
+        
+        <Container>
+            <Row>          
+                    {props.characterArray.map((character)=>{
+                        return(
+                            <Col xs='4'>
+                                <Card key={character.created}>
+                                        <CardTitle>{character.name}</CardTitle>
+                                        <CardSubtitle>Gender: {character.gender}</CardSubtitle>
+                                        <CardBody>
+                                            Born: {character.birth_year}<br/>
+                                            Height: {character.height}<br/>
+                                            Mass: {character.mass}
+                                        </CardBody>
+                                </Card> 
+                            </Col>
+                        )
+                    })}
+            </Row>
+        </Container>
+
+        
+    )
+}
+
+export default Character
