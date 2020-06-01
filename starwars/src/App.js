14920739fce044eb39af06046b9fa7b4593e422a
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-05-31 20:32:41 -0600

    added list of characters and mapped them

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [data, setData] = useState([])
+const [searchTerm, setSearchTerm] = useState('');
+const handleChange = e => {
+  setSearchTerm(e.target.value)
+  console.log(handleChange)
+  console.log(searchTerm)
+}
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // it starts on
+useEffect(() => {
+    axios
+    .get('https://swapi.py4e.com/api/vehicles')
+    .then(res => {
+     // console.log(res.data.results)
+      //setData(res.data.results);      
+      const initialArray = res.data.results
+      const filteredData = initialArray.filter((Character) => {
+        return Character.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      })
+      setData(filteredData);
+    })
+},[searchTerm])
+//console.log(data)
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // https://swapi.py4e.com/api/ use for axios
+  // https://swapi.py4e.com/api/vehicles/ use for axios
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <label>
+        search for a vehicle here:
+        <input type='text' name='search' value={searchTerm} onChange={handleChange}/>
+      </label>
+      <Character characterArray={data}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+function Character(props){
+  return (
+    <Row>
+      <Col sm="6">
+        <Card body>
+          {props.characterArray.map((Character) => {
+             return(
+           <Card key = {Character.created}>
+           <CardTitle>{Character.name}</CardTitle>
+           <CardText>{Character.manufacturer}</CardText>
+           <Button>------</Button>
+          </Card>
+            )
+         })}
+          <Button >Clear</Button>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default Character;
