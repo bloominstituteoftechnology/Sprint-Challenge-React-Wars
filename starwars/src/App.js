c0657477a6602bed40972f0da5b5e8589100077e
bradpittstwin <47493177+bradpittstwin@users.noreply.github.com>
2020-06-01 19:41:24 -0600

    finished

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [data, setData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(() => {
+  axios.get("https://cors-anywhere.herokuapp.com/swapi.py4e.com/api/people")
+  .then(response => {
+    console.log(response.data.results)
+    setData(response.data.results)
+  })
+},[])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterArray = {data}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from "react";
+import {Card, CardTitle, CardSubtitle, Container, Row, Col } from 'reactstrap'
+
+function Character(props){
+   console.log(props.characterArray, 'these are your props')
+    return (
+            <Container>
+             <Row>
+            {props.characterArray.map((person) =>{
+                return(
+                    <Col xs='12'>
+                    <Card key ={person.created}>
+                        <div>Name</div>
+                        <CardTitle>{person.name}</CardTitle>
+                        <div>Height</div>
+                        <CardSubtitle>{person.height}</CardSubtitle>
+                    </Card>
+                    </Col>
+                )
+        })}
+             </Row>
+            </Container>
+    )
+};
+export default Character
