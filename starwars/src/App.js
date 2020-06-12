2edf610aaab66df7c4d2b0d456c014468c9fe26f
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-06-12 12:34:50 -0400

    MVP requirments met

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Characters from './components/Characters'
+
+import {Button} from 'reactstrap'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +12,39 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  // const Baseurl = 'https://rickandmortyapi.com/api/character/'
+
+  // const apiPage =  `?page=`
+  const[characters, setCharacters]=useState([])
+  const[pageNumber, setPageNumber]=useState(1)
+  // useEffect(()=>{
+  //   function nextPage(){
+  //     setPageNumber(pageNumber+1)
+  //   }
+  // })
+ 
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(response=>{
+      console.log(response.data.results)
+      setCharacters(response.data.results)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+  },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+
+      
+      <div>
+      <Button color="secondary">Previous Page</Button>
+    
+      <Button color="primary">Next Page</Button>
+      </div>
+      <Characters characters = {characters} />
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React, {useEffect} from 'react'
+import Container from './Container'
+
+export const Character = (props) => {
+        
+    
+   
+    return (
+        
+        <div>
+            <Container>
+                <h2>{props.character.name}</h2>
+                <img src={props.character.image}/>     
+                {console.log(props.character.name)}
+            </Container>
+            
+        </div>
+    )
+}
+export default Character
