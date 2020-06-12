4d14f3ef7c78aea14af7bf61f3283a1c740aa561
Dumars-Romulus <Dumarsrom@gmail.com>
2020-06-12 19:57:10 -0400

    Changed the for loop into a .map also added hair color to the CharContainer

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,7 +6,7 @@ import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -14,25 +14,30 @@ const App = () => {
   
   useEffect(() => {
     const fetchData = () => {
-      for(let charNum = 1; charNum <= 10; charNum++){
         axios
-        .get(`https://swapi.dev/api/people/${charNum}/`)
+        .get(`https://swapi.dev/api/people/`)
         .then(response => {
-          console.log(response.data);
-          setData(response.data);
+          console.log(response.data.results);
+          setData(response.data.results);
         })
-      }
     }
     fetchData();
   }, []);
 
   return (
     <div className="App">
-      <Character data={data} />
+      <h1 className="Header">React Wars</h1>
+      {
+        data.length ?
+          data.map(data => (
+            <Character data={data} />
+          ))
+          :
+          <h1>Loading...</h1>
+      }
     </div>
   );
 }
 
 export default App;
 
-//Notes make an empty array push everything that im looping through in the charater api then use that as the main array and list the characters info from then on
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,13 +2,12 @@
 import React from 'react';
 
 function Character(props){
+     
     return(
-        <div className="Body">
-            <h1 className="Header">React Wars ></h1>
-            <div className="CharContainer">
-                <h1>{props.data.name}</h1>
-                <h2>{props.data.birth_year}</h2>
-            </div>
+        <div className="CharContainer">
+            <h1>{props.data.name}</h1>
+            <h2>{props.data.birth_year}</h2>
+            <h2>{props.data.hair_color}</h2>
         </div>
     );
 }
