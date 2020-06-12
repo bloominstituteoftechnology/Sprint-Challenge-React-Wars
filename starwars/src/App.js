0059f5c5f93ec469ad0132db12dbea9c864dff2a
Alex Kemper <Kemper@Alexanders-MacBook-Air.local>
2020-06-12 12:11:30 -0400

    finally got the map function to work - issue was that map can't be called on null so it was breaking before subsequent logic. crying of happiness after figuring this out

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from '../src/components/Character'
 
 const App = () => {
+
+  const [characters, setCharacters] = useState('')
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +13,37 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people/')
+
+    .then(res => {
+      console.log(res.data)
+      setCharacters(res.data.results)
+      
+    })
+
+    .catch(err => {
+      console.log('Something went wrong with API request',err)
+    })
+
+    .finally(() => {
+      console.log('Done with API request')
+    })
+  },[])
+
+  console.log('characters array:', characters)
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">sanity check</h1>
+  
+      {console.log('return statement:',characters[0])}
+
+      {characters === '' ? '' : characters.map(info => {
+          return <Character props={info}></Character>
+        })
+      }
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+
+import React from 'react'
+
+export default function Character (props) {
+    console.log(props)
+    return (
+        <div>
+            'character sanity check'
+            {console.log('character sanity check')}
+        </div>
+    )
+}
