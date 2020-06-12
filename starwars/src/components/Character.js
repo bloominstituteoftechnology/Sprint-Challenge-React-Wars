afb6f23d8af621d7f2c4580f5fae8a979f6d03df
Dumars-Romulus <Dumarsrom@gmail.com>
2020-06-12 12:20:41 -0400

    Got useState and useEffect working

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+function Character(props){
+    return(
+        <div className="Body">
+            <h1 className="Header">Character</h1>
+            <h1>{props.data.name}</h1>
+        </div>
+    );
+}
+
+export default Character;
