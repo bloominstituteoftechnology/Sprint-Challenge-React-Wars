85371496e47b17b629e610184824d1c427814ad7
CCortinas <crcortinas@gmail.com>
2020-09-02 22:34:30 -0500

    using https instead of http

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,67 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
-import StarWars from './components/StarWars';
-
+import React, { Component } from "react";
+import "./App.css";
+import StarWars from "./components/StarWars";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       starwarsChars: [],
-      prevPage: '',
-      nextPage: ''
+      prevPage: "",
+      nextPage: "",
     };
     this.getCharacters = this.getCharacters.bind(this);
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.dev/api/people');
+    this.getCharacters("https://swapi.dev/api/people");
   }
 
-  getCharacters = URL => {
+  getCharacters = (URL) => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    console.log('called with url: ', URL);
-    if (URL.length>0) {
+    console.log("called with url: ", URL);
+    if (URL.length > 0) {
       fetch(URL)
-        .then(res => {
+        .then((res) => {
+          console.log("res", res);
           return res.json();
         })
-        .then(data => {
+        .then((data) => {
           if (data.next) {
-            this.setState({ nextPage: data.next });
+            let next = data.next;
+            if (next.indexOf("https" === -1)) {
+              next.replace("http", "https");
+            }
+            this.setState({ nextPage: next });
           } else {
-            this.setState({ nextPage: '' });
+            this.setState({ nextPage: "" });
           }
           if (data.previous) {
-            this.setState({  prevPage: data.previous });
+            let prev = data.previous;
+            if (prev.indexOf("https" === -1)) {
+              prev.replace("http", "https");
+            }
+            this.setState({ prevPage: data.previous });
           } else {
-            this.setState({ prevPage: '' });
+            this.setState({ prevPage: "" });
           }
           this.setState({ starwarsChars: data.results });
         })
-        .catch(err => {
+        .catch((err) => {
           throw new Error(err);
         });
-      }
+    }
   };
 
   clicked = (event) => {
-    if (event.target.className === 'next-page') {
+    if (event.target.className === "next-page") {
       this.getCharacters(this.state.nextPage);
-    } else if (event.target.className === 'prev-page'){
+    } else if (event.target.className === "prev-page") {
       this.getCharacters(this.state.prevPage);
     } else {
-      console.log('You reached this function by mistake');
+      console.log("You reached this function by mistake");
     }
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <StarWars chars={this.state.starwarsChars} prevPage={this.state.prevPage} nextPage={this.state.nextPage} clicked={this.clicked} />
+        <StarWars
+          chars={this.state.starwarsChars}
+          prevPage={this.state.prevPage}
+          nextPage={this.state.nextPage}
+          clicked={this.clicked}
+        />
       </div>
     );
   }
